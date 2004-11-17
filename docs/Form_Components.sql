f7de902675e73aa21731a820bb0b1c958f84f48b
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2004-11-17 19:05:46 +0000

    newer versions
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- docs/Form_Components.sql
+++ docs/Form_Components.sql
@@ -60,10 +60,12 @@ CREATE TABLE IF NOT EXISTS crm_form_fields (
 	ffid		INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'field id',
 	field_name	VARCHAR(255) NOT NULL default '' COMMENT 'name assigned to HTML form element'
 	label		VARCHAR(255) NOT NULL default '' COMMENT 'field label for display',
-	field_type ENUM('Individual', 'Organization', 'Family') COMMENT 'type of 
+	field_type ENUM('text_string','text_int','text_money','text_float','date','select','radio','checkbox(_int?_text?)','memo') COMMENT 'type of 
 contact',
+-- Maybe have data_type AND field_type ??
 	validate	VARCHAR(255) COMMENT 'optional input validation function (hook) for this group of fields'
 	help		TEXT NOT NULL default '',
+	extends		VARCHAR(255) DEFAULT 'contact' COMMENT 'Which table/object does this property extend',
 	is_deleted	BOOLEAN NOT NULL DEFAULT 0 COMMENT 'is this entry deleted ?',
 	PRIMARY KEY (fgid)
 
