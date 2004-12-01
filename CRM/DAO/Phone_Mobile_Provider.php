550e139188905b1c57ebc4aa38160e33826ab278
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2004-12-01 20:00:25 +0000

    modified class name to CRM_DAO_Phone_Mobile_Provider
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@56 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/DAO/Phone_Mobile_Provider.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'CRM/DAO/Base.php';
+
+class CRM_DAO_Phone_Mobile_Provider extends CRM_DAO_Base {
+
+  // name of the mobile provider
+  public $name;
+
+  function __construct() {
+    parent::$_construct();
+  }
+
+  function dbFields() {
+    static $fields;
+    if ( $fields === null ) {
+      $fields = array_merge(
+                             parent::dbFields(),
+                             array(
+                                   'name'         => array( self::TYPE_STRING),
+                                   )
+                             );
+    }
+    return $fields;
+  }
+
+}
+
+?>
