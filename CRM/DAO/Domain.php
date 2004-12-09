77a86c8c86a99d76d5fc70418b0dc18dc747214a
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2004-12-09 13:50:40 +0000

    removed 2 fields    is_deleted, created
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@97 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/DAO/Domain.php
+++ CRM/DAO/Domain.php
@@ -10,19 +10,6 @@ class CRM_DAO_Domain extends CRM_DAO_Base {
    */
   public $name;
 
-  /*
-   * has this record been deleted
-   * @var boolean
-   */
-  public $is_deleted;
-
-  /*
-   * date and time of creation of this record. Since all records are revisioned, any
-   * modifications would be in a seperate record
-   * @var object
-   */
-  public $created;
-
   function __construct() {
     parent::__construct();
   }
@@ -34,8 +21,6 @@ class CRM_DAO_Domain extends CRM_DAO_Base {
                              parent::dbFields(),
                              array(
                                    'name'        => array( self::TYPE_STRING, self::NOT_NULL ),
-                                   'is_deleted'  => array( self::TYPE_BOOLEAN, null ),
-                                   'created'     => array( self::TYPE_TIMESTAMP, self::NOT_NULL ),
                                    )
                              );
     }
