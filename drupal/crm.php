22221b7889c4a7e0558cc78d46bff391b66392f0
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2004-12-15 17:35:37 +0000

    adding a file for config options used by crm for drupal.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@100 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ drupal/crm.php
@@ -0,0 +1,15 @@
+<?php
+#######################################################
+#
+# crm.php
+#
+# defines constants used by the crm module.
+#
+#######################################################
+define( 'CRM_SMARTYDIR'          , '/usr/local/lib/php/Smarty/');
+define( 'CRM_DSN'                , 'mysql://nobody:test12345@localhost/crm');
+define( 'CRM_DAO_DEBUG_LVL'      , 0 );
+define( 'CRM_TEMPLATE_COMPILEDIR', '/home/yvb/svn/crm/templates_c');
+define( 'CRM_TEMPLATEDIR'        , '/home/yvb/svn/crm/templates');
+
+?>
