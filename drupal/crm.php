ebccef1dc23de1ed21f25239b8addccc441fa67e
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2004-12-21 19:35:00 +0000

    a temp copy of some logging changes.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@102 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- drupal/crm.php
+++ drupal/crm.php
@@ -12,4 +12,15 @@ define( 'CRM_DAO_DEBUG_LVL'      , 0 );
 define( 'CRM_TEMPLATE_COMPILEDIR', '/home/yvb/svn/crm/templates_c');
 define( 'CRM_TEMPLATEDIR'        , '/home/yvb/svn/crm/templates');
 
+# logging related constants.
+define('CRM_LOG_LEVEL_PRIORITY', 5);
+define('CRM_LOG_FILENAME', '/var/log/crm/debug.log');
+
+# different log levels
+define('CRM_LOG_LEVEL_ENTER_EXIT', 0);  // log all entry and exits of functions
+define('CRM_LOG_LEVEL_PARAM', 1);       // log all parameters
+define('CRM_LOG_LEVEL_L2', 2);          // L2 - for more details
+define('CRM_LOG_LEVEL_L3', 3);          // L3 - for a lot more details
+define('CRM_LOG_LEVEL_L4', 4);          // L4 - for a lot lot more details
+define('CRM_LOG_LEVEL_L5', 5);          // L5 - for a lot lot lot more details
 ?>
