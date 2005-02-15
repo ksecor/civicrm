ee8ccc2602ec1d96d0dc78ed7aa0c72fa6b80462
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-15 14:56:08 +0000

    added feature to add household
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@177 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Contacts/BAO/Contact_Household.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once 'CRM/Contacts/DAO/Contact.php';
+
+require_once 'CRM/Contacts/DAO/Contact_Household.php';
+
+require_once 'CRM/Contacts/DAO/Contact_Location.php';
+
+class CRM_Contacts_BAO_Contact_Household extends CRM_Contacts_DAO_Contact_Household 
+{
+    
+    protected $_contactDAO;
+    
+    /**
+     * This is a contructor of the class.
+     */
+    function __construct() 
+    {
+        parent::__construct();
+        
+        $this->_contactDAO = new CRM_Contacts_DAO_Contact();
+    }
+    
+    /**
+     * This function sets the values in the form.
+     */
+    function setContactValues() 
+    {
+
+        $dbFields  = $this->_contactDAO->dbFields();
+
+        foreach ($dbFields as $fieldName => $dontCare) {
+            $this->_contactDAO->$fieldName = isset($this->$fieldName) ? $this->$fieldName : null;
+        }
+    }
+    
+    function fillContactValues() 
+    {
+        $dbFields  = $this->_contactDAO->dbFields();
+        $tableName = $this->_contactDAO->tableName();
+        
+        foreach ($dbFields as $fieldName => $dontCare) {
+            $selectFieldName = $tableName . '_' . $fieldName;
+            $this->_contactDAO->$fieldName = isset($this->$selectFieldName) ? $this->$selectFieldName : null;
+        }
+    }
+    
+    function getContactValues() 
+    {
+        $dbFields  = $this->_contactDAO->dbFields();
+        
+        foreach ($dbFields as $fieldName => $dontCare) {
+            $this->$fieldName = $this->_contactDAO->$fieldName;
+        }
+    }
+    
+    function insertContact() 
+    {
+        $this->setContactValues();
+        
+        $this->_contactDAO->insert();
+        
+        /* above insertion triggers setting the contact_id */
+        $this->contact_id = $this->_contactDAO->id;
+    }
+    
+    function insert() 
+    {
+        /**
+         * first insert a contact record
+         **/
+        $this->insertContact();
+        
+        parent::insert();
+    }
+    
+    function find($get = false) 
+    {
+        
+        $this->setContactValues();
+        $this->joinAdd( $this->_contactDAO );
+        $this->selectAdd();
+        $this->selectAs( $this, '%s' );
+        $this->selectAs( $this->_contactDAO, $this->_contactDAO->getTableName() . '_%s' );
+        parent::find($get);
+        
+        if ($get) {
+            $this->fillContactValues();
+        }
+        
+    }
+    
+    function fetch() 
+    {
+        $result = parent::fetch();
+        if ($result) {
+            $this->fillContactValues();
+        }
+        return $result;
+    }
+    
+}
+
+?>
