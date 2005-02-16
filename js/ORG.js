d5c6352c82f144978ff92ecf615a4a8e352af591
windy <windy@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-16 08:11:19 +0000

    // This file provides the javascripting for Organizations
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@197 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- js/ORG.js
+++ js/ORG.js
@@ -1,4 +1,5 @@
 
+
 	/** 
 	*  This function executes when there is an error within a form element and the page is relayed from the server
 	*  with errors. The mechanism by which this function is fired is present within the template file where the 
@@ -13,80 +14,41 @@
 	function on_error_execute( ) 
 	{  
 	   var j = 1;	
-           var analyit = Array ("location2",
-		             "location3"
-		         );
 	
+      	   var email_name_tail = Array ("_secondary",
+					"_tertiary"
+				       );
+
 	   for (k=0; k<2; k++) {
-		if (document.getElementById('ph'+String(k+2)+String(j)).value != '') {
-		    document.getElementById('phone0_'+String(k+2)+'_'+String(j)).style.display = 'block'; 					                document.getElementById('expand_phone0_'+String(k+2)+'_'+String(j)).style.display = 'none';
- 		    if (k<1) {
+
+		//if (document.getElementsByName('location[phone_'+String(k+2)+']').value != '') 
+		  if (document.forms['ORG'].elements['location[phone_'+String(k+2)+']'].value != '') {
+    	    	      document.getElementById('phone0_'+String(k+2)+'_'+String(j)).style.display = 'block'; 
+		      document.getElementById('expand_phone0_'+String(k+2)+'_'+String(j)).style.display = 'none';
+ 		      if (k<1) {
 			document.getElementById('expand_phone0_'+String(k+3)+'_'+String(j)).style.display = 'block';
 		    }
 		}
 
-		if (document.getElementById('im'+String(k+2)+String(j)).value != '') {
-		    document.getElementById('IM0_'+String(k+2)+'_'+String(j)).style.display = 'block'; 				                                document.getElementById('expand_IM0_'+String(k+2)+'_'+String(j)).style.display = 'none';
+		//if (document.getElementByName('location[im_screenname_'+String(k+2)+']').value != '') 
+		  if (document.forms['ORG'].elements['location[im_screenname_'+String(k+2)+']'].value != '') {
+		    document.getElementById('IM0_'+String(k+2)+'_'+String(j)).style.display = 'block';
+	            document.getElementById('expand_IM0_'+String(k+2)+'_'+String(j)).style.display = 'none';
 		    if (k<1) {
 			document.getElementById('expand_IM0_'+String(k+3)+'_'+String(j)).style.display = 'block';
 		    }
-		}		                        
-		if (document.getElementById('em'+String(k+2)+String(j)).value != '') {
-		    document.getElementById('email0_'+String(k+2)+'_'+String(j)).style.display = 'block'; 				                        document.getElementById('expand_email0_'+String(k+2)+'_'+String(j)).style.display = 'none';
+		}		        
+             
+		//if (document.getElementByName('location[email'+email_name_tail[k]+']').value != '') 
+		  if (document.forms['ORG'].elements['location[email'+email_name_tail[k]+']'].value != '') {
+		   document.getElementById('email0_'+String(k+2)+'_'+String(j)).style.display = 'block'; 
+		   document.getElementById('expand_email0_'+String(k+2)+'_'+String(j)).style.display = 'none';
 		    if (k<1) {
-			document.getElementById('expand_email0_'+String(k+3)+'_'+String(j)).style.display = 'block';
+		      document.getElementById('expand_email0_'+String(k+3)+'_'+String(j)).style.display = 'block';
 		    }
 		}
 
 	  }
-	
-	   for (j = 0; j < analyit.length; j++) {
-		   for (i = 0; i < document.CRUD.length; i++) {
-			if (document.CRUD.elements[i].name.indexOf(analyit[j]) != -1) {
-			    if (document.CRUD.elements[i].type.indexOf("text")!= -1){
-				if (document.CRUD.elements[i].value != ''){
-			            document.getElementById(analyit[j]).style.display = 'block';
-				    if (j<1) {
-					document.getElementById('expand_loc'+String(j+3)).style.display = 'block';
-				    }
-				    for (k=0; k<2; k++) {
-				         if (document.getElementById('ph'+String(k+2)+String(j+2)).value != '') {
-					     document.getElementById('phone0_'+String(k+2)+'_'+String(j+2)).style.display = 'block'; 					                 document.getElementById('expand_phone0_'+String(k+2)+'_'+String(j+2)).style.display = 'none';
-			 		    if (k<1) {
-						document.getElementById('expand_phone0_'+String(k+3)+'_'+String(j+2)).style.display = 'block';                   		    	        }			                         
-					  }
-
-				         if (document.getElementById('im'+String(k+2)+String(j+2)).value != '') {
-					     document.getElementById('IM0_'+String(k+2)+'_'+String(j+2)).style.display = 'block'; 				                 	 document.getElementById('expand_IM0_'+String(k+2)+'_'+String(j+2)).style.display = 'none';
-		    			     if (k<1) {
-						document.getElementById('expand_IM0_'+String(k+3)+'_'+String(j+2)).style.display = 'block';
-		    			     }			                         
-					 }
-
-				         if (document.getElementById('em'+String(k+2)+String(j+2)).value != '') {
-		   			     document.getElementById('email0_'+String(k+2)+'_'+String(j+2)).style.display = 'block'; 				                         document.getElementById('expand_email0_'+String(k+2)+'_'+String(j+2)).style.display = 'none';			                             if (k<1) {
-						document.getElementById('expand_email0_'+String(k+3)+'_'+String(j+2)).style.display = 'block';
-		    			     }			                         
-					 }             	
-						
-
-				     }
-			             break;
-				 }
-			     }
-
-			}
-	           }
-	    }
-
-
-		if (document.getElementById("addnote").value != '') {
-			document.getElementById("notes").style.display = 'block';
-		}
-
-		if (document.getElementById('mdy').value == "click") {
-			document.getElementById("demographics").style.display = 'block';
-		}
 
 	}
 
@@ -103,17 +65,7 @@
 			new Array( 'phone0_2_1', 	'phone0_3_1',
 				   'email0_2_1', 	'email0_3_1',
 				   'IM0_2_1','IM0_3_1', 'expand_phone0_3_1',
-				   'expand_email0_3_1', 'expand_IM0_3_1',
-				   'phone0_2_2', 	'phone0_3_2',
-				   'email0_2_2', 	'email0_3_2',
-				   'IM0_2_2','IM0_3_2', 'expand_phone0_3_2',
-				   'expand_email0_3_2', 'expand_IM0_3_2',
-				   'phone0_2_3', 	'phone0_3_3',
-				   'email0_2_3',	'email0_3_3',
-				   'IM0_2_3','IM0_3_3',	'expand_phone0_3_3',
-				   'expand_email0_3_3',	'expand_IM0_3_3',
-				   'notes','location2',	'demographics',
-				   'location3',		'expand_loc3' 
+				   'expand_email0_3_1', 'expand_IM0_3_1'
 				 );
 
 
@@ -127,7 +79,7 @@
 			document.getElementById(sections[i]).style.display = 'none';
 		}
 
-		document.getElementById('fem').checked = 'checked';
+
 	}
 
 	/** This function is used to display a block. It is usually called by various links which handle requests to display
@@ -151,62 +103,3 @@
 	}
 
 
-	/** This is a trimming function which is used to remove leading and trailing whitespaces. 
-	*   The parameter text_value must have the string type value to be trimmed.	
-	*/
-/* 
-	function trim(text_value) 
-	{
-		chrx = String.fromCharCode(32);
-
-		for(i = 0; i < text_value.length; i++) {
-			if (text_value.charAt(i) == chrx) {
-				  text_value = text_value.substring(i+1,text_value.length);
-			}
- 	 
-	  		if (text_value.charAt(text_value.length - 1) == chrx) {
-	  			text_value = text_value.substring(0,text_value.length -2);
-			}
-		}
-	return text_value;
-	}
-
-*/
-	/** This is a defacto function which verifies for errors on the client side before the page is passed to the server. 
-	   The variable er has the error value which directs the status of submit 
-	   Currently this function is not in use. 	
-	*/
-/*
-	function verify_on_submit( ) 
-	{
-
-		er = 0;
-
-		if (trim(document.getElementById('firstname').value) == '') {
-			alert( 'Please enter the first name' );
-			er = 1;
-		}
-	
-		if (document.getElementById('lastname').value == '') {
-			alert( 'Please enter the last name' );
-			er = 1;
-		}
-
-		var regstr = "^[a-z A-Z]+[a-z A-Z 0-9]*[@][a-z A-Z]+[a-z A-Z 0-9]*[.][a-z A-Z][a-z A-Z][a-z A-Z]$"
-		var r = "^[@][a-z A-Z]+[a-z A-Z 0-9]*[.][a-z A-Z][a-z A-Z][a-z A-Z]$"
-		var regexp = new RegExp( regstr );
-
-		if (regexp.exec(document.getElementById('em11').value) == null) {
-			alert("Please enter a valid email address");
-			er = 1;
-		} 
-
-		if (er ==0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-*/
