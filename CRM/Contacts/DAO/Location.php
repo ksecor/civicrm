5c319e61747aafc0e2ce2ce615747446d68c888e
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-17 05:52:48 +0000

    added DAO for crm_location table
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@211 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Contacts/DAO/Location.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'CRM/Contacts/DAO/LocationBase.php';
+
+/**
+ * This is a dataobject class for Contact Location table.
+ */
+class CRM_Contacts_DAO_Location extends CRM_Contacts_DAO_ContactBase 
+{
+  
+    /**
+     * boolean operator
+     * @var boolean
+     */
+    public $is_primary;
+    
+    /**
+     * @param int location_type_id : foreign key from crm_location_type
+     */
+    
+    public $location_type_id ;
+    
+    /**
+     * This the constructor of the class
+     */
+    
+    function __construct() 
+    {
+        parent::__construct();
+    }
+    
+    /**
+     * This function is used to create the array of the feilds from Contact Location table.
+     * @return array array contains the feilds of the table
+     */
+    function dbFields() 
+    {
+        static $fields;
+        if ($fields === null) {
+            $fields = array_merge(
+                                  parent::dbFields(),
+                                  array(
+                                        'is_primary'        => array(self::TYPE_BOOLEAN),
+                                        'location_type_id'  => array(self::TYPE_INT),
+                                        ) // end of array
+                                  );
+        }
+        return $fields;
+    } // end of method dbFields
+    
+    function links() {
+        static $links;
+        if ($links === null) {
+            $links = array_merge(parent::links(),
+                                 array('location_type_id'  => 'crm_location_type:id'
+                                       )
+                                 );
+        }
+        return $links;
+    } // end of method links()
+    
+} // end of class CRM_Contacts_DAO_Contact_Location
+
+?>
