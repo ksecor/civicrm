48377961585cc7e0005d44554d4b87b4cd4b2c29
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-17 05:51:00 +0000

    added DAO for crm_phone
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@209 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Contacts/DAO/Phone.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once 'CRM/Contacts/DAO/LocationBase.php';
+
+/**
+ * This is a dataobject class for crm_location table.
+ */
+class CRM_Contacts_DAO_Phone extends CRM_Contacts_DAO_LocationBase 
+{
+    
+    /**
+     * boolean operator
+     * @var boolean
+     */
+    public $is_primary;
+    
+    /**
+     * @var string
+     */
+
+    public $phone;
+    public $phone_type;
+    public $mobile_provider_id;
+    
+    /**
+     * This the constructor of the class
+     */
+    
+    function __construct() 
+    {
+        parent::__construct();
+    }
+    
+    /**
+     * This function is used to create the array of the feilds from crm_phone table.
+     * @return array array contains the feilds of the table
+     */
+    function dbFields() 
+    {
+        static $fields;
+        if ($fields === null) {
+            $fields = array_merge(
+                                  parent::dbFields(),
+                                  array(
+                                        'is_primary'         => array(self::TYPE_BOOLEAN),
+                                        'phone'              => array(self::TYPE_STRING),
+                                        'phone_type'         => array(self::TYPE_ENUM),
+                                        'mobile_provider_id' => array(self::TYPE_INT)
+                                        ) // end of array
+                                  );
+        }
+        return $fields;
+    } // end of method dbFields
+    
+    function links() {
+        static $links;
+        if ($links === null) {
+            $links = array_merge(parent::links(),
+                                 array(
+                                       'mobile_provider_id' => 'crm_phone_mobile_provider:id'
+                                       )
+                                 );
+        }
+        return $links;
+    } // end of method links()
+    
+} // end of class CRM_Contacts_DAO_Contact_Phone
+
+?>
