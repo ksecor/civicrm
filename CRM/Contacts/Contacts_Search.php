83b27e52037b3340c68bc5e704faa4076fd3c87e
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-21 09:54:58 +0000

    file for search page
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@247 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Contacts/Contacts_Search.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'CRM/Base.php';
+require_once 'CRM/Controller/SinglePage.php';
+
+require_once 'CRM/DAO/Domain.php';
+
+require_once 'CRM/Contacts/BAO/Contact_Individual.php';
+
+class CRM_Contacts_Contacts_Search extends CRM_Base {
+  
+  protected $_controller;
+
+  function __construct() {
+    parent::__construct();
+  }
+
+  function run( $mode, $id = 0 ) 
+    {
+        $session = CRM_Session::instance();
+        $config  = CRM_Config::instance();
+        
+        // store the return url. Note that this is typically computed by the framework at runtime
+        // based on multiple things (typically where the link was clicked from / http_referer
+        // since we are just starting and figuring out navigation, we are hard coding it here
+        $returnUrl = $config->httpBase . "crm/contact/search?reset=1";
+        //   $returnUrl = $config->httpBase . "crm/contact/add";
+        $session->pushReturnUrl($returnUrl);
+        $this->_controller = new CRM_Controller_SinglePage('CRM_Contacts_Form_SEARCH', 'Contact SEARCH Page', $mode);
+        $this->_controller->process();
+        $this->_controller->run();
+        
+        /**
+        $contact    = new CRM_Contacts_BAO_Contact_Individual();
+        
+        $contact->domain_id = 1;
+        $contact->find();
+        while ( $contact->fetch() ) {
+            // CRM_Utils::debug( 'contactInd', $contact );
+        }
+        
+        $contact = new CRM_Contacts_BAO_Contact_Individual();
+        $contact->contact_type = 'Individual';
+        $contact->sort_name    = 'Donald Lobo';
+        $contact->hash         = 9876543;
+        $contact->domain_id    = 1;
+        $contact->first_name   = 'Donald';
+        $contact->last_name    = 'Lobo';
+        $contact->insert();
+        **/
+    }
+    
+  function display() 
+    {
+        return $this->_controller->getContent();
+    }
+    
+}
+
+?>
