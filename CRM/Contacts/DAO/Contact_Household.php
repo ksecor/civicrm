69c8c0ecccdce8c0c9fb6c8db68f3610a6ba30af
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-02-23 06:20:37 +0000

    made changes according to new schema
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@291 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contacts/DAO/Contact_Household.php
+++ CRM/Contacts/DAO/Contact_Household.php
@@ -2,67 +2,53 @@
 
 require_once 'CRM/Contacts/DAO/ContactBase.php';
 
-class CRM_Contacts_DAO_Contact_Household extends CRM_Contacts_DAO_ContactBase {
-
-  /**
-   * name and other related things for the household
-   * @var string
-   */
-  public $household_name;
-  public $nick_name;
-
-  /**
-   * FK to who the primary contact for the organization is
-   * @var int
-   */
-  public $primary_contact_id;
-
-  /**
-   * various booleans that determine how to contact this household vs
-   * contacting individual members of the household
-   * @var boolean
-   */
-  public $phone_to_household;
-  public $email_to_household;
-  public $postal_to_household;
-
-  /**
-   * annual income of household
-   * @var int
-   */
-  public $annual_income;
-
-  function __construct() {
-    parent::__construct();
-  }
-
-  function links() {
-    static $links;
-    if ( $links === null ) {
-      $links = array_merge( parent::links(),
-                            array( 'primary_contact_id' => 'crm_contact:id' ) );
+class CRM_Contacts_DAO_Contact_Household extends CRM_Contacts_DAO_ContactBase 
+{
+    
+    /**
+     * name and other related things for the household
+     * @var string
+     */
+    public $household_name;
+    public $nick_name;
+    
+    /**
+     * FK to who the primary contact for the organization is
+     * @var int
+     */
+    public $primary_contact_id;
+    
+    function __construct() 
+    {
+        parent::__construct();
     }
-    return $links;
-  }
-
-  function dbFields() {
-    static $fields;
-    if ( $fields === null ) {
-      $fields = array_merge(
-                             parent::dbFields(),
-                             array(
-                                   'household_name'    => array( CRM_Type::T_STRING, null ),
-                                   'nick_name'  => array( CRM_Type::T_STRING, null ),
-                                   'primary_contact_id' => array( CRM_Type::T_INT, null ),
-                                   'phone_to_household' => array( CRM_Type::T_BOOLEAN, null ),
-                                   'email_to_household' => array( CRM_Type::T_BOOLEAN, null ),
-                                   'mail_to_household' => array( CRM_Type::T_BOOLEAN, null ),
-                                   )
-                             );
+    
+    function links() 
+    {
+        static $links;
+        if ($links === null) {
+            $links = array_merge(parent::links(),
+                                 array('primary_contact_id' => 'crm_contact:id'));
+        }
+        return $links;
     }
-    return $fields;
-  }
-
+    
+    function dbFields() 
+    {
+        static $fields;
+        if ($fields === null) {
+            $fields = array_merge(
+                                  parent::dbFields(),
+                                  array(
+                                        'household_name'     => array(CRM_Type::T_STRING, null),
+                                        'nick_name'          => array(CRM_Type::T_STRING, null),
+                                        'primary_contact_id' => array(CRM_Type::T_INT, null)
+                                        )
+                                  );
+        }
+        return $fields;
+    }
+    
 }
 
 ?>
