8bca85d0e1a8feb514ae40944a48c0bf07b14b37
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-03-07 06:15:34 +0000

    fixed the bug to display the Add Organization form correctly removed the constant CRM_DAO_DEBUG_LVL.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@405 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- modules/config.main.php
+++ modules/config.main.php
@@ -7,7 +7,6 @@ $include_path = "$user_home:$include_path";
 ini_set('include_path', $include_path);
 
 define( 'CRM_DSN'                , 'mysql://crm:Mt!Everest@localhost/crm' );
-define( 'CRM_DAO_DEBUG_LVL'      , 0 );
 
 define( 'CRM_TEMPLATE_COMPILEDIR', "$user_home/templates_c" );
 define( 'CRM_TEMPLATEDIR'        , "$user_home/templates"   );
