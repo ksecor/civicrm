5f24cecbf006d7e11d7c87f63dda7e65dc51e817
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-03-09 09:17:32 +0000

    added find and fetch function  in this file now class extends to DAO class of Contact.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@421 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/Contact.php
+++ CRM/Contact/BAO/Contact.php
@@ -1,24 +1,46 @@
 <?php
 
-require_once 'CRM/Contacts/DAO/Contact.php';
+require_once 'CRM/Contact/DAO/Contact.php';
 
-require_once 'CRM/Contacts/BAO/Base.php';
+require_once 'CRM/Contact/BAO/Base.php';
 
 /**
  * rare case where because of inheritance etc, we actually store a reference
  * to the dao object rather than inherit from it
  */
 
-class CRM_Contacts_BAO_Contact extends CRM_Contacts_BAO_Base 
+class CRM_Contact_BAO_Contact extends CRM_Contact_DAO_Contact 
 {
   
-  protected $_contactDAO;
-  
-  function __construct()
-  {
-    parent::__construct();
-    _contactDAO = new CRM_Contacts_DAO_Contact();
-  }
+    protected $_contactDAO;
+    
+    function __construct()
+    {
+        parent::__construct();
+        $this->_contactDAO = new CRM_Contact_DAO_Contact();
+    }
+    
+    function count($countWhat = false,$whereAddOnly = false) {
+        //$this->setContactValues();
+        return parent::count($countWhat, $whereAddOnly);
+    }
+    
+    function find($get = false) 
+    {
+        $this->selectAdd();
+        $this->selectAs($this, '%s');
+        $this->selectAs($this->_contactDAO, $this->_contactDAO->getTableName() . '_%s');
+        parent::find($get);
+    }
+    
+    function fetch() 
+    {
+        $result = parent::fetch();
+        if ($result) {
+            // $this->fillContactValues();
+        }
+        return $result;
+    }
   
 }
 
