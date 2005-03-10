e7cbdab16ec65e6b2c800b20658885bc1f7acae8
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-03-10 18:53:11 +0000

    cleaned up search.php. removed the minisearch part completely.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@433 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Search.php
+++ CRM/Contact/Form/Search.php
@@ -37,7 +37,6 @@
 require_once 'CRM/Form.php';
 require_once 'CRM/SelectValues.php';
 require_once 'CRM/Selector/Controller.php';
-//require_once 'CRM/Contact/Selector/Individual.php';
 require_once 'CRM/Contact/Selector/Selector.php';
 
 /**
@@ -46,136 +45,77 @@ require_once 'CRM/Contact/Selector/Selector.php';
  */
 class CRM_Contact_Form_Search extends CRM_Form {
 
-    /**
-     * Class construtor
-     *
-     * @param string    $name  name of the form
-     * @param CRM_State $state State object that is controlling this form
-     * @param int       $mode  Mode of operation for this form
-     *
-     * @return CRM_Contact_Form_Search
-     * @access public
-     */
-    function __construct( $name, $state, $mode = self::MODE_NONE ) {
+  /**
+   * Class construtor
+   *
+   * @param string    $name  name of the form
+   * @param CRM_State $state State object that is controlling this form
+   * @param int       $mode  Mode of operation for this form
+   *
+   * @return CRM_Contact_Form_Search
+   * @access public
+   */
+  function __construct($name, $state, $mode = self::MODE_NONE)
+  {
+    parent::__construct($name, $state, $mode);
+  }
 
-        //        CRM_Error::debug_stacktrace(2);
+  /**
+   * Build the form
+   *
+   * @access protected
+   * @return void
+   */
+  function buildQuickForm( ) {
+    $this->addElement('text','mode',self::MODE_SEARCH);
+    $this->add('select', 'contact_type', 'Contact Type', CRM_SelectValues::$contactType);
+    $this->addDefaultButtons( array(
+				    array ( 'type'      => 'refresh',
+					    'name'      => 'Submit' ,
+					    'isDefault' => true     ),
+				    array ( 'type'      => 'done'  ,
+					    'name'      => 'Done'   ),
+				    array ( 'type'      => 'reset' ,
+					    'name'      => 'Reset'  ),
+				    array ( 'type'      => 'cancel',
+					    'name'      => 'Cancel' ),
+				    )
+			      );
+  }
 
-        parent::__construct($name, $state, $mode);
+
+  /**
+   * Set the default form values
+   *
+   * @access protected
+   * @return array the default array reference
+   */
+  function &setDefaultValues( ) {
+    $defaults = array( );
+    return $defaults;
+  }
+
+  /**
+   * Add local and global form rules
+   *
+   * @access protected
+   * @return void
+   */
+  function addRules( ) {
+  }
+
+  function postProcess() 
+  {
+    $params = array( );
+    $contact_type = trim($this->controller->exportValue($this->_name, 'contact_type'));
+    if (!empty( $contact_type ))  {
+      $params['contact_type'] = $contact_type;
     }
 
-    /**
-     * Build the form
-     *
-     * @access protected
-     * @return void
-     */
-    function buildQuickForm( ) {
-        
-        switch($this->_mode) {
-        case self::MODE_SEARCH:
-            $this->addElement('text','mode',self::MODE_SEARCH);
-            $this->_buildSearchForm();
-            break;
-        case self::MODE_SEARCH_MINI:
-            $this->addElement('text', 'mode', self::MODE_SEARCH_MINI);
-            $this->_buildMiniSearchForm();
-            break;          
-        }
-    }
-
-
-    private function _buildSearchForm()
-    {
-        //$this->setFormAction('index.php?q=crm/contact/list');
-        $this->add('select', 'contact_type', 'Contact Type', CRM_SelectValues::$contactType);
-        
-        $this->addDefaultButtons( array(
-                                        array ( 'type'      => 'refresh',
-                                                'name'      => 'Submit' ,
-                                                'isDefault' => true     ),
-                                        array ( 'type'      => 'done'  ,
-                                                'name'      => 'Done'   ),
-                                        array ( 'type'      => 'reset' ,
-                                                'name'      => 'Reset'  ),
-                                        array ( 'type'      => 'cancel',
-                                                'name'      => 'Cancel' ),
-                                        )
-                                  );
-    }
-    
-    /**
-     * Set the default form values
-     *
-     * @access protected
-     * @return array the default array reference
-     */
-    function &setDefaultValues( ) {
-        $defaults = array( );
-        return $defaults;
-    }
-
-    /**
-     * Add local and global form rules
-     *
-     * @access protected
-     * @return void
-     */
-    function addRules( ) {
-    }
-
-    /**
-     * Pre-process the environment before building the form
-     *
-     * @access protected
-     * @return void
-     */
-    function preProcess() 
-    {
-        switch ($this->_mode) {
-        case self::MODE_SEARCH:
-            $this->_searchPreProcess();
-            break;            
-        case self::MODE_SEARCH_MINI:
-            $this->_miniSearchPreProcess();
-            break;            
-        }
-    }
-
-
-    function postProcess() 
-    {
-        switch ($this->_mode) {
-        case self::MODE_SEARCH:
-            $this->_searchPostProcess();
-            break;            
-        case self::MODE_SEARCH_MINI:
-            $this->_miniSearchPostProcess();
-            break;            
-        }
-    }
-
-    function _searchPreProcess() {
-    }
-
-    /**
-     * Process the form submit
-     *
-     *
-     * @access protected
-     * @return void
-     */
-    function _searchPostProcess( ) {
-        $params = array( );
-        $contact_type = trim($this->controller->exportValue($this->_name, 'contact_type'));
-        if (!empty( $contact_type ))  {
-            $params['contact_type'] = $contact_type;
-        }
-
-        $selector   = new CRM_Contact_Selector_Selector($params);
-        $controller = new CRM_Selector_Controller($contact , null, null, CRM_Action::VIEW, CRM_Selector_Controller::TEMPLATE);
-        $controller->run();
-    }
-
+    $selector   = new CRM_Contact_Selector_Selector($params);
+    $controller = new CRM_Selector_Controller($selector , null, null, CRM_Action::VIEW, CRM_Selector_Controller::TEMPLATE);
+    $controller->run();
+  }
 }
 
 ?>
