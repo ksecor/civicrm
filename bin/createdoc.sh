e1fde00ac7d874a6d0ab710630e21acd5bb8c706
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-04-01 15:15:29 +0000

    - script to generate documentation
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@611 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ bin/createdoc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -v
+
+cd ..
+
+# /*
+#  * folder to be parsed
+#  */
+PARSE_FOLDER=$PWD
+
+# /*
+#  * target folder (documents will be generated in this folder)
+#  */
+TARGET_FOLDER=$PWD/doc/documentation/
+
+# /*
+#  * title of generated documentation
+#  */
+TITLE="civiCRM"
+
+# /* 
+#  * parse @internal and elements marked private with @access
+#  */
+PRIVATE=on
+
+# /*
+#  * JavaDoc-compliant description parsing
+#  */
+JAVADOC_STYLE=off
+
+# /*
+#  * parse a PEAR-style repository
+#  */
+PEAR_STYLE=on
+
+# /*
+#  * generate highlighted sourcecode for every parced file
+#  */
+SOURCECODE=off
+
+# /*
+#  * output information (output:converter:templatedir)
+#  */
+OUTPUT=HTML:frames:phpedit
+
+phpdoc -t $TARGET_FOLDER -o $OUTPUT -d $PARSE_FOLDER -ti "$TITLE" -pp $PRIVATE -j $JAVADOC_STYLE -p $PEAR_STYLE -s $SOURCECODE
