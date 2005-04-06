bbbe1430bde46423869739e777c8380344a27d86
yvb <yvb@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-04-06 05:26:53 +0000

    removed the CRM_DSN defination from confic.main.php to config.user.php
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@656 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- modules/config.main.php
+++ modules/config.main.php
@@ -6,8 +6,6 @@ $include_path = ini_get('include_path');
 $include_path = ".:$user_home:$user_home/packages:$include_path";
 ini_set('include_path', $include_path);
 
-define( 'CRM_DSN'                , 'mysql://crm:Mt!Everest@localhost/crm' );
-
 define( 'CRM_TEMPLATE_COMPILEDIR', "$user_home/templates_c" );
 define( 'CRM_TEMPLATEDIR'        , "$user_home/templates"   );
 
--- modules/config.user.php
+++ modules/config.user.php
@@ -3,11 +3,12 @@
 global $user_home;
 $user_home = '/Users/lobo/svn/crm';
 
-define( 'CRM_SMARTYDIR', $user_home . '/packages/Smarty/' );
-define( 'CRM_HTTPBASE' , '/lobo/drupal/crm/'              );
-define( 'CRM_MAINMENU' , '/lobo/drupal/crm/'              );
-define( 'CRM_DAO_DEBUG', 0                                );
-define( 'CRM_TEST_DIR' , $user_home . '/test/'            );
+define( 'CRM_SMARTYDIR', $user_home . '/packages/Smarty/'          );
+define( 'CRM_HTTPBASE' , '/lobo/drupal/crm/'                       );
+define( 'CRM_MAINMENU' , '/lobo/drupal/crm/'                       );
+define( 'CRM_DAO_DEBUG', 0                                         );
+define( 'CRM_TEST_DIR' , $user_home . '/test/'                     );
+define( 'CRM_DSN'      , 'mysql://crm:YOUR_PASSWORD@localhost/crm' );
 
 include_once 'config.main.php';
 
