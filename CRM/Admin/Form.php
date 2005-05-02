1d2395613e45328190e99c37a0814c616a7ec24f
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-05-02 22:49:57 +0000

    add weights to menu. fix admin forms, set more embedding values
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@898 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Admin/Form.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ +----------------------------------------------------------------------+
+ | CiviCRM version 1.0                                                  |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                    |
+ +----------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                      |
+ |                                                                      |
+ | CiviCRM is free software; you can redistribute it and/or modify it   |
+ | under the terms of the Affero General Public License Version 1,      |
+ | March 2002.                                                          |
+ |                                                                      |
+ | CiviCRM is distributed in the hope that it will be useful, but       |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 |
+ | See the Affero General Public License for more details at            |
+ | http://www.affero.org/oagpl.html                                     |
+ |                                                                      |
+ | A copy of the Affero General Public License has been been            |
+ | distributed along with this program (affero_gpl.txt)                 |
+ +----------------------------------------------------------------------+
+*/
+
+/**
+ *
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo 01/15/2005
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * This class generates form components generic to Mobile provider
+ * 
+ */
+class CRM_Admin_Form extends CRM_Form
+{
+    /**
+     * The id of the object being edited / created
+     *
+     * @var int
+     */
+    protected $_id;
+
+    /**
+     * The name of the BAO object for this form
+     *
+     * @var string
+     */
+    protected $_BAOName;
+
+    /**
+     * class constructor
+     *
+     * @param string $name        Name of the form.
+     * @param string $state       The state object associated with this form
+     * @param int    $mode        The mode of the form
+     *
+     * @return CRM_Admin_Form
+     * @access public
+     */
+    function __construct($name, $state, $mode = self::MODE_NONE) {
+        parent::__construct($name, $state, $mode);
+    }
+
+    function preProcess( ) {
+        $this->_id      = $this->get( 'id'      );
+        $this->_BAOName = $this->get( 'BAOName' );
+    }
+
+    /**
+     * This function sets the default values for the form. MobileProvider that in edit/view mode
+     * the default values are retrieved from the database
+     * 
+     * @access public
+     * @return None
+     */
+    function setDefaultValues( ) {
+        $defaults = array( );
+        $params   = array( );
+
+        if ( isset( $this->_id ) ) {
+            $params = array( 'id' => $this->_id );
+            eval( $this->_BAOName . '::retrieve( $params, $defaults );' );
+        }
+  
+        return $defaults;
+    }
+
+    /**
+     * Function to actually build the form
+     *
+     * @return None
+     * @access public
+     */
+    public function buildQuickForm( ) {
+        $this->addButtons( array(
+                                 array ( 'type'      => 'next',
+                                         'name'      => 'Save',
+                                         'isDefault' => true   ),
+                                 array ( 'type'       => 'cancel',
+                                         'name'      => 'Cancel' ),
+                                 )
+                           );
+        
+    }
+
+}
+
+?>
