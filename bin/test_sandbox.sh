092a20bb9a27008ce8bea5070c8b4c844bf8177a
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-05-26 12:33:10 +0000

    - Worked on CRM-99 - deleted old Notes related script - added new Notes script for sandbox testing - modified test_sandbox.sh to include new scripts
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1189 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/test_sandbox.sh
+++ bin/test_sandbox.sh
@@ -2,6 +2,14 @@
 
 cd ../test/maxq
 
+## To run Test Script uncomment following files..
+## One can uncomment one or more than one file(s) to carry on the test.
+## Also, please do not change the sequence of the files.
+## The -r mode is for running maxq..hence please do not remove it
+## The -q mode is for quite mode while testing..
+## Instead of -q (quite mode), one can give -d(debug mode)
+## For all other options, fire $ masq --help   
+
 #maxq -r -q testViewContactIndividual.py 
 
 #maxq -r -q testViewContactHousehold.py 
@@ -20,21 +28,36 @@ cd ../test/maxq
 
 #maxq -r -q testEditContactOrganization.py
 
-maxq -r -q testViewRelByRelTab.py
+#maxq -r -q testViewRelByRelTab.py
 
-maxq -r -q testEditRelByRelTab.py
+#maxq -r -q testEditRelByRelTab.py
 
-maxq -r -q testAddRelByRelTab.py
+#maxq -r -q testAddRelByRelTab.py
 
-maxq -r -q testDeleteRelByRelTab.py
+#maxq -r -q testDeleteRelByRelTab.py
 
-maxq -r -q testDisableEnableRelByRelTab.py
+#maxq -r -q testDisableEnableRelByRelTab.py
 
-maxq -r -q testViewRelByContactTab.py
+#maxq -r -q testViewRelByContactTab.py
 
-maxq -r -q testEditRelByContactTab.py
+#maxq -r -q testEditRelByContactTab.py
 
-maxq -r -q testAddRelByContactTab.py
+#maxq -r -q testAddRelByContactTab.py
 
+#maxq -r -q testGroupAllByGroupTab.py
 
+#maxq -r -q testGroupAllByContactTab.py
 
+#maxq -r -q testTagsAllByTagsTab.py
+
+maxq -r -q testViewNoteByNoteTab.py
+
+maxq -r -q testAddNoteByNoteTab.py
+
+maxq -r -q testEditNoteByNoteTab.py
+
+maxq -r -q testDeleteNoteByNoteTab.py
+
+maxq -r -q testAddNoteByContactTab.py
+
+maxq -r -q testEditNoteByContactTab.py
--- test/maxq/testEditNoteByNoteTab.py
+++ test/maxq/testEditNoteByNoteTab.py
@@ -9,13 +9,15 @@ exec 'from '+validatorPkg+' import Validator'
 
 
 # definition of test class
-class testAddNoteByNotesTab(PyHttpTestCase):
+class testEditNoteByNoteTab(PyHttpTestCase):
     def runTest(self):
         self.msg('Test started')
 
-        drupal_path = "http://localhost/drupal"
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/''') % drupal_path)
+        #drupal_path = "http://localhost/drupal"
+        drupal_path = "http://sandbox.openngo.org/crm"
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/''') % drupal_path)
         url = "%s/" % drupal_path
+        self.msg("Testing URL: %s" % url)
         params = None
         Validator.validateRequest(self, self.getMethod(), "get", url, params)
         self.get(url, params)
@@ -28,16 +30,18 @@ class testAddNoteByNotesTab(PyHttpTestCase):
             ('''edit[name]''', self.userInput('Enter Drupal UserName')),
             ('''edit[pass]''', self.userInput('Enter Drupal Password')),
             ('''op''', '''Log in'''),]
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/user/login?edit[destination]=node&edit[name]=manishzope&edit[pass]=manish&op=Log in''') % drupal_path)
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/user/login?edit[destination]=node&edit[name]=manishzope&edit[pass]=manish&op=Log in''') % drupal_path)
         url = "%s/user/login" % drupal_path
+        self.msg("Testing URL: %s" % url)
         Validator.validateRequest(self, self.getMethod(), "post", url, params)
         self.post(url, params)
         self.msg("Response code: %s" % self.getResponseCode())
         self.assertEquals("Assert number 2 failed", 302, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
         
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/node''') % drupal_path)
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/node''') % drupal_path)
         url = "%s/node" % drupal_path
+        self.msg("Testing URL: %s" % url)
         params = None
         Validator.validateRequest(self, self.getMethod(), "get", url, params)
         self.get(url, params)
@@ -45,83 +49,75 @@ class testAddNoteByNotesTab(PyHttpTestCase):
         self.assertEquals("Assert number 3 failed", 200, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
 
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note''') % drupal_path)
-        url = "%s/civicrm/contact/view/note" % drupal_path
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/search''') % drupal_path)
+        url = "%s/civicrm/contact/search" % drupal_path
+        self.msg("Testing URL: %s" % url)
         params = None
         Validator.validateRequest(self, self.getMethod(), "get", url, params)
         self.get(url, params)
         self.msg("Response code: %s" % self.getResponseCode())
         self.assertEquals("Assert number 4 failed", 200, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
-        
-        # self.msg("Testing URL: %s" % self.replaceURL('''http://localhost/favicon.ico'''))
-        # url = "http://localhost/favicon.ico"
-        # params = None
-        # Validator.validateRequest(self, self.getMethod(), "get", url, params)
-        # self.get(url, params)
-        # self.msg("Response code: %s" % self.getResponseCode())
-        # self.assertEquals("Assert number 5 failed", 404, self.getResponseCode())
-        # Validator.validateResponse(self, self.getMethod(), url, params)
-        
+
         params = [
-            ('''cid''', '''70'''),
-            ('''action''', '''add'''),]
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?cid=70&action=add''') % drupal_path)
+            ('''nid''', '''201'''),
+            ('''action''', '''update'''),]
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?nid=201&action=update''') % drupal_path)
         url = "%s/civicrm/contact/view/note" % drupal_path
+        self.msg("Testing URL: %s" % url)
         Validator.validateRequest(self, self.getMethod(), "get", url, params)
         self.get(url, params)
         self.msg("Response code: %s" % self.getResponseCode())
-        self.assertEquals("Assert number 6 failed", 200, self.getResponseCode())
+        self.assertEquals("Assert number 5 failed", 200, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
         
-        # self.msg("Testing URL: %s" % self.replaceURL('''http://localhost/favicon.ico'''))
-        # url = "http://localhost/favicon.ico"
-        # params = None
-        # Validator.validateRequest(self, self.getMethod(), "get", url, params)
-        # self.get(url, params)
-        # self.msg("Response code: %s" % self.getResponseCode())
-        # self.assertEquals("Assert number 7 failed", 404, self.getResponseCode())
-        # Validator.validateResponse(self, self.getMethod(), url, params)
+        #self.msg("Testing URL: %s" % self.replaceURL('''http://192.168.2.9/favicon.ico'''))
+        #url = "http://192.168.2.9/favicon.ico"
+        #params = None
+        #Validator.validateRequest(self, self.getMethod(), "get", url, params)
+        #self.get(url, params)
+        #self.msg("Response code: %s" % self.getResponseCode())
+        #self.assertEquals("Assert number 6 failed", 404, self.getResponseCode())
+        #Validator.validateResponse(self, self.getMethod(), url, params)
         
         params = [
-            ('''note''', '''New Note for this contact
-Checking it with the maxQ test case
-'''),
+            ('''_qf_default''', '''Note:next'''),
+            ('''note''', '''Adding test Note....Added note is being tested for editing..'''),
             ('''_qf_Note_next''', '''Save'''),]
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?note=New Note for this contact
-Checking it with the maxQ test case
-&_qf_Note_next=Save''') % drupal_path)
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?_qf_default=Note:next&note=Adding test Note....Added note is being tested for editing..&_qf_Note_next=Save''') % drupal_path)
         url = "%s/civicrm/contact/view/note" % drupal_path
+        self.msg("Testing URL: %s" % url)
         Validator.validateRequest(self, self.getMethod(), "post", url, params)
         self.post(url, params)
         self.msg("Response code: %s" % self.getResponseCode())
-        self.assertEquals("Assert number 8 failed", 302, self.getResponseCode())
+        self.assertEquals("Assert number 7 failed", 200, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
         
         params = [
             ('''action''', '''browse'''),]
-        self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?action=browse''') % drupal_path)
+        #self.msg("Testing URL: %s" % self.replaceURL('''%s/civicrm/contact/view/note?action=browse''') % drupal_path)
         url = "%s/civicrm/contact/view/note" % drupal_path
+        self.msg("Testing URL: %s" % url)
         Validator.validateRequest(self, self.getMethod(), "get", url, params)
         self.get(url, params)
         self.msg("Response code: %s" % self.getResponseCode())
-        self.assertEquals("Assert number 9 failed", 200, self.getResponseCode())
+        self.assertEquals("Assert number 8 failed", 200, self.getResponseCode())
         Validator.validateResponse(self, self.getMethod(), url, params)
         
-        # self.msg("Testing URL: %s" % self.replaceURL('''http://localhost/favicon.ico'''))
-        # url = "http://localhost/favicon.ico"
-        # params = None
-        # Validator.validateRequest(self, self.getMethod(), "get", url, params)
-        # self.get(url, params)
-        # self.msg("Response code: %s" % self.getResponseCode())
-        # self.assertEquals("Assert number 10 failed", 404, self.getResponseCode())
-        # Validator.validateResponse(self, self.getMethod(), url, params)
+        #self.msg("Testing URL: %s" % self.replaceURL('''http://192.168.2.9/favicon.ico'''))
+        #url = "http://192.168.2.9/favicon.ico"
+        #params = None
+        #Validator.validateRequest(self, self.getMethod(), "get", url, params)
+        #self.get(url, params)
+        #self.msg("Response code: %s" % self.getResponseCode())
+        #self.assertEquals("Assert number 9 failed", 404, self.getResponseCode())
+        #Validator.validateResponse(self, self.getMethod(), url, params)
         
-
+        self.msg('Test successfully complete.')
     # ^^^ Insert new recordings here.  (Do not remove this line.)
 
 
 # Code to load and run the test
 if __name__ == 'main':
-    test = testAddNoteByNotesTab("testAddNoteByNotesTab")
+    test = testEditNoteByNoteTab("testEditNoteByNoteTab")
     test.Run()
