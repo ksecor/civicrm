43bed65043aaee0954161c2403ef7d2b1bfcb367
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-05-30 07:47:31 +0000

    a version that generates the POT file lines, as opposed to C gettext() calls
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1208 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/smarty-extractor.php
+++ bin/smarty-extractor.php
@@ -1,7 +1,7 @@
 #!/opt/php5/bin/php -qn
 <?php
 /**
- * tsmarty2c.php - rips gettext strings from smarty template
+ * smarty-extractor.php - rips gettext strings from Smarty {ts} calls
  *
  * ------------------------------------------------------------------------- *
  * This library is free software; you can redistribute it and/or             *
@@ -19,19 +19,20 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
  * ------------------------------------------------------------------------- *
  *
- * This command line script rips gettext strings from smarty file, and prints them to stdout in C format, 
- * that can later be used with the standard gettext tools.
+ * This command line script rips gettext strings from smarty file, and prints
+ * them to stdout; this can later be used with the standard gettext tools.
  *
  * Usage:
- * ./tsmarty2c.php <filename or directory> <file2> <..> > smarty.c
+ * ./smarty-extractor.php <filename or directory> [file2, ...]
  *
  * If a parameter is a directory, the template files within will be parsed.
  *
- * @package	smarty-gettext
- * @version	$Id: tsmarty2c.php,v 1.2 2004/04/30 11:40:22 sagi Exp $
- * @link	http://smarty-gettext.sf.net/
- * @author	Sagi Bashari <sagi@boom.org.il>
+ * @version   $Id$
+ * @link      http://smarty-gettext.sf.net/
+ * @author    Sagi Bashari <sagi@boom.org.il>
+ * @author    Piotr Szotkowski <shot@caltha.pl>
  * @copyright 2004 Sagi Bashari
+ * @license   http://www.gnu.org/licenses/lgpl.html  GNU Lesser General Public License
  */
 
 // smarty open tag
@@ -49,66 +50,76 @@ $extensions = array('tpl');
 // "fix" string - strip slashes, escape and convert new lines to \n
 function fs($str)
 {
-	$str = stripslashes($str);
-	$str = str_replace('"', '\"', $str);
-	$str = str_replace("\n", '\n', $str);
-	return $str;
+    $str = stripslashes($str);
+    $str = str_replace('"', '\"', $str);
+    $str = str_replace("\n", '\n', $str);
+    return $str;
 }
 
 // rips gettext strings from $file and prints them in C format
 function do_file($file)
 {
-	$content = @file_get_contents($file);
+    $content = @file_get_contents($file);
 
-	if (empty($content)) {
-		return;
-	}
+    if (empty($content)) {
+        return;
+    }
 
-	global $ldq, $rdq, $cmd;
+    global $ldq, $rdq, $cmd;
 
-	preg_match_all("/{$ldq}\s*({$cmd})\s*([^{$rdq}]*){$rdq}([^{$ldq}]*){$ldq}\/\\1{$rdq}/", $content, $matches);
-	
-	for ($i=0; $i < count($matches[0]); $i++) {
-		if (preg_match('/plural\s*=\s*["\']?\s*(.[^\"\']*)\s*["\']?/', $matches[2][$i], $match)) {
-			print 'ngettext("'.fs($matches[3][$i]).'","'.fs($match[1]).'",x);'."\n";
-		} else {
-			print 'gettext("'.fs($matches[3][$i]).'");'."\n";
-		}
-	}
+    preg_match_all("/{$ldq}\s*({$cmd})\s*([^{$rdq}]*){$rdq}([^{$ldq}]*){$ldq}\/\\1{$rdq}/", $content, $matches);
+    
+    for ($i=0; $i < count($matches[0]); $i++) {
+        $output = array();
+        if (preg_match('/plural\s*=\s*["\']?\s*(.[^\"\']*)\s*["\']?/', $matches[2][$i], $match)) {
+            //print 'ngettext("' . fs($matches[3][$i]) . '","' . fs($match[1]) . '",x);' . "\n";
+            $output[] = 'msgid "' . fs($matches[3][$i]) . '"';
+            $output[] = 'msgid_plural "' . fs($match[1]) . '"';
+            $output[] = 'msgstr[0] ""';
+            $output[] = 'msgstr[1] ""';
+        } else {
+            //print 'gettext("' . fs($matches[3][$i]) . '");' . "\n";
+            $output[] = 'msgid "' . fs($matches[3][$i]) . '"';
+            $output[] = 'msgstr ""';
+        }
+        print implode("\n", $output) . "\n\n";
+    }
 }
 
 // go through a directory
 function do_dir($dir)
 {
-	$d = dir($dir);
+    $d = dir($dir);
 
-	while (false !== ($entry = $d->read())) {
-		if ($entry == '.' || $entry == '..') {
-			continue;
-		}
+    while (false !== ($entry = $d->read())) {
+        if ($entry == '.' || $entry == '..') {
+            continue;
+        }
 
-		$entry = $dir.'/'.$entry;
+        $entry = $dir.'/'.$entry;
 
-		if (is_dir($entry)) { // if a directory, go through it
-			do_dir($entry);
-		} else { // if file, parse only if extension is matched
-			$pi = pathinfo($entry);
-			
-			if (isset($pi['extension']) && in_array($pi['extension'], $GLOBALS['extensions'])) {
-				do_file($entry);
-			}
-		}
-	}
+        if (is_dir($entry)) {
+            // if a directory, go through it
+            do_dir($entry);
+        } else {
+            // if file, parse only if extension is matched
+            $pi = pathinfo($entry);
+            
+            if (isset($pi['extension']) && in_array($pi['extension'], $GLOBALS['extensions'])) {
+                do_file($entry);
+            }
+        }
+    }
 
-	$d->close();
+    $d->close();
 }
 
 for ($ac=1; $ac < $_SERVER['argc']; $ac++) {
-	if (is_dir($_SERVER['argv'][$ac])) { // go through directory
-		do_dir($_SERVER['argv'][$ac]);
-	} else { // do file
-		do_file($_SERVER['argv'][$ac]);
-	}
+    if (is_dir($_SERVER['argv'][$ac])) {
+        do_dir($_SERVER['argv'][$ac]);
+    } else {
+        do_file($_SERVER['argv'][$ac]);
+    }
 }
 
 ?>
