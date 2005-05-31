20599fc468d6abb67e012a7616d181b530d6333f
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-05-31 12:27:54 +0000

    more tagging in progress
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1228 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Individual.php
+++ CRM/Contact/Form/Individual.php
@@ -58,34 +58,34 @@ class CRM_Contact_Form_Individual {
         $attributes = CRM_Core_DAO::getAttribute('CRM_Contact_DAO_Individual');
 
         // first_name
-        $form->addElement('text', 'first_name', 'First Name', $attributes['first_name'] );
+        $form->addElement('text', 'first_name', ts('First Name'), $attributes['first_name'] );
         
         // last_name
-        $form->addElement('text', 'last_name', 'Last Name', $attributes['last_name'] );
+        $form->addElement('text', 'last_name', ts('Last Name'), $attributes['last_name'] );
         
         // suffix
         $form->addElement('select', 'suffix', null, CRM_Core_SelectValues::$suffixName);
         
         // greeting type
-        $form->addElement('select', 'greeting_type', 'Greeting type :', CRM_Core_SelectValues::$greeting);
+        $form->addElement('select', 'greeting_type', ts('Greeting type:'), CRM_Core_SelectValues::$greeting);
         
         // job title
-        $form->addElement('text', 'job_title', 'Job title :', $attributes['job_title']);
+        $form->addElement('text', 'job_title', ts('Job title:'), $attributes['job_title']);
         
         // radio button for gender
         $genderOptions = array( );
-        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', 'Gender', 'Female', 'Female');
-        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', 'Gender', 'Male', 'Male');
-        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', 'Gender', 'Transgender','Transgender');
-        $form->addGroup( $genderOptions, 'gender', 'Gender' );
+        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', ts('Gender'), ts('Female'), ts('Female'));
+        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', ts('Gender'), ts('Male'), ts('Male'));
+        $genderOptions[] = HTML_QuickForm::createElement('radio', 'gender', ts('Gender'), ts('Transgender'), ts('Transgender'));
+        $form->addGroup($genderOptions, 'gender', ts('Gender'));
         
-        $form->addElement('checkbox', 'is_deceased', null, 'Contact is deceased');
+        $form->addElement('checkbox', 'is_deceased', null, ts('Contact is deceased'));
         
-        $form->addElement('date', 'birth_date', 'Date of birth', CRM_Core_SelectValues::date( 'birth' ) );
-        $form->addRule('birth_date', 'Select a valid date.', 'qfDate' );
+        $form->addElement('date', 'birth_date', ts('Date of birth'), CRM_Core_SelectValues::date('birth'));
+        $form->addRule('birth_date', ts('Select a valid date.'), 'qfDate');
 
-        $config = CRM_Core_Config::singleton( );
-        CRM_Core_ShowHideBlocks::links( $this, 'demographics', '<img src="'.$config->resourceBase.'i/TreePlus.gif" class="action-icon" alt="open section">' , '<img src="'.$config->resourceBase.'i/TreeMinus.gif" class="action-icon" alt="close section">'  );
+        $config = CRM_Core_Config::singleton();
+        CRM_Core_ShowHideBlocks::links($this, 'demographics', '<img src="' . $config->resourceBase . 'i/TreePlus.gif" class="action-icon" alt="' . ts('open section') . '">' , '<img src="' . $config->resourceBase . 'i/TreeMinus.gif" class="action-icon" alt="' . ts('close section') . '">');
     }
 
     static function formRule( &$fields ) {
@@ -100,7 +100,7 @@ class CRM_Contact_Form_Individual {
         if (! ( (CRM_Utils_Array::value( 'first_name', $fields ) && 
                  CRM_Utils_Array::value( 'last_name' , $fields )    ) ||
                 !empty( $primaryEmail ) ) ) {
-            $errors['first_name'] = "First Name and Last Name OR an email in the Primary Location should be set.";
+            $errors['first_name'] = ts('First Name and Last Name OR an email in the Primary Location should be set.');
         }
         
         // add code to make sure that the uniqueness criteria is satisfied
--- templates/CRM/Block/Add.tpl
+++ templates/CRM/Block/Add.tpl
@@ -2,17 +2,17 @@
 <form action="{$postURL}" method="post">
 
 <div class="form-item">
-    <label for="firstname">First Name:</label></br>
+    <label for="firstname">{ts}First Name:{/ts}</label></br>
     <input type="text" name="first_name" class="form-text" maxlength="64" />
 </div>
 
 <div class="form-item">
-    <label for="lastname">Last Name:</label><br/>
+    <label for="lastname">{ts}Last Name:{/ts}</label><br/>
     <input type="text" name="last_name" class="form-text required" maxlength="64" />
 </div>
 
 <div class="form-item">
-    <label for="email">Email:</label><br/>
+    <label for="email">{ts}Email:{/ts}</label><br/>
     <input type="text" name="location[1][email][1][email]" class="form-text" maxlength="64" />
 </div>
 
@@ -20,7 +20,7 @@
 <input type="hidden" name="location[1][is_primary]"       value="1" />
 <input type="hidden" name="c_type"                        value="Individual" />
 
-<div class="form-item"><input type="submit" name="_qf_Edit_next" value="Save" class="form-submit" /></div>
+<div class="form-item"><input type="submit" name="_qf_Edit_next" value="{ts}Save{/ts}" class="form-submit" /></div>
 
 </form>
 </div>
