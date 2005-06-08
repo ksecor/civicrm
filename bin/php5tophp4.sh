18f5d91b74cfc494edeebf502ece88f73ccad8c9
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-06-08 17:16:25 +0000

    small tweak to generator to be able to use different phps installed on one machine
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1384 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/php5tophp4.sh
+++ bin/php5tophp4.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -v
 
+export PHPPATH=/opt/bin
+
+
 echo Start of code conversion from php5 to php4....
-php converter.php
+$PHPPATH/php converter.php
 
-php converter.php ../modules/civicrm.module > ../../civicrm/modules/civicrm.module
+$PHPPATH/php converter.php ../modules/civicrm.module > ../../civicrm/modules/civicrm.module
 
-php converter.php ../../civicrm/CRM/Utils/Type.php > /tmp/Type.php
+$PHPPATH/php converter.php ../../civicrm/CRM/Utils/Type.php > /tmp/Type.php
 mv /tmp/Type.php ../../civicrm/CRM/Utils/Type.php
 
-php converter.php ../../civicrm/CRM/Contact/Task.php > /tmp/Task.php
+$PHPPATH/php converter.php ../../civicrm/CRM/Contact/Task.php > /tmp/Task.php
 mv /tmp/Task.php ../../civicrm/CRM/Contact/Task.php
 
-php converter.php ../../civicrm/CRM/Contact/Form/Location.php > /tmp/Location.php
+$PHPPATH/php converter.php ../../civicrm/CRM/Contact/Form/Location.php > /tmp/Location.php
 mv /tmp/Location.php ../../civicrm/CRM/Contact/Form/Location.php
 
-php converter.php ../../civicrm/CRM/Import/Parser.php > /tmp/Parser.php
+$PHPPATH/php converter.php ../../civicrm/CRM/Import/Parser.php > /tmp/Parser.php
 mv /tmp/Parser.php ../../civicrm/CRM/Import/Parser.php
 
 echo End of code conversion from php5 to php4....
