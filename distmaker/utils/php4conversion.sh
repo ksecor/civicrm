f53db90704cdb9e42080ed30bd26a96a78c696aa
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-06-13 01:51:26 +0000

    Final touch, last bugfixes
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1435 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- distmaker/utils/php4conversion.sh
+++ distmaker/utils/php4conversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 P=`dirname $0`
-CFFILE=$P/distmaker.conf
+CFFILE=$P/../distmaker.conf
 
 if [ ! -f $CFFILE ] ; then	
 	echo "NO DISTMAKER.CONF FILE!"
@@ -13,7 +13,7 @@ fi
 echo;echo Start of code conversion from php5 to php4....;echo;
 $DM_PHP5PATH/php $P/converter.php
 
-$DM_PHP5PATH/php $P/converter.php $DM_SOURCEPATH/modules/civicrm.module > $DM_GENFILESDIR/modules/civicrm.module
+$DM_PHP5PATH/php $P/converter.php $DM_SOURCEDIR/modules/civicrm.module > $DM_GENFILESDIR/modules/civicrm.module
 
 $DM_PHP5PATH/php $P/converter.php $DM_GENFILESDIR/CRM/Utils/Type.php > $DM_TMPDIR/Type.php
 mv $DM_TMPDIR/Type.php $DM_GENFILESDIR/CRM/Utils/Type.php
