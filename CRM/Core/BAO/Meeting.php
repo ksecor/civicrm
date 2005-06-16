5efb41c37127562d3e43d723081c9f235b289125
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-06-16 13:16:52 +0000

    // added missing files    CRM-136
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@1511 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Core/BAO/Meeting.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ +----------------------------------------------------------------------+
+ | CiviCRM version 1.0                                                  |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                    |
+ +----------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                      |
+ |                                                                      |
+ | CiviCRM is free software; you can redistribute it and/or modify it   |
+ | under the terms of the Affero General Public License Version 1,      |
+ | March 2002.                                                          |
+ |                                                                      |
+ | CiviCRM is distributed in the hope that it will be useful, but       |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 |
+ | See the Affero General Public License for more details at            |
+ | http://www.affero.org/oagpl.html                                     |
+ |                                                                      |
+ | A copy of the Affero General Public License has been been            |
+ | distributed along with this program (affero_gpl.txt)                 |
+ +----------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo 01/15/2005
+ * $Id$
+ *
+ */
+
+/**
+ * This class is for Meeting functions
+ *
+ */
+
+class CRM_Core_BAO_Meeting extends CRM_Core_DAO_ActivityMeeting 
+{
+
+    /**
+     * class constructor
+     */
+    function __construct( ) {
+        parent::__construct( );
+    }
+
+    /**
+     * Takes a bunch of params that are needed to match certain criteria and
+     * retrieves the relevant objects. Typically the valid params are only
+     * contact_id. We'll tweak this function to be more full featured over a period
+     * of time. This is the inverse function of create. It also stores all the retrieved
+     * values in the default array
+     *
+     * @param array $params   (reference ) an assoc array of name/value pairs
+     * @param array $defaults (reference ) an assoc array to hold the flattened values
+     *
+     * @return object CRM_Core_BAO_Meeting object
+     * @access public
+     * @static
+     */
+    static function retrieve( &$params, &$defaults ) {
+        $meeting =& new CRM_Core_DAO_ActivityMeeting( );
+        $meeting->copyValues( $params );
+        if ( $meeting->find( true ) ) {
+            CRM_Core_DAO::storeValues( $meeting, $defaults );
+            return $meeting;
+        }
+        return null;
+    }
+
+}
+
+?>
