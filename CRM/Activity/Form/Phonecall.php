d41bb86546dfd06d120333eaecfc6ab8519fdcd2
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-06-24 10:28:45 +0000

    ts() tagging
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.0@1731 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Form/Phonecall.php
+++ CRM/Activity/Form/Phonecall.php
@@ -57,7 +57,7 @@ class CRM_Activity_Form_Phonecall extends CRM_Activity_Form
     public function buildQuickForm( ) 
     {
         $this->applyFilter('__ALL__', 'trim');
-        $contactPhone[''] = 'Select Phone Number';
+        $contactPhone[''] = ts('Select Phone Number');
         if ( is_array(CRM_Contact_BAO_Phone::getphoneNumber($this->_contactId))) {
             $contactPhone = CRM_Contact_BAO_Phone::getphoneNumber($this->_contactId);
         }
