4ddca36f3c93e9a6bde9ddba4ac198f02b7b6100
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-07-16 10:02:58 +0000

    //added missing file // CRM-176
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@2098 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Contact/Form/Test.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ +----------------------------------------------------------------------+
+ | CiviCRM version 1.0                                                  |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                    |
+ +----------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                      |
+ |                                                                      |
+ | CiviCRM is free software; you can redistribute it and/or modify it   |
+ | under the terms of the Affero General Public License Version 1,      |
+ | March 2002.                                                          |
+ |                                                                      |
+ | CiviCRM is distributed in the hope that it will be useful, but       |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 |
+ | See the Affero General Public License for more details at            |
+ | http://www.affero.org/oagpl.html                                     |
+ |                                                                      |
+ | A copy of the Affero General Public License has been been            |
+ | distributed along with this program (affero_gpl.txt)                 |
+ +----------------------------------------------------------------------+
+*/
+
+/**
+ *
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo 01/15/2005
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Form.php';
+
+
+/**
+ * This class generates form components generic to all the contact types.
+ * 
+ * It delegates the work to lower level subclasses and integrates the changes
+ * back in. It also uses a lot of functionality with the CRM API's, so any change
+ * made here could potentially affect the API etc. Be careful, be aware, use unit tests.
+ *
+ */
+class CRM_Contact_Form_Test extends CRM_Core_Form
+{
+
+    function preProcess( ) 
+    {
+
+    }
+
+    /**
+     * This function sets the default values for the form. Note that in edit/view mode
+     * the default values are retrieved from the database
+     * 
+     * @access public
+     * @return None
+     */
+    function setDefaultValues( ) 
+    {
+        $defaults = array( );
+        $params   = array( );
+    }
+
+    /**
+     * Function to actually build the form
+     *
+     * @return None
+     * @access public
+     */
+    public function buildQuickForm( ) 
+    {
+        
+
+    }
+
+       
+    /**
+     * Form submission of new/edit contact is processed.
+     *
+     * @access public
+     * @return None
+     */
+    public function postProcess() 
+    {
+        
+    }
+}
+
+?>
