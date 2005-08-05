4bb640a8ab029c0de46cd554bfaccf4069429fda
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-08-05 07:27:20 +0000

    - licensing change part 3 -  	test/, modules/, api/, js/ folders modified
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@2417 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test/RSTest/Run.php
+++ test/RSTest/Run.php
@@ -1,4 +1,39 @@
 <?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright (c) 2005 Social Source Foundation                        |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                       |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ * 
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo 01/15/2005
+ * $Id$
+ *
+ */
 require_once 'Common.php';
 require_once 'GenDataset.php';
 require_once 'InsertContact.php';
