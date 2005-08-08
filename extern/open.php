b6b156a5afd87b4694cd3cfec35d23e599cfff5c
brian <brian@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-08-08 22:40:19 +0000

    Fixed the open tracker, temporarily.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@2470 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- extern/open.php
+++ extern/open.php
@@ -8,19 +8,17 @@ require_once 'CRM/Core/Error.php';
 $config =& CRM_Core_Config::singleton();
 
 
-CRM_Core_Error::debug('c', $config);
-
 $queue_id = $_GET['q'];
 CRM_Mailing_Event_BAO_Opened::open($queue_id);
 
-$filename = "{$config->resourceBase}/i/tracker.gif";
+$filename = "../i/tracker.gif";
 
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Content-Description: File Transfer');
 header('Content-type: image/gif');
 header('Content-Length: ' . filesize($filename));
 
-header('Content-Disposition: attachment; filename=tracker.gif');
+header('Content-Disposition: inline; filename=tracker.gif');
 
 readfile($filename);
 
