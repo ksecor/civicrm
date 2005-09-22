6f8dd37f2b60d3cde1e6a894f375afb83cefcaad
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-09-22 23:05:31 +0000

    fix crm-421
    git-svn-id: http://svn.civicrm.org/civicrm/trunk/packages@3122 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- SimpleTest/simple_test.php
+++ SimpleTest/simple_test.php
@@ -350,7 +350,8 @@
                 $this->addTestCase(new BadGroupTest($test_file, 'No new test cases'));
                 return;
             }
-            $this->addTestCase($this->_createGroupFromClasses($test_file, $classes));
+            $testCase =& $this->_createGroupFromClasses($test_file, $classes);
+            $this->addTestCase($testCase);
         }
 
         /**
