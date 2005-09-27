eae58e3faeb69fadfd809507f97623aa10e51b3f
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-09-27 13:55:29 +0000

    displaying the profile's dates according to the config
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@3157 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -536,7 +536,7 @@ class CRM_Core_BAO_UFGroup extends CRM_Core_DAO_UFGroup {
                     break;
 
                 case "Select Date":
-                    $values[$index] = $cv->getValue( true );
+                    $values[$index] = CRM_Utils_Date::customFormat($cv->getValue());
                     break;
 
                 default:
