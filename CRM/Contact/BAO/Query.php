ada16cc9583d4f499154a40bff9c7f9943f286f5
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-10-06 12:50:26 +0000

    // changed email checking condition from LIKE to = , since generated data is not unique.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@3300 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -384,8 +384,10 @@ class CRM_Contact_BAO_Query {
             } else if ( $name === 'email') {
                 // during dupe checking, for email $value is an array, 
                 // for email we are adding only primary email to where clause
-                $this->_where[] = 'LOWER(' . $field['where'] . ') LIKE "%' . strtolower( str_replace( "\"", "",$value[0])  ) . '%"';  
-                $this->_qill[]  = ts( '%1 like "%2"', array( 1 => $field['title'], 2 => $value[0] ) );
+                // like is commented becasue generated data is not unqiue
+                //$this->_where[] = 'LOWER(' . $field['where'] . ') LIKE "%' . strtolower( str_replace( "\"", "",$value[0])  ) . '%"';  
+                $this->_where[] = 'LOWER(' . $field['where'] . ') = "' . strtolower( str_replace( "\"", "",$value[0])  ) . '"';  
+                $this->_qill[]  = ts( '%1 = "%2"', array( 1 => $field['title'], 2 => $value[0] ) );
             } else {
                 $this->_where[] = 'LOWER(' . $field['where'] . ') LIKE "%' . strtolower( addslashes( $value ) ) . '%"';  
                 $this->_qill[]  = ts( '%1 like "%2"', array( 1 => $field['title'], 2 => $value ) );
