acc2066e164a9fd5ffa5e0448bede764bcdeb267
anil <anil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-10-15 14:07:25 +0000

    worked on Relationship api
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@3428 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- api/Relationship.php
+++ api/Relationship.php
@@ -51,25 +51,72 @@ require_once 'CRM/Utils/Array.php';
  *
  */
 function crm_create_relationship($contact =null, $target_contact= null, $relationship_type_name, $params) {
+    $relationTypeID = null;
+    if( ! isset( $contact->id ) and ! isset( $target_contact->id )) {
+        return _crm_error('source or  target contact object does not have contact ID');
+    }
+
+    $sourceContact          = $contact->id;
+    $targetContact          = $target_contact->id;
+    require_once 'CRM/Contact/DAO/RelationshipType.php';
+    $reletionType = & new CRM_Contact_DAO_RelationshipType();
+    $reletionType->name_a_b = $relationship_type_name;
+    $reletionType->find();
+    if($reletionType->fetch()) {
+        
+        $relationTypeID = $reletionType->id;
+        $relationTypeID .='_a_b';
+    }
+   
+    if (!$relationTypeID) {
+        $reletionType = & new CRM_Contact_DAO_RelationshipType();
+        $reletionType->name_b_a = $relationship_type_name;
+        $reletionType->find();
+        if($reletionType->fetch()) {
+            
+            $relationTypeID = $reletionType->id;
+            $relationTypeID .='_b_a';
+        }
+    }
     
-    //$sourceContact          = CRM_Utils_Array::value('id', $contact);
-    //$targetContact          = CRM_Utils_Array::value('id', $target_contact);
-    $sourceContact          = 101;
-    $targetContact          = 102;
-    
-    $params['relationship_type_id' ] = $relationship_type_name;
+    if (!$relationTypeID) {
+        return _crm_error('$relationship_type_name is not valid relationship type ');
+    }
+    $params['relationship_type_id' ] = $relationTypeID;
     $ids   ['contact'      ] = $sourceContact;
     $params['contact_check'] = array ( $targetContact => $targetContact) ;
     require_once 'CRM/Contact/BAO/Relationship.php';
-    CRM_Contact_BAO_Relationship::create($params, $ids);
-
+    return CRM_Contact_BAO_Relationship::create($params, $ids);
+    
 }
 
 /**
  * Function to get the relationship
  *
  */
-function crm_get_relationships(&$contact, $relationship_type_name = null, $returnProperties = null, $sort = null, $offset = 0, $row_count = 25 ) {
+function crm_get_relationships(&$contact_a,&$contact_b=null, $relationship_type_name = null, $returnProperties = null, $sort = null, $offset = 0, $row_count = 25 ) {
+   
+    if( ! isset( $contact_a->id ) ) {
+        return _crm_error('$contact_a is not valid contact datatype');
+    }
+   
+    require_once 'CRM/Contact/BAO/Relationship.php';
+    $contactID = $contact_a->id;
+    $relationships = CRM_Contact_BAO_Relationship::getRelationship($contactID);
+    $relationships_b =array();
+    
+    $cid = $contact_b->id;
+    if(isset( $contact_b->id )) {
+        foreach($relationships as $key => $relationship) {
+            if ($relationship['cid'] == $cid ) {
+                $relationships_b[$key] = $relationship;
+            }
+        }
+         return $relationships_b;
+    }
+    
+    return $relationships;
+    
 }
 
 /**
@@ -77,6 +124,47 @@ function crm_get_relationships(&$contact, $relationship_type_name = null, $retur
  *
  */
 function crm_delete_relationship(&$contact, &$target_contact, $relationship_type_name) {
+   
+    $relationTypeID = null;
+    if( ! isset( $contact->id ) and ! isset( $target_contact->id )) {
+        return _crm_error('source or  target contact object does not have contact ID');
+    }
+    
+    $sourceContact          = $contact->id;
+    $targetContact          = $target_contact->id;
+    require_once 'CRM/Contact/DAO/RelationshipType.php';
+    $reletionType = & new CRM_Contact_DAO_RelationshipType();
+    $reletionType->name_a_b = $relationship_type_name;
+    $reletionType->find();
+    if($reletionType->fetch()) {
+        $relationTypeID = $reletionType->id;
+    }
+    
+    if (!$relationTypeID) {
+        $reletionType = & new CRM_Contact_DAO_RelationshipType();
+        $reletionType->name_b_a = $relationship_type_name;
+        $reletionType->find();
+        if($reletionType->fetch()) {
+            $relationTypeID = $reletionType->id;
+            
+        }
+    }
+   
+    if (!$relationTypeID) {
+        return _crm_error('$relationship_type_name is not valid relationship type ');
+    }
+    require_once 'CRM/Contact/DAO/Relationship.php';
+    $relationShip =  & new CRM_Contact_DAO_Relationship();
+    $relationShip->contact_id_a = $sourceContact;
+    $relationShip->contact_id_b = $targetContact;
+    $relationShip->relationship_type_id = $relationTypeID ;
+    $relationShip->find();
+    if($relationShip->fetch()) {
+        require_once 'CRM/Contact/BAO/Relationship.php';
+        CRM_Contact_BAO_Relationship::del($relationShip->id);   
+        return null;
+    }
+
 }
 
 /**
@@ -84,9 +172,34 @@ function crm_delete_relationship(&$contact, &$target_contact, $relationship_type
  *
  */
 function crm_create_relationship_type($params) {
+   
+    if(! isset($params['name_a_b']) and ! isset($params['name_b_a']) and ! isset($params['contact_type_a']) and ! isset($params['contact_type_b'] )) {
+        return _crm_error('Return array is not properly set');
+    }
+    require_once 'CRM/Contact/BAO/RelationshipType.php';
+    $relationType = CRM_Contact_BAO_RelationshipType::add( $params, $ids);
+   
+    return $relationType;
+    
 }
 
+/**
+ * Function to get all relationship type
+ *
+ */
 
+function crm_get_relationship_types() {
+    require_once 'CRM/Contact/DAO/RelationshipType.php';
+    $relationshipTypes = array();
+    $relationType = & new CRM_Contact_DAO_RelationshipType();
+    $relationType->find();
+    while($relationType->fetch())
+        {
+            $relationshipTypes[] = $relationType;
+        }
+    return $relationshipTypes;
+    
+}
 
 
 
--- test/CRM/api/Relationship.php
+++ test/CRM/api/Relationship.php
@@ -14,10 +14,65 @@ class TestOfCreateRelationshipAPI extends UnitTestCase
 
     function testCreateRelationship() 
     {
-        $params = array('start_date' => 20051013, 'end_date' => 20051014);
-        
-        $rel = crm_create_relationship('', '', '1_a_b', $params);
+        require_once 'CRM/Contact/DAO/Contact.php';
+        $params = array('start_date' => array('d'=>'10','M'=>'1','Y'=>'2005'),'end_date' => array('d'=>'10','M'=>'1','Y'=>'2006'));
+        $conatct1 = & new CRM_Contact_DAO_Contact();
+        $conatct1->id = 102;
+        $conatct2 = & new CRM_Contact_DAO_Contact();
+        $conatct2->id = 103;
+        $relationShip = 'Child of';
+        $rel = crm_create_relationship($conatct1, $conatct2, $relationShip, $params);
+       
     }
+
+    function testGetRelationship()
+    {
+        require_once 'CRM/Contact/DAO/Contact.php';
+        $conatct = & new CRM_Contact_DAO_Contact();
+        $conatct->id = 102;
+        $conatct2 = & new CRM_Contact_DAO_Contact();
+        $conatct2->id = 103;
+        $rel = crm_get_relationships($conatct,$conatct2);
+        //print_r($rel);
+       
+    }
+    
+    function testDeleteRelationship() 
+    {
+        
+        require_once 'CRM/Contact/DAO/Contact.php';
+        $params = array('start_date' => array('d'=>'10','M'=>'1','Y'=>'2005'),'end_date' => array('d'=>'10','M'=>'1','Y'=>'2006'));
+        $conatct1 = & new CRM_Contact_DAO_Contact();
+        $conatct1->id = 102;
+        $conatct2 = & new CRM_Contact_DAO_Contact();
+        $conatct2->id = 103;
+        $relationShip = 'Child of';
+        $rel = crm_delete_relationship($conatct1,$conatct12,$relationShip);
+        
+    }
+    
+    function testCreateRelationType() 
+    {
+        
+        $params = array(
+                        'name_a_b'=>'Friend of',
+                        'name_b_a'=>'Friend of',
+                        'contact_type_a'=>'Individual',
+                        'contact_type_b'=>'Individual'
+                        );
+        $relType = crm_create_relationship_type($params);
+        
+   }
+        
+    function testGetRelationType()
+    {
+        $relationTypes =crm_get_relationship_types();
+        //print_r($relationTypes);
+    }
+    
+    
+
+    
 }
 
 ?>
