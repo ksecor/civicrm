ab067054a65c5d6c651cf2ed6b95e427e233248c
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-10-26 06:06:14 +0000

    //fixed the link
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@3563 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -79,7 +79,7 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
                                                                           'qs'    => 'action=update&id=%%id%%',
                                                                           'title' => ts('Edit CiviCRM Profile Group') 
                                                                           ),
-                                        CRM_Core_Action::UPDATE  => array(
+                                        CRM_Core_Action::PREVIEW => array(
                                                                           'name'  => ts('Preview'),
                                                                           'url'   => 'civicrm/admin/uf/group',
                                                                           'qs'    => 'action=preview&id=%%id%%',
