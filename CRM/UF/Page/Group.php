50252083b8cff1de327ce5d2d5a2282ed5377310
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-11-10 00:56:10 +0000

    fix crm-536
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.2@3697 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -155,7 +155,7 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
         $preview  =  trim( $template->fetch( 'CRM/form.tpl' ) ); 
         // not sure how to circumvent our own navigation system to generate the right form url
         $preview = str_replace( 'civicrm/admin/uf/group', 'civicrm/profile/create&amp;reset=1', $preview );
-        $this->assign( 'preview', $preview );
+        $this->assign( 'preview', htmlentities( $preview ) );
         $this->assign( 'action' , CRM_Core_Action::PREVIEW );
         $this->assign( 'isForm' , 0 );
     }
