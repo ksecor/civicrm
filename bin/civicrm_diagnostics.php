6932d75f6c451fe0a67f3915edd1d0ffd22eaae3
anil <anil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-11-30 15:44:33 +0000

    worked on Issue CRM-601
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@3936 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/civicrm_diagnostics.php
+++ bin/civicrm_diagnostics.php
@@ -1,27 +1,47 @@
 <?php
 
-$drupalPath = '/opt/apache2/htdocs/drupal/';
+function civicrm_diagnostic() {
+    require_once '../modules/config.inc.php';
+    require_once 'CRM/Core/Config.php';
+    
+    $config = & new CRM_Core_Config();
+    
+    $drupalPath = explode('/',$config->templateDir);
+    
+    $pathArray = array();
+    foreach($drupalPath as $path ) {
+        if ($path == 'modules') {
+            break;
+        }
+        $pathArray[] = $path;
+    }
+    
+    $drupalPath = implode( '/',$pathArray );
+    $PHPVersion = phpversion();
+    
+    $mysqlPath  = $config->mysqlPath;
+    $mysqlVersion = exec($mysqlPath.'mysql --version');
+    
+    $versionFile = "../xml/version.xml";
+    $dom = DomDocument::load( $versionFile );
+    $dom->xinclude( );
+    $versionXML = simplexml_import_dom( $dom );
+    
+    $civicrmVersion = $versionXML->version_no;
 
+    
+    $f = fopen($drupalPath.'/CHANGELOG.txt', 'r'); 
+    list(, $version) = explode(' ', fgets($f, 13)); fclose($f); 
+    $drupalVersion = $version;
+    
+    //echo "PHP Version      : ".$PHPVersion."\n";
+    //echo "MySQL Version    : ".$mysqlVersion."\n";
+    //echo "CiviCRM Version  : ".$civicrmVersion."\n";
+    //echo "Drupal Version   : ".$drupalVersion."\n";
+    
+    return $returnString = "PHP Version:".$PHPVersion."<br>"."MySQL Version:".$mysqlVersion."<br>"."CiviCRM Version:".$civicrmVersion."<br>"."Drupal Version:".$drupalVersion."<br>";
+}
 
-$PHPVersion = phpversion();
-$mysqlVersion = exec('mysql --version');
-
-$versionFile = "../xml/version.xml";
-$dom = DomDocument::load( $versionFile );
-$dom->xinclude( );
-$versionXML = simplexml_import_dom( $dom );
-
-$civicrmVersion = $versionXML->version_no;
-
-
-$f = fopen($drupalPath.'CHANGELOG.txt', 'r'); 
-list(, $version) = explode(' ', fgets($f, 13)); fclose($f); 
-$drupalVersion = $version;
-
-echo "PHP Version      : ".$PHPVersion."\n";
-echo "MySQL Version    : ".$mysqlVersion."\n";
-echo "CiviCRM Version  : ".$civicrmVersion."\n";
-echo "Drupal Version   : ".$drupalVersion."\n";
-
+echo  civicrm_diagnostic();
 
 ?>
