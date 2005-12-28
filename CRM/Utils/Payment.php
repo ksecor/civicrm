0a8bf8295bc3fc8b48f44629b04cf1e2b78a672d
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2005-12-28 05:58:48 +0000

    -changed the eval function at line no 72
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@4284 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/Payment.php
+++ CRM/Utils/Payment.php
@@ -69,7 +69,8 @@ abstract class CRM_Utils_Payment {
             
             $classPath = str_replace( '_', '/', $config->paymentClass ) . '.php';
             require_once($classPath);
-            self::$_singleton =& eval( 'return ' . $config->paymentClass . '::singleton( $mode );' );
+            //self::$_singleton =& eval( 'return ' . $config->paymentClass . '::singleton( $mode );' );
+            self::$_singleton = eval( 'return & ' . $config->paymentClass . '::singleton( $mode );' );
         }
         return self::$_singleton;
     }
