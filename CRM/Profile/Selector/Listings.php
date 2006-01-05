8b04d1cdd4201f033df576b6147ef35700fbc8b0
anil <anil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-01-05 09:44:26 +0000

    worked on Issue CRM-663
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.3@4342 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Profile/Selector/Listings.php
+++ CRM/Profile/Selector/Listings.php
@@ -141,7 +141,6 @@ class CRM_Profile_Selector_Listings extends CRM_Core_Selector_Base implements CR
 
         $returnProperties =& CRM_Contact_BAO_Contact::makeHierReturnProperties( $this->_fields );
         $returnProperties['contact_type'] = 1;
-
         $this->_query   =& new CRM_Contact_BAO_Query( $this->_params, $returnProperties, $this->_fields );
         $this->_options =& $this->_query->_options;
         // CRM_Core_Error::debug( 'q', $this->_query );
@@ -264,21 +263,26 @@ class CRM_Profile_Selector_Listings extends CRM_Core_Selector_Base implements CR
         
         //$sort object processing for location fields
         if( $sort ) {
-            foreach ($sort->_vars as $key => $field) {
-                $fieldArray = explode('-' , $field['name']);
-                if( is_numeric($fieldArray[1]) ) {
-                    $locationType = & new CRM_Core_DAO_LocationType();
-                    $locationType->id = $fieldArray[1];
-                    $locationType->find(true);
-                    if($fieldArray[0]!='email') {
-                        $field['name'] = "`".$locationType->name."-".$fieldArray[0]."`";
-                    } else if ($fieldArray[0] == 'email') {
-                        $field['name'] = "`".$locationType->name."-".$fieldArray[0]."-1`";
+            $vars = $sort->_vars;
+            $varArray = array();
+            foreach ($vars as $key => $field) {
+                    $field = $vars[$key];
+                    $fieldArray = explode('-' , $field['name']);
+                    if( is_numeric($fieldArray[1]) ) {
+                        $locationType = & new CRM_Core_DAO_LocationType();
+                        $locationType->id = $fieldArray[1];
+                        $locationType->find(true);
+                        if ($fieldArray[0] == 'email' || $fieldArray[0] == 'im' || $fieldArray[0] == 'phone') {
+                            $field['name'] = "`".$locationType->name."-".$fieldArray[0]."-1`";
+                        } else {
+                            $field['name'] = "`".$locationType->name."-".$fieldArray[0]."`";
+                        }
                     }
-                }
+                    $varArray[$key] = $field;
             }
         }
-     
+       
+        $sort->_vars = $varArray;
         $result = $this->_query->searchQuery( $offset, $rowCount, $sort );
 
         // process the result of the query
