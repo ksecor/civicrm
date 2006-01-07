d0f9d03f23389b10cc68f262c8aad10bec0168cd
swati <swati@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-01-07 14:37:55 +0000

    clean up the code and fixed the bug in updating profile field
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.3@4359 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/UF/Form/Field.php
+++ CRM/UF/Form/Field.php
@@ -215,19 +215,18 @@ class CRM_UF_Form_Field extends CRM_Core_Form {
         
         $js = "<script type='text/javascript'>\n";
         $formName = 'document.forms.' . $this->_name;
-        
+                
         //$sel =& $this->addElement('hierselect', "field_name", ts('CiviCRM Field Name'), 'onclick="getHelpText(this,event, false);"  onblur="getHelpText(this,event, false);" autocomplete="off"', '&nbsp;&nbsp;'); //this commented to fix javascript error
         
         $sel =& $this->addElement('hierselect', "field_name", ts('CiviCRM Field Name'));
-
+        
         for ( $k = 1; $k < 3; $k++ ) {
             if (!$defaults['field_name'][$k]) {
                 $js .= "{$formName}['field_name[$k]'].style.display = 'none';\n"; 
             }
         }
-        
         $sel->setOptions(array($sel1, $sel2, $sel3));
-
+       
         $js .= "</script>\n";
         $this->assign('initHideBoxes', $js);
 
@@ -283,7 +282,7 @@ class CRM_UF_Form_Field extends CRM_Core_Form {
      */
     public function postProcess()
     {
-        if($this->_action & CRM_Core_Action::DELETE) {
+        if ($this->_action & CRM_Core_Action::DELETE) {
             CRM_Core_BAO_UFField::del($this->_id);
             CRM_Core_Session::setStatus(ts('Selected Profile Field has been deleted.'));
             return;
@@ -292,105 +291,23 @@ class CRM_UF_Form_Field extends CRM_Core_Form {
         // store the submitted values in an array
         $params = $this->controller->exportValues('Field');
         $ids = array( );
-        $ids['uf_field'] = $this->_id;
+        
+        if ($this->_action & CRM_Core_Action::UPDATE ) {
+            $ids['uf_field'] = $this->_id;
+        }
+        
         $ids['uf_group'] = $this->_gid;
         
         //check for duplicate fields
         if (CRM_Core_BAO_UFField::duplicateField($params, $ids) ) {
             CRM_Core_Session::setStatus(ts('The selected field was not added. It already exists in this profile.'));
             return;
-        }
-        
-        // set values for uf field properties and save
-        $ufField                   =& new CRM_Core_DAO_UFField();
-        $ufField->field_name       = $params['field_name'][0];
-        $ufField->location_type_id = $params['field_name'][1];
-        $ufField->phone_type       = $params['field_name'][2];
-        
-
-        $ufField->listings_title = $params['listings_title'];
-        $ufField->visibility     = $params['visibility'];
-        $ufField->help_post      = $params['help_post'];
-
-        $ufField->is_required     = CRM_Utils_Array::value( 'is_required'    , $params, false );
-        $ufField->is_active       = CRM_Utils_Array::value( 'is_active'      , $params, false );
-        $ufField->in_selector     = CRM_Utils_Array::value( 'in_selector'    , $params, false );
-        $ufField->is_view         = CRM_Utils_Array::value( 'is_view'        , $params, false );
-        $ufField->is_registration = CRM_Utils_Array::value( 'is_registration', $params, false );
-        $ufField->is_match        = CRM_Utils_Array::value( 'is_match'       , $params, false );
-
-        if ($this->_action & CRM_Core_Action::UPDATE) {
-            $ufField->id = $this->_id;
-        }
-
-
-        // fix for CRM-316
-        if ($this->_action & CRM_Core_Action::UPDATE) {
-
-            $uf =& new CRM_Core_DAO_UFField();
-            $uf->id = $this->_id;
-            $uf->find();
-
-            
-            if ( $uf->fetch() && $uf->weight != CRM_Utils_Array::value( 'weight', $params, false ) ) {
-                    
-                $searchWeight =& new CRM_Core_DAO_UFField();
-                $searchWeight->uf_group_id = $this->_gid;
-                $searchWeight->weight = CRM_Utils_Array::value( 'weight', $params, false );
-                
-                if ( $searchWeight->find() ) {                   
-                    
-                    $tempDAO =& new CRM_Core_DAO();
-                    $query = "SELECT id FROM civicrm_uf_field WHERE weight >= ". $searchWeight->weight ." AND uf_group_id = ".$this->_gid;
-                    $tempDAO->query($query);
-
-                    $fieldIds = array();
-                    while($tempDAO->fetch()) {
-                        $fieldIds[] = $tempDAO->id; 
-                    }
-                    
-                    if ( !empty($fieldIds) ) {
-                        $ufDAO =& new CRM_Core_DAO();
-                        $updateSql = "UPDATE civicrm_uf_field SET weight = weight + 1 WHERE id IN ( ".implode(",", $fieldIds)." ) ";
-                        $ufDAO->query($updateSql);                    
-                    }
-                }
-            }                
-             
-            $ufField->weight = CRM_Utils_Array::value( 'weight', $params, false );
-            
         } else {
-            $uf =& new CRM_Core_DAO_UFField();
-            $uf->uf_group_id = $this->_gid;
-            $uf->weight = CRM_Utils_Array::value( 'weight', $params, false );
-            
-            if ( $uf->find() ) {
-                $tempDAO =& new CRM_Core_DAO();
-                $query = "SELECT id FROM civicrm_uf_field WHERE weight >= ". CRM_Utils_Array::value( 'weight', $params, false ) ." AND uf_group_id = ".$this->_gid;
-                $tempDAO->query($query);
-
-                $fieldIds = array();                
-                while($tempDAO->fetch()) {
-                    $fieldIds[] = $tempDAO->id;                
-                }                
-
-                if ( !empty($fieldIds) ) {
-                    $ufDAO =& new CRM_Core_DAO();
-                    $updateSql = "UPDATE civicrm_uf_field SET weight = weight + 1 WHERE id IN ( ".implode(",", $fieldIds)." ) ";
-                    $ufDAO->query($updateSql);
-                }
-            }
-
-            $ufField->weight = CRM_Utils_Array::value( 'weight', $params, false );
+            $ufField = CRM_Core_BAO_UFField::add($params,$ids);
+            $name = $this->_selectFields[$ufField->field_name];
+            CRM_Core_Session::setStatus(ts('Your civicrm profile field "%1" has been saved.', array(1 => $name)));
         }
-
-
-        // need the FKEY - uf group id
-        $ufField->uf_group_id = $this->_gid;
-        $ufField->save();
-        
-        $name = $this->_selectFields[$ufField->field_name];
-        CRM_Core_Session::setStatus(ts('Your civicrm profile field "%1" has been saved.', array(1 => $name)));
+                
     }
 
     /**
@@ -420,7 +337,7 @@ class CRM_UF_Form_Field extends CRM_Core_Form {
             $errors['field_name'] = 'Please select a field name';
         }
         
-        /*
+        /*   
         if (CRM_Core_Action::ADD && empty($fields['field_id'])) {
             $groupId = $fields['group_id'];
             $query = "SELECT count(*) FROM civicrm_uf_field WHERE uf_group_id = " . CRM_Utils_Type::escape($groupId, 'Integer')  . " AND field_name = '" . CRM_Utils_Type::escape($fieldName, 'String') . "'";
