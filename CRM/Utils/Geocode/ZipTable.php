0c0e11893a9b018e7925bb77d3a23db9a6e5e5aa
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-02-14 06:18:13 +0000

    Merging 4494:4527 from v1.3 branch
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@4528 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/Geocode/ZipTable.php
+++ CRM/Utils/Geocode/ZipTable.php
@@ -84,4 +84,4 @@ class CRM_Utils_Geocode_ZipTable {
     }
 }
 
-?>
+
---
+++ test/OneTest.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once '../civicrm.settings.php';
+
+require_once 'SimpleTest/unit_tester.php';
+require_once 'SimpleTest/reporter.php';
+
+if ( !defined( 'TEST' ) ) {
+    define( 'TEST', __FILE__ );
+}
+
+class OneTest extends GroupTest {
+    
+    function OneTest( ) {
+        $this->GroupTest( 'Unit Tests for CRM' );
+
+        $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateDrupalContact.php' );
+    }
+
+}
+
+function user_access( $str ) {
+    return true;
+}
+
+function module_list( ) {
+    return array( );
+}
+
+if ( TEST == __FILE__ ) {
+
+    require_once 'CRM/Core/Config.php';
+    $test =& new OneTest( );
+
+    $config =& CRM_Core_Config::singleton();
+
+    if (SimpleReporter::inCli()) {
+        exit($test->run(new TextReporter()) ? 0 : 1);
+    }
+    $test->run(new HtmlReporter());
+}
+
+?>
