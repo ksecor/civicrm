082356a2f25111fabd3c197a48f6410784c41d5c
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-03-14 04:57:35 +0000

    Onward and Upward with Quest
    git-svn-id: http://svn.civicrm.org/civicrm/branches/quest@4769 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/StateMachine/PreApp.php
+++ CRM/Quest/StateMachine/PreApp.php
@@ -54,7 +54,18 @@ class CRM_Quest_StateMachine_PreApp extends CRM_Core_StateMachine {
         parent::__construct( $controller, $action );
         
         $this->_pages = array(
-                              'CRM_Quest_Form_App_Personal'
+                              'CRM_Quest_Form_App_Personal',
+                              'CRM_Quest_Form_App_Scholarship',
+                              'CRM_Quest_Form_App_Educational',
+                              'CRM_Quest_Form_App_Household',
+                              'CRM_Quest_Form_App_Guardian',
+                              'CRM_Quest_Form_App_Sibling',
+                              'CRM_Quest_Form_App_Income',
+                              'CRM_Quest_Form_App_HighSchool',
+                              'CRM_Quest_Form_App_SchoolOther',
+                              'CRM_Quest_Form_App_Academic',
+                              'CRM_Quest_Form_App_Testing',
+                              'CRM_Quest_Form_App_Essay',
                               );
         
         $this->addSequentialPages( $this->_pages, $action );
