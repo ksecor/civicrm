a57acf49f93ccc2b45369704f86406aa59e2e53d
anil <anil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-03-14 09:07:10 +0000

    fixed some bugs in addselect method
    git-svn-id: http://svn.civicrm.org/civicrm/branches/quest@4773 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/App.php
+++ CRM/Quest/Form/App.php
@@ -106,8 +106,13 @@ class CRM_Quest_Form_App extends CRM_Core_Form
     }
 
     function addSelect( $id, $label, $prefix = null ) {
-        $this->addElement('select', $id . '_id' . $prefix, $label,
-                          array('' => ts('- Select -')) + CRM_Core_OptionGroup::values($id) );
+        if ( $prefix ) {
+            $this->addElement('select', $id . $prefix, $label,
+                              array('' => ts('- Select -')) + CRM_Core_OptionGroup::values($id) );
+        } else {
+            $this->addElement('select', $id , $label,
+                              array('' => ts('- Select -')) + CRM_Core_OptionGroup::values($id) );
+        }
     }
 
     function addCountry( $id, $title ) {
