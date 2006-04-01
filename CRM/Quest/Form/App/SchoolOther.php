4fdfa6c9c992752d0e5b85bc804b899fb0959b04
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-04-01 13:09:36 +0000

    worked on quest appication form
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@5019 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/App/SchoolOther.php
+++ CRM/Quest/Form/App/SchoolOther.php
@@ -45,8 +45,8 @@ require_once 'CRM/Core/OptionGroup.php';
  */
 class CRM_Quest_Form_App_SchoolOther extends CRM_Quest_Form_App
 {
-    static $_orgIDOther;
-    static $_relIDOther;
+    static $_orgIDsOther;
+    static $_relIDsOther;
     
     
     /**
@@ -65,21 +65,32 @@ class CRM_Quest_Form_App_SchoolOther extends CRM_Quest_Form_App
         $relID  = $dao->id ;
 
         $contactID = $this->get( 'contact_id' );
-        //to get relationship id
-        require_once 'CRM/Contact/DAO/Relationship.php';
-        $relDAO = & new CRM_Contact_DAO_Relationship();
-        $relDAO->contact_id_a            = $contactID;
-        $relDAO->relationship_type_id    = $relID;//need to fix  	  
-        $relDAO->find(); 
-        while ( $relDAO->fetch()) {
-           $orgID = $this->get('orgID');
-           if ( $relDAO->contact_id_b != $orgID  ) {
-               $this->_orgIDOther = $relDAO->contact_id_b;
-               $this->_relIDOther = $relDAO->id;
-               $this->set('orgIDOther' , $relDAO->contact_id_b);
-               $this->set('relIDOther' , $relDAO->id);
-           }
+        
+        // to get  OrganizationId and Relationship ID's
+        
+        require_once 'CRM/Core/DAO/CustomValue.php';
+        $customDAO = & new CRM_Core_DAO_CustomValue();
+        $customDAO->char_data    = 'Other School';
+        $customDAO->find();
+        while ( $customDAO->fetch() ) {
+            $count = count( $this->_orgIDsOther)+1;
+            $this->_orgIDsOther[$count] = $customDAO->entity_id;
         }
+        //get relationshipID
+        
+        require_once 'CRM/Contact/DAO/Relationship.php';
+        if (is_array($this->_orgIDsOther)) {
+            foreach ( $this->_orgIDsOther as $key => $value ) {
+                $dao = & new CRM_Contact_DAO_Relationship();
+                $dao->contact_id_b =$value;
+                $dao->find(true);
+                $this->_relIDsOther[$key] = $dao->id;
+            }
+        }
+        
+        $this->set('relIDs' , $this->_relIDsOther);
+        $this->set('orgIDs' , $this->_orgIDsOther);
+        
         
         
     }
@@ -95,33 +106,53 @@ class CRM_Quest_Form_App_SchoolOther extends CRM_Quest_Form_App
     function setDefaultValues( ) 
     {
         $defaults = array( );
-        if ( $this->_orgIDOther ) {
-            $ids = array();
-            $params  = array('contact_id' => $this->_orgIDOther ,'contact_type' => 'Organization'); 
-            require_once 'CRM/Contact/BAO/Contact.php';
-            $contact =& CRM_Contact_BAO_Contact::retrieve( &$params, &$defaults, &$ids );
-            
-            //set custom data defaults
-            require_once 'CRM/Core/BAO/CustomGroup.php';
-            $this->_groupTree =& CRM_Core_BAO_CustomGroup::getTree('Organization',$this->_orgIDOther, 0);
-            $viewMode = false;
-            $inactiveNeeded = false;
-            if( isset($this->_groupTree) ) {
-                CRM_Core_BAO_CustomGroup::setDefaults( $this->_groupTree, $defaults, $viewMode, $inactiveNeeded );
+        if (is_array($this->_orgIDsOther) ) {
+            foreach ($this->_orgIDsOther as $key => $value ) {
+                if ( $value  ) {
+                    $ids = array();
+                    $params  = array('contact_id' => $value ,'contact_type' => 'Organization'); 
+                    require_once 'CRM/Contact/BAO/Contact.php';
+                    $contact =& CRM_Contact_BAO_Contact::retrieve( &$params, &$orgDefaults, &$ids );
+                    
+                    //set custom data defaults
+                    require_once 'CRM/Core/BAO/CustomGroup.php';
+                    $this->_groupTree =& CRM_Core_BAO_CustomGroup::getTree('Organization',$value , 0);
+                    $viewMode = false;
+                    $inactiveNeeded = false;
+                    if( isset($this->_groupTree) ) {
+                        CRM_Core_BAO_CustomGroup::setDefaults( $this->_groupTree,$orgDefaults, $viewMode, $inactiveNeeded );
+                    }
+                    
+                    // set relationship defaults
+                    require_once 'CRM/Utils/Date.php';
+                    require_once 'CRM/Contact/DAO/Relationship.php';
+                    $relDAO = & new CRM_Contact_DAO_Relationship();
+                    $relDAO->id = $this->_relIDs[$key]; 
+                    if ( $relDAO->find(true) ) {
+                        $orgDefaults['date_of_entry'] =  CRM_Utils_Date::unformat( $relDAO->start_date , '-' );
+                        $orgDefaults['date_of_exit'] =  CRM_Utils_Date::unformat( $relDAO->end_date , '-' );
+
+                    }
+                    
+                }
+                foreach ($orgDefaults as $k => $value ) {
+                    $defaults[$k."_".$key] = $value;
+                }
             }
-            
-            // set relationship defaults
-            require_once 'CRM/Utils/Date.php';
-            require_once 'CRM/Contact/DAO/Relationship.php';
-            $relDAO = & new CRM_Contact_DAO_Relationship();
-            $relDAO->id = $this->_relIDOther; 
-            if ( $relDAO->find(true) ) {
-                $defaults['date_of_entry'] = CRM_Utils_Date::unformat( $relDAO->start_date,'-' );
-                $defaults['date_of_exit']  = CRM_Utils_Date::unformat( $relDAO->start_date,'-' );
-            }
-            
+
         }
         
+        // Assign show and hide blocks lists to the template for optional test blocks (SATII and AP)
+        $this->_showHide =& new CRM_Core_ShowHideBlocks( );
+        for ( $i = 2; $i <= 5; $i++ ) {
+            if ( CRM_Utils_Array::value( "otherSchool_org_name_$i", $defaults )) {
+                $this->_showHide->addShow( "otherSchool_info_$i" );
+            } else {
+                $this->_showHide->addHide( "otherSchool_info_$i" );
+            }
+        }
+
+        $this->_showHide->addToTemplate( );
         return $defaults;
     }
     
@@ -136,26 +167,23 @@ class CRM_Quest_Form_App_SchoolOther extends CRM_Quest_Form_App
     {
         $attributes = CRM_Core_DAO::getAttribute('CRM_Contact_DAO_Organization' );
         
-        // name of school
-        $this->addElement('text', 'organization_name',
-                          ts( 'Name of Institution' ),
-                          $attributes['organization_name'] );
-        
-        $this->addElement('date', 'date_of_entry',
-                          ts( 'Dates Attended' ),
-                          CRM_Core_SelectValues::date( 'custom', 7, 0, "M\001Y" ) );
-        
-        $this->addElement('date', 'date_of_exit',
-                          ts( 'Dates Attended' ),
-                          CRM_Core_SelectValues::date( 'custom', 7, 0, "M\001Y" ) );
-        
-        $this->buildAddressBlock( 1,
-                                  ts( 'Location' ),null );
+        require_once 'CRM/Core/ShowHideBlocks.php';
 
-        $this->addElement('textarea',
-                          'note',
-                          ts( 'School Description' ),
-                          CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Note', 'note' ) );
+        $otherSchool_info = array( );
+        for ( $i = 1; $i <= 5; $i++ ) {
+            $this->addElement('text', 'organization_name_'.$i, ts( 'Name of Institution' ), $attributes['organization_name'] );
+            $this->addElement('date', 'date_of_entry_'.$i, ts( 'Dates Attended' ), 
+                              CRM_Core_SelectValues::date( 'custom', 7, 0, "M\001Y" ) );
+            $this->addElement('date', 'date_of_exit_'.$i, ts( 'Dates Attended' ), 
+                              CRM_Core_SelectValues::date( 'custom', 7, 0, "M\001Y" ) );
+            $this->buildAddressBlock( 1, ts( 'Location' ), null, null, null, null, null, "location_$i" );
+            $this->addElement('textarea', 'note_'.$i, ts( 'School Description' ), array('row'=> 5, 'cols' => 60) );
+            $otherSchool_info[$i] = CRM_Core_ShowHideBlocks::links( $this,"otherSchool_info_$i",
+                                                                    ts('Add another information'),
+                                                                    ts('Hide this information'),
+                                                                    false );
+        }
+        $this->assign( 'otherSchool_info', $otherSchool_info );
 
         parent::buildQuickForm( );
     }
@@ -169,60 +197,73 @@ class CRM_Quest_Form_App_SchoolOther extends CRM_Quest_Form_App
     public function postProcess() 
     {
         $params = $this->controller->exportValues( $this->_name );
-        $params['location'][1]['location_type_id'] = 1;
-        $params['location'][1]['is_primary'] = 1 ;
-
-        $contactID = $this->get('contact_id');
-        $params['contact_type'] = 'Organization';
-        
-        $ids = array();
-        $this->_orgIDOther = $this->get('orgIDOther');
-        
-        if ( $this->_orgIDOther ) {
-            $idParams = array( 'id' => $this->_orgIDOther, 'contact_id' => $this->_orgIDOther );
-            CRM_Contact_BAO_Contact::retrieve( $idParams, $defaults, $ids );
+        //print_r($params);
+        foreach( $params as $key => $value ) {
+            $keyArray = explode( '_', $key );
+            $orgnizationParams[$keyArray[count($keyArray)-1]][substr($key, 0, -2)] = $value ;
         }
+        // print_r($orgnizationParams);
         
-        $org = CRM_Contact_BAO_Contact::create($params, $ids, 2);
-        $this->set('orgIDOther' , $org->id );
+        foreach( $orgnizationParams as $key => $orgParams) {
+            if (! $orgParams['organization_name']) {
+                continue;
+            }
+            $orgParams['location'][1]['location_type_id'] = 1;
+            $orgParams['location'][1]['is_primary'] = 1;
+            
+            $contactID = $this->get('contact_id');
+            $orgParams['contact_type'] = 'Organization';
+            $orgParams['custom_4']     = 'Other School';
+            $ids = array();
+            $this->_orgIDsOther = $this->get('orgIDsOther');
+            
+            if ( $this->_orgIDsOther[$key] ) {
+                $idParams = array( 'id' => $this->_orgIDsOther[$key], 'contact_id' => $this->_orgIDsOther[$key] );
+                CRM_Contact_BAO_Contact::retrieve( $idParams, $defaults, $ids );
+            }
         
-        // add data for custom fields 
-        require_once 'CRM/Core/BAO/CustomGroup.php';
-        $this->_groupTree = & CRM_Core_BAO_CustomGroup::getTree('Organization',$org->id, 0 );
-        
-        CRM_Core_BAO_CustomGroup::postProcess( $this->_groupTree, $params );
-        
-
-        CRM_Core_BAO_CustomGroup::updateCustomData($this->_groupTree,'Organization',$org->id); 
-        
-        //create a realtionship
-        require_once 'CRM/Utils/Date.php';
-        $relationshipParams = array();
-        
-        require_once 'CRM/Contact/DAO/RelationshipType.php';
-        $dao = & new CRM_Contact_DAO_RelationshipType();
-        $dao->name_a_b = 'Student of';
-        $dao->find(true);
-        $relID  = $dao->id ;
-        
-        $relationshipParams['relationship_type_id'] = $relID.'_a_b';
-        $relationshipParams['start_date']           = $params['date_of_entry'];
-        $relationshipParams['end_date']            =  $params['date_of_exit'];
-        $relationshipParams['contact_check']        = array("$org->id" => 1 ); 
-        
-        $this->relIDOther = $this->get('relIDOther');
-        
-        if ( $this->relIDOther ) {
-            $ids = array('contact' =>$contactID,'relationship' => $this->relIDOther ,'contactTarget' =>$organizationID);
-        } else {
-            $ids = array('contact' =>$contactID);
+            $org = CRM_Contact_BAO_Contact::create($orgParams, $ids, 2);
+            $this->_orgIDsOther[$key] = $org->id; 
+            $this->set('orgIDsOther' , $this->_orgIDsOther );
+            
+            // add data for custom fields 
+            require_once 'CRM/Core/BAO/CustomGroup.php';
+            $this->_groupTree = & CRM_Core_BAO_CustomGroup::getTree('Organization',$org->id, 0 );
+            
+            CRM_Core_BAO_CustomGroup::postProcess( $this->_groupTree, $orgParams );
+            
+            CRM_Core_BAO_CustomGroup::updateCustomData($this->_groupTree,'Organization',$org->id); 
+            
+            //create a realtionship
+            require_once 'CRM/Utils/Date.php';
+            $relationshipParams = array();
+            
+            require_once 'CRM/Contact/DAO/RelationshipType.php';
+            $dao = & new CRM_Contact_DAO_RelationshipType();
+            $dao->name_a_b = 'Student of';
+            $dao->find(true);
+            $relID  = $dao->id ;
+            
+            $relationshipParams['relationship_type_id'] = $relID.'_a_b';
+            $relationshipParams['start_date']           = $orgParams['date_of_entry'];
+            $relationshipParams['end_date']            =  $orgParams['date_of_exit'];
+            $relationshipParams['contact_check']        = array("$org->id" => 1 ); 
+            
+            $this->relIDsOther = $this->get('relIDsOther');
+            
+            if ( $this->relIDsOther[$key] ) {
+                $ids = array('contact' =>$contactID,'relationship' => $this->relIDsOther[$key] ,'contactTarget' =>$organizationID);
+            } else {
+                $ids = array('contact' =>$contactID);
+            }
+            
+            $organizationID = $org->id;
+            
+            require_once 'CRM/Contact/BAO/Relationship.php';
+            $relationship= CRM_Contact_BAO_Relationship::add($relationshipParams,$ids,$organizationID);
+            $this->relIDsOther[$key] = $relationship->id;
+            $this->set('relIDsOther' , $this->relIDsOther );
         }
-        
-        $organizationID = $org->id;
-        
-        require_once 'CRM/Contact/BAO/Relationship.php';
-        $relationship= CRM_Contact_BAO_Relationship::add($relationshipParams,$ids,$organizationID);
-        $this->set('relIDOther' , $relationship->id );
     }//end of function
 
     /**
