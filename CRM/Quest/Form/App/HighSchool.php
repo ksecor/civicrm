0f3f2694ef9cba5ad26f7d4718deeae81a5a38b8
paras <paras@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-04-21 16:10:57 +0000

    add 07 and 08 in exit drop down
    git-svn-id: http://svn.civicrm.org/civicrm/branches/quest@5288 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/App/HighSchool.php
+++ CRM/Quest/Form/App/HighSchool.php
@@ -210,7 +210,7 @@ class CRM_Quest_Form_App_HighSchool extends CRM_Quest_Form_App
             $this->addRule('date_of_entry_'.$i, ts('Select a valid date.'), 'qfDate');
 
             $this->addElement('date', 'date_of_exit_'.$i, ts( 'Dates attended (month/year)' ),
-                              CRM_Core_SelectValues::date( 'custom', 7, 0, "M\001Y" ) );
+                              CRM_Core_SelectValues::date( 'custom', 7, 2, "M\001Y" ) );
             $this->addRule('date_of_exit_'.$i, ts('Select a valid date.'), 'qfDate');
 
             $this->addRadio( 'custom_2_'.$i,
