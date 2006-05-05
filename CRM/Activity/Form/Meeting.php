f9f367e53e61cbaa3ee05e82934e9364dcc02f6b
rupam <rupam@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-05-05 12:04:50 +0000

    Worked on Activity-Meeting's CustomData. Fixed a small bug in Activity-History CustomData.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@5344 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Form/Meeting.php
+++ CRM/Activity/Form/Meeting.php
@@ -98,8 +98,12 @@ class CRM_Activity_Form_Meeting extends CRM_Activity_Form
         $status =& $this->add('select','status',ts('Status'), CRM_Core_SelectValues::activityStatus());
         $this->addRule( 'status', ts('Please select status.'), 'required' );
 
-        
-        CRM_Core_BAO_CustomGroup::buildQuickForm( $this, $this->_groupTree, 'showBlocks1', 'hideBlocks1' );
+
+        if ($this->_action & CRM_Core_Action::VIEW ) { 
+            CRM_Core_BAO_CustomGroup::buildViewHTML( $this, $this->_groupTree );
+        } else {
+            CRM_Core_BAO_CustomGroup::buildQuickForm( $this, $this->_groupTree, 'showBlocks1', 'hideBlocks1' );
+        }
     }
 
     /**
