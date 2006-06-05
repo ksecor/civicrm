1664491d16797dc424fd560cd5d424a075f3b8d4
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-06-05 22:35:03 +0000

    //changed the display of profile fields to display labels in browse mode
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@5560 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/UF/Page/Field.php
+++ CRM/UF/Page/Field.php
@@ -146,27 +146,14 @@ class CRM_UF_Page_Field extends CRM_Core_Page {
         }
         $select['group'] = ts('Group(s)'); 
         $select['tag'  ] = ts('Tag(s)');
-
+	//print_r($fields);
         while ($ufFieldBAO->fetch()) {
             $ufField[$ufFieldBAO->id] = array();
             $phoneType = $locType = '';
             CRM_Core_DAO::storeValues( $ufFieldBAO, $ufField[$ufFieldBAO->id]);
             CRM_Core_DAO_UFField::addDisplayEnums($ufField[$ufFieldBAO->id]);
 
-            // fix the field_name value
-            $ufField[$ufFieldBAO->id]['field_name'] = $select[$ufField[$ufFieldBAO->id]['field_name']];
-            
-            if ($ufFieldBAO->location_type_id) {
-                $locType = ' (' . $locationType[$ufFieldBAO->location_type_id] . ') ';
-            }
-            
-            if ($ufFieldBAO->phone_type) {
-                if ($ufFieldBAO->phone_type != 'Phone') { // this hack is to prevent Phone Phone 
-                    $phoneType .= '-'.$ufFieldBAO->phone_type;
-                }
-            }
-          
-            $ufField[$ufFieldBAO->id]['field_name'] .= $phoneType . $locType;
+	    $ufField[$ufFieldBAO->id]['label'] = $ufFieldBAO->label; 
 
             $action = array_sum(array_keys($this->actionLinks()));
             if ($ufFieldBAO->is_active) {
