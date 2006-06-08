c7f85b742e2a9eda109712c885ef02f394484f20
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-06-08 11:18:16 +0000

    worked on CRM-1013.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@5598 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Member/Form.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.4                                                |
+ +--------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                       |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo (c) 2005
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * This class generates form components generic to Mobile provider
+ * 
+ */
+class CRM_Member_Form extends CRM_Core_Form
+{
+    /**
+     * The id of the object being edited / created
+     *
+     * @var int
+     */
+    protected $_id;
+
+    /**
+     * The name of the BAO object for this form
+     *
+     * @var string
+     */
+    protected $_BAOName;
+
+    function preProcess( ) {
+        $this->_id      = $this->get( 'id'      );
+        $this->_BAOName = $this->get( 'BAOName' );
+    }
+
+    /**
+     * This function sets the default values for the form. MobileProvider that in edit/view mode
+     * the default values are retrieved from the database
+     * 
+     * @access public
+     * @return None
+     */
+    function setDefaultValues( ) {
+        $defaults = array( );
+        $params   = array( );
+
+        if ( isset( $this->_id ) ) {
+            $params = array( 'id' => $this->_id );
+            require_once(str_replace('_', DIRECTORY_SEPARATOR, $this->_BAOName) . ".php");
+            eval( $this->_BAOName . '::retrieve( $params, $defaults );' );
+        }
+
+        if ($this->_action == CRM_Core_Action::DELETE) {
+            $this->assign( 'delName', $defaults['name'] );
+        }
+
+        // its ok if there is no element called is_active
+        $defaults['is_active'] = 1;
+
+        return $defaults;
+    }
+
+    /**
+     * Function to actually build the form
+     *
+     * @return None
+     * @access public
+     */
+    public function buildQuickForm( ) {
+        $this->addButtons( array(
+                                 array ( 'type'      => 'next',
+                                         'name'      => ts('Save'),
+                                         'isDefault' => true   ),
+                                 array ( 'type'      => 'cancel',
+                                         'name'      => ts('Cancel') ),
+                                 )
+                           );
+     
+        if($this->_action & CRM_Core_Action::DELETE) {
+            $this->addButtons(array(
+                                    array ('type'      => 'next',
+                                           'name'      => ts('Delete'),
+                                           'isDefault' => true),
+                                    array ('type'      => 'cancel',
+                                           'name'      => ts('Cancel')),
+                                    )
+                              );
+        }
+   
+    }
+
+}
+
+?>
