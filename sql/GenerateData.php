a8a35275d5af84a2aa95b6c6612284b069c7bde5
abhilasha <abhilasha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-05 13:26:23 +0000

    --added sample records for Membership
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@5942 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- sql/GenerateData.php
+++ sql/GenerateData.php
@@ -1383,36 +1383,36 @@ INSERT INTO civicrm_membership
         (contact_id, membership_type_id, join_date, start_date, end_date, source, status_id)
 VALUES
         ( ". $randomContacts[0]  .", 1, '2006-01-21', '2006-01-21', '2006-12-21', 'Payment', 1),
-        ( ". $randomContacts[1]  .", 2, '2006-02-10', '2006-02-10', '2007-12-09', 'Donation', 1),
-        ( ". $randomContacts[2]  .", 2, '2006-05-05', '2006-05-05', '2007-01-04', 'Check', 1) ,
-        ( ". $randomContacts[3]  .", 1, '2005-10-21', '2005-10-21', '2006-01-20', 'Payment', 1),
-        ( ". $randomContacts[4]  .", 2, '2005-01-10', '2005-01-10', '2006-12-09', 'Donation', 1),
-        ( ". $randomContacts[5]  .", 2, '2005-03-05', '2005-03-05', '2006-01-04', 'Check', 1),
+        ( ". $randomContacts[1]  .", 2, '2005-05-07', '2005-05-07', '2006-05-07', 'Donation', 1),
+        ( ". $randomContacts[2]  .", 1, '2005-05-05', '2005-05-05', '2006-05-10', 'Check', 1) ,
+        ( ". $randomContacts[3]  .", 1, '2005-10-21', '2005-10-21', '2006-10-22', 'Payment', 1),
+        ( ". $randomContacts[4]  .", 2, '2005-01-10', '2005-01-10', '2006-07-09', 'Donation', 1),
+        ( ". $randomContacts[5]  .", 2, '2005-03-05', '2005-03-05', '2005-12-04', 'Check', 1),
         ( ". $randomContacts[6]  .", 1, '2006-07-21', '2006-07-21', '2007-01-20', 'Payment', 1),
         ( ". $randomContacts[7]  .", 2, '2006-03-07', '2006-03-07', '2006-12-31', 'Donation', 1),
-        ( ". $randomContacts[8]  .", 2, '2005-03-05', '2005-03-05', '2006-01-04', 'Check', 1),
-        ( ". $randomContacts[9]  .", 1, '2006-10-21', '2006-10-21', '2006-12-31', 'Payment', 1),
-        ( ". $randomContacts[10]  .", 2, '2006-01-10','2006-01-10', '2006-12-09', 'Donation', 1),
-        ( ". $randomContacts[11]  .", 2, '2006-03-06','2006-03-06', '2007-01-04', 'Check', 1),
-        ( ". $randomContacts[12]  .", 1, '2006-07-04', '2006-07-04', '2006-12-20', 'Payment', 1),
-        ( ". $randomContacts[13]  .", 2, '2006-01-10', '2006-01-10', '2008-12-09', 'Donation', 1),
-        ( ". $randomContacts[14]  .", 2, '2005-08-15', '2005-08-15', '2006-08-10', 'Check', 1),
+        ( ". $randomContacts[8]  .", 3, '2005-02-05', '2005-02-05', '2006-02-10', 'Check', 1),
+        ( ". $randomContacts[9]  .", 1, '2005-02-01', '2005-02-01', '2006-02-10', 'Payment', 1),
+        ( ". $randomContacts[10]  .", 2, '2006-01-10','2006-01-10', '2007-12-09', 'Donation', 1),
+        ( ". $randomContacts[11]  .", 3, '2006-03-06','2006-03-06', '2007-01-04', 'Check', 1),
+        ( ". $randomContacts[12]  .", 1, '2005-06-04', '2005-06-04', '2006-06-07', 'Payment', 1),
+        ( ". $randomContacts[13]  .", 2, '2004-01-10', '2004-01-10', '2005-02-09', 'Donation', 1),
+        ( ". $randomContacts[14]  .", 2, '2005-07-04', '2005-07-04', '2006-07-04', 'Check', 1),
         ( ". $randomContacts[15]  .", 1, '2006-01-21', '2006-01-21', '2007-01-20', 'Payment', 1),
         ( ". $randomContacts[16]  .", 2, '2005-01-10', '2005-01-10', '2007-12-09', 'Donation', 1),
-        ( ". $randomContacts[17]  .", 2, '2006-03-05', '2006-03-05', '2007-11-04', 'Check', 1),
+        ( ". $randomContacts[17]  .", 3, '2006-03-05', '2006-03-05', '2007-11-04', 'Check', 1),
         ( ". $randomContacts[18]  .", 1, '2005-10-21', '2005-10-21', '2006-01-20', 'Payment', 1),
         ( ". $randomContacts[19]  .", 2, '2006-01-10', '2006-01-10', '2006-12-09', 'Donation', 1),
-        ( ". $randomContacts[20]  .", 2, '2005-03-25', '2005-03-25', '2006-01-01', 'Check', 1),
+        ( ". $randomContacts[20]  .", 2, '2005-03-25', '2005-03-25', '2006-03-26', 'Check', 1),
         ( ". $randomContacts[21]  .", 1, '2006-10-21', '2006-10-21', '2006-11-20', 'Payment', 1),
         ( ". $randomContacts[22]  .", 2, '2005-01-10', '2005-01-10', '2007-12-09', 'Donation', 1),
         ( ". $randomContacts[23]  .", 2, '2005-03-11', '2005-03-11', '2006-01-04', 'Check', 1),
-        ( ". $randomContacts[24]  .", 2, '2005-04-05', '2005-04-05', '2006-01-04', 'Check', 1);
+        ( ". $randomContacts[24]  .", 3, '2005-04-05', '2005-04-05', '2006-01-04', 'Check', 1);
 ";
         CRM_Core_DAO::executeQuery( $membership, CRM_Core_DAO::$_nullArray );
         
         require_once 'CRM/Member/DAO/Membership.php';
         $membership = new CRM_Member_DAO_Membership();
-        $membership->query("SELECT id  FROM civicrm_membership");
+        $membership->query("SELECT id FROM civicrm_membership");
         while ($membership->fetch()) {
             $ids[]=$membership->id;
         }
@@ -1420,15 +1420,14 @@ VALUES
         require_once 'api/crm.php';
         $membership = new CRM_Member_DAO_Membership();
         foreach ( $ids as $membershipID) {
-            $status = crm_calc_membership_status(  $membershipID, '20041230' );
+            $status = crm_calc_membership_status($membershipID);
             $membership->id = $membershipID;
             if ($membership->find()) {
                 $membership->status_id  = $status['id'];
             }
             $membership->save();
         }
-        
-        
+           
     }
     
     static function repairDate($date) {
