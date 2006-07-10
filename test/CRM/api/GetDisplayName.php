3edfa9dc8f41a2474f9f6a82e06feeef791b0e6a
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-10 23:32:17 +0000

    unit test to retrieve display name
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6016 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test/CRM/api/GetDisplayName.php
+++ test/CRM/api/GetDisplayName.php
@@ -15,13 +15,19 @@ class TestOfGetDisplayName extends UnitTestCase
     
     function testGetDisplayName( )
     {
-        $drupalID  = 1;
+        $drupalID  = 12345;
         $contactID = crm_uf_get_match_id( $drupalID );
-        
+        if ( ! $contactID ) {
+            return null;
+        }
+
         $params = array( 'id' => $contactID );
         $properties = array( 'display_name' => 1 );
         $contact = crm_fetch_contact( $params, $properties );
-        CRM_Core_Error::debug( 'displayName', $contact['display_name'] );
+        if ( is_a( $contact, 'CRM_Core_Error' ) ) {
+            return null;
+        }
+        return $contact['display_name'];
     }
 }
 ?>
