1d5c73d410f86cf67e0e55c9b0c010c5f96f17a0
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-12 10:43:07 +0000

    - delete for membership type fixed
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6044 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Member/BAO/MembershipType.php
+++ CRM/Member/BAO/MembershipType.php
@@ -130,11 +130,14 @@ class CRM_Member_BAO_MembershipType extends CRM_Member_DAO_MembershipType
     static function del($membershipTypeId) 
     {
         //check dependencies
-        require_once 'CRM/Member/DAO/Membership.php';
-        $membership =& new CRM_Member_DAO_Membership( );
-        $query = 'DELETE FROM civicrm_membership 
-                  WHERE       membership_type_id=' . $membershipTypeId . ';';
-        $membership->query($query);
+        require_once 'CRM/Member/BAO/Membership.php';
+        $membership =& new CRM_Member_BAO_Membership();
+        $membership->membership_type_id = $membershipTypeId;
+        if ( $membership->find() ) {
+            while ( $membership->fetch() ) {
+                CRM_Member_BAO_Membership::deleteMembership($membership->id);
+            }
+        }
         
         //delete from membership Type table
         require_once 'CRM/Member/DAO/MembershipType.php';
