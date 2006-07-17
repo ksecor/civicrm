821db6a4f24d9cf5fc5cb4d736a652cfdb27bf18
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-17 06:46:48 +0000

    modification for javascript
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6103 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/MatchApp/Essay.php
+++ CRM/Quest/Form/MatchApp/Essay.php
@@ -116,7 +116,7 @@ class CRM_Quest_Form_MatchApp_Essay extends CRM_Quest_Form_App
     public function buildQuickForm( ) 
     {
         foreach ( $this->_essays as $name => $essay ) {
-            $essay['attributes'] .= " onkeyup=countit(\"{$essay['name']}\");";
+            $essay['attributes'] .= " onkeyup=countit(\"{$essay['name']}\",\"{$essay['wordCount']}\");";
             // primary method to access internet
             $this->add( 'textarea',
                         "essay[{$essay['name']}]",
