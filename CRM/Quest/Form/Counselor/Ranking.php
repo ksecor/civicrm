a6fd3339d79b22ee5ad037ea5dfd0c210cb650c4
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-19 22:16:55 +0000

    add counselor forms
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6168 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Quest/Form/Counselor/Ranking.php
@@ -0,0 +1,189 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.5                                                |
+ +--------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                      |
+ +--------------------------------------------------------------------+
+*/
+
+
+/**
+ * Personal Information Form Page
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo (c) 2005
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Quest/Form/App.php';
+require_once 'CRM/Quest/BAO/Student.php'; 
+require_once 'CRM/Core/OptionGroup.php';
+
+
+/**
+ * This class generates form components for relationship
+ * 
+ */
+class CRM_Quest_Form_Counselor_Ranking extends CRM_Quest_Form_App
+{
+    /**
+     * Function to set variables up before form is built
+     *
+     * @return void
+     * @access public
+     */
+    public function preProcess()
+    {
+        parent::preProcess();
+    }
+
+    /**
+     * This function sets the default values for the form. Relationship that in edit/view action
+     * the default values are retrieved from the database
+     * 
+     * @access public
+     * @return void
+     */
+    function setDefaultValues( ) 
+    {
+        $defaults = array( );
+
+        return $defaults;
+    }
+    
+
+    /**
+     * Function to actually build the form
+     *
+     * @return void
+     * @access public
+     */
+    public function buildQuickForm( ) 
+    {
+        $attributes = CRM_Core_DAO::getAttribute('CRM_Quest_DAO_StudentRanking');
+
+        $this->add( 'text',
+                    'counselor_years',
+                    ts( 'How long have you known this student?' ),
+                    $attributes['counselor_years'],
+                    true );
+
+        $this->add( 'text',
+                    'counselor_capacity',
+                    ts( 'In what capacity?' ),
+                    $attributes['counselor_capacity'],
+                    true );
+
+        $this->add( 'text',
+                    'counselor_meet',
+                    ts( 'How often do you meet with this student?' ),
+                    $attributes['counselor_meet'],
+                    true );
+
+        $this->addCheckBox( 'counselor_basis'
+                            ts( 'This evaluation is based on (check all that apply)' ),
+                            CRM_Core_OptionGroup::values( 'counselor_basis', true ),
+                            true, '<br/>',true,
+                            array ('onclick' => "return showHideByValue('counselor_basis[5]', '1', 'counselor_basis_other', '', 'radio', false);") );
+       
+        $radioBoxes = array(
+                            'Leadership potential'             => 'leadership_id',
+                            'Intellectual Curiosity'           => 'intellectual_id',
+                            'Atttitude toward challenges'      => 'challenge_id',
+                            'Emotional Maturity'               => 'maturity_id',
+                            'Work ethic'                       => 'work_ethic_id',
+                            'Originality of thought'           => 'originality_id',
+                            'Sense of humor'                   => 'humor_id',
+                            'Energy'                           => 'energy_id',
+                            'Respect for differences'          => 'respect_differences_id',
+                            'Respect accorded by faculty'      => 'respect_faculty_id',
+                            'Respect accorded by peers'        => 'respect_peers_id',
+                            'Academically'                     => 'compare_academic_id',
+                            'Extracurricular accomplishment'   => 'compare_extracurricular_id',
+                            'Personal qualities and character' => 'compare_personal_id',
+                            'Overall'                          => 'compare_overall_id',
+                            );
+        $optionValues = array('' => $select ) + CRM_Core_OptionGroup::values( 'recommender_ranking' );
+
+        foreach( $radioBoxes as $label => $name ) {
+            $this->addRadio( $name, $label, $optionValues, null, '</td><td>' );
+        }
+
+        $this->addSelect( 'recommend_student',
+                          ts( 'I recommend this student' ) );
+
+        parent::buildQuickForm( );
+
+    }
+
+    /**
+     * process the form after the input has been submitted and validated
+     *
+     * @access public
+     * @return void
+     */
+    public function postProcess() 
+    {
+        if ( ! ( $this->_action &  CRM_Core_Action::VIEW ) ) {
+            $params = $this->controller->exportValues( $this->_name );
+
+            require_once 'CRM/Quest/BAO/Student.php';
+            $params['contact_type'] = 'Individual';
+            $params['contact_sub_type'] = 'Student';
+
+            $params['location'][1]['location_type_id'] = 1;
+            $params['location'][1]['is_primary'] = 1 ;
+            $params['location'][2]['location_type_id'] = 2;
+            
+            $idParams = array( 'id' => $this->_contactID, 'contact_id' => $this->_contactID );
+          
+            CRM_Contact_BAO_Contact::retrieve( $idParams, $defaults, $ids );
+            $contact = CRM_Contact_BAO_Contact::create($params, $ids, 2);
+            
+            $dao =& new CRM_Contact_DAO_Contact( );
+            $dao->id = $this->_contactID;
+            if ( $dao->find( true ) ) {
+                $this->set( 'welcome_name',
+                            $dao->display_name );
+            }
+
+       }
+
+        parent::postProcess( );
+     
+    } //end of function
+
+    /**
+     * Return a descriptive name for the page, used in wizard header
+     *
+     * @return string
+     * @access public
+     */
+    public function getTitle()
+    {
+        return ts('Personal Information');
+    }
+}
+
+?>
