be6dc47acaf60125dee4a654321eebc5b94a2d78
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-20 06:17:30 +0000

    worked on quest forms.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6180 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/MatchApp/Essay/PersonalStat.php
+++ CRM/Quest/Form/MatchApp/Essay/PersonalStat.php
@@ -67,12 +67,11 @@ class CRM_Quest_Form_MatchApp_Essay_PersonalStat extends CRM_Quest_Form_MatchApp
     {
         $attributes = CRM_Core_DAO::getAttribute('CRM_Quest_DAO_Student' );
         $radioAttributeArray = array('onclick' => "return show_element(\"id_upload_photo\")");
-        //$radioAttributeArray = array();
-        $this->addRadio( 'personalStat_quests', null,
-                         array( 'Choose a photograph of something important to you and explain its significance. (Stanford University, 2004). You must upload a photograph if you answer this prompt.',
+
+        $personalStatQuests = array( 'Choose a photograph of something important to you and explain its significance. (Stanford University, 2004). You must upload a photograph if you answer this prompt.',
                                 ' Evaluate a significant experience, achievement, risk you have taken, or ethical dilemma you have faced and its impact on you (Common Application, 2004)'
-                                ), 
-                         $radioAttributeArray, '<br/>' );
+                                );
+        $this->addRadio( 'personalStat_quests', null, $personalStatQuests , $radioAttributeArray, '<br/>' );
 
         //file upload
         $this->addElement('file', 'upload_photo', ts( 'Upload photograph:' ), $attributes['upload_photo'] );
