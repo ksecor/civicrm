9d1cfe45904effec6c9d7d2494a896867f9e4355
anil <anil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-21 11:49:29 +0000

    fixed a broken page
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6198 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/MatchApp/ExtracurricularInfo.php
+++ CRM/Quest/Form/MatchApp/ExtracurricularInfo.php
@@ -246,7 +246,9 @@ class CRM_Quest_Form_MatchApp_ExtracurricularInfo extends CRM_Quest_Form_App
 
             CRM_Quest_BAO_Essay::create( $this->_essays, $params, $this->_contactID, $this->_contactID );
 
-            $ids['id'] = $this->_studentID;
+            //$ids['id'] = $this->_studentID;
+            $ids = array( 'id'         => $this->_studentID,
+                          'contact_id' => $this->_contactID );
             CRM_Quest_BAO_Student::create( $params, $ids );
 
 
