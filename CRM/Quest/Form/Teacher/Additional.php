ed99321d8ce11dbe2541630212146e4bf2b95658
abhilasha <abhilasha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-24 11:37:12 +0000

    --added files for quest application
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6208 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Quest/Form/Teacher/Additional.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.5                                                |
+ +--------------------------------------------------------------------+
+ | Copyright (c) 2005 Donald A. Lobo                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                      |
+ +--------------------------------------------------------------------+
+*/
+
+
+/**
+ * Additional Information Form Page
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@yahoo.com>
+ * @copyright Donald A. Lobo (c) 2005
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Quest/Form/Recommender.php';
+require_once 'CRM/Quest/Form/MatchApp/Essay.php';
+
+
+/**
+ * This class generates form components for relationship
+ * 
+ */
+class CRM_Quest_Form_Teacher_Additional extends CRM_Quest_Form_Recommender
+{
+
+    /**
+     * Function to set variables up before form is built
+     *
+     * @return void
+     * @access public
+     */
+    public function preProcess()
+    {
+        $this->_grouping = 'cm_teacher_additional';
+        $this->_essays = CRM_Quest_BAO_Essay::getFields( $this->_grouping, $this->_contactID, $this->_contactID );
+        parent::preProcess();
+    }
+
+      /**
+     * Function to actually build the form
+     *
+     * @return void
+     * @access public
+     */
+    public function buildQuickForm( ) 
+    {
+
+        require_once "CRM/Quest/BAO/Essay.php";
+        CRM_Quest_BAO_Essay::buildForm( $this, $this->_essays );
+
+        parent::buildQuickForm();
+    }//end of function
+
+    /**
+     * Return a descriptive name for the page, used in wizard header
+     *
+     * @return string
+     * @access public
+     */
+    public function getTitle()
+    {
+        return ts('Additional Information');
+    }
+}
+
+?>
---
+++ templates/CRM/Quest/Form/Teacher/Additional.tpl
@@ -0,0 +1,13 @@
+{* Quest Teacher: Additional Information *}
+{include file="CRM/Quest/Form/MatchApp/AppContainer.tpl" context="begin"}
+
+<table cellpadding=0 cellspacing=1 border=1 width="90%" class="app">
+<tr></tr>
+<tr>
+    <td colspan=2 id="category">{$wizard.currentStepRootTitle}{$wizard.currentStepTitle}</td>
+    <td></td>
+</tr>
+
+<br />
+{include file="CRM/Quest/Form/MatchApp/Essay.tpl"}
+
