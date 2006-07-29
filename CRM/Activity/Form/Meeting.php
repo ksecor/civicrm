398750d0619843239fb2e4a015fde853e1de363f
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-29 09:00:06 +0000

    - more fixes for activity
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6344 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Form/Meeting.php
+++ CRM/Activity/Form/Meeting.php
@@ -66,7 +66,7 @@ class CRM_Activity_Form_Meeting extends CRM_Activity_Form
 
         $this->applyFilter('__ALL__', 'trim');
        
-        $this->add('text', 'subject', ts('Subject') , CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Meeting', 'subject' ) );
+        $this->add('text', 'subject', ts('Subject') , CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Meeting', 'subject' ) );
         $this->addRule( 'subject', ts('Please enter a valid subject.'), 'required' );
 
         $this->addElement('date', 'scheduled_date_time', ts('Date and Time'), CRM_Core_SelectValues::date('datetime'));
@@ -76,7 +76,7 @@ class CRM_Activity_Form_Meeting extends CRM_Activity_Form
         $this->add('select','duration_hours',ts('Duration'),CRM_Core_SelectValues::getHours());
         $this->add('select','duration_minutes', null,CRM_Core_SelectValues::getMinutes());
 
-        $this->add('text', 'location', ts('Location'), CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Meeting', 'location' ) );
+        $this->add('text', 'location', ts('Location'), CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Meeting', 'location' ) );
         
         $this->add('textarea', 'details', ts('Details'), CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Meeting', 'details' ) );
         
