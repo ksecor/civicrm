d5f592d289a1f2aee095546fb1986ca6f627d98c
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-07-30 22:49:55 +0000

    fix CRM-1098
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6360 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Task/Export.php
+++ CRM/Contribute/Form/Task/Export.php
@@ -74,9 +74,28 @@ class CRM_Contribute_Form_Task_Export extends CRM_Contribute_Form_Task {
     { 
         // create the selector, controller and run - store results in session
         $queryParams =  $this->get( 'queryParams' );
-        $query       =& new CRM_Contact_BAO_Query( $queryParams, null, null, false, false, 
-                                                   CRM_Contact_BAO_Query::MODE_CONTRIBUTE );
         $returnProperties =& CRM_Contact_BAO_Query::defaultReturnProperties( CRM_Contact_BAO_Query::MODE_CONTRIBUTE );
+
+        // also add addresss fields
+        $addressProperties    = array( 
+                                      'street_address'         => 1, 
+                                      'supplemental_address_1' => 1, 
+                                      'supplemental_address_2' => 1, 
+                                      'city'                   => 1, 
+                                      'postal_code'            => 1, 
+                                      'postal_code_suffix'     => 1, 
+                                      'state_province'         => 1, 
+                                      'country'                => 1,
+                                      'geo_code_1'             => 1,
+                                      'geo_code_2'             => 1,
+                                      'email'                  => 1, 
+                                      'phone'                  => 1, 
+                                      'im'                     => 1, 
+                                      );
+        $returnProperties = array_merge( $returnProperties, $addressProperties );
+        
+        $query            =& new CRM_Contact_BAO_Query( $queryParams, $returnProperties, null, false, false, 
+                                                        CRM_Contact_BAO_Query::MODE_CONTRIBUTE );
         $properties = array( 'contact_id', 'contribution_id' );
         $header     = array( ts( 'Contact ID' ), ts( 'Contribution ID' ) );
         foreach ( $returnProperties as $name => $dontCare ) {
