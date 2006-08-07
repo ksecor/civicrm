b64a82a38ac592dfefc42b5dccd1b6abb729bc67
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-07 19:26:10 +0000

    a few locker fixes
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6483 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/App.php
+++ CRM/Quest/Form/App.php
@@ -111,6 +111,9 @@ class CRM_Quest_Form_App extends CRM_Core_Form
                 $this->addDefaultButtons( ts('Save & Continue') );
             }
             $this->add('hidden','is_save_draft', '0');
+
+            // also make sure we set this as a constant
+            $this->setConstants( array( 'is_save_draft' => 0 ) );
         }
     }
 
@@ -156,6 +159,11 @@ class CRM_Quest_Form_App extends CRM_Core_Form
         }
 
         $this->controller->changeCategoryValues( $changes );
+
+        // if save draft is set, redirect to locker
+        if ( CRM_Utils_Array::value( 'is_save_draft', $_POST ) ) {
+            CRM_Utils_System::redirect( CRM_Utils_System::url( 'locker', 'reset=1' ) );
+        }
 
     }//end of function
 
