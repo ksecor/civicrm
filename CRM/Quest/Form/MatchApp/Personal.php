46714e3d0e99a3199f012353278bbc7d2e4a2b1a
paras <paras@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-12 22:58:47 +0000

    add few more yrs to birthdate
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6547 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Quest/Form/MatchApp/Personal.php
+++ CRM/Quest/Form/MatchApp/Personal.php
@@ -197,7 +197,7 @@ class CRM_Quest_Form_MatchApp_Personal extends CRM_Quest_Form_App
 
         $this->add('date', 'birth_date',
                    ts(' Birthdate (month/day/year)'),
-                   CRM_Core_SelectValues::date('custom', 20, 0, "M\001d\001Y" ),
+                   CRM_Core_SelectValues::date('custom', 23, 0, "M\001d\001Y" ),
                    true);        
         $this->addRule('birth_date', ts('Select a valid date for Birthdate.'), 'qfDate');
         
