858ef0dce190f88b22bb38266b85be3f740ac4ae
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-14 18:48:21 +0000

    //worked on CRM-1143
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6607 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Task/Record.php
+++ CRM/Contact/Form/Task/Record.php
@@ -88,7 +88,7 @@ class CRM_Contact_Form_Task_Record extends CRM_Contact_Form_Task {
                    $this->_activityType,
                    true);
 
-        $this->add('text', 'subject', ts('Subject') , CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Activity', 'subject' ), true ); 
+        $this->add('text', 'subject', ts('Subject') , CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'subject' ), true ); 
  
         $this->add('date', 'scheduled_date_time', ts('Date and Time'), CRM_Core_SelectValues::date('datetime'), true); 
         $this->addRule('scheduled_date_time', ts('Select a valid date.'), 'qfDate'); 
@@ -96,9 +96,9 @@ class CRM_Contact_Form_Task_Record extends CRM_Contact_Form_Task {
         $this->add('select','duration_hours',ts('Duration'),CRM_Core_SelectValues::getHours()); 
         $this->add('select','duration_minutes', null,CRM_Core_SelectValues::getMinutes()); 
  
-        $this->add('text', 'location', ts('Location'), CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Activity', 'location' ) ); 
+        $this->add('text', 'location', ts('Location'), CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'location' ) ); 
          
-        $this->add('textarea', 'details', ts('Details'), CRM_Core_DAO::getAttribute( 'CRM_Core_DAO_Activity', 'details' ) ); 
+        $this->add('textarea', 'details', ts('Details'), CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'details' ) ); 
          
         $this->add('select','status',ts('Status'), CRM_Core_SelectValues::activityStatus(), true); 
 
@@ -130,50 +130,36 @@ class CRM_Contact_Form_Task_Record extends CRM_Contact_Form_Task {
                                       'module'           => 'CiviCRM', 
                                       'activity_summary' => $params['subject'],
                                       'activity_date'    => $params['scheduled_date_time'],
+                                      'callback'         => 'CRM_Activity_BAO_Activity::showActivityDetails'
                                       );
-            switch ( $params['activity_type_id'] ) {
-            case 1:
-                $completedParams['callback'] = 'CRM_Activity_Form_Meeting::showMeetingDetails';
-                break;
-
-            case 2:
-                $completedParams['callback'] = 'CRM_Activity_Form_Phonecall::showCallDetails';
-                break;
-
-            default:
-                $completedParams['callback'] = 'CRM_Activity_Form_OtherActivity::showOtherActivityDetails';
-                break;
-            }
-
-            
         }
-
             
         list( $total, $added, $notAdded ) = array( count( $this->_contactIds ), 0, 0 );
 
-        require_once 'CRM/Core/BAO/Meeting.php';
-        require_once 'CRM/Core/BAO/Phonecall.php';
-        require_once 'CRM/Core/BAO/OtherActivity.php';
+        require_once 'CRM/Activity/BAO/Activity.php';
 
         foreach ( $this->_contactIds as $contactId ) {
             $params['target_entity_id'] = $contactId; 
             $activity = null;
+
             switch ( $params['activity_type_id'] ) {
             case 1:
-                $activity = CRM_Core_BAO_Meeting::add( $params, $ids );
+                $activityType = 'Meeting';
                 break;
 
             case 2:
-                $activity = CRM_Core_BAO_Phonecall::add( $params, $ids );
+                $activityType = 'Phonecall';
                 break;
 
             default:
-                if ( $params['activity_type_id'] > 3 ) {
-                    $activity = CRM_Core_BAO_OtherActivity::add( $params, $ids );
-                }
+                //if ( $params['activity_tpe_id'] > 3 ) {
+                $activityType = 'Activity';
+                //}
                 break;
             }
 
+            $activity = CRM_Activity_BAO_Activity::add( $params, $ids, $activityType );
+            
             if ( $activity ) {
                 $added++;
                 if ( $activity->status == 'Completed' ) {
