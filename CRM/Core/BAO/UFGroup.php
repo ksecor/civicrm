1957d18538082bf7eda0a6c7fbe8ee7243a7e5b1
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-19 10:06:23 +0000

    //fixed the issue in map and collapse checkboxes in uf group
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.5@6684 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -792,11 +792,12 @@ class CRM_Core_BAO_UFGroup extends CRM_Core_DAO_UFGroup
      * @return object
      */
     static function add(&$params, &$ids) {
-        $params['is_active'   ] = CRM_Utils_Array::value('is_active', $params, false);
-        $params['add_captcha' ] = CRM_Utils_Array::value('add_captcha', $params, false);
-        
+        $params['is_active'              ] = CRM_Utils_Array::value('is_active', $params, false);
+        $params['add_captcha'            ] = CRM_Utils_Array::value('add_captcha', $params, false);
+        $params['is_map'                 ] = CRM_Utils_Array::value('is_map', $params, false);
+        $params['collapse_display'       ] = CRM_Utils_Array::value('collapse_display', $params, false);
         $params['limit_listings_group_id'] = CRM_Utils_Array::value('group', $params);
-        $params['add_to_group_id'] = CRM_Utils_Array::value('add_contact_to_group', $params);
+        $params['add_to_group_id'        ] = CRM_Utils_Array::value('add_contact_to_group', $params);
     
         $ufGroup             =& new CRM_Core_DAO_UFGroup();
         $ufGroup->domain_id  = CRM_Core_Config::domainID( );
