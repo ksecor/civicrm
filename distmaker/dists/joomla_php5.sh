e93f359506f056fccbed7d78ab4716328d012857
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-23 05:17:48 +0000

    merge 6705:6708 from branches/v1.5
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6710 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- distmaker/dists/joomla_php5.sh
+++ distmaker/dists/joomla_php5.sh
@@ -63,7 +63,7 @@ cp $SRC/civicrm.config.php $TRG
 cp $SRC/civicrm.settings.php.sample $TRG
 
 # final touch
-REV=`svnversion -n $SRC | cut -d'M' -f1`
+REV=`svnversion -n $SRC`
 echo "trunk.$REV Joomla PHP5" > $TRG/civicrm-version.txt
 
 
