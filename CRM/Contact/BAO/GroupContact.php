efaca7c96f67a5a2312e2d590dfe3b3779c5c3d2
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-08-25 09:39:58 +0000

    fix for broken queries generated by getGroupContacts()
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6738 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/GroupContact.php
+++ CRM/Contact/BAO/GroupContact.php
@@ -426,12 +426,12 @@ class CRM_Contact_BAO_GroupContact extends CRM_Contact_DAO_GroupContact {
         
         $query = '';
         if ( empty($returnProperties) ) {
-            $query = "SELECT civicrm_contact.id as contact_id,
+            $query = "SELECT contact_a.id as contact_id,
                       civicrm_email.email as email";
             //$query = "SELECT *,civicrm_contact.id as contact_id, (talk to lobo before re-enabling this)
             //civicrm_email.email as email";
         } else {
-            $query  = "SELECT civicrm_contact.id as contact_id ,";
+            $query  = "SELECT contact_a.id as contact_id ,";
             $query .= implode( ',', $returnProperties );
         }
   
