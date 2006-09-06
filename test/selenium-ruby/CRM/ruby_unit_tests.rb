e9669a3fde5764fb9e67610c7741ae9b11b54805
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-09-06 06:56:52 +0000

    - completed manage_group test case for selenium-ruby
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6839 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test/selenium-ruby/CRM/ruby_unit_tests.rb
+++ test/selenium-ruby/CRM/ruby_unit_tests.rb
@@ -5,24 +5,24 @@ require 'crm_tests'
 class TS_CiviCRMTests
   def self.suite
     suite = Test::Unit::TestSuite.new
-    suite << TC_TestAdminGender.suite
-    suite << TC_TestAdminIMProvider.suite
-    suite << TC_TestAdminMobileProvider.suite
-    suite << TC_TestAdminActivity.suite
-    suite << TC_TestAdminPrefix.suite
-    suite << TC_TestAdminSuffix.suite
-    suite << TC_TestAdminTag.suite
-    suite << TC_TestAdminLocation.suite
-    suite << TC_TestAdminProfile.suite
-    suite << TC_TestAdminMembershipType.suite
-    suite << TC_TestAdminOptionGroup.suite
+    #suite << TC_TestAdminGender.suite
+    #suite << TC_TestAdminIMProvider.suite
+    #suite << TC_TestAdminMobileProvider.suite
+    #suite << TC_TestAdminActivity.suite
+    #suite << TC_TestAdminPrefix.suite
+    #suite << TC_TestAdminSuffix.suite
+    #suite << TC_TestAdminTag.suite
+    #suite << TC_TestAdminLocation.suite
+    #suite << TC_TestAdminProfile.suite
+    #suite << TC_TestAdminMembershipType.suite
+    #suite << TC_TestAdminOptionGroup.suite
     suite << TC_TestManageGroup.suite
-    suite << TC_TestAdminContributionTypes.suite
-    suite << TC_TestAdminCreditCard.suite
-    suite << TC_TestAdminPaymentInstrument.suite
-    suite << TC_TestAdminDuplicateMatching.suite
-    suite << TC_TestAdminImportExportMapping.suite
-    suite << TC_TestAdminCustomData.suite
+    #suite << TC_TestAdminContributionTypes.suite
+    #suite << TC_TestAdminCreditCard.suite
+    #suite << TC_TestAdminPaymentInstrument.suite
+    #suite << TC_TestAdminDuplicateMatching.suite
+    #suite << TC_TestAdminImportExportMapping.suite
+    #suite << TC_TestAdminCustomData.suite
     return suite
   end
 end
--- test/selenium-ruby/CRM/test_manage_group.rb
+++ test/selenium-ruby/CRM/test_manage_group.rb
@@ -27,12 +27,34 @@ class TC_TestManageGroup < Test::Unit::TestCase
     assert @selenium.is_text_present("Create New Group")
     @selenium.type "title", "Insert Group"
     @selenium.type "description", "This is test group"
-    @selenium.select "visibility", "label=Public User Pages and Listings"
+    @selenium.select "visibility", "label=Public User Pages"
     @page.click_and_wait "//input[@type='submit' and @value='Continue']"
     assert @selenium.is_text_present("The Group \"Insert Group\" has been saved.")
   end
   
-  def test_2_delete_group
+  def test_2_settings_group
+    assert @selenium.is_text_present("Insert Group")
+    @page.click_and_wait "//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Settings')]"
+    assert @selenium.is_text_present("Group Settings: Insert Group")
+    @selenium.select "visibility", "label=Public User Pages and Listings"
+    @page.click_and_wait "//div[@id='group']/descendant::input[@type='submit' and @value='Save']"
+    assert @selenium.is_text_present("The Group \"Insert Group\" has been saved.")
+  end
+  
+  def test_3_disable_group
+    # assert_equal "Disable", @selenium.get_text("link=Disable")
+    assert_equal "Disable", @selenium.get_text("//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Disable')]")
+    @page.click_and_wait "//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Disable')]"
+    assert_equal 'Are you sure you want to disable this Group?' ,@selenium.get_confirmation()
+  end
+  
+  def test_4_enable_group
+    # assert_equal "Enable", @selenium.get_text("link=Enable")
+    assert_equal "Enable", @selenium.get_text("//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Enable')]")
+    @page.click_and_wait "//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Enable')]"
+  end
+  
+  def test_5_delete_group
     assert @selenium.is_text_present("Insert Group")
     @page.click_and_wait "//div[@id='group']/descendant::tr[td[contains(.,'Insert Group')]]/descendant::a[contains(.,'Delete')]"
     assert @selenium.is_text_present("Are you sure you want to delete the group Insert Group?")
