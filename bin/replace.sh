a87cf90dada68be285ec90371466c1f1672297d4
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-09-11 13:53:39 +0000

    make bin/replace.sh so much more functional
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6909 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/replace.sh
+++ bin/replace.sh
@@ -5,6 +5,7 @@ for j in bin xml js extern; do
     echo $i;
     perl -pi -e 's|Copyright \(c\) 2005 Social Source Foundation|copyright CiviCRM LLC (c) 2004-2006|g' $i;
     perl -pi -e 's|Copyright \(c\) 2005 Donald A. Lobo|copyright CiviCRM LLC (c) 2004-2006|g' $i;
+    perl -pi -e 's|copyright Donald A. Lobo \(c\) 2005|copyright CiviCRM LLC (c) 2004-2006|g' $i;
     perl -pi -e 's|copyright Donald A. Lobo 01/15/2005|copyright CiviCRM LLC (c) 2004-2006|g' $i;
   done
 done
