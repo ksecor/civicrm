198948e53b504dcb4e24f9749a62a79e033d3dd9
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-09-13 12:38:41 +0000

    CiviMail: works towards CRM-1169 (mailing preview)
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@6939 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Mailing/Page/Preview.php
@@ -0,0 +1,91 @@
+<?php 
+
+/* 
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.6                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2006                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.civicrm.org/licensing/                               |
+ +--------------------------------------------------------------------+
+
+*/ 
+ 
+/** 
+ * 
+ * @package CRM 
+ * @author Piotr Szotkowski <shot@civicrm.org> 
+ * @copyright CiviCRM LLC (c) 2004-2006 
+ * $Id$ 
+ * 
+ */ 
+
+require_once 'CRM/Core/Page.php';
+/**
+ * a page for mailing preview
+ */
+class CRM_Mailing_Page_Preview extends CRM_Core_Form {
+
+    /** 
+     * run this page (figure out the action needed and perform it).
+     * 
+     * @return void
+     */ 
+    function run() {
+        require_once 'CRM/Mailing/BAO/Mailing.php';
+        $session =& CRM_Core_Session::singleton();
+
+        $options = array();
+        $session->getVars($options, 'CRM_Mailing_Controller_Send');
+
+        $type = CRM_Utils_Request::retrieve('type', 'String', CRM_Core_DAO::$_nullObject, false, 'text');
+
+        // FIXME: the below and CRM_Mailing_Form_Test::testMail()
+        // should be refactored
+
+        $mailing =& new CRM_Mailing_BAO_Mailing();
+        $mailing->domain_id = $session->get('domainID');
+
+        $mailing->header_id = $options['header_id'];
+        $mailing->footer_id = $options['footer_id'];
+        $mailing->name = $options['mailing_name'];
+        $mailing->from_name = $options['from_name'];
+        $mailing->from_email = $options['from_email'];
+        $mailing->subject = $options['subject'];
+
+        $mailing->body_html = file_get_contents($options['htmlFile']);
+        $mailing->body_text = file_get_contents($options['textFile']);
+
+        $mime =& $mailing->compose(null, null, null, $session->get('userID'), $options['from_email'], $options['from_email'], true);
+
+        // there doesn't seem to be a way to get to Mail_Mime's text and HTML
+        // parts, so we render the message steal a peek at Mail_Mime's private
+        // properties
+        $mime->get();
+        if ($type == 'html') {
+            print $mime->_htmlbody;
+        } else {
+            print '<html><head><title>' . ts('CiviMail Preview') . '</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body><pre>' . htmlspecialchars($mime->_txtbody) . '</pre></body></html>';
+        }
+        exit;
+    }
+
+}
+
+?>
--- templates/CRM/Mailing/Form/Test.tpl
+++ templates/CRM/Mailing/Form/Test.tpl
@@ -4,6 +4,7 @@
 <fieldset>
   <legend></legend>
   <dl>
+    <dt class="label">{ts}Preview Mailing:{/ts}</dt><dd><a href="{$preview.text_link}" onclick="window.open(this.href); return false;">{ts}text version{/ts}</a> | <a href="{$preview.html_link}" onclick="window.open(this.href); return false;">{ts}HTML version{/ts}</a>
     <dt class="label">{$form.test.label}</dt><dd>{$form.test.html}</dd>
     <dt class="label">{$form.test_email.label}</dt><dd>{$form.test_email.html} {ts}(filled with your contact's token values){/ts}</dd>
     <dt class="label">{$form.test_group.label}</dt><dd>{$form.test_group.html}</dd>
