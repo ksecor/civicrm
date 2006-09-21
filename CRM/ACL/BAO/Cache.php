4d3523f869e49d940969515cc75629bb272d7256
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-09-21 04:30:26 +0000

    add caching to acl scheme
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7026 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/ACL/BAO/Cache.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.5                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2006                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                       |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@civicrm.org>
+ * @copyright CiviCRM LLC (c) 2004-2006
+ * $Id$
+ *
+ */
+
+require_once 'CRM/ACL/DAO/Cache.php';
+
+/**
+ *  Access Control Cache
+ */
+class CRM_ACL_BAO_Cache extends CRM_ACL_DAO_Cache {
+
+    static $_cache = null;
+
+    static function build( $id ) {
+        if ( ! self::$_cache ) {
+            self::$_cache = array( );
+        }
+
+        if ( array_key_exists( $id, self::$_cache ) ) {
+            return self::$_cache[$id];
+        }
+
+        // check if this entry exists in db
+        // if so retrieve and return
+        self::$_cache[$id] = self::retrieve( $id );
+        if ( self::$_cache[$id] ) {
+            return self::$_cache[$id];
+        }
+
+        self::$_cache[$id] = CRM_ACL_BAO_ACL::build( $id );
+        self::store( $id, self::$_cache[$id] );
+        return self::$_cache[$id];
+    }
+
+    static function retrieve( $id ) {
+        $query = "
+SELECT acl_id, data
+  FROM civicrm_acl_cache
+ WHERE contact_id = %1
+";
+        $params = array( 1 => array( $id, 'Integer' ) );
+        $dao =& CRM_Core_DAO::executeQuery( $query, $params );
+
+        $cache = array( );
+        while ( $dao->fetch( ) ) {
+            $cache[$dao->acl_id] = $dao->data;
+        }
+        return $cache;
+    }
+
+    static function store( $id, $cache ) {
+        foreach ( $cache as $aclID => $data ) {
+            $dao =& new CRM_ACL_DAO_Cache( );
+            $dao->contact_id = $id;
+            $dao->acl_id     = $aclID;
+            $dao->data       = $data;
+            $dao->save( );
+        }
+    }
+
+    static function delete( $id ) {
+        if ( self::$_cache &&
+             array_key_exists( $id, self::$_cache ) ) {
+            unset( self::$_cache[$id] );
+        }
+
+        $query = "
+DELETE FROM civicrm_acl_cache
+WHERE contact_id = %1
+";
+        $params = array( 1 => array( $id, 'Integer' ) );
+        $dao =& CRM_Core_DAO::executeQuery( $query, $params );
+    }
+
+    static function update( $id ) {
+        self::delete( $id );
+        self::build( $id );
+    }
+
+    // deletes all the cache entries for this domain
+    static function reset( ) {
+        self::$_cache = null;
+
+        $domainID = CRM_Core_Config::domainID( );
+        $query = "
+DELETE FROM civicrm_acl_cache
+USING civicrm_acl_cache c,
+      civicrm_acl       a
+WHERE c.acl_id = a.id
+  AND a.domain_id = $domainID
+";
+
+        $dao =& CRM_Core_DAO::executeQuery( $query, CRM_Core_DAO::$_nullArray );
+    }
+
+}
+
+?>
