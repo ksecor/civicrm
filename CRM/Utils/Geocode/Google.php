b470ccc91639e1f53ff99be3a0b16bb121b206b9
abhilasha <abhilasha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-09-27 14:28:37 +0000

    --added a file to retrieve the lat/long of an address(Temporary Commit) --CRM 1241
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7101 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Utils/Geocode/Google.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.5                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2006                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]socialsourcefoundation[DOT]org.  If you have |
+ | questions about the Affero General Public License or the licensing |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | at http://www.openngo.org/faqs/licensing.html                       |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@civicrm.org>
+ * @copyright CiviCRM LLC (c) 2004-2006
+ * $Id$
+ *
+ */
+ 
+/**
+ * Class that uses geocoder.us to retrieve the lat/long of an address
+ */
+class CRM_Utils_Geocode_Google {
+    /**
+     * server to retrieve the lat/long
+     *
+     * @var string
+     * @static
+     */
+    static protected $_server = 'maps.google.com';
+
+    /**
+     * uri of service
+     *
+     * @var string
+     * @static
+     */
+    static protected $_uri = '/maps/geo';
+    
+    /**
+     * function that takes an address object and gets the latitude / longitude for this
+     * address. Note that at a later stage, we could make this function also clean up
+     * the address into a more valid format
+     *
+     * @param object $address
+     *
+     * @return boolean true if we modified the address, false otherwise
+     * @static
+      */
+    static function format( &$values ) {
+        require_once 'CRM/Utils/Array.php';
+        // we need a valid state and country, else we ignore
+        if ( ! CRM_Utils_Array::value( 'state_province' , $values  ) &&
+             ! CRM_Utils_Array::value( 'country'        , $values  ) ) {
+            return false;
+        }
+        if ( $values['country'] != 'United States' ) {
+            return false;
+        }
+        
+        $config =& CRM_Core_Config::singleton( );
+        
+        $arg = array( );
+        $arg[] = "appid=" . urlencode( $config->mapAPIKey );
+        
+        if (  CRM_Utils_Array::value( 'street_address', $values ) ) {
+            $arg[] = "street=" . urlencode( $values['street_address'] );
+        }
+        
+         if (  CRM_Utils_Array::value( 'city', $values ) ) { 
+             $arg[] = "city=" . urlencode( $values['city'] );
+         }
+         
+         if (  CRM_Utils_Array::value( 'state_province', $values ) ) { 
+             $arg[] = "state=" . urlencode( $values['state_province'] );
+         }
+         
+         if (  CRM_Utils_Array::value( 'postal_code', $values ) ) { 
+             $arg[] = "zip=" . urlencode( $values['postal_code'] );
+         }
+        
+         $args = implode( '&', $arg );
+         $query = 'http://' . self::$_server . self::$_uri . '?' . $args;
+         
+
+         require_once 'HTTP/Request.php';
+         $request =& new HTTP_Request( $query );
+         $request->sendRequest( );
+         $string = $request->getResponseBody( );
+         $xml = simplexml_load_string( $string );
+         $ret = array( );
+         $ret['precision'] = (string)$xml->Result['precision'];
+        
+         if ( $xml->Result ) {
+             foreach($xml->Result->children() as $key=>$val) {
+                 if(strlen($val)) $ret[(string)$key] =  (string)$val;
+             } 
+             $values['geo_code_1'] = $ret['Latitude'];
+             $values['geo_code_2'] = $ret['Longitude'];
+         }
+         return true;
+     }
+}
+?>
