9067dbfe20a107aa34da61a0e2ddbe2075940101
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-10-12 18:16:40 +0000

    fix contact id listing in profile
    git-svn-id: http://svn.civicrm.org/civicrm/branches/quest@7320 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Profile/Page/Listings.php
+++ CRM/Profile/Page/Listings.php
@@ -215,11 +215,7 @@ class CRM_Profile_Page_Listings extends CRM_Core_Page {
                                                           '_qf_Search_display=true&map=1' ) );
                 }
                 
-                $editLink = CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_UFGroup', $this->_gid, 'is_edit_link' );
-                if ( ! CRM_Core_Permission::check( 'add contacts' ) ) {
-                    $editLink = false;
-                }
-                
+		$editLink = false;
                 $selector =& new CRM_Profile_Selector_Listings( $this->_params, $this->_customFields, $this->_gid,
                                                                 $map, $editLink );
 
