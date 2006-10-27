db281f437833c9bf9eb0ed9eb80bf22289ea68a4
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-10-27 06:03:37 +0000

    merge 7425:7451 from branches/v1.5 (CRM-1361 fix)
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7452 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Form.php
+++ CRM/Core/Form.php
@@ -276,6 +276,14 @@ class CRM_Core_Form extends HTML_QuickForm_Page {
 
         $this->preProcess();
 
+	// add the drupal form token hidden value to allow things to work
+	$config =& CRM_Core_Config::singleton( );
+	if ( $config->userFramework == 'Drupal' &&
+	     $config->userFramework <= 4.6      &&
+             function_exists( 'drupal_get_token' ) ) {
+	   $this->addElement( 'hidden', 'edit[token]', drupal_get_token( ) );
+        }
+
         $this->buildQuickForm();
 
         $defaults =& $this->setDefaultValues( );
