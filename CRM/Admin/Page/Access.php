32d9299cb4fc321caeb4fd14ec8714f2f856e7f4
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-10-28 15:06:38 +0000

    ACL dashboard - draft.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7476 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Admin/Page/Access.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.6                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2006                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]civicrm[DOT]org.  If you have questions       |
+ | about the Affero General Public License or the licensing  of       |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | http://www.civicrm.org/licensing/                                  |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @author David Greenberg <dave@civicrm.org>
+ * @copyright CiviCRM LLC (c) 2004-2006
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Page/Basic.php';
+
+/**
+ * Dashboard page for managing Access Control
+ * For initial version, this page only contains static links - so this class is empty for now.
+ */
+class CRM_Admin_Page_Access extends CRM_Core_Page_Basic 
+{
+    function run( ) {
+        $ufAccessURL = CRM_Utils_System::url( 'admin/access' );
+        $this->assign('ufAccessURL', $ufAccessURL);
+    }
+}
+
+?>
---
+++ templates/CRM/Admin/Page/Access.tpl
@@ -0,0 +1,22 @@
+<div id="help">
+    {ts}Access Control links are here...{/ts}
+</div>
+
+<table class="report"> 
+<tr>
+    <td class="nowrap"><a href="{crmURL p='civicrm/acl' q="reset=1"}" id="editACLs">&raquo; {ts}Add or Edit ACLs{/ts}</a></td>
+    <td>{ts}ACLs define an operation permitted on a set of contacts by a specific role.{/ts}</td>
+</tr>
+<tr>
+    <td class="nowrap"><a href="{crmURL p='civicrm/acl/entityrole' q="reset=1"}" id="editACLs">&raquo; {ts}Add or Edit ACL Role Assignments{/ts}</a></td>
+    <td>{ts}ACLs define an operation permitted on a set of contacts by a specific role.{/ts}</td>
+</tr>
+<tr>
+    <td class="nowrap"><a href="{crmURL p='civicrm/admin/options' q="reset=1&group=acl_role"}" id="editACLRoles">&raquo; {ts}Add or ACL Roles{/ts}</a></td>
+    <td>{ts}ACLs define an operation permitted on a set of contacts by a specific role.{/ts}</td>
+</tr>
+<tr>
+    <td class="nowrap"><a href="{$ufAccessURL}" id="adminAccess">&raquo; {ts}Drupal Access Control{/ts}</a></td>
+    <td>{ts}Manage Access Control for Drupal Roles.{/ts}</td>
+</tr>
+</table>
