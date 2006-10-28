3270918d837223f7dff0db9f8c0ae77b8797f89f
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-10-28 15:29:31 +0000

    ACL dashboard - draft.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7477 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Admin/Page/Access.php
+++ CRM/Admin/Page/Access.php
@@ -40,7 +40,7 @@ require_once 'CRM/Core/Page/Basic.php';
  * Dashboard page for managing Access Control
  * For initial version, this page only contains static links - so this class is empty for now.
  */
-class CRM_Admin_Page_Access extends CRM_Core_Page_Basic 
+class CRM_Admin_Page_Access extends CRM_Core_Page 
 {
     function run( ) {
         $ufAccessURL = CRM_Utils_System::url( 'admin/access' );
