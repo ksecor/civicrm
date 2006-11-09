d0130dc1d89dd1f552ed3052e9045f9a4dedeff5
manish <manish@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2006-11-09 08:33:36 +0000

    - changes for ruby tests
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@7643 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/runTest.sh
+++ bin/runTest.sh
@@ -9,8 +9,15 @@ ORIGPWD=`pwd`
 
 # File for Storing Log Of UnitTest.
 logUT=UnitTestResult
+logSRT=SeleniumTestResult
 
-# Function to Create Log Folder if it does not Exists.
+###########################################################
+##
+##  Create log for the tests
+##
+###########################################################
+
+# Method to Create Log Folder if it does not Exists.
 create_log()
 {
     cd $ORIGPWD/../test/
@@ -22,7 +29,13 @@ create_log()
     fi
 }
 
-# Function to Run Unit Tests.
+###########################################################
+##
+##  Following methods are used to run the different tests
+##
+###########################################################
+
+# Method to Run Unit Tests.
 run_UnitTest()
 {
     cd $ORIGPWD/../test
@@ -30,7 +43,20 @@ run_UnitTest()
     php UnitTests.php > $PATH4LOG/Result/$logUT
 }
 
-# Function to Run Stress Test.
+# Method to Run Selenium Ruby Tests.
+run_seleniumTest()
+{
+    sub_menu
+    
+    echo "Enter Your Option: "
+    read option
+
+    cd $ORIGPWD/../test/selenium-ruby/CRM
+    # Running Selenium (ruby) Tests
+    ruby ruby_unit_tests.rb $option
+}
+
+# Method to Run Stress Test.
 run_stressTest()
 {
     cd $ORIGPWD/
@@ -38,57 +64,134 @@ run_stressTest()
     ./runStressTest.sh
 }
 
-# Function used for Displaying Menu.
-display_menu()
+###########################################################
+##
+##  Menu system for different purpos
+##
+###########################################################
+
+main_menu()
 {
     clear
     echo
-    echo " *********************** Testing with Different Tests *********************** "
+    echo " *********************** Select Method for Test *********************** "
     echo 
     echo "Options available: "
-    echo "  U   - Carry out Unit Tests"
-    echo "  S   - Carry out Stress Tests"
-    echo "  All - Carry out all the above mentioned Tests i.e. Unit Tests, Stress Test"
+    echo "  UT   - Carry out Unit Tests"
+    echo "  ST   - Carry out Stress Tests"
+    echo "  SRT  - Carry out Selenium (Ruby) Tests"
+    echo "  All  - Carry out all the above mentioned Tests i.e. Unit Tests, Stress Test, Selenium Test"
     echo
     echo
 }
 
-# Main Execution Starts Here.
+sub_menu()
+{
+    clear
+    
+    echo
+    echo " *********************** Select the Option *********************** "
+    echo 
+    echo "Options available: "
+    echo "  CON-I   : Contact Individual"
+    echo "  CON-H   : Contact Household"
+    echo "  CON-O   : Contact Organization"
+    echo "  ADM-C   : Administer - Configuration Section"
+    echo "  ADM-S   : Administer - Setup Section"
+    echo "  ADM-CC  : Administer - CiviContribute"
+    echo "  ADM-CM  : Administer - CiviMember"
+    echo "  SRC-BS  : Find Contact - Basic Search"
+    echo "  SRC-AS  : Advanced Search"
+    echo "  SRC-SB  : Search Builder"
+    #echo "  IMP-C   : Import - Contacts"
+    #echo "  IMP-AH  : Import - Activity History"
+    echo "  CCT-FC  : CiviContribute - Find Contribution"
+    #echo "  CCT-IC  : CiviContribute - Import Contribution"
+    echo "  CMB-FM  : CiviMember - Find Memberships"
+    #echo "  CMB-IM  : CiviMember - Import Memberships"
+    echo "  CML     : CiviMail"
+}
 
+###########################################################
+##
+##  Main execution method.
+##
+##  All test scripts will run usnig this method
+##
+###########################################################
+
+run_option()
+{
+    # Following Case Structure is used for Executing Menuing System.
+    case $1 in
+    # Unit Tests
+	"UT" | "ut" | "Ut")
+	    echo "Running Unit Tests"; echo;
+	    run_UnitTest
+	    echo "Unit Tests Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logUT; echo;
+	    echo " **************************************************************************** ";
+	    ;;
+	
+    # Stress Tests
+	"ST" | "st" | "St")
+	    echo "Running Stress Tests"; echo;
+	    run_stressTest
+	    echo "Stress Tests Successfully Completed."; echo;
+	    echo " **************************************************************************** ";
+	    ;;
+	
+    # Stress Tests
+	"SRT" | "srt" | "Srt")
+	    echo "Running Selenium (Ruby) Tests"; echo;
+	    run_seleniumTest
+	    #echo "Selenium (Ruby) Testing Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logSRT; echo;
+	    echo " **************************************************************************** ";
+	    ;;
+        
+    # All the Tests will be Executed one after other 
+	"All" | "all" )
+	    echo "Running all three Tests i.e. Unit Tests, Web Tests, maxQ Tests and Stress Test "; echo;
+	    echo "Running Unit Tests"; echo;
+	    run_UnitTest
+	    echo "Unit Tests Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logUT; echo;
+	    echo "Running Stress Tests"; echo;
+	    run_stressTest
+	    echo "Stress Tests Successfully Completed."; echo;
+	    echo " **************************************************************************** ";
+	    echo "Running Selenium (ruby) Tests"; echo;
+	    run_seleniumTest
+	    #echo "Selenium (Ruby) Testing Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logSRT; echo;
+	    echo " **************************************************************************** ";
+	    ;;
+	*)
+	    echo "You have ntered Invalid Option."; echo;
+	    exit
+	    ;;
+    esac
+}
+
+###########################################################
+##
+##  Start of the script.
+##
+###########################################################
+
+start()
+{
 create_log
 
-display_menu
+main_menu
 echo "Enter Your Option: "
 read option
+run_option $option
 echo;
 
-# Following Case Structure is used for Executing Menuing System.
-case $option in
-    # Unit Tests
-    "U" | "u" )
-	echo "Running Unit Tests"; echo;
-	run_UnitTest
-	echo "Unit Tests Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logUT; echo;
-	echo " **************************************************************************** ";
-	;;
-    
-    # Stress Tests
-    "S" | "s" )
-	echo "Running Stress Tests"; echo;
-	run_stressTest
-	echo "Stress Tests Successfully Completed."; echo;
-	echo " **************************************************************************** ";
-	;;
-    
-    # All the Tests will be Executed one after other 
-    "All" | "all" )
-	echo "Running all three Tests i.e. Unit Tests, Web Tests, maxQ Tests and Stress Test "; echo;
-	echo "Running Unit Tests"; echo;
-	run_UnitTest
-	echo "Unit Tests Successfully Completed. Log stored in the File : " $PATH4LOG/Result/$logUT; echo;
-	echo "Running Stress Tests"; echo;
-	run_stressTest
-	echo "Stress Tests Successfully Completed."; echo;
-	echo " **************************************************************************** ";
-	;;
-esac
+}
+
+###########################################################
+##
+##  Call to start of the script
+##
+###########################################################
+
+start
--- test/selenium-ruby/CRM/ruby_unit_tests.rb
+++ test/selenium-ruby/CRM/ruby_unit_tests.rb
@@ -6,30 +6,98 @@ class TS_CiviCRMTests
   def self.suite
     suite = Test::Unit::TestSuite.new
     
-    #Dashboard section
+    if ARGV.length != 1
+      exit
+    end
+    
+    option = ARGV[0]
+    
+    if (option[0,3] == 'con') or (option[0,3] == 'CON')
+      #create_contact()
+    end
+    
+    if (option[0,3] == 'adm') or (option[0,3] == 'ADM')
+      #admin()
+    end
+    
+    if (option[0,3] == 'imp') or (option[0,3] == 'IMP')
+      #print 'IMPORT'
+    end
+    
+    if (option[0,3] == 'src') or (option[0,3] == 'SRC')
+      #print 'Search'
+    end
+    
+    if (option[0,3] == 'cct') or (option[0,3] == 'CCT')
+      #print 'Contribute'
+    end
+    
+    if (option[0,3] == 'cmb') or (option[0,3] == 'CMB')
+      #print 'CiviMember'
+    end
+    
+    if (option[0,3] == 'cml') or (option[0,3] == 'CML')
+      #print 'CiviMail'
+    end
+    
+    
+    # Contact Related
+    # if option  == 'contact'
+#       create_contact()
+#     end
+    
+#     # Groups Related
+#     if option == 'group'
+#       group()
+#     end
+    
+#     # Import Contact, Activity History
+#     if option == 'import'
+#       #import()
+#     end
+    
+#     ## Administer CiviCRM Section
+    
+#     #Configure section
+#     if option == 'admin_configure'
+#       admin_configure()
+#     end
+    
+#     #Setup section
+#     admin_setup()
+    
+#     #CiviContribute section
+#     admin_civicontribute()
+    
+#     #CiviMember section
+#     admin_civimember()
+    
+#     # CiviCRM Components
+#     civicontribute()
+#     civimember()
+    
+#     # All types of search
+#     search()
+        
+    return suite
+  end
+  
+  def mine
+    print "==+"
+  end
+  
+  def create_contact
     suite << TC_TestNewIndividual.suite
-    suite << TC_TestNewGroup.suite
-    suite << TC_TestManageGroup.suite
     suite << TC_TestNewHousehold.suite
     suite << TC_TestNewOrganization.suite
-    #suite << TC_TestImportContacts.suite
-    #suite << TC_TestCiviContribute.suite
-    #suite << TC_TestCiviMember.suite
-    
-    #Individual Test Cases
-    suite << TC_TestContactMembership.suite
-    suite << TC_TestContactContribution.suite
-    suite << TC_TestFindMembership.suite
-    suite << TC_TestFindContribution.suite
-      
-    #Search section
-    suite << TC_TestFindContacts.suite
-    suite << TC_TestAdvancedSearch.suite
-    suite << TC_TestSearchBuilder.suite
-
-    ## Administer CiviCRM Section
-    
-    #Configure section
+  end
+  
+  def group
+    suite << TC_TestNewGroup.suite
+    suite << TC_TestManageGroup.suite
+  end
+  
+  def admin_configure
     suite << TC_TestAdminActivity.suite
     suite << TC_TestAdminProfile.suite
     suite << TC_TestAdminCustomData.suite
@@ -40,28 +108,52 @@ class TS_CiviCRMTests
     suite << TC_TestAdminDomainInformation.suite
     suite << TC_TestAdminOptionGroup.suite
     suite << TC_TestAdminImportExportMapping.suite
-    
-    #Setup section
+  end
+  
+  def admin_setup
     suite << TC_TestAdminGender.suite
     suite << TC_TestAdminIMProvider.suite
     suite << TC_TestAdminMobileProvider.suite
     suite << TC_TestAdminPrefix.suite
     suite << TC_TestAdminSuffix.suite
     suite << TC_TestPrefCommMethod.suite
-    
-    #CiviContribute section
+  end
+  
+  def admin_civicontribute
     #suite << TC_TestAdminOnlineContribution.suite
     suite << TC_TestAdminManagePremium.suite
     suite << TC_TestAdminContributionTypes.suite
     suite << TC_TestAdminPaymentInstrument.suite
     suite << TC_TestAdminCreditCard.suite
-    
-    #CiviMember section
+  end
+  
+  def admin_civimember
     suite << TC_TestAdminMembershipType.suite
     suite << TC_TestAdminMembershipStatus.suite
-
-    return suite
   end
+  
+  def import
+    suite << TC_TestImportContacts.suite
+  end
+  
+  def search
+    suite << TC_TestFindContacts.suite
+    suite << TC_TestAdvancedSearch.suite
+    suite << TC_TestSearchBuilder.suite
+  end
+
+  def civicontribute
+    suite << TC_TestContactContribution.suite
+    #suite << TC_TestCiviContribute.suite
+    suite << TC_TestFindContribution.suite
+  end
+  
+  def civimember
+    suite << TC_TestContactMembership.suite
+    #suite << TC_TestCiviMember.suite
+    suite << TC_TestFindMembership.suite
+  end
+  
 end
 
 if __FILE__ == $0
