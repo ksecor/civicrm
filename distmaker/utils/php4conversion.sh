08419461b5a09e0d4788a00a3dcdd5a449b816e8
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-01-01 21:17:01 +0000

    fix CRM-1485
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.6@8060 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- distmaker/utils/php4conversion.sh
+++ distmaker/utils/php4conversion.sh
@@ -14,17 +14,15 @@ fi
 echo;echo Start of code conversion from php5 to php4....;echo;
 $DM_PHP $P/converter.php
 
-#[ ! -d $DM_GENFILESDIR/modules ] && mkdir $DM_GENFILESDIR/modules
-#$DM_PHP $P/converter.php $DM_SOURCEDIR/modules/civicrm.module > $DM_GENFILESDIR/modules/civicrm.module
-
-[ ! -d $DM_GENFILESDIR/drupal ] && mkdir $DM_GENFILESDIR/drupal
-$DM_PHP $P/converter.php $DM_SOURCEDIR/drupal/civicrm.module > $DM_GENFILESDIR/drupal/civicrm.module
-
 rsyncOptions="-avC --exclude=svn --ignore-existing"
 rsync="$DM_RSYNC $rsyncOptions"
-for code in css i js l10n packages PEAR templates bin sql joomla; do
+for code in css i js l10n packages PEAR templates bin sql joomla drupal; do
   echo $code
   [ -d $DM_SOURCEDIR/$code ] && $rsync $DM_SOURCEDIR/$code $DM_GENFILESDIR
 done
+
+# we first copy all the drupal files before we convert the civicrm module
+$DM_PHP $P/converter.php $DM_SOURCEDIR/drupal/civicrm.module > $DM_GENFILESDIR/drupal/civicrm.module
+$DM_PHP $P/converter.php $DM_SOURCEDIR/drupal/api.php > $DM_GENFILESDIR/drupal/api.php
 
 echo;echo End of code conversion from php5 to php4....;echo;
