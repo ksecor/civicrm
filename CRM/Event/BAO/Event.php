b93fbab945cf935d93a7f48a944a248e4eaad1a0
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-01-05 12:39:44 +0000

    - work towards CRM-1437
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@8092 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Event/BAO/Event.php
@@ -0,0 +1,112 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 1.6                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2006                                  |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the Affero General Public License Version 1,    |
+ | March 2002.                                                        |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the Affero General Public License for more details.            |
+ |                                                                    |
+ | You should have received a copy of the Affero General Public       |
+ | License along with this program; if not, contact the Social Source |
+ | Foundation at info[AT]civicrm[DOT]org.  If you have questions       |
+ | about the Affero General Public License or the licensing  of       |
+ | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
+ | http://www.civicrm.org/licensing/                                  |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @author Donald A. Lobo <lobo@civicrm.org>
+ * @copyright CiviCRM LLC (c) 2004-2006
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Event/DAO/Event.php';
+
+class CRM_Event_BAO_Event extends CRM_Event_DAO_Event 
+{
+
+    /**
+     * class constructor
+     */
+    function __construct( ) 
+    {
+        parent::__construct( );
+    }
+    
+    /**
+     * Takes a bunch of params that are needed to match certain criteria and
+     * retrieves the relevant objects. Typically the valid params are only
+     * contact_id. We'll tweak this function to be more full featured over a period
+     * of time. This is the inverse function of create. It also stores all the retrieved
+     * values in the default array
+     *
+     * @param array $params   (reference ) an assoc array of name/value pairs
+     * @param array $defaults (reference ) an assoc array to hold the flattened values
+     *
+     * @return object CRM_Event_BAO_ManageEvent object
+     * @access public
+     * @static
+     */
+    static function retrieve( &$params, &$defaults ) 
+    {
+        $event  = new CRM_Event_DAO_Event( );
+        $event->copyValues( $params );
+        if ( $event->find( true ) ) {
+            CRM_Core_DAO::storeValues( $event, $defaults );
+            return $event;
+        }
+        return null;
+    }
+
+    /**
+     * update the is_active flag in the db
+     *
+     * @param int      $id        id of the database record
+     * @param boolean  $is_active value we want to set the is_active field
+     *
+     * @return Object             DAO object on sucess, null otherwise
+     * @static
+     */
+    static function setIsActive( $id, $is_active ) 
+    {
+        return CRM_Core_DAO::setFieldValue( 'CRM_Event_DAO_Event', $id, 'is_active', $is_active );
+    }
+
+    /**
+     * function to add the eventship types
+     *
+     * @param array $params reference array contains the values submitted by the form
+     * @param array $ids    reference array contains the id
+     * 
+     * @access public
+     * @static 
+     * @return object
+     */
+    static function add(&$params, $id) 
+    {
+        $event =& new CRM_Event_DAO_Event( );
+        $event->domain_id = CRM_Core_Config::domainID( );
+        $event->id = CRM_Utils_Array::value( 'event_id', $id );
+        
+        $event->copyValues( $params );
+        $event->save( );
+                
+        return $event;
+    }
+    
+}
+?>
