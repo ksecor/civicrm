8da32ae83ee4b79518a716bcbaaa5a6950d77588
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-02-23 14:59:21 +0000

    merge 8568:8569 from branches/v1.6.tmf
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@8579 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/PDF/PDFtk.php
+++ CRM/Utils/PDF/PDFtk.php
@@ -20,9 +20,18 @@ class CRM_Utils_PDF_PDFtk {
  	 * A stream of PDF data (in the case of no output file);
  	 * 
  	 */
+    
+    /**
+     * fill_form: Takes a PDF form template, fdf or xfdf data and optionally an output file name.  Merges in the data and outputs.
+     *
+     * @param string $templateFile
+     * @param string $fdfData contents of fdf or xfdf file | a file name
+     * @param string $outputFile
+     * @param bool $flatten If supplies pdftk will make the form fields no longer editable.
+     * @return binary data or a filename.
+     */
     function fill_form($templateFile,$fdfData,$outputFile = null,$flatten = 0) {
-        $args = func_get_args();
-
+        
         $cmd = new System_Command();
         $pdftkOptions = array();
         $pdftkOptions[] = PDFTK_BIN;
@@ -49,15 +58,17 @@ class CRM_Utils_PDF_PDFtk {
         if ( $flatten ) {
             $pdftkOptions[] =  "flatten";
         }
-        //$ret = $cmd->pushCommand($pdftkCommand);
+        
         $ret = call_user_method_array("pushCommand",$cmd,$pdftkOptions);
         //CRM_Core_Error::debug('$ret',$ret);
 
         //CRM_Core_Error::debug('$cmd->systemCommand',$cmd->systemCommand);
 
-
         try {
             $res = $cmd->execute();
+            if (!$outputFile) { 
+               return $res;
+            }
             return $outputFile;
         }catch (Exception $e) {
             die("Error running pdftk");
@@ -65,14 +76,13 @@ class CRM_Utils_PDF_PDFtk {
 
     }
 
-    function merge_files ($files) {
-        foreach($files as $file) {
-            $args .= " " . escapeshellargs($file);
-        }
-
-        //run it;
-    }
-
+    /**
+     * Takes XML input, runs an xslt transformation to generate xfdf
+     *
+     * @param string $xml
+     * @param string $templateFile // not used, but should generate meta-data in the resultant xfdf
+     * @return string xml
+     */
     public function createXfdfFromXML($xml,$templateFile) {
         $config = & CRM_Core_Config :: singleton();
         //CRM_Core_Error::debug('a',$xml);
@@ -97,10 +107,14 @@ class CRM_Utils_PDF_PDFtk {
         }catch(Exception $e) {
             echo $e->getMessage();
         }
-
     }
 
-    
+    /**
+     * fixFDFV7 small hack to get pdftk generated merged forms to be readable by older versions of acrobat.
+     *
+     * @param string $file
+     * @return unknown
+     */
     public function fixPDFV7($file) {
         $cmd = new System_Command();
         $pdftkCommand[] = PDFTK_BIN;
@@ -144,6 +158,13 @@ class CRM_Utils_PDF_PDFtk {
         //Just run 'pdftk d7.pdf -burst' and then open the resulting 'pg_0001.pdf' in Acrobat 7 Pro and you now have editable form fields!
     }
 
+    /**
+     * Joins two pdfs together
+     *
+     * @param array $inputFiles
+     * @param string $outputFile If not provided, output will be returned.
+     * @return binary output of cat operation or true;
+     */
     public function cat($inputFiles,$outputFile = '-') {
 
         $cmd = new System_Command();
@@ -160,19 +181,27 @@ class CRM_Utils_PDF_PDFtk {
 
     }
 
+    /**
+     * Creates an xfdf from an Array.  This required some hacking to get field names to match up if file is generated in Adobe Designer. 
+     * Hopefully this can be eliminated in future versions.  In Adobe Designer, every new page becomes a new subform.  So a field placed
+     * on the first page will be called RootForm[0].#subform[0].fieldname[0].  The same field on the 2nd page is RootForm[0].#subform[1].fieldname[0].
+     * So an array with simply the key "fieldname" won't work.  This function dumps the current data fields in the template file, and matches
+     * against the last portion (after the last dot).  So both of the previous fieldnames using different subforms will match "fieldname"
+     *
+     * @param array $array
+     * @param string $templateFile
+     * @return unknown
+     */
     public function createXfdfFromArray($array,$templateFile = "") {
         
         $xfdf = new DOMDocument('1.0',"UTF-8");
         $xfdf->formatOutput = true;
-        //<xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve">
+
         $root = $xfdf->appendChild(new DOMElement('xfdf'));
 
-        
         $root->appendChild(new DOMAttr("xmlns","http://ns.adobe.com/xfdf/"));
         $root->appendChild(new DOMAttr("xml:space","preserve"));
         $fieldsNode = $root->appendChild(new DOMElement('fields'));
-        
-        
         
         if($templateFile) {
             if(!is_file($templateFile)) {
@@ -189,10 +218,8 @@ class CRM_Utils_PDF_PDFtk {
                     $fieldNode = $fieldsNode->appendChild(new DOMElement('field'));
                     $fieldNode->appendChild(new DOMAttr("name",$fieldPath . "." . $fields[2][$key] . $fields[3][$key]));
                     $valueNode = $fieldNode->appendChild(new DOMElement('value'));
-                    $valueNode->appendChild(new DOMText($array[$fields[2][$key]]));
-                    
+                    $valueNode->appendChild(new DOMText($array[$fields[2][$key]]));                    
                 }
-                
             }
             
             $idsNode = $root->appendChild(new DOMElement('ids'));
@@ -204,18 +231,15 @@ class CRM_Utils_PDF_PDFtk {
             return $xfdf->saveXML();
         
         }
-        
-        
-
-   
-        //load up the template file, dump the fields, take the output
-        //foreach field, chop it off after the last dot.
-        //match against the keys in the array
-        //add a DOM node for xfdf
-        //be pissed at adobe for this crap
-
     }
 
+    /**
+     * Wrapper for dump_data_fields function
+     *
+     * @param string $templateFile
+     * @param array $options not used currently
+     * @return string
+     */
     public function dump_data_fields($templateFile,$options = "") {
         $cmd = new System_Command();
         $pdftkCommand[] = PDFTK_BIN;        
