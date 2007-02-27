ab38d9a8471403469ba58fbb345744bf0ee9a468
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-02-27 05:12:57 +0000

    more CRM-1648 works: instead of -core, -common and -modules, simply split -modules off civicrm.pot (I should shower with my laptop more often)
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@8618 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/create-pot-files.sh
+++ bin/create-pot-files.sh
@@ -4,15 +4,14 @@ bin=`dirname $0`
 potdir="$bin/../l10n/pot/LC_MESSAGES"
 
 echo ' * extracting core strings'
-$bin/extractor.php core > $potdir/civicrm-core.full.pot
+$bin/extractor.php core > $potdir/civicrm.pot
 
 echo ' * extracting modules strings'
 $bin/extractor.php modules > $potdir/civicrm-modules.full.pot
 
-echo ' * building civicrm-{common,core,modules}.pot files'
-msgcomm $potdir/civicrm-core.full.pot $potdir/civicrm-modules.full.pot > $potdir/civicrm-common.pot
-msgcomm -u $potdir/civicrm-core.full.pot $potdir/civicrm-common.pot > $potdir/civicrm-core.pot
+echo ' * building the proper civicrm-modules.pot file'
+msgcomm $potdir/civicrm.pot $potdir/civicrm-modules.full.pot > $potdir/civicrm-common.pot
 msgcomm -u $potdir/civicrm-modules.full.pot $potdir/civicrm-common.pot > $potdir/civicrm-modules.pot
 
 echo ' * cleanup'
-rm $potdir/civicrm-core.full.pot $potdir/civicrm-modules.full.pot
+rm $potdir/civicrm-modules.full.pot $potdir/civicrm-common.pot
