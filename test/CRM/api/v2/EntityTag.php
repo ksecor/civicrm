8a58934bb5edb51907f12fdaccadc1d66a61a322
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-02-28 00:36:52 +0000

    more RESTful api work
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@8632 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test/CRM/api/v2/EntityTag.php
+++ test/CRM/api/v2/EntityTag.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'api/v2/GroupContact.php';
+require_once 'api/v2/EntityTag.php';
 require_once 'api/v2/Contact.php';
 
 class TestOfEntityTag extends UnitTestCase 
@@ -14,34 +14,34 @@ class TestOfEntityTag extends UnitTestCase
     {
     }
   
-    function testAddToGroup( ) {
+    function testAddToTag( ) {
         $params = array(
                         'contact_id.1' => 1,
                         'contact_id.2' => 2,
                         'contact_id.3' => 3,
-                        'group_id'     => 1 );
+                        'tag_id'     => 1 );
 
-        $this->listGroupMembers( );
-        civicrm_group_contact_add( $params );
-        $this->listGroupMembers( );
+        $this->listTagMembers( );
+        civicrm_entity_tag_add( $params );
+        $this->listTagMembers( );
     }
 
-    function listGroupMembers( ) {
-        $params = array( 'group' => array( 1 => 1 ),
+    function listTagMembers( ) {
+        $params = array( 'tag' => array( 1 => 1 ),
                          'return.contact_id' => 1,
                          'return.display_name' => 1 );
         $contacts = civicrm_contact_search( $params );
         CRM_Core_Error::debug( 'c', $contacts );
     }
 
-    function testRemoveFromGroup( ) {
+    function testRemoveFromTag( ) {
         $params = array(
                         'contact_id.1' => 1,
                         'contact_id.2' => 2,
                         'contact_id.3' => 3,
-                        'group_id'     => 1 );
-        civicrm_group_contact_remove( $params );
-        $this->listGroupMembers( );
+                        'tag_id'       => 1 );
+        civicrm_entity_tag_remove( $params );
+        $this->listTagMembers( );
     }
 
 }
