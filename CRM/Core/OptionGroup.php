2ca45bcdce9df1ead19de183a3d52c1fef3067ae
bharat <bharat@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-12 13:08:24 +0000

    #replaced (' ',',','\') with '_'
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@8848 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/OptionGroup.php
+++ CRM/Core/OptionGroup.php
@@ -137,6 +137,7 @@ WHERE  v.option_group_id = g.id
                         AND    $lookupBy";
 
                     $newValue[] = CRM_Core_DAO::singleValueQuery( $query, $p );
+                    $newValue = str_replace( array( ' ', ',', '/', '.', '(', ')' ), '_', $newValue );
                 }
                 $params[$value['newName']] = implode(', ', $newValue);
             }
