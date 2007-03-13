97bb63e0e86fc3cdf6a1f779c9a3c476191e762c
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-13 13:08:08 +0000

    //fixed default value for custom check-box
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@8875 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -1512,7 +1512,7 @@ SELECT g.* from civicrm_uf_group g, civicrm_uf_join j
                             $v = explode( CRM_Core_BAO_CustomOption::VALUE_SEPERATOR, $details[$name] );
                             foreach ( $v as $item ) {
                                 if ($item) {
-                                    $defaults[$fldName][$item] = $item;
+                                    $defaults[$fldName."[$item]"] = 1;
                                 }
                             }
                             break;
