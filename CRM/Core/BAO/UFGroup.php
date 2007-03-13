086891a0fae740e09b8a065c24e33444c63afa9f
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-13 09:31:26 +0000

    //for batch profile update(event/contribution) we only allow pure profiles //CRM-1681
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@8868 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -1665,7 +1665,7 @@ SELECT g.* from civicrm_uf_group g, civicrm_uf_join j
         $profiles = array();
         $ufGroups = CRM_Core_PseudoConstant::ufgroup( );
         foreach ($ufGroups as $id => $title) {
-            $ptype = CRM_Core_BAO_UFField::getProfileType($id);
+            $ptype = CRM_Core_BAO_UFField::getProfileType($id, false);
             if ( in_array ($ptype, $types) ) {
                 $profiles[$id] = $title;
             }
