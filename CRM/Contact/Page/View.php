dd7ce9e8f876f1b26f9fd68cce616fb66c222f49
junia <junia@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-29 12:19:27 +0000

    Modified header
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@9100 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Page/View.php
+++ CRM/Contact/Page/View.php
@@ -1,9 +1,10 @@
 <?php
+
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 1.7                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2007                                  |
+ | Copyright CiviCRM LLC (c) 2004-2007                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -17,11 +18,10 @@
  | See the Affero General Public License for more details.            |
  |                                                                    |
  | You should have received a copy of the Affero General Public       |
- | License along with this program; if not, contact the Social Source |
- | Foundation at info[AT]civicrm[DOT]org.  If you have questions       |
- | about the Affero General Public License or the licensing  of       |
- | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
- | http://www.civicrm.org/licensing/                                  |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org.  If you have questions about the       |
+ | Affero General Public License or the licensing  of CiviCRM,        |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
 */
 
@@ -32,7 +32,7 @@
  * $Id$
  *
  */
-
+              
 require_once 'CRM/Core/Page.php';
 require_once 'CRM/Core/BAO/CustomGroup.php';
 require_once 'CRM/Core/BAO/CustomOption.php';
