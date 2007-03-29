bd68315fd85d29dce5fd5611d9f5b81620bb7234
sameer <sameer@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-29 11:17:13 +0000

    v1.7 Header changes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@9093 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/Address.php
+++ CRM/Utils/Address.php
@@ -1,9 +1,10 @@
 <?php
+
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 1.7                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2007                                  |
+ | Copyright CiviCRM LLC (c) 2004-2007                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -17,21 +18,13 @@
  | See the Affero General Public License for more details.            |
  |                                                                    |
  | You should have received a copy of the Affero General Public       |
- | License along with this program; if not, contact the Social Source |
- | Foundation at info[AT]civicrm[DOT]org.  If you have questions       |
- | about the Affero General Public License or the licensing  of       |
- | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
- | http://www.civicrm.org/licensing/                                  |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org.  If you have questions about the       |
+ | Affero General Public License or the licensing  of CiviCRM,        |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
 */
 
-/**
- *
- * @package CRM
- * @copyright CiviCRM LLC (c) 2004-2007
- * $Id$
- *
- */
 
 /**
  * Address utilties
--- CRM/Utils/Array.php
+++ CRM/Utils/Array.php
@@ -1,9 +1,11 @@
 <?php
+
+
 /*
  +--------------------------------------------------------------------+
  | CiviCRM version 1.7                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2007                                  |
+ | Copyright CiviCRM LLC (c) 2004-2007                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -17,23 +19,21 @@
  | See the Affero General Public License for more details.            |
  |                                                                    |
  | You should have received a copy of the Affero General Public       |
- | License along with this program; if not, contact the Social Source |
- | Foundation at info[AT]civicrm[DOT]org.  If you have questions       |
- | about the Affero General Public License or the licensing  of       |
- | of CiviCRM, see the Social Source Foundation CiviCRM license FAQ   |
- | http://www.civicrm.org/licensing/                                  |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org.  If you have questions about the       |
+ | Affero General Public License or the licensing  of CiviCRM,        |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
 */
 
 /**
- * Simple wrapper class to add a few essential core functions to
- * arrays in PHP
  *
  * @package CRM
  * @copyright CiviCRM LLC (c) 2004-2007
  * $Id$
  *
  */
+
 
 class CRM_Utils_Array {
 
