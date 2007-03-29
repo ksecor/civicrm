2bc2bc1f57c464546ae40ebf9eec253a0d9cfd3b
rupam <rupam@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-03-29 12:02:47 +0000

    change in header
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@9097 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Member/Import/Form/MapField.php
+++ CRM/Member/Import/Form/MapField.php
@@ -18,11 +18,10 @@
  | See the Affero General Public License for more details.            |
  |                                                                    |
  | You should have received a copy of the Affero General Public       |
- | License along with this program; if not, contact the Social Source |
- | Foundation at info[AT]civicrm[DOT]org.  If you have questions      |
- | about the Affero General Public License or the licensing  of       |
- | CiviCRM, see the CiviCRM license FAQ at                            |
- | http://civicrm.org/licensing/                                      |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org.  If you have questions about the       |
+ | Affero General Public License or the licensing  of CiviCRM,        |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
 */
 
