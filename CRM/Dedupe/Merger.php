8ad2859bf4e21f0edd90b9df0a2406be9d45fec8
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-04-22 22:09:31 +0000

    further CRM_Dedupe_Merger works
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9290 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Dedupe/Merger.php
+++ CRM/Dedupe/Merger.php
@@ -34,18 +34,30 @@
 
 class CRM_Dedupe_Merger
 {
+    // FIXME: this should be auto-generated from the schema
+    static $validFields = array(
+        'household'    => array('household_name'),
+        'organization' => array('organization_name', 'legal_name', 'sic_code'),
+        'individual'   => array('first_name', 'middle_name', 'last_name',
+            'prefix_id', 'suffix_id', 'greeting_type', 'custom_greeting',
+            'job_title', 'gender_id', 'birth_date', 'is_deceased', 'deceased_date'),
+        'contact'      => array('do_not_email', 'do_not_phone', 'do_not_mail',
+            'legal_identifier', 'external_identifier', 'home_URL', 'image_URL',
+            'preferred_communication_method', 'preferred_mail_format',
+            'do_not_trade', 'is_opt_out', 'source'),
+    );
+
     /**
      * Based on the provided two contact_ids and a set of tables, move the 
      * belongings of the other contact to the main one.
      */
-    function moveContactBelongings($mainId, $otherId, $tables = array())
+    function moveContactBelongings($mainId, $otherId, $delete = false, $tables = array())
     {
         // FIXME: this should be generated dynamically from the schema's 
         // foreign keys referencing civicrm_contact(id)
         static $cidRefs = array(
-            'civicrm_household'               => array('contact_id', 'primary_contact_id'),
-            'civicrm_individual'              => array('contact_id'),
-            'civicrm_organization'            => array('contact_id', 'primary_contact_id'),
+            'civicrm_household'               => array('primary_contact_id'),
+            'civicrm_organization'            => array('primary_contact_id'),
             'civicrm_contribution_recur'      => array('contact_id'),
             'civicrm_sms_history'             => array('contact_id'),
             'civicrm_activity'                => array('source_contact_id'),
@@ -67,10 +79,12 @@ class CRM_Dedupe_Merger
             'civicrm_mailing_event_queue'     => array('contact_id'),
             'civicrm_contribution'            => array('contact_id', 'solicitor_id', 'honor_contact_id'),
         );
-        // if we ever referenced civicrm_{household,individual,organization}(id)
+        // if we ever reference civicrm_{household,individual,organization}(id)
         // we should define here further reference arrays
+        
+        // FIXME: handle civicrm_{household,individual,organization}(contact_id) sanely
 
-        // get the affected tables and sanitize everything for SQL
+        // get the affected tables and sanitize ids for SQL
         $affected = array_keys($cidRefs);
         if ($tables) $affected = array_intersect($affected, $tables);
         $mainId  = (int) $mainId;
@@ -82,7 +96,52 @@ class CRM_Dedupe_Merger
             }
         }
 
-        // call the SQLs in one transaction
+        // call the SQL queries in one transaction
+        $dao =& new CRM_Core_DAO();
+        $dao->transaction('BEGIN');
+        foreach ($sqls as $sql) {
+            $dao->query($sql);
+        }
+        $dao->transaction('COMMIT');
+    }
+
+    /**
+     * Find differences between contacts.
+     */
+    function findDifferences($mainId, $otherId)
+    {
+        require_once 'api/Contact.php';
+        $main  = crm_get_contact(array('contact_id' => (int) $mainId));
+        $other = crm_get_contact(array('contact_id' => (int) $otherId));
+        if ($main->contact_type != $other->contact_type) {
+            return false;
+        }
+        $cType = strtolower($main->contact_type);
+
+        $diffs = array();
+        foreach (self::$validFields['contact'] as $validField) {
+            if ($main->$validField != $other->$validField) {
+                $diffs['contact'][] = $validField;
+            }
+        }
+        foreach (self::$validFields[$cType] as $validField) {
+            if ($main->contact_type_object->$validField != $other->contact_type_object->$validField) {
+                $diffs[$cType][] = $validField;
+            }
+        }
+        foreach ($main->custom_values as $mainCV) {
+            foreach ($other->custom_values as $otherCV) {
+                if ($mainCV['custom_field_id'] == $otherCV['custom_field_id']) {
+                    foreach ($mainCV as $key => $value) {
+                        if (substr($key, -5) == '_data' and $mainCV[$key] != $otherCV[$key]) {
+                            $diffs['custom'][] = $mainCV['custom_field_id'];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $diffs;
     }
 }
 
