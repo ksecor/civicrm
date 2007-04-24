be16077931234fe690b70b395d373da80547f414
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-04-24 21:29:02 +0000

    further CRM_Contact_Form_Merge works
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9313 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Merge.php
+++ CRM/Contact/Form/Merge.php
@@ -37,42 +37,63 @@ require_once 'CRM/Core/Form.php';
 
 class CRM_Contact_Form_Merge extends CRM_Core_Form
 {
+    var $_defaults = array();
+
     function preProcess()
     {
         require_once 'api/Contact.php';
         require_once 'api/Search.php';
         require_once 'CRM/Dedupe/Merger.php';
-        $cid = CRM_Utils_Request::retrieve('cid', 'Positive', $this, false);
-        $oid = CRM_Utils_Request::retrieve('oid', 'Positive', $this, false);
+        $cid   = CRM_Utils_Request::retrieve('cid', 'Positive', $this, false);
+        $oid   = CRM_Utils_Request::retrieve('oid', 'Positive', $this, false);
+        $diffs = CRM_Dedupe_Merger::findDifferences($cid, $oid);
         $main  = crm_get_contact(array('contact_id' => $cid));
         $other = crm_get_contact(array('contact_id' => $oid));
-        $diffs = CRM_Dedupe_Merger::findDifferences($cid, $oid);
+        $this->assign('contact_type', $main->contact_type);
         $this->assign('main_name',    $main->display_name);
         $this->assign('other_name',   $other->display_name);
-        $this->assign('contact_type', $main->contact_type);
-        $this->assign('diffs',        $diffs);
 
         foreach (array('Contact', $main->contact_type) as $ct) {
             require_once "CRM/Contact/DAO/$ct.php";
             eval("\$fieldNames['$ct'] =& CRM_Contact_DAO_$ct::fields();");
         }
 
-        foreach ($diffs['Contact'] as $field) {
-            $group['main']  = HTML_QuickForm::createElement('radio', null, null, $main->$field,  'main');
-            $group['other'] = HTML_QuickForm::createElement('radio', null, null, $other->$field, 'other');
-            $this->addGroup($group, $field, $fieldNames['Contact'][$field]['title']);
-        }
         foreach ($diffs[$main->contact_type] as $field) {
-            $group['main']  = HTML_QuickForm::createElement('radio', null, null, $main->contact_type_object->$field, 'main');
-            $group['other'] = HTML_QuickForm::createElement('radio', null, null, $other->contact_type_object->$field, 'other');
+            $rows[]  = $field;
+            $this->_defaults["{$field}[column]"] = $main->contact_type_object->$field;
+            $group['main']  = HTML_QuickForm::createElement('radio', 'column', null, $main->contact_type_object->$field,  $main->contact_type_object->$field);
+            $group['other'] = HTML_QuickForm::createElement('radio', 'column', null, $other->contact_type_object->$field, $other->contact_type_object->$field);
             $this->addGroup($group, $field, $fieldNames[$main->contact_type][$field]['title']);
         }
+        foreach ($diffs['Contact'] as $field) {
+            $rows[]  = $field;
+            $this->_defaults["{$field}[column]"] = $main->$field;
+            $group['main']  = HTML_QuickForm::createElement('radio', 'column', null, $main->$field,  $main->$field);
+            $group['other'] = HTML_QuickForm::createElement('radio', 'column', null, $other->$field, $other->$field);
+            $this->addGroup($group, $field, $fieldNames['Contact'][$field]['title']);
+        }
+        foreach (array('main', 'other') as $moniker) {
+            $contact =& $$moniker;
+            foreach ($contact->custom_values as $cv) {
+                if (in_array($cv['custom_field_id'], $diffs['custom'])) {
+                    $customValues[$moniker][$cv['custom_field_id']] = $cv['value'];
+                    $customLabels[$moniker][$cv['custom_field_id']] = CRM_Core_BAO_CustomOption::getOptionLabel($id, $cv['value']);
+                }
+            }
+        }
+        foreach ($diffs['custom'] as $id) {
+            $rows[] = "custom_$id";
+            $this->_defaults["custom_{$id}[column]"] = $customValues['main'][$id];
+            $group['main']  = HTML_QuickForm::createElement('radio', 'column', null, $customLabels['main'][$id],  $customValues['main'][$id]);
+            $group['other'] = HTML_QuickForm::createElement('radio', 'column', null, $customLabels['other'][$id], $customValues['other'][$id]);
+            $this->addGroup($group, "custom_$id", CRM_Core_BAO_CustomField::getTitle($id));
+        }
+        $this->assign('rows', $rows);
     }
     
     function setDefaultValues()
     {
-        $defaults = array();
-        return $defaults;
+        return $this->_defaults;
     }
     
     function addRules()
--- templates/CRM/Contact/Form/Merge.tpl
+++ templates/CRM/Contact/Form/Merge.tpl
@@ -1,10 +1,13 @@
 <div class='spacer'></div>
 <table>
   <tr><th></th><th>{$main_name}</th><th>{$other_name}</th></tr>
-  {foreach from=$diffs key=type item=group}
-    {foreach from=$group item=field}
-      <tr><th>{$form.$field.label}</th><td>{$form.$field.main.html}</td><td>{$form.$field.other.html}</td></tr>
-    {/foreach}
+  {foreach from=$rows item=field}
+    <tr>
+      <th>{$form.$field.label}</th>
+      {foreach from=$form.$field.column item=column}
+        <td>{$column.html}</td>
+      {/foreach}
+    </tr>
   {/foreach}
 </table>
 <div class='form-item'>
