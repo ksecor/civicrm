bbbd944f88a47dc523ca6c8e9dde7d695ecaa7b4
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-05-01 22:59:54 +0000

    Making unwanted Quest code conditional
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.7@9363 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Component.php
+++ CRM/Core/Component.php
@@ -330,16 +330,19 @@ class CRM_Core_Component
     {
         if ( self::$_contactSubTypes == null ) {
             self::$_contactSubTypes = array( );
+
+            if ( CRM_Core_Permission::access( 'Quest' ) ) {
             
-            //Generalize this at some point
-            //self::$_contactSubTypes =
-            //    array(
-            //         'Student' =>
-            //          array( 'View' => 
-            //                 array( 'file'  => 'CRM/Quest/Page/View/Student.php',
-            //                        'class' => 'CRM_Quest_Page_View_Student' ),
-            //                 )
-            //          );
+            // Generalize this at some point
+            self::$_contactSubTypes =
+                array(
+                     'Student' =>
+                      array( 'View' => 
+                             array( 'file'  => 'CRM/Quest/Page/View/Student.php',
+                                    'class' => 'CRM_Quest_Page_View_Student' ),
+                             )
+                      );
+            }
         }
         return self::$_contactSubTypes;
     }
