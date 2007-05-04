23a9162a760569bf8851bccdafb517a7ba7277a5
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-05-04 14:09:20 +0000

    New testing infrastructure in progress. If you want to try it out, change CIVICRM_TEST_DIR in civicrm.settings.php to test-new and run 'php SimpleTest.php' in the command line in SimpleTest directory.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9398 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ test-new/SimpleTest/SimpleTest.php
@@ -0,0 +1,284 @@
+<?php
+require_once '../../civicrm.config.php';
+
+require_once 'SimpleTest/unit_tester.php';
+require_once 'SimpleTest/reporter.php';
+
+if ( !defined( 'TEST' ) ) {
+    define( 'TEST', __FILE__ );
+}
+
+class VerboseHtmlReporter extends HtmlReporter {
+    
+    function VerboseHtmlReporter() {
+        $this->HtmlReporter();
+    }
+
+    function paintPass($message) {
+        // TBD
+    }
+}
+
+class VerboseTextReporter extends TextReporter {
+
+    function VerboseTextReporter() {
+        $this->TextReporter();
+    }
+
+    function paintPass($message) {
+        parent::paintPass($message);
+        
+        $out = "Assertion passed: ";
+        $currentTest = $this->getTestList();
+        // removing test suite name, not needed
+        array_shift( $currentTest );
+        // get filename from testcase path
+        $out .= array_pop( explode( '/', $currentTest[0] ) );
+        // get test method name
+        $out .= '->' . array_pop( $currentTest );
+
+        print "$out\n";
+    }
+}                                                                                
+
+
+abstract class CRMUnitTests extends GroupTest {
+
+
+    // collects test files from given dir
+    function addTestDirectory( $dir ) {
+
+        $files = scandir( $dir );
+        
+        foreach ( $files as $file ) {
+            // shouldn't be directory or backup file...
+            if ( !is_dir($file) && $file[strlen($file)-1] !== '~' ) { 
+                 $this->addTestFile( "$dir/$file" );
+            }        
+        }
+
+    }
+}
+
+class ApiV2Tests extends CRMUnitTests {
+    
+    function ApiV2Tests() {
+        $this->GroupTest( 'Unit Tests for API v2' );
+        $this->addTestDirectory( CIVICRM_TEST_DIR . 'SimpleTest/api-v2' );        
+    }
+}
+
+
+class ApiTests extends CRMUnitTests {
+    
+    function ApiTests() {
+        $this->GroupTest( 'Unit Tests for API' );
+        $this->addTestDirectory( CIVICRM_TEST_DIR . 'SimpleTest/api' );
+        // contact api
+        // MOVED to API $this->addTestFile( CIVICRM_TEST_DIR . 'UnitTests/api/CreateContact.php' );
+        // MOVED to API $this->addTestFile( CIVICRM_TEST_DIR . 'UnitTests/api/GetContact.php'    );
+        // MOVED to API $this->addTestFile( CIVICRM_TEST_DIR . 'UnitTests/api/FetchContact.php'    );
+        // MOVED to API $this->addTestFile( CIVICRM_TEST_DIR . 'UnitTests/api/UpdateContact.php' );
+        // MOVED tp API $this->addTestFile( CIVICRM_TEST_DIR . 'UnitTests/api/DeleteContact.php'    );
+        // $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateContactWithCustomValues.php' );
+        // FAULTY $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateContactWithCustomValues.php' );
+        
+        // WHAT KIND OF TEST IS THIS!? $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContactFlat.php'    );
+        // THIS IS NOT A UNIT TEST! $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContactCustom.php'    );
+        // THIS IS NOT A UNIT TEST! $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/Search.php'    );
+        // THIS IS NOT A UNIT TEST! $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContactHierarchical.php'    );
+        // THIS IS NOT A UNIT TEST! $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContactGroups.php' );
+        // FAILS $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM619.php' );
+        // DOESN EXIST! $this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM600.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM558.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM562.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM39.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM2474.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM491.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM503.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM514.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM520.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM521.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM522.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM523.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM531.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM627.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM645.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM707.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM778.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM703.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM785.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM787.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM764.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM652.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM825.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM878.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM881Get.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM881Delete.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM881Update.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM922.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM966.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM980.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM983.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1011.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1012.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1184.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1233.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1469.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1584.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1657.php' );
+       
+        //api for ActivityType
+        
+        
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/MultiValuedCheckBox.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/Note.php'    );
+
+        //api for Contribution
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateContribution.php'      );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContribution.php'      );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateContribution.php'      );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteContribution.php'      );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContributions.php'      );
+        
+        // group api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/AddGroupContact.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteGroupContact.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetGroups.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetGroupContacts.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/SubscribeGroupContacts.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/ConfirmGroupContacts.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/getClassProperties.php'    );
+        
+        // location api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateLocation.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetLocation.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateLocation.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CRM1282UpdateLocation.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteLocation.php'    );
+        
+
+        // membership type api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateMembershipType.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetMembershipTypes.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateMembershipType.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteMembershipType.php' );
+
+
+         // membership status api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateMembershipStatus.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetMembershipStatuses.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateMembershipStatus.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteMembershipStatus.php' );
+
+        // membership 
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateContactMembership.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetContactMemberships.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateContactMembership.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteMembership.php' );
+
+        // history api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateActivityHistory.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetActivityHistory.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateActivityHistory.php' );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteActivityHistory.php' );
+        
+        // custom group api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateCustomGroup.php');
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateCustomGroup.php');
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateCustomField.php');
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateCustomField.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateCustomValue.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetCustomField.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetCustomOptionValue.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/UpdateDeleteCustomOption.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteCustomGroup.php');
+         
+        //note api
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateNote.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetNote.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/UpdateNote.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteNote.php');
+
+        //file api
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateFile.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetFile.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/UpdateFile.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteFile.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateEntityFile.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetFilesByEntity.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteEntityFile.php');
+        
+        // relationship api
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateRelationship.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetRelationship.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteRelationship.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateRelationshipType.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetRelationshipType.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/UpdateRelationship.php');
+        
+        // tag api
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateTag.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteTag.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/CreateEntityTag.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/TagsByEntity.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetEntitiesByTag.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/DeleteEntityTag.php');
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/GetTag.php');
+        
+        //Participant api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateParticipant.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetParticipants.php'      );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateParticipant.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteParticipant.php'    );
+        
+        //Participant Payment api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateParticipantPayment.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateParticipantPayment.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteParticipantPayment.php'    );
+        
+        // UFGroup api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateUFGroup.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateUFGroup.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateUFField.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateUFField.php'    );
+        
+        //$this->addTestFile(CIVICRM_TEST_DIR . 'CRM/api/Token.php' );
+
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateProfileContact.php'    );
+        
+        //Event api
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/CreateEvent.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/GetEvent.php'       );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/UpdateEvent.php'    );
+        //$this->addTestFile( CIVICRM_TEST_DIR . 'CRM/api/DeleteEvent.php'    );
+
+    }
+
+}
+
+function user_access( $str ) {
+    return true;
+}
+
+function module_list( ) {
+    return array( );
+}
+
+if ( TEST == __FILE__ ) {
+
+    require_once 'CRM/Core/Config.php';
+    $test =& new ApiTests( );
+    $test2 =& new ApiV2Tests( );
+
+    $config =& CRM_Core_Config::singleton();
+
+    if (SimpleReporter::inCli()) {
+        $test->run(new VerboseTextReporter());
+        $test2->run(new VerboseTextReporter());
+        exit();
+    }
+    $test->run(new VerboseHtmlReporter());
+}
+
+?>
---
+++ test-new/SimpleTest/api-v2/Contact.php
@@ -0,0 +1,183 @@
+<?php
+
+require_once 'api/v2/Contact.php';
+
+class TestOfContactAPIV2 extends UnitTestCase 
+{
+    /**
+     * Assume empty database with just civicrm_data
+     */
+    protected $_contacts   = array();
+    
+    function setUp() 
+    {
+        // make sure this is just _41 and _data
+    }
+    
+    function tearDown() 
+    {
+    }
+    
+    function testCreateEmptyContact() 
+    {
+        $params = array();
+        $contact =& civicrm_contact_add($params);
+        $this->assertEqual( $contact['is_error'], 1 );
+    }
+    
+    function testCreateBadTypeContact()
+    {
+        $params = array('email' => 'man1@yahoo.com',
+                        'contact_type' => 'Does not Exist' );
+        $contact =& civicrm_contact_add($params);
+        $this->assertEqual( $contact['is_error'], 1 );
+    }
+    
+    function testCreateBadRequiredFieldsIndividual() 
+    {
+        $params = array('middle_name' => 'This field is not required',
+                        'contact_type' => 'Individual' );
+        $contact =& civicrm_contact_add($params);
+        $this->assertEqual( $contact['is_error'], 1 );
+    }
+    
+    function testCreateBadRequiredFieldsHousehold() 
+    {
+        $params = array('middle_name' => 'This field is not required',
+                        'contact_type' => 'Household' );
+        $contact =& civicrm_contact_add($params);
+        $this->assertEqual( $contact['is_error'], 1 );
+    }
+    
+    function testCreateBadRequiredFieldsOrganization() 
+    {
+        $params = array('middle_name' => 'This field is not required',
+                        'contact_type' => 'Organization' );
+        $contact =& civicrm_contact_add($params);
+        $this->assertEqual( $contact['is_error'], 1 );
+    }
+    
+    function testCreateEmailIndividual() 
+    {
+        $params = array('email'            => 'man2@yahoo.com',
+                        'contact_type'     => 'Individual',
+                        'location_type_id' => 1,
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+
+    function testCreateNameIndividual() 
+    {
+        $params = array('first_name' => 'abc1',
+                        'contact_type'     => 'Individual',
+                        'last_name' => 'xyz1'
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateNameHousehold() 
+    {
+        $params = array('household_name' => 'The abc Household',
+                        'contact_type' => 'Household',
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateNameOrganization() 
+    {
+        $params = array('organization_name' => 'The abc Organization',
+                        'contact_type' => 'Organization',
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateIndividualwithEmail() 
+    {
+        $params = array('first_name' => 'abc3',
+                        'last_name'  => 'xyz3',
+                        'contact_type'     => 'Individual',
+                        'email'      => 'man3@yahoo.com'
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateIndividualwithEmailLocationType() 
+    {
+        $params = array('first_name'    => 'abc4',
+                        'last_name'     => 'xyz4',
+                        'email'         => 'man4@yahoo.com',
+                        'contact_type'     => 'Individual',
+                        'location_type_id' => 2,
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateIndividualwithPhone() 
+    {
+        $params = array('first_name'    => 'abc5',
+                        'last_name'     => 'xyz5',
+                        'contact_type'     => 'Individual',
+                        'location_type_id' => 2,
+                        'phone'         => '11111',
+                        'phone_type'    => 'Phone'
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateIndividualwithAll() 
+    {
+        $params = array('first_name'    => 'abc7',
+                        'last_name'     => 'xyz7', 
+                        'contact_type'     => 'Individual',
+                        'phone'         => '999999',
+                        'phone_type'    => 'Phone',
+                        'email'         => 'man7@yahoo.com'
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+    
+    function testCreateHouseholdDetails() 
+    {
+        $params = array('household_name' => 'abc8\'s House',
+                        'nick_name'      => 'x House',
+                        'email'          => 'man8@yahoo.com',
+                        'contact_type'     => 'Household',
+                        );
+        $contact =& civicrm_contact_add($params);
+        $this->assertNotNull( $contact['contact_id'] );
+        $this->_contacts[] = $contact['contact_id'];
+    }
+
+    function testDeleteContacts() 
+    {
+        foreach ($this->_contacts as $id) {
+            $params = array( 'contact_id' => $id );
+            $result = civicrm_contact_delete( $params );
+            $this->assertEqual( $result['is_error'], 0 );
+        }
+        
+        // delete an unknown id
+        $params = array( 'contact_id' => 1000567 );
+        $result = civicrm_contact_delete( $params );
+        $this->assertEqual( $result['is_error'], 1 );
+    }
+
+}
+
+?>
