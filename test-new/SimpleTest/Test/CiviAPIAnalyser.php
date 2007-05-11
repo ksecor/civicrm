c74828bca58d8a09e103c13443a71c8b39ba56d3
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-05-11 12:32:40 +0000

    first draft of testcase generator
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9512 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test-new/SimpleTest/Test/CiviAPIAnalyser.php
+++ test-new/SimpleTest/Test/CiviAPIAnalyser.php
@@ -12,7 +12,7 @@ class APImethod {
     
     function __construct( $rawName ) {
 
-        $this->isByReference = ( ( strpos($rawName, '&') === false ) ? 0 : 1 );
+        $this->isByReference = ( ( strpos($rawName, '&') === FALSE ) ? 0 : 1 );
         // trimming whitespace should have happend in _getAPIMethodsFromFile
         // but I'm not a big fan of regexps and didn't know how to do it
         $rawName = trim( $rawName );
@@ -94,43 +94,113 @@ class APIAnalyser {
     public function getAPI() {
         return $this->api;
     }
-    
-    function getAPIGroupNames() {
+
+    function getAPIGroups() {
         $a = array();
-        foreach ( $this->api as $apiGroup ) {
-            $a[] = $apiGroup->name;
+        foreach( $this->api as $apiGroup ) {
+            $a[] = $apiGroup;
         }
         return $a;
     }
     
+    function getAllAPIMethods() {
+        $m = array();
+        foreach( $this->getAPIGroups() as $group ) {
+            foreach( $group->methods as $method ) {
+            $m[] = $method;
+            }
+        }
+        return $m;
+    }
+
 }
-
-
-
 
 class APITestGenerator {
 
-    function CiviAPIAnalyser() {
+    private $_analyser = null;
+    private $_targetDir = null;
+
+    public function __construct( $analyser) {
+        $this->_analyser = $analyser;
+        $this->_targetDir = '/tmp';
     }
 
+    private function _getTestcaseName( $method ) {
+                $testcaseName = '';
 
-    function _getAPIFiles() {
-        
-    }
+                // private methods get different prefix
+                // leading underscore will disappear on
+                // explode a few lines below
+                if( $method->isPrivate ) {
+                    $testcaseName .= "testPrivate";
+                } else {
+                    $testcaseName .= "testPublic";
+                }
 
-    function getAPI() {
+                // uppercase words in function name
+                $n = explode( '_', $method->name);
+                foreach( $n as $key => $namePart ) {
+                    $n[$key] = ucwords( $namePart);
+                }
+
+                // merge
+                $testcaseName .= implode( $n );
+
+                return $testcaseName;
     }
     
+    
+    private function _createTestFile( $testcaseName ) {
+        $testFilename = $testcaseName . '.php';
+        $fullpath = $this->_targetDir . DIRECTORY_SEPARATOR . $testFilename;
+        if( file_exists( $fullpath ) === TRUE ) {
+            return FALSE;
+        } else {
+            if( ( $handle = fopen( $fullpath, "x" ) ) === FALSE ) {
+                trigger_error("Cannot open file for writing: $fullpath ", E_USER_ERROR );
+            } else {
+                fwrite( $handle, $this->_getTestcaseTemplate( $testcaseName ) );
+                fclose( $handle );
+                return TRUE;
+            }
+        }
+
+    }
+
+
+    private function _getTestcaseTemplate( $testcaseName ) {
+        return "TBD - doing $testcaseName";
+    }
+
+    public function setTargetDir( $dir ) {
+        if ( file_exists( $dir ) === TRUE ) {
+            $this->_targetDir = $dir;
+        } else {
+            trigger_error("Target directory ($dir) doesn't exist", E_USER_ERROR );
+        }
+    }
+
+    public function getTargetDir() {
+        return $this->_targetDir;
+    }
+
+    public function buildTests() {
+        foreach( $this->_analyser->getAllAPIMethods() as $method ) {
+            $tn = $this->_getTestcaseName( $method );
+            if ( $this->_createTestFile( $tn ) ) {
+                echo "CREATED $tn.php\n";                
+            } else {
+                echo "Skipping $tn.php - file exists\n";
+            }
+        }
+    }
 
 }
 
 $a = new APIAnalyser( '/home/mover/Work/CiviCRM/trunk/api/v2' );
-foreach( $a->api as $group ) {
-    echo $group;
-    foreach( $group->methods as $method ) {
-        echo $method;
-    }
-}
+$t = new APITestGenerator( $a );
+$t->setTargetDir('/tmp/tests/');
+$t->buildTests();
 
 
 ?>
