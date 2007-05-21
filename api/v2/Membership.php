4674f2f0376917f759da2a5c5d22b2c8c6686653
swati <swati@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-05-21 09:49:12 +0000

    Worked on CRM-1898
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9589 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- api/v2/Membership.php
+++ api/v2/Membership.php
@@ -86,7 +86,7 @@ function civicrm_membership_type_create($params)
         return civicrm_create_error( "Membership is not created" );
     } else {
         $member = array();
-        _civicrm_object_to_array($membershipTypeBAO, $member);
+        _civicrm_object_to_array($membershipTypeBAO, $member);//CRM_CORE_ERROR::DEBUG('membershipe',$member);
         $values = array( );
         $values['member_id'] = $member['id'];
         $values['is_error']   = 0;
@@ -95,5 +95,262 @@ function civicrm_membership_type_create($params)
     return $values;
 }
 
+/**
+ * Update an existing membership type
+ *
+ * This api is used for updating an existing membership type.
+ * Required parrmeters : id of a membership type
+ * 
+ * @param  Array   $params  an associative array of name/value property values of civicrm_membership_type
+ * 
+ * @return array of updated membership type property values
+ * @access public
+ */
+function &civicrm_membership_type_update( $params ) {
+    if ( !is_array( $params ) ) {
+        return civicrm_create_error( 'Params is not an array' );
+    }
+    
+    if ( !isset($params['id']) ) {
+        return civicrm_create_error( 'Required parameter missing' );
+    }
+    
+    require_once 'CRM/Member/BAO/MembershipType.php';
+    $membershipTypeBAO =& new CRM_Member_BAO_MembershipType( );
+    $membershipTypeBAO->id = $params['id'];
+    if ($membershipTypeBAO->find(true)) {
+        $fields = $membershipTypeBAO->fields( );
+        foreach ( $fields as $name => $field) {
+            if (array_key_exists($name, $params)) {
+                $membershipTypeBAO->$name = $params[$name];
+            }
+        }
+        $membershipTypeBAO->save();
+    }
+    
+    $membershipType = array();
+    _civicrm_object_to_array( $membershipTypeBAO, $membershipType );
+    $membershipTypeBAO->free( );
+    return $membershipType;
+}
+
+/**
+ * Deletes an existing membership type
+ * 
+ * This API is used for deleting a membership type
+ * 
+ * @param  Int  $membershipTypeID    ID of membership type to be deleted
+ * 
+ * @return boolean        true if success, else false
+ * @access public
+ */
+function &civicrm_membership_type_delete( $membershipTypeID ) {
+    if ( ! $membershipTypeID ) {
+        return civicrm_create_error( 'Invalid value for membershipTypeID' );
+    }
+    require_once 'CRM/Member/BAO/MembershipType.php';
+    $memberDelete = CRM_Member_BAO_MembershipType::del($membershipTypeID);
+    return $memberDelete ? null : civicrm_create_error('Error while deleting membership type');
+}
+
+/**
+ * Create a Membership Status
+ *  
+ * This API is used for creating a Membership Status
+ * 
+ * @param   array  $params  an associative array of name/value property values of civicrm_membership_status
+ * @return array of newly created membership status property values.
+ * @access public
+ */
+function civicrm_membership_status_create($params) 
+{
+    _civicrm_initialize();
+    if ( ! is_array($params) ) {
+        return _civicrm_create_error('Params is not an array.');
+    }
+    
+    if ( empty($params) ) {
+        return _civicrm_error('Params can not be empty.');
+    }
+    
+    if (! $params["name"] ) {
+        return _civicrm_error('Missing require fileds');
+    }
+    
+    if ( !$params['domain_id'] ) {
+        require_once 'CRM/Core/Config.php';
+        $config =& CRM_Core_Config::singleton();
+        $params['domain_id'] = $config->domainID();
+    }
+    
+    require_once 'CRM/Member/BAO/MembershipStatus.php';
+    $ids = array();
+    $membershipStatusBAO = CRM_Member_BAO_MembershipStatus::add($params, $ids);
+    $membershipStatus = array();
+    _civicrm_object_to_array($membershipStatusBAO, $membershipStatus);
+    
+    return $membershipStatus;
+}
+
+/**
+ * Get a membership status.
+ * 
+ * This api is used for finding an existing membership status.
+ * Required parrmeters : id of a membership status
+ * 
+ * @params  array $params  an associative array of name/value property values of civicrm_membership_status
+ *
+ * @return  Array of all found membership status property values.
+ * @access public
+ */
+function civicrm_membership_statuses_get($params) 
+{
+    _civicrm_initialize();
+    if ( ! is_array($params) ) {
+        return _civicrm_create_error('Params is not an array.');
+    }
+    
+    if ( ! isset($params['id'])) {
+        return _civicrm_create_error('Required parameters missing.');
+    }
+    
+    require_once 'CRM/Member/BAO/MembershipStatus.php';
+    $membershipStatusBAO = new CRM_Member_BAO_MembershipStatus();
+    
+    $properties = array_keys($membershipStatusBAO->fields());
+    
+    foreach ($properties as $name) {
+        if (array_key_exists($name, $params)) {
+            $membershipStatusBAO->$name = $params[$name];
+        }
+    }
+    
+    if ( $membershipStatusBAO->find() ) {
+        $membershipStatus = array();
+        while ( $membershipStatusBAO->fetch() ) {
+            _civicrm_object_to_array( clone($membershipStatusBAO), $membershipStatus );
+            $membershipStatuses[$membershipStatusBAO->id] = $membershipStatus;
+        }
+    } else {
+        return _civicrm_error('Exact match not found');
+    }
+    return $membershipStatuses;
+}
+
+/**
+ * Update an existing membership status
+ *
+ * This api is used for updating an existing membership status.
+ * Required parrmeters : id of a membership status
+ * 
+ * @param  Array   $params  an associative array of name/value property values of civicrm_membership_status
+ * 
+ * @return array of updated membership status property values
+ * @access public
+ */
+function &civicrm_membership_status_update( $params ) 
+{
+    _civicrm_initialize();
+    if ( !is_array( $params ) ) {
+        return _civicrm_create_error( 'Params is not an array' );
+    }
+    
+    if ( !isset($params['id']) ) {
+        return _civicrm_create_error( 'Required parameter missing' );
+    }
+    
+    require_once 'CRM/Member/BAO/MembershipStatus.php';
+    $membershipStatusBAO =& new CRM_Member_BAO_MembershipStatus( );
+    $membershipStatusBAO->id = $params['id'];
+    if ($membershipStatusBAO->find(true)) {
+        $fields = $membershipStatusBAO->fields( );
+        foreach ( $fields as $name => $field) {
+            if (array_key_exists($name, $params)) {
+                $membershipStatusBAO->$name = $params[$name];
+            }
+        }
+        $membershipStatusBAO->save();
+    }
+    $membershipStatus = array();
+    _civicrm_object_to_array( clone($membershipStatusBAO), $membershipStatus );
+    return $membershipStatus;
+}
+
+/**
+ * Deletes an existing membership status
+ * 
+ * This API is used for deleting a membership status
+ * 
+ * @param  Int  $membershipStatusID   Id of the membership status to be deleted
+ * 
+ * @return null if successfull, object of CRM_Core_Error otherwise
+ * @access public
+ */
+function &civicrm_membership_status_delete( $membershipStatusID ) 
+{
+    _civicrm_initialize();
+    if ( empty($membershipStatusID) ) {
+        return _civicrm_create_error( 'Invalid value for membershipStatusID' );
+    }
+    
+    require_once 'CRM/Member/BAO/MembershipStatus.php';
+    CRM_Member_BAO_MembershipStatus::del($membershipStatusID);
+}
+
+/**
+ * Create a Contct Membership
+ *  
+ * This API is used for creating a Membership for a contact.
+ * Required parameters : membership_type_id and status_id.
+ * 
+ * @param   array  $params     an associative array of name/value property values of civicrm_membership
+ * @param   int    $contactID  ID of a contact
+ * 
+ * @return array of newly created membership property values.
+ * @access public
+ */
+function civicrm_contact_membership_create($params, $contactID)
+{
+    _civicrm_initialize();
+    if ( !is_array( $params ) ) {
+        return civicrm_create_error( 'Params is not an array' );
+    }
+    
+    if ( !isset($params['membership_type_id']) || !isset($params['status_id']) || empty($contactID)) {
+        return civicrm_create_error( 'Required parameter missing' );
+    }
+    
+    $values  = array( );   
+    $error = _civicrm_format_membership_params( $params, $values );
+    if (is_a($error, 'CRM_Core_Error') ) {
+        return $error;
+    }
+    $params = array_merge($values,$params);
+    $params['contact_id'] = $contactID;
+    
+    require_once 'CRM/Member/BAO/Membership.php';
+    $ids = array();
+    $membershipBAO = CRM_Member_BAO_Membership::create($params, $ids);
+    
+    if ( ! is_a( $membershipBAO, 'CRM_Core_Error') ) {
+        $relatedContacts = CRM_Member_BAO_Membership::checkMembershipRelationship( 
+                                                            $membershipBAO->id,
+                                                            $contactID,
+                                                            CRM_Core_Action::ADD
+                                                            );
+    }
+    
+    foreach ( $relatedContacts as $contactId ) {
+        $params['contact_id'         ] = $contactId;
+        $params['owner_membership_id'] = $membershipBAO->id;
+        unset( $params['id'] );
+        
+        CRM_Member_BAO_Membership::create( $params, CRM_Core_DAO::$_nullArray );
+    }
+    
+    $membership = array();
+    _civicrm_object_to_array($membershipBAO, $membership);
+    return $membership;
+}
 
 ?>
--- test/CRM/api/v2/MembershipType.php
+++ test/CRM/api/v2/MembershipType.php
@@ -119,4 +119,94 @@ class TestOfMembershipTypeAPIV2 extends UnitTestCase
         $this->_membershiptype = & civicrm_membership_type_create($params);
         $this->assertNotNull( $this->_membershiptype['member_id'] );   
     }
+    
+
+    function testUpdateWrongMembershipTypeEmptyParams()
+    {
+        $params = array();                        
+        $membershiptype = & civicrm_membership_type_update($params);
+        $this->assertEqual( $membershiptype['is_error'], 1 );
+    }
+
+
+    function testUpdateWrongMembershipTypeWithoutId()
+    {
+        $params = array(
+                        'name'                 => '60+ Membership',
+                        'description'          => 'people above 60 are given health instructions',
+                        'member_of_contact_id' => '33',
+                        'contribution_type_id' => '1',
+                        'minimum_fee'          => '1200',
+                        'duration_unit'        => 'month',
+                        'duration_interval'    => '10',
+                        'period_type'          => 'rolling',
+                        'visibility'           => 'public'
+                        );
+        
+        $membershiptype = & civicrm_membership_type_update($params);
+        $this->assertEqual( $membershiptype['is_error'], 1 );
+    }
+
+
+    function testUpdateMembershipType()
+    {
+        $params = array(
+                        'id'                        => $this->_membershiptype['member_id'],
+                        'name'                      => '30+ Membership',
+                        'description'               => 'people above 30 are given health instructions',
+                        'member_of_contact_id'      => '33',
+                        'contribution_type_id'      => '1',
+                        'minimum_fee'               => '200',
+                        'duration_unit'             => 'year',
+                        'duration_interval'         => '1',
+                        'period_type'               => 'fixed',
+                        'fixed_period_start_day'    => '101',
+                        'fixed_period_rollover_day' => '1231',
+                        'relationship_type_id'      => '7',
+                        'visibility'                => 'public',
+                        'is_active'                 => '1'
+                        );
+        
+        $membershiptype = & civicrm_membership_type_update($params);
+        $this->assertEqual($membershiptype['name'],'30+ Membership');
+        $this->assertEqual($membershiptype['description'],'people above 30 are given health instructions');
+        $this->assertEqual($membershiptype['member_of_contact_id'],'33');
+        $this->assertEqual($membershiptype['contribution_type_id'],'1');
+        $this->assertEqual($membershiptype['minimum_fee'],'200');
+        $this->assertEqual($membershiptype['duration_unit'],'year');
+        $this->assertEqual($membershiptype['duration_interval'],'1');
+        $this->assertEqual($membershiptype['period_type'],'fixed');
+        $this->assertEqual($membershiptype['fixed_period_start_day'],'101');
+        $this->assertEqual($membershiptype['fixed_period_rollover_day'],'1231');
+        $this->assertEqual($membershiptype['relationship_type_id'],'7');
+        $this->assertEqual($membershiptype['is_active'],'1');
+        $this->assertEqual($membershiptype['visibility'],'public');
+    }
+    function testDeleteBadMembershipTypeWrongId()
+    {
+        $id = -14588;
+        $val = &civicrm_membership_type_delete($id);
+    }
+
+    function testDeleteBadMembershipTypeWithoutId()
+    {
+        $val = &civicrm_membership_type_delete($this->_membershiptype['name']);
+    }
+
+    
+
+     function testDeleteBadMembershipTypeEmptyParam()
+    {
+        $param = array();
+        $val = &civicrm_membership_type_delete($param);
+    }
+
+   
+    function testDeleteMembershipType()
+    {
+        $val1 = &civicrm_membership_type_delete($this->_membershiptype1['member_id']);
+        $val2 = &civicrm_membership_type_delete($this->_membershiptype2['member_id']);
+        $val3 = &civicrm_membership_type_delete($this->_membershiptype['member_id']);
+
+    }
 }
