f616cc30eb7b0c6b0e8e3223b0581b2107026775
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-05-31 19:22:30 +0000

    Add links to Joomla installer display to online documentation. CRM-1950.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9727 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- joomla/install.civicrm.php
+++ joomla/install.civicrm.php
@@ -29,7 +29,21 @@ function com_install() {
   </tr>
   <tr>
     <td>
-      <code>Installation: <font color="green">Succesful</font></code>
+        <p><code>Installation: <font color="green">Succesful</font></code></p>
+        <p>If this is a <strong>new installation</strong> of CiviCRM, please review the
+            <a href="http://wiki.civicrm.org/confluence//x/ixI">online installation
+            documentation</a>.
+        </p>
+        <p>If you are <strong>upgrading</strong> an existing installation of CiviCRM, please review the
+            <a href="http://wiki.civicrm.org/confluence//x/lxs">upgrade
+            documentation</a>.
+        </p>
+        <p>CiviCRM includes the ability to expose Profile forms and listings, as well as
+         Online Contribution Pages, to users and visitors of the 'front-end' of your Joomla!
+         site. If you wish to use this option, you will need to follow the
+         <a href="http://wiki.civicrm.org/confluence//x/6Bk">instructions for
+         modifying your Joomla! installation</a>.
+        </p>
     </td>
   </tr>
 </table>
