7f49b648c6083e3db801a970315b4404e834f2eb
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-06-14 23:03:09 +0000

    integrate some joomla 1.5 changes made by paul and stephen
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@9962 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- joomla/install.civicrm.php
+++ joomla/install.civicrm.php
@@ -9,32 +9,18 @@ function com_install() {
 ?>
 
 <center>
-<table width="100%" border="0">
-  <tr>
-    <td>
-      <strong>Install Successful</strong><br/>
-      <br/>
-     CiviCRM has been successfully installed.
-    </td>
-  </tr>
-  <tr>
-    <td>
-        <p><code>Installation: <font color="green">Succesful</font></code></p>
-        <p>If this is a <strong>new installation</strong> of CiviCRM, please review the
-            <a href="http://wiki.civicrm.org/confluence//x/ixI">online installation
-            documentation</a>.
-        </p>
-        <p>If you are <strong>upgrading</strong> an existing installation of CiviCRM, please review the
-            <a href="http://wiki.civicrm.org/confluence//x/lxs">upgrade
-            documentation</a>.
-        </p>
+  <table width="100%" border="0">
+    <tr>
+      <td><strong>Files uploaded<font color="green">Succesful</font></strong><br/>
+      </td>
+    </tr>
+    <tr>
+      <td><p>If this is a <strong>new installation</strong> of CiviCRM, please review the <a href="http://wiki.civicrm.org/confluence//x/ixI">online installation documentation</a>. </p>
+        <p>If you are <strong>upgrading</strong> an existing installation of CiviCRM, please review the <a href="http://wiki.civicrm.org/confluence//x/lxs">upgrade documentation</a>. </p>
         <p>CiviCRM includes the ability to expose Profile forms and listings, as well as
-         Online Contribution Pages, to users and visitors of the 'front-end' of your Joomla!
-         site. If you wish to use this option, you will need to follow the
-         <a href="http://wiki.civicrm.org/confluence//x/6Bk">instructions for
-         modifying your Joomla! installation</a>.
-        </p>
-    </td>
-  </tr>
-</table>
+          Online Contribution Pages, to users and visitors of the 'front-end' of your Joomla!
+          site. If you wish to use this option, you will need to follow the <a href="http://wiki.civicrm.org/confluence//x/6Bk">instructions for
+          modifying your Joomla! installation</a>. </p></td>
+    </tr>
+  </table>
 </center>
