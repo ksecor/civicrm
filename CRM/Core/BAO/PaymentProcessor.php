7b69e141e3b7494aacc31f35c41b490a6ac500d2
marshal <marshal@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-06-28 05:28:59 +0000

    work on CRM-2009
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10169 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/PaymentProcessor.php
+++ CRM/Core/BAO/PaymentProcessor.php
@@ -177,8 +177,8 @@ class CRM_Core_BAO_PaymentProcessor extends CRM_Core_DAO_PaymentProcessor
     static function buildPayment( $dao ) 
     {
         $fields = array( 'name', 'payment_processor_type', 'user_name', 'password',
-                         'signature', 'url_site', 'url_button', 'subject',
-                         'class_name', 'is_recur', 'billing_mode');
+                         'signature', 'url_site', 'url_recur', 'url_button',
+                         'subject', 'class_name', 'is_recur', 'billing_mode');
         $result = array( );
         foreach ( $fields as $name ) {
             $result[$name] = $dao->$name;
