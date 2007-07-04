3381d192b5366faa5adf442af6e731e822aa3f28
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-04 08:54:11 +0000

    v1.8: use the untranslated Billing location type, fixes CRM-2064
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10253 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Offline.php
+++ CRM/Contribute/Form/Offline.php
@@ -75,7 +75,7 @@ class CRM_Contribute_Form_Offline extends CRM_Core_Form {
         // also check for billing information
         // get the billing location type
         $locationTypes =& CRM_Core_PseudoConstant::locationType( );
-        $this->_bltID = array_search( ts('Billing'),  $locationTypes );
+        $this->_bltID = array_search( 'Billing',  $locationTypes );
         if ( ! $this->_bltID ) {
             CRM_Core_Error::fatal( ts( 'Please set a location type of %1', array( 1 => 'Billing' ) ) );
         }
