d4d297debbf9d854de59579c6206e891cbf82d28
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-04 18:48:16 +0000

    //fixed yahoo geocoding
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10265 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/Geocode/Yahoo.php
+++ CRM/Utils/Geocode/Yahoo.php
@@ -119,10 +119,9 @@ class CRM_Utils_Geocode_Yahoo {
         $ret = array( );
         $ret['precision'] = (string)$xml->Result['precision'];
         if ( is_a($xml->Result, 'SimpleXMLElement') ) {
-            foreach($xml->Result->children() as $key=>$val) {
+            foreach ($xml->Result as $key=>$val) {
                 if(strlen($val)) $ret[(string)$key] =  (string)$val;
-            } 
-            
+            }
             $values['geo_code_1'] = $ret['Latitude' ];
             $values['geo_code_2'] = $ret['Longitude'];
         }
