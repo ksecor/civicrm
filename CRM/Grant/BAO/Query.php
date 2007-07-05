ca803b3a532395c239c853358cafb76a80c0de55
swati <swati@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-05 15:05:02 +0000

    Worked on advanced Search for Grant
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@10285 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Grant/BAO/Query.php
+++ CRM/Grant/BAO/Query.php
@@ -35,6 +35,83 @@
 
 class CRM_Grant_BAO_Query 
 {
+   
+    static function where( &$query ) 
+    {
+        foreach ( array_keys( $query->_params ) as $id ) {
+            if ( substr( $query->_params[$id][0], 0, 7) == 'grant_' ) {
+                
+                self::whereClauseSingle( $query->_params[$id], $query );
+            }
+        }
+    }
+  
+    static function whereClauseSingle( &$values, &$query ) 
+    {
+        list( $name, $op, $value, $grouping, $wildcard ) = $values;
+
+        switch( $name ) {
+            
+        case 'grant_money_transfer_date_low':
+        case 'grant_money_transfer_date_high':
+            $query->dateQueryBuilder( $values,
+                                      'civicrm_grant', 'grant_money_transfer_date_low', 'money_transfer_date', 'Money Transfer Date' );
+            return;
+
+        case 'grant_application_received_date_low':
+        case 'grant_application_received_date_high':
+            $query->dateQueryBuilder( $values,
+                                       'civicrm_grant', 'grant_application_received_date', 'application_received_date', 'Application Received Date' );
+            return;
+
+        case 'grant_type_id':
+            
+            $value = strtolower(addslashes(trim($value)));
+
+            $query->_where[$grouping][] = "civicrm_grant.grant_type_id $op '{$value}'";
+            
+            require_once 'CRM/Core/OptionGroup.php';
+            $grantTypes  = CRM_Core_OptionGroup::values("grant_type" );
+           
+            $query->_qill[$grouping ][] = ts( 'Grant Type %2 %1', array( 1 => $value, 2 => $op) );
+            $query->_tables['civicrm_grant'] = $query->_whereTables['civicrm_grant'] = 1;
+
+            return;
+
+        case 'grant_status_id':
+            
+            $value = strtolower(addslashes(trim($value)));
+
+            $query->_where[$grouping][] = "civicrm_grant.grant_status_id $op '{$value}'";
+            
+            require_once 'CRM/Core/OptionGroup.php';
+            $grantTypes  = CRM_Core_OptionGroup::values("grant_status" );
+           
+            $query->_qill[$grouping ][] = ts( 'Grant Type %2 %1', array( 1 => $value, 2 => $op) );
+            $query->_tables['civicrm_grant'] = $query->_whereTables['civicrm_grant'] = 1;
+
+            return;
+   
+        case 'grant_report_received':
+            $query->_where[$grouping][] = "civicrm_grant.grant_report_received $op $value";
+            $query->_qill[$grouping][]  = "Grant Report Received?";
+            $query->_tables['civicrm_grant'] = $query->_whereTables['civicrm_grant'] = 1;
+            
+            return;
+        }
+    }
+
+
+    /**
+     * getter for the qill object
+     *
+     * @return string
+     * @access public
+     */
+    function qill( ) {
+        return (isset($this->_qill)) ? $this->_qill : "";
+    }
+   
     /**
      * add all the elements shared between grant search and advanaced search
      *
@@ -55,16 +132,20 @@ class CRM_Grant_BAO_Query
         $form->add('select', 'grant_status_id',  ts( 'Grant Status' ),
                    array( '' => ts( '-select-' ) ) + $grantStatus );
         
-        $form->addElement('date', 'grant_application_received_date_low', ts('Application Recieved Date - From'), CRM_Core_SelectValues::date('relative')); 
+        $form->addElement('date', 'grant_application_received_date_low', ts('Application Recieved Date - From'), 
+                          CRM_Core_SelectValues::date('relative')); 
         $form->addRule('grant_application_received_date_low', ts('Select a valid date.'), 'qfDate'); 
         
-        $form->addElement('date', 'grant_application_received_date_high', ts('To'), CRM_Core_SelectValues::date('relative')); 
+        $form->addElement('date', 'grant_application_received_date_high', ts('To'), 
+                          CRM_Core_SelectValues::date('relative')); 
         $form->addRule('grant_application_received_date_high', ts('Select a valid date.'), 'qfDate'); 
 
-        $form->addElement('date', 'grant_money_transfer_date_low', ts('Money Sent Date - From'), CRM_Core_SelectValues::date('relative')); 
+        $form->addElement('date', 'grant_money_transfer_date_low', ts('Money Sent Date - From'), 
+                          CRM_Core_SelectValues::date('relative')); 
         $form->addRule('grant_money_transfer_date_low', ts('Select a valid date.'), 'qfDate'); 
         
-        $form->addElement('date', 'grant_money_transfer_date_high', ts('To'), CRM_Core_SelectValues::date('relative')); 
+        $form->addElement('date', 'grant_money_transfer_date_high', ts('To'),
+                          CRM_Core_SelectValues::date('relative')); 
         $form->addRule('grant_money_transfer_date_high', ts('Select a valid date.'), 'qfDate'); 
 
         $form->addElement('checkbox','grant_report_received', ts('Grant report received'),null );
