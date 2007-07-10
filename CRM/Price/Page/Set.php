9b639d0ea4f5dd0d0d8767edbde3c273753b0e61
marshal <marshal@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-10 19:34:12 +0000

    Fix deletion of price sets used by events in the past. Only remove price sets from inactive events when the price set is actually deleted.
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10332 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Price/Page/Set.php
+++ CRM/Price/Page/Set.php
@@ -149,19 +149,6 @@ class CRM_Price_Page_Set extends CRM_Core_Page {
             if ($action & (CRM_Core_Action::DISABLE | CRM_Core_Action::DELETE)) {
                 $usedBy =& CRM_Core_BAO_PriceSet::getUsedBy( $id );
                 if ( empty( $usedBy ) ) {
-                    // remove from all inactive forms
-                    $usedBy =& CRM_Core_BAO_PriceSet::getUsedBy( $id, true, true );
-                    if ( isset( $usedBy['civicrm_event_page'] ) ) {
-                        require_once 'CRM/Event/DAO/EventPage.php';
-                        foreach ( $usedBy['civicrm_event_page'] as $eventId => $unused ) {
-                            $eventPageDAO =& new CRM_Event_DAO_EventPage( );
-                            $eventPageDAO->event_id = $eventId;
-                            $eventPageDAO->find();
-                            while ( $eventPageDAO->fetch() ) {
-                                CRM_Core_BAO_PriceSet::removeFrom( 'civicrm_event_page', $eventPageDAO->id );
-                            }
-                        }
-                    }
                     if ( $action & CRM_Core_Action::DISABLE) {
                         // disable price set
                         CRM_Core_BAO_PriceSet::setIsActive( $id, 0 );
