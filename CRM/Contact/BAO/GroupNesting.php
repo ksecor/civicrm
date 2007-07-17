78773dd8eb57d91c1991b3de54223cd550b9bb3d
wmorgan <wmorgan@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-17 02:49:44 +0000

    added checks for cyclical references and a function to get parent group ids
    git-svn-id: http://svn.civicrm.org/civicrm/branches/codeforchange@10420 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/GroupNesting.php
+++ CRM/Contact/BAO/GroupNesting.php
@@ -72,9 +72,39 @@ class CRM_Contact_BAO_GroupNesting extends CRM_Contact_DAO_GroupNesting {
         $dao->query( $query );
         return $dao->fetch( );
     }
+    
+    /**
+     * Returns array of group ids of parent groups of the specified group.
+     *
+     * @param             $groupIds               An array of valid group ids (passed by reference)
+     *
+     * @return            $groupIdArray         List of groupIds that represent the requested group and its parents
+     *
+     * @access public
+     */
+
+    static function getParentGroupIds( $groupIds ) {
+        $numGroupIds = count( $groupIds );
+        $dao = new CRM_Contact_DAO_GroupNesting( );
+        $query = "SELECT parent_group_id FROM civicrm_group_nesting WHERE child_group_id IN (" . implode( ',', $groupIds ) . ")";
+        $dao->query( $query );
+        $tmpGroupIds = array( );
+        while ( $dao->fetch( ) ) {
+            // make sure we're not following any cyclical references
+            if ( ! array_key_exists( $dao->child_group_id, $parentGroupIds ) ) {
+                $tmpGroupIds[] = $dao->parent_group_id;
+            }
+        }
+        $parentGroupIds = $groupIds;
+        if ( ! empty( $tmpGroupIds ) ) {
+            $newParentGroupIds = self::getParentGroupIds( $tmpGroupIds );
+            $parentGroupIds = array_merge( $parentGroupIds, $newParentGroupIds );
+        }
+        return $parentGroupIds;
+    }
 
     /**
-     * Returns array of contacts who are members of the specified group.
+     * Returns array of group ids of child groups of the specified group.
      *
      * @param             $groupIds               An array of valid group ids (passed by reference)
      *
@@ -82,7 +112,7 @@ class CRM_Contact_BAO_GroupNesting extends CRM_Contact_DAO_GroupNesting {
      *
      * @access public
      */
-     
+
     static function getChildGroupIds( $groupIds ) {
         $numGroupIds = count( $groupIds );
         $dao = new CRM_Contact_DAO_GroupNesting( );
@@ -90,7 +120,10 @@ class CRM_Contact_BAO_GroupNesting extends CRM_Contact_DAO_GroupNesting {
         $dao->query( $query );
         $tmpGroupIds = array( );
         while ( $dao->fetch( ) ) {
-            $tmpGroupIds[] = $dao->child_group_id;
+            // make sure we're not following any cyclical references
+            if ( ! array_key_exists( $dao->parent_group_id, $childGroupIds ) ) {
+                $tmpGroupIds[] = $dao->child_group_id;
+            }
         }
         $childGroupIds = $groupIds;
         if ( ! empty( $tmpGroupIds ) ) {
