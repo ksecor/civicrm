656fb75b744e83e55d87c90b4b7ed53e117790a5
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-24 08:37:01 +0000

    fix for qfKey being in defaults
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10510 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Form.php
+++ CRM/Core/Form.php
@@ -297,11 +297,13 @@ class CRM_Core_Form extends HTML_QuickForm_Page {
         $this->buildQuickForm();
 
         $defaults =& $this->setDefaultValues( );
+        unset( $defaults['qfKey'] );
         if ( ! empty( $defaults ) ) {
             $this->setDefaults( $defaults );
         }
 
         $this->addRules();
+
     }
 
     /**
