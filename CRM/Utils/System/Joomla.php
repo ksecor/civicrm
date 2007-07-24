47a84ea0611f8c14b4d44c3da769dd63b859d9dc
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-24 23:20:30 +0000

    fix duplicate breadcrumb issue
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10530 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Utils/System/Joomla.php
+++ CRM/Utils/System/Joomla.php
@@ -72,6 +72,8 @@ class CRM_Utils_System_Joomla {
     static function appendBreadCrumb( $title, $url ) {
         $template =& CRM_Core_Smarty::singleton( );
         $bc = $template->get_template_vars( 'breadcrumb' );
+        $lastElement = count( $bc );
+        if ( $bc[$lastElement] 
         $bc[] = array( 'title' => $title,
                        'url'   => $url );
         $template->assign_by_ref( 'breadcrumb', $bc );
