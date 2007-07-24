f66acbb1bd52e6b4ec3f4fc58858560ecef4051b
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-24 10:10:45 +0000

    svn merge -r 10496:10511 http://svn.civicrm.org/branches/v1.8 including CRM-1821, CRM-2123 and CRM-2002 works/fixes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/kabissa@10513 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Invoke.php
+++ CRM/Core/Invoke.php
@@ -68,7 +68,10 @@ class CRM_Core_Invoke
 
         if ( $config->userFramework == 'Joomla' ) {
             require_once 'CRM/Core/Joomla.php';
-            Crm_Core_Joomla::sidebarLeft( );
+            // joomla 1.5RC1 seems to push this in the POST variable, which messes
+            // QF and checkboxes
+            unset( $_POST['option'] );
+            CRM_Core_Joomla::sidebarLeft( );
         } else if ( $config->userFramework == 'Standalone' ) {
             require_once 'CRM/Core/Standalone.php';
             CRM_Core_Standalone::sidebarLeft( );
