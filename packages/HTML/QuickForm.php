06d91ef38ebd1e0d439efc3f2e09ea5ed4263fbf
dfrishberg <dfrishberg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-27 19:48:34 +0000

    Fixed some indentation
    git-svn-id: http://svn.civicrm.org/civicrm/branches/codeforchange@10600 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/HTML/QuickForm.php
+++ packages/HTML/QuickForm.php
@@ -601,9 +601,7 @@ class HTML_QuickForm extends HTML_Common {
            $elementObject->onQuickFormEvent('updateValue', null, $this);
         } else {
             $args = func_get_args();
-	    //print_r($args);
-	    //print "\n<br>";
-    $elementObject =& $this->_loadElement('addElement', $element, array_slice($args, 1));
+	    $elementObject =& $this->_loadElement('addElement', $element, array_slice($args, 1));
             if (PEAR::isError($elementObject)) {
                 return $elementObject;
             }
