31df36540a641db6a4d8356729ae1198b60c3cd3
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-31 23:28:38 +0000

    fix group mapping for CRM-2088
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v1.8@10650 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Profile/Page/Listings.php
+++ CRM/Profile/Page/Listings.php
@@ -286,6 +286,7 @@ class CRM_Profile_Page_Listings extends CRM_Core_Page {
         CRM_Contact_Form_Task_Map::createMapXML( $contactIds, null, $this, false );
 
         $template =& CRM_Core_Smarty::singleton( );
+        $template->assign( 'isProfile', 1 );
         $content = $template->fetch( 'CRM/Contact/Form/Task/Map.tpl' );
         echo CRM_Utils_System::theme( 'page', $content, true, false );
         return;
