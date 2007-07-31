8d8b9a17c89e4e889119774af0cb7a2fbd242587
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-07-31 05:30:48 +0000

    kabissa: svn merge -r 10618:10638 http://svn.civicrm.org/branches/v1.8 including CRM-1578, CRM-2009, CRM-2145 and CRM-2148 works/fixes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/kabissa@10640 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Key.php
+++ CRM/Core/Key.php
@@ -36,6 +36,8 @@
 class CRM_Core_Key {
     static $_key = null;
 
+    static $_sessionID = null;
+
     /**
      * Generate a private key per session and store in session
      *
@@ -57,6 +59,18 @@ class CRM_Core_Key {
         return self::$_key;
     }
 
+    static function sessionID( ) {
+        if ( ! self::$_sessionID ) {
+            $session =& CRM_Core_Session::singleton( );
+            self::$_sessionID = $session->get( 'qfSessionID' );
+            if ( ! self::$_sessionID ) {
+                self::$_sessionID = session_id( );
+                $session->set( 'qfSessionID', self::$_sessionID );
+            }
+        }
+        return self::$_sessionID;
+    }
+
     /**
      * Generate a form key based on form name, the current user session
      * and a private key. Modelled after drupal's form API
@@ -70,7 +84,8 @@ class CRM_Core_Key {
      */
     static function get( $name, $addSequence = false ) {
         $privateKey = self::privateKey( );
-        $key = md5( session_id( ) . $name . $privateKey );
+        $sessionID  = self::sessionID ( );
+        $key = md5( $sessionID . $name . $privateKey );
 
         if ( $addSequence ) {
             // now generate a random number between 1 and 100K and add it to the key
@@ -101,7 +116,8 @@ class CRM_Core_Key {
         }
 
         $privateKey = self::privateKey( );
-        if ( $k != md5( session_id( ) . $name . $privateKey ) ) {
+        $sessionID  = self::sessionID ( );
+        if ( $k != md5( $sessionID . $name . $privateKey ) ) {
             return null;
         }
         return $key;
