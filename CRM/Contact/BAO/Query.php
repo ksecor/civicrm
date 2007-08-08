ba6a30fdb02e14b57c1c7f53bbd504c73b7d93c5
wmorgan <wmorgan@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-08-08 17:44:28 +0000

    cleaned up formatting in CRM/Contact/BAO/Query.php
    git-svn-id: http://svn.civicrm.org/civicrm/branches/codeforchange@10779 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -913,7 +913,7 @@ class CRM_Contact_BAO_Query {
              ( substr( $values[0], 0, 4  ) == 'tmf_' )) {
             return;
         }
-	
+    
         switch ( $values[0] ) {
             
         case 'contact_type':
@@ -921,17 +921,17 @@ class CRM_Contact_BAO_Query {
             return;
 
         case 'group':
-	    list( $name, $op, $value, $grouping, $wildcard ) = $values;
-	    $includeChildGroups = true;
-	    $subgroups_dummy = $this->getWhereValues( 'subgroups_dummy', $grouping );
-	    if ( $subgroups_dummy ) {
-	        $subgroup = $this->getWhereValues( 'subgroups', $grouping );
-	        if ( !$subgroup ){
+            list( $name, $op, $value, $grouping, $wildcard ) = $values;
+            $includeChildGroups = true;
+            $subgroups_dummy = $this->getWhereValues( 'subgroups_dummy', $grouping );
+            if ( $subgroups_dummy ) {
+                $subgroup = $this->getWhereValues( 'subgroups', $grouping );
+                if ( !$subgroup ) {
                     $includeChildGroups = false;
-		}
-	    }
-	    $this->group( $values , $includeChildGroups );
-	    return;
+                }
+            }
+            $this->group( $values , $includeChildGroups );
+            return;
 
         case 'tag':
             $this->tag( $values );
@@ -1764,7 +1764,7 @@ class CRM_Contact_BAO_Query {
             foreach ( $gcsValues[2] as $k => $v ) {
                   if ( $v ) {
                     if ( $k == 'Added' ) {
-		      $in = true;
+              $in = true;
                     }
                     $statii[] = "'" . CRM_Utils_Type::escape($k, 'String') . "'";
                 }
