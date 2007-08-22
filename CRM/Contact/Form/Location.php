8394bed340dc44e461b3c80f559c3852d9673445
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-08-22 19:05:14 +0000

    svn merge -r 11009:11010 http://svn.civicrm.org/branches/kabissa
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@11017 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Location.php
+++ CRM/Contact/Form/Location.php
@@ -73,14 +73,15 @@ class CRM_Contact_Form_Location extends CRM_Core_Form
         for ($locationId = 1; $locationId <= $maxLocationBlocks; $locationId++) {
             if ( $locationType ) {
                 $location[$locationId]['location_type_id'] =& $form->addElement('select'  , "location[$locationId][location_type_id]", null, CRM_Core_PseudoConstant::locationType( ) );
+                
+                $location[$locationId]['is_primary']       =& $form->addElement(
+                                                                                'checkbox', 
+                                                                                "location[$locationId][is_primary]", 
+                                                                                ts('Primary location for this contact'),  
+                                                                                ts('Primary location for this contact'), 
+                                                                                array('onchange' => "location_is_primary_onclick('" . $form->getName() . "', $locationId, $maxLocationBlocks);" ) );
             }
             
-            $location[$locationId]['is_primary']       =& $form->addElement(
-                                                           'checkbox', 
-                                                           "location[$locationId][is_primary]", 
-                                                           ts('Primary location for this contact'),  
-                                                           ts('Primary location for this contact'), 
-                                                           array('onchange' => "location_is_primary_onclick('" . $form->getName() . "', $locationId, $maxLocationBlocks);" ) );
             $location[$locationId]['name']             =& $form->addElement(
                                                            'text', 
                                                            "location[$locationId][name]",
