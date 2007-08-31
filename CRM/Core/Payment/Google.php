b1e5f43b119c6d7e00bbaf422d086f590e721515
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-08-31 07:41:04 +0000

    hrd: svn merge -r 11109:11126 http://svn.civicrm.org/branches/v1.8 including CRM-2242 and CRM-2234 works/fixes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/hrd@11133 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Payment/Google.php
+++ CRM/Core/Payment/Google.php
@@ -70,11 +70,11 @@ class CRM_Core_Payment_Google extends CRM_Core_Payment {
         $error = array( );
 
         if ( empty( $this->_paymentProcessor['user_name'] ) ) {
-            $error[] = ts( 'user_name is not set in the Administer CiviCRM &raquo; Payment Processor.' );
+            $error[] = ts( 'User Name is not set in the Administer CiviCRM &raquo; Payment Processor.' );
         }
         
         if ( empty( $this->_paymentProcessor['password'] ) ) {
-            $error[] = ts( 'password is not set in the Administer CiviCRM &raquo; Payment Processor.' );
+            $error[] = ts( 'Password is not set in the Administer CiviCRM &raquo; Payment Processor.' );
         }
         
         if ( ! empty( $error ) ) {
