f9b98661218b2b66120a3123b0570173afabee60
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-09-18 09:37:07 +0000

    upgrade version info to 2.0
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@11514 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/replace.sh
+++ bin/replace.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-for j in extern CRM api test; do
+for j in extern CRM api test js; do
   cd ../$j;
-  for i in `find . -name \*.php`; do
+  for i in `find . -name \*.js`; do
     echo $i;
-    perl -pi -e 's/CiviCRM version 1.8/CiviCRM version 1.9/' $i;
+    perl -pi -e 's/CiviCRM version 1.9/CiviCRM version 2.0/' $i;
   done
 done
 
