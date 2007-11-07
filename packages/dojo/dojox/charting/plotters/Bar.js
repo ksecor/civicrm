8ca257ce5e331c3691a86a40caf536ea5d0bbfe6
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-11-07 14:02:29 +0000

    multiorg: svn merge -r 12120:12121 http://svn.civicrm.org/civicrm/trunk
    git-svn-id: http://svn.civicrm.org/civicrm/branches/multiorg@12161 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/dojo/dojox/charting/plotters/Bar.js
+++
@@ -1,7 +0,0 @@
-if(!dojo._hasResource["dojox.charting.plotters.Bar"]){ //_hasResource checks added by build. Do not use _hasResource directly in your code.
-dojo._hasResource["dojox.charting.plotters.Bar"] = true;
-dojo.provide("dojox.charting.plotters.Bar");
-dojox.charting.plotters.Bar=function(chart, shape, plot){
-};
-
-}
--- packages/dojo/dojox/charting/plotters/DataBar.js
+++
@@ -1,7 +0,0 @@
-if(!dojo._hasResource["dojox.charting.plotters.DataBar"]){ //_hasResource checks added by build. Do not use _hasResource directly in your code.
-dojo._hasResource["dojox.charting.plotters.DataBar"] = true;
-dojo.provide("dojox.charting.plotters.DataBar");
-dojox.charting.plotters.DataBar=function(chart, shape, plot, series){
-};
-
-}
