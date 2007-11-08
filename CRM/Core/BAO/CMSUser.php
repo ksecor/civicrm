71c81bab39cb999720ac53cfc2feb8fa25320ca9
rupam <rupam@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-11-08 13:33:28 +0000

    fix for CRM-2020.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12191 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/CMSUser.php
+++ CRM/Core/BAO/CMSUser.php
@@ -412,18 +412,51 @@ SELECT count(*)
         $date  = date('y-m-d h:i:s');
         
         //In Joomla, Registerd User is fixed to 18.
-        $regiterUser = '18';
-       
-        $sql = "INSERT INTO {$config->userFrameworkUsersTableName} VALUES 
-              ('', '$fname', '$uname', '$email', '$pwd', 'Registered', 1, 0, $regiterUser, '$date', '0000-00-00 00:00:00', '', '')";
+        $userType   = 'Registered';
+        $userTypeId = '18';
+
+        //Get MySQL Table Prefix eg.'jos_'
+        list( $prefix, $table ) = split( '_', $config->userFrameworkUsersTableName );        
        
         $db_cms = DB::connect($config->userFrameworkDSN);
         
         if ( DB::isError( $db_cms ) ) { 
             die( "Cannot connect to UF db via $dsn, " . $db_cms->getMessage( ) ); 
         }
-        $query = $db_cms->query( $sql );
+
+        require_once 'CRM/Core/Transaction.php';
+        $transaction = new CRM_Core_Transaction( );
+
+        //1.Insert into 'jos_users' table
+        $sql   = "INSERT INTO {$config->userFrameworkUsersTableName} VALUES 
+              ('', '$fname', '$uname', '$email', '$pwd', '$userType', 0, 1, $userTypeId, '$date', '0000-00-00 00:00:00', '', '')";
         
+        $query = $db_cms->query( $sql );
+
+        //Fetch id of newly added user
+        $id_sql   = "SELECT id FROM {$config->userFrameworkUsersTableName} where username = '$uname'";
+        $id_query = $db_cms->query( $id_sql );
+        $id_row   = $id_query->fetchRow( DB_FETCHMODE_ASSOC ) ;
+        $id       = $id_row['id'];
+        
+        //2.Insert into 'jos_core_acl_aro' table
+        $table     = "{$prefix}_core_acl_aro";
+        $acl_sql   = "INSERT INTO {$table} VALUES ('','users','$id',0,'$userType',0)";
+        $acl_query = $db_cms->query( $acl_sql );
+
+        //Fetch aro_id of newly added acl
+        $aro_id_sql   = "SELECT aro_id FROM {$table} where value = '$id'";
+        $aro_id_query = $db_cms->query( $aro_id_sql );
+        $aro_id_row   = $aro_id_query->fetchRow( DB_FETCHMODE_ASSOC ) ;
+        $aro_id       = $aro_id_row['aro_id'];
+
+        //3.Insert into 'jos_core_acl_groups_aro_map' table
+        $table       = "{$prefix}_core_acl_groups_aro_map";
+        $group_sql   = "INSERT INTO {$table} VALUES (25,'','$aro_id')";
+        $group_query = $db_cms->query( $group_sql );
+
+        $transaction->commit( );
+
         return true;
     }
     
