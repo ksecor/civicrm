97e34b412d06a33181301eb61514a12d494b6ecc
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-11-22 23:08:49 +0000

    fix spacing
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12363 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -856,6 +856,7 @@ class CRM_Core_BAO_UFGroup extends CRM_Core_DAO_UFGroup
                     $values[$index] = '<a href="' . $url . '">' . $values[$index] . '</a>';
                 }
             }
+
             if ( $field['visibility'] == "User and User Admin Only"|| $field['visibility'] == "Public User Pages" ) {
                 $customFieldID = CRM_Core_BAO_CustomField::getKeyID($field['name']);
                  if ( CRM_Core_BAO_CustomField::getKeyID($field['name']) ) {
