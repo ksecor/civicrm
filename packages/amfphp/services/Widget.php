3576a2610219bfda8c3f3a8c1eaeb216586917b0
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-11-29 01:34:14 +0000

    Work on civiwidget and add debuggin.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12435 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/amfphp/services/Widget.php
+++ packages/amfphp/services/Widget.php
@@ -24,27 +24,27 @@ class Widget {
 	/**
 	 * Not implemented - registers an action and unique widget ID.  Useful for stats and debugging
 	 *
-	 * @param int $campaignId
-	 * @param string $widgetId
+	 * @param int $contributionPageID
+	 * @param string $widgetID
 	 * @param string $action
 	 * @return string
 	 */
-	private function registerRequest( $campaignId, $widgetId, $action) {
-        return CRM_Widget_Widget::registerRequest( $campaignId,
-                                                   $widgetId,
+	private function registerRequest( $contributionPageID, $widgetID, $action) {
+        return CRM_Widget_Widget::registerRequest( $contributionPageID,
+                                                   $widgetID,
                                                    $action );
 	}
 
 	/**
 	 * Gets all campaign related data and returns it as a std class.
 	 *
-	 * @param int $campaignId
-	 * @param string $widgetId
+	 * @param int $contributionPageID
+	 * @param string $widgetID
 	 * @return stdClass
 	 */
-	public function getCampaignData( $campaignId, $widgetId ) {
-        $data = CRM_Widget_Widget::getCampaignData( $campaignId, $widgetId );
-        CRM_Core_Error::debug_var( $campaignId, $data );
+	public function getContributionPageData( $contributionPageID, $widgetID ) {
+        $data = CRM_Widget_Widget::getContributionPageData( $contributionPageID, $widgetID );
+        CRM_Core_Error::debug_var( $contributionPageID, $data );
         return $data;
     }
 
@@ -52,15 +52,15 @@ class Widget {
 	 * Gets embed code.  Perhaps overkill, but we can track dropoffs in this case.
      * by # of people reqeusting emebed code / number of unique instances.
 	 *
-     * @param int $campaignId
-	 * @param string $widgetId
+     * @param int $contributionPageID
+	 * @param string $widgetID
 	 * @param string $format - either myspace or normal
 	 * @return string
 	 */
-	public function getEmbedCode( $campaignId,
-                                  $widgetId,
+	public function getEmbedCode( $contributionPageID,
+                                  $widgetID,
                                   $format = "normal" ) {
-        return CRM_Widget_Widget::getEmbedCode( $campaignId, $widgetId, $format );
+        return CRM_Widget_Widget::getEmbedCode( $contributionPageID, $widgetID, $format );
 	}
 
 }
