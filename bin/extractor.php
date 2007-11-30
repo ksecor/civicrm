7ad51ff8462b108fa3fe3e4e14843ecb85ac2550
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-11-30 17:00:08 +0000

    port CiviCRM to GNU Affero General Public License Version 3
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12458 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/extractor.php
+++ bin/extractor.php
@@ -1,7 +1,38 @@
 #!/usr/bin/php
 <?php
 
-/* $Id$ */
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.0                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2007                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2007
+ * $Id$
+ *
+ */
 
 /**
  * ts() and {ts} calls extractor
@@ -9,10 +40,6 @@
  * Extracts translatable strings from CiviCRM PHP source (ts() calls) and
  * Smarty templates ({ts} calls). Outputs a POT file on STDOUT, errors on
  * STDERR.
- *
- * @author Piotr Szotkowski <shot@caltha.pl>
- * @copyright CiviCRM LLC (c) 2004-2007
- * @license http://affero.org/oagpl.html  Affero General Public License
  */
 
 $modules = array('Contribute', 'Event', 'Mailing', 'Member');
--- templates/CRM/common/footer.tpl
+++ templates/CRM/common/footer.tpl
@@ -1,7 +1,7 @@
 {include file="CRM/common/version.tpl" assign=version}
 
 {if $contactId} {* Display contact-related footer. *}
-    <div class="footer" id="record-log"> 
+    <div class="footer" id="record-log">
     <span class="col1">{if $external_identifier}{ts}External ID{/ts}:&nbsp;{$external_identifier}{/if}&nbsp; &nbsp;{ts}CiviCRM ID{/ts}:&nbsp;{$contactId}</span>
     {if $lastModified}
         {ts}Last Change by{/ts} <a href="{crmURL p='civicrm/contact/view' q="action=view&reset=1&cid=`$lastModified.id`"}">{$lastModified.name}</a> ({$lastModified.date|crmDate}) &nbsp; <a href="{crmURL p='civicrm/contact/view' q="reset=1&action=browse&selectedChild=log&cid=`$contactId`"}">&raquo; {ts}View Change Log{/ts}</a>
@@ -9,6 +9,6 @@
     </div>
 {/if}
 
-<div class="footer" id="civicrm-footer"> 
-{ts 1=$version 2='http://www.affero.org/oagpl.html' 3='http://civicrm.org/download' 4='http://issues.civicrm.org/jira/browse/CRM?report=com.atlassian.jira.plugin.system.project:roadmap-panel' 5='http://wiki.civicrm.org/confluence//x/KSg'}Powered by CiviCRM %1. CiviCRM is openly available under the <a href="%2">Affero General Public License (AGPL)</a>. <a href="%3">Download source</a>. <a href="%4">View issues and report bugs</a>. <a href="%5">Online documentation</a>.{/ts}
-</div> 
+<div class="footer" id="civicrm-footer">
+{ts 1=$version 2='http://www.gnu.org/licenses/agpl-3.0.html' 3='http://civicrm.org/download' 4='http://issues.civicrm.org/jira/browse/CRM?report=com.atlassian.jira.plugin.system.project:roadmap-panel' 5='http://wiki.civicrm.org/confluence//x/KSg'}Powered by CiviCRM %1. CiviCRM is openly available under the <a href="%2">GNU Affero General Public License (GNU AGPL)</a>. <a href="%3">Download source</a>. <a href="%4">View issues and report bugs</a>. <a href="%5">Online documentation</a>.{/ts}
+</div>
