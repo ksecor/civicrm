fa75853fa92c9f37f16d01f95cca78ec5dba7d14
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-12-11 19:04:58 +0000

    //fixed noticees
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12574 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- civicrm.config.php
+++ civicrm.config.php
@@ -60,7 +60,7 @@ function conf_init() {
         include $currentDir . 'settings_location.php';
     }
   
-    if ( defined( 'CIVICRM_CONFDIR' ) && ! defined( $confdir ) ) {
+    if ( defined( 'CIVICRM_CONFDIR' ) && ! isset( $confdir ) ) {
       	$confdir = CIVICRM_CONFDIR;
     } else {
         // make it relative to civicrm.config.php, else php makes it relative
