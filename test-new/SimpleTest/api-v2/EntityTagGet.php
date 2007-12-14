15819ccf6d8c6f743c9b45f32a0cce21186816a3
sunil <sunil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2007-12-14 13:59:39 +0000

    //Fix for testcase for EntityTagGet
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12641 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test-new/SimpleTest/api-v2/EntityTagGet.php
+++ test-new/SimpleTest/api-v2/EntityTagGet.php
@@ -4,65 +4,107 @@ require_once 'api/v2/EntityTag.php';
 
 class TestOfGetEntitiesByTagAPIV2 extends CiviUnitTestCase 
 {
-       
+    protected $_individualID;
+    
+    protected $_householdID;
+    
+    protected $_organizationID;
+
+    protected $_tagID; 
+
     function setUp() 
-    {
+    { 
+        $this->_individualID   = $this->individualCreate( );
+        $this->_tagID          = $this->tagCreate( ); 
+        $this->_householdID    = $this->houseHoldCreate( );
+        $this->_organizationID = $this->organizationCreate( );
     }
     
-    function testIndividualEntityTagGetWithoutContactID()
+    function testIndividualEntityTagGetWithoutContactID( )
     {
         $paramsEntity = array( );
-        $entity =& civicrm_entity_tag_get($paramsEntity); 
+        $entity       =& civicrm_entity_tag_get( $paramsEntity ); 
         $this->assertEqual( $entity['is_error'], 1 );
-        $this->assertNotNull($entity['error_message']);
+        $this->assertNotNull( $entity['error_message'] );
     }
     
-    function testIndividualEntityTagGet()
+    function testIndividualEntityTagGet( )
     {
-        $contactId = $this->individualCreate( );
-        $paramsEntity = array('contact_id' =>  $ContactId);
-        $entity =& civicrm_entity_tag_get($paramsEntity); 
+        $ContactId = $this->_individualID; 
+        $tagID     = $this->_tagID ;  
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $individualEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEqual( $individualEntity['is_error'], 0 );
+        $this->assertEqual( $individualEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' =>  $contactId );
+        $entity =& civicrm_entity_tag_get( $paramsEntity );
         $this->assertEqual( $entity['is_error'], 0 );
-        $this->assertNotNull($entity['tag_id']);
     }
     
-    function testHouseholdEntityGetWithoutContactID()
+    function testHouseholdEntityGetWithoutContactID( )
     {
         $paramsEntity = array( );
-        $entity =& civicrm_entity_tag_get($paramsEntity);
+        $entity       =& civicrm_entity_tag_get( $paramsEntity );
         $this->assertEqual( $entity['is_error'], 1 );
-        $this->assertNotNull($entity['error_message']);
+        $this->assertNotNull( $entity['error_message'] );
     }
 
-    function testHouseholdEntityGet()
+    function testHouseholdEntityGet( )
     {
-        $contactId = $this->householdCreate( );
-        $paramsEntity = array('contact_id' => $contactId); 
-        $entity =& civicrm_entity_tag_get($paramsEntity);
+       
+        $ContactId = $this->_householdID;
+        $tagID     = $this->_tagID;
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $householdEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEqual( $householdEntity['is_error'], 0 );
+        $this->assertEqual( $householdEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' => $contactId ); 
+        $entity =& civicrm_entity_tag_get( $paramsEntity );
         $this->assertEqual( $entity['is_error'], 0 );
-        $this->assertNotNull($entity['tag_id']);
     }
-     
+    
     function testOrganizationEntityGetWithoutContactID()
     {
         $paramsEntity = array( );
-        $entity =& civicrm_entity_tag_get($paramsEntity); 
+        $entity =& civicrm_entity_tag_get( $paramsEntity ); 
         $this->assertEqual( $entity['is_error'], 1 );
-        $this->assertNotNull($entity['error_message']);
+        $this->assertNotNull( $entity['error_message'] );
     }
 
-    function testOrganizationEntityGet()
+    function testOrganizationEntityGet( )
     {
-        $contactId = $this->organizationCreate( );
-        $paramsEntity = array('contact_id' => $contactId);
-        $entity =& civicrm_entity_tag_get($paramsEntity); 
+        $ContactId = $this->_organizationID;
+        $tagID     = $this->_tagID;
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $organizationEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEqual( $organizationEntity['is_error'], 0 );
+        $this->assertEqual( $organizationEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' => $contactId );
+        $entity =& civicrm_entity_tag_get( $paramsEntity ); 
         $this->assertEqual( $entity['is_error'], 0 ); 
-        $this->assertNotNull($entity['tag_id']);
+        
     }
-  
-    function tearDown() 
+    
+    function tearDown( ) 
     {
+        $this->contactDelete( $this->_individualID );
+        $this->contactDelete( $this->_householdID );
+        $this->contactDelete( $this->_organizationID );
+        $this->tagDelete( $this->_tagID );
+        
     }
-       
+    
 }
 ?>
