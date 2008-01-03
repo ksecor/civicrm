c443fb6efa17e882c3c162da2140d793f5e381b7
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-01-03 12:49:20 +0000

    //small fixes for ParticipantUpdate testcase.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@12782 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- test-new/SimpleTest/api-v2/ParticipantUpdate.php
+++ test-new/SimpleTest/api-v2/ParticipantUpdate.php
@@ -5,9 +5,12 @@ require_once 'api/v2/Participant.php';
 class TestOfParticipantUpdateAPIV2 extends CiviUnitTestCase 
 {
     protected $_participant;
-               
+    protected $_individualId; 
+    
     function setUp() 
     {
+        // Create test contacts.
+        $this->_individualId = $this->individualCreate();
     }
     
     function testParticipantUpdateEmptyParams()
@@ -20,28 +23,28 @@ class TestOfParticipantUpdateAPIV2 extends CiviUnitTestCase
 
     function testParticipantUpdateWithoutEventId()
     {  
-        $contactId =$this->individualCreate(); 
-        $participantId = $this->participantCreate( $contactId ); 
+        $participantId = $this->participantCreate( $this->_individualId );
         $params = array(
-                        'contact_id'    => $contactId,
+                        'contact_id'    => $this->_individualId,
                         'status_id'     => 3,
                         'role_id'       => 3,
                         'register_date' => '2006-01-21',
                         'source'        => 'US Open',
                         'event_level'   => 'Donation'                        
                         );
-        $participant = & civicrm_participant_create($params);  
+        $participant = & civicrm_participant_create($params);
         $this->assertEqual( $participant['is_error'], 1 );
         $this->assertEqual( $participant['error_message'],'Required parameter missing' );
+        // Cleanup created participant records.
+        $result = $this->participantDelete( $participantId );
     }
 
     function testParticipantUpdate()
     {  
-        $contactId =$this->individualCreate(); 
-        $participantId = $this->participantCreate( $contactId ); 
+        $participantId = $this->participantCreate( $this->_individualId ); 
         $params = array(
                         'id'            => $participantId,
-                        'contact_id'    => $contactId,
+                        'contact_id'    => $this->_individualId,
                         'event_id'      => 2,
                         'status_id'     => 3,
                         'role_id'       => 3,
@@ -51,7 +54,7 @@ class TestOfParticipantUpdateAPIV2 extends CiviUnitTestCase
                         );
         $participant = & civicrm_participant_create($params);
         $this->assertNotEqual( $participant['is_error'],1 );
-
+        
         if ( ! $participant['is_error'] ) {
             $params['id'] = CRM_Utils_Array::value('id', $participant);
             
@@ -62,11 +65,14 @@ class TestOfParticipantUpdateAPIV2 extends CiviUnitTestCase
             // assertDBState compares expected values in $match to actual values in the DB              
             $this->assertDBState( 'CRM_Event_DAO_Participant', $participant['id'], $match );
         }
+        // Cleanup created participant records.
+        $result = $this->participantDelete( $params['id'] );
     }
     
     function tearDown() 
     {
-    
+        // Cleanup test contacts.
+        $this->contactDelete( $this->_individualId );
     }
 
 }
