625e3e831055a1ed74bd82d9b0fce74baa300bad
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-02-21 12:28:28 +0000

    PEAR upgrade: HTML_QuickForm_Controller
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@13352 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/HTML/QuickForm/Action.php
+++ packages/HTML/QuickForm/Action.php
@@ -1,33 +1,38 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4.0                                                      |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Alexey Borzov <avb@php.net>                                 |
-// |          Bertrand Mansion <bmansion@mamasam.com>                     |
-// +----------------------------------------------------------------------+
-//
-// $Id: Action.php,v 1.2 2006/05/31 08:58:44 avb Exp $
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * Class representing an action to perform on HTTP request. The Controller
- * will select the appropriate Action to call on the request and call its
- * perform() method. The subclasses of this class should implement all the
- * necessary business logic.
+ * Class representing an action to perform on HTTP request.
+ * 
+ * PHP versions 4 and 5
  *
- * @author  Alexey Borzov <avb@php.net>
- * @package HTML_QuickForm_Controller
- * @version $Revision: 1.2 $
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category    HTML
+ * @package     HTML_QuickForm_Controller
+ * @author      Alexey Borzov <avb@php.net>
+ * @copyright   2003-2007 The PHP Group
+ * @license     http://www.php.net/license/3_01.txt PHP License 3.01
+ * @version     CVS: $Id: Action.php,v 1.3 2007/05/18 09:34:18 avb Exp $
+ * @link        http://pear.php.net/package/HTML_QuickForm_Controller
+ */
+
+/**
+ * Class representing an action to perform on HTTP request. 
+ * 
+ * The Controller will select the appropriate Action to call on the request and
+ * call its perform() method. The subclasses of this class should implement all 
+ * the necessary business logic.
+ *
+ * @category    HTML
+ * @package     HTML_QuickForm_Controller
+ * @author      Alexey Borzov <avb@php.net>
+ * @version     Release: 1.0.8
+ * @abstract
  */
 class HTML_QuickForm_Action
 {
@@ -35,10 +40,12 @@ class HTML_QuickForm_Action
     * Processes the request. This method should be overriden by child classes to
     * provide the necessary logic.
     *
-    * @access public
-    * @param  object HTML_QuickForm_Page    the current form-page
-    * @param  string    Current action name, as one Action object can serve multiple actions
-    * @throws PEAR_Error
+    * @access   public
+    * @param    HTML_QuickForm_Page    The current form-page
+    * @param    string                 Current action name, as one Action object
+    *                                  can serve multiple actions
+    * @throws   PEAR_Error
+    * @abstract
     */
     function perform(&$page, $actionName)
     {
--- packages/HTML/QuickForm/Action/Back.php
+++ packages/HTML/QuickForm/Action/Back.php
@@ -1,31 +1,38 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4.0                                                      |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Alexey Borzov <avb@php.net>                                  |
-// +----------------------------------------------------------------------+
-//
-// $Id: Back.php,v 1.5 2006/05/31 08:58:44 avb Exp $
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * The action for a 'back' button of wizard-type multipage form.
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category    HTML
+ * @package     HTML_QuickForm_Controller
+ * @author      Alexey Borzov <avb@php.net>
+ * @copyright   2003-2007 The PHP Group
+ * @license     http://www.php.net/license/3_01.txt PHP License 3.01
+ * @version     CVS: $Id: Back.php,v 1.6 2007/05/18 09:34:18 avb Exp $
+ * @link        http://pear.php.net/package/HTML_QuickForm_Controller
+ */
+
+/**
+ * Class representing an action to perform on HTTP request.
+ */
 require_once 'HTML/QuickForm/Action.php';
 
 /**
  * The action for a 'back' button of wizard-type multipage form.
  *
- * @author  Alexey Borzov <avb@php.net>
- * @package HTML_QuickForm_Controller
- * @version $Revision: 1.5 $
+ * @category    HTML
+ * @package     HTML_QuickForm_Controller
+ * @author      Alexey Borzov <avb@php.net>
+ * @version     Release: 1.0.8
  */
 class HTML_QuickForm_Action_Back extends HTML_QuickForm_Action
 {
