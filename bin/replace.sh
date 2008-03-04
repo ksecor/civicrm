e02e68dd9179811d4bd5c3dce0bc665b86394a76
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-03-04 11:19:40 +0000

    upgrade CiviCRM version number to 2.1
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@13540 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/replace.sh
+++ bin/replace.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-for j in extern CRM api test js; do
-  cd ../$j;
-  for i in `find . -name \*.js`; do
-    echo $i;
-    perl -pi -e 's/CiviCRM version 1.9/CiviCRM version 2.0/' $i;
-  done
+for i in `find . -name '*.js' -or -name '*.module' -or -name '*.php' -or -name '*.tpl' -or -name '*.txt'`; do
+  echo $i;
+  perl -pi -e 's/CiviCRM version .\../CiviCRM version 2.1/' $i;
 done
-
-
