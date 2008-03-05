bcc0258b60f90251936371e618cc252f42561dd4
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-03-05 19:16:32 +0000

    Spacing fix in status msg - participant receipt sent.
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.0@13578 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Participant.php
+++ CRM/Event/Form/Participant.php
@@ -806,13 +806,13 @@ class CRM_Event_Form_Participant extends CRM_Contact_Form_Task
         if ( ( $this->_action & CRM_Core_Action::UPDATE ) ) {
             $statusMsg = ts('Event registration information for %1 has been updated.', array(1 => $this->_contributorDisplayName));
             if ( $params['send_receipt'] ) {
-                $statusMsg .= ts('A confirmation email has been sent to %1', array(1 => $this->_contributorEmail));
+                $statusMsg .= ' ' .  ts('A confirmation email has been sent to %1', array(1 => $this->_contributorEmail));
             }
         } elseif ( ( $this->_action & CRM_Core_Action::ADD ) ) {
             if ( $this->_single ) {
                 $statusMsg = ts('Event registration for %1 has been added.', array(1 => $this->_contributorDisplayName));
                 if ( $params['send_receipt'] ) {
-                    $statusMsg .= ts('A confirmation email has been sent to %1.', array(1 => $this->_contributorEmail));
+                    $statusMsg .= ' ' .  ts('A confirmation email has been sent to %1.', array(1 => $this->_contributorEmail));
                 }
             } else {
                 $statusMsg = ts('Total Participant(s) added to event: %1', array(1 => count($this->_contactIds)));
