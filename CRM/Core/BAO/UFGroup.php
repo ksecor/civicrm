b63cf81722086f20ff6268e9cf286c99262ef7ca
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-15 22:27:02 +0000

    CRM-2960
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@13984 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -385,8 +385,8 @@ class CRM_Core_BAO_UFGroup extends CRM_Core_DAO_UFGroup
                 }
             }
         } else {
-            CRM_Core_Error::fatal( ts( 'The requested Profile (gid=%1) is disabled, OR it is not configured to be used for \'Profile\' listings in its Settings, or there is no Profile with that ID. Contact the site administrator if you need assistance.',
-                                      array( 1 => $id )) );        
+            CRM_Core_Error::fatal( ts( 'The requested Profile (gid=%1) is disabled, OR it is not configured to be used for \'Profile\' listings in its Settings, or there is no Profile with that ID, or you do not have permission to access this profile. Contact the site administrator if you need assistance.',
+                                                   array( 1 => $id )) );        
         }
 
         return $fields;
