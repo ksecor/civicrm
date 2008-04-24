6e9971068c366defa3505366b8d0d536913c34a8
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-24 05:47:00 +0000

    Remove world_region from default address display. Fix references to rowcount in custom search Base.php and ActivitySearch.php
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.0@14031 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Search/Custom/Base.php
+++ CRM/Contact/Form/Search/Custom/Base.php
@@ -62,7 +62,7 @@ class CRM_Contact_Form_Search_Custom_Base {
     }
 
     function sql( $selectClause,
-                  $offset = 0, $rowCount = 0, $sort = null,
+                  $offset = 0, $rowcount = 0, $sort = null,
                   $includeContactIDs = false,
                   $groupBy = null ) {
 
@@ -83,7 +83,7 @@ class CRM_Contact_Form_Search_Custom_Base {
             $sql .= " $groupBy ";
         }
         
-        $this->addSortOffset( $sql, $offset, $rowCount, $sort );
+        $this->addSortOffset( $sql, $offset, $rowcount, $sort );
         return $sql;
     }
 
@@ -117,7 +117,7 @@ class CRM_Contact_Form_Search_Custom_Base {
     }
 
     function addSortOffset( &$sql,
-                            $offset, $rowCount, $sort ) {
+                            $offset, $rowcount, $sort ) {
         if ( ! empty( $sort ) ) {
             if ( is_string( $sort ) ) {
                 $sql .= " ORDER BY $sort ";
@@ -126,8 +126,8 @@ class CRM_Contact_Form_Search_Custom_Base {
             }
         }
         
-        if ( $row_count > 0 && $offset >= 0 ) {
-            $sql .= " LIMIT $offset, $row_count ";
+        if ( $rowcount > 0 && $offset >= 0 ) {
+            $sql .= " LIMIT $offset, $rowcount ";
         }
     }
 
