db0798735c9592d7ed6d3c1828199bb7f74b1f5d
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-24 23:43:17 +0000

    CRM-2526
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14045 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Smarty/plugins/function.help.php
+++ CRM/Core/Smarty/plugins/function.help.php
@@ -69,8 +69,8 @@ function smarty_function_help( $params, &$smarty ) {
     return <<< EOT
 
 <span dojoType="dijit.form.DropDownButton" class="tundra">
-  <span><img class="action-icon" src="{$smarty->_tpl_vars[ 'config']->resourceBase}i/Help.png"></span>
-  <span dojoType="dijit.TooltipDialog" id="{$id}_help" class="tundra" >$help</span>
+  <div><img class="action-icon" src="{$smarty->_tpl_vars[ 'config']->resourceBase}i/Help.png" /></div>
+  <div dojoType="dijit.TooltipDialog" id="{$id}_help" class="tundra" >$help</div>
 </span>
 EOT;
 
