aa5d47a6eb9bbf646c719613dd3204f53e063b3d
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-25 09:50:20 +0000

    fix caching of custom field stuf
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14064 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/CustomField.php
+++ CRM/Core/BAO/CustomField.php
@@ -179,7 +179,7 @@ class CRM_Core_BAO_CustomField extends CRM_Core_DAO_CustomField
             require_once 'CRM/Core/BAO/Cache.php'; 
             $fields =& CRM_Core_BAO_Cache::getItem( 'contact fields', "custom importableFields $cacheKey" );
 
-            if ( $fields !== null ) {
+            if ( $fields == null ) {
                 $cfTable = self::getTableName();
                 $cgTable = CRM_Core_DAO_CustomGroup::getTableName();
 
