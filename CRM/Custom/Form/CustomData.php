773a66ae9d0d318c5c6309143cbdbc6ea0d38c16
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-28 00:27:58 +0000

    worked on CRM-3000
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14096 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Custom/Form/CustomData.php
+++ CRM/Custom/Form/CustomData.php
@@ -43,12 +43,11 @@ class CRM_Custom_Form_CustomData
     static function preProcess( &$form )
     {
         //Custom Group Inline Edit form
-        $form->_type     = CRM_Utils_Array::value( 'type', $_GET );
-        $form->_subType  = CRM_Utils_Array::value( 'subType', $_GET );
-        $form->_entityId = CRM_Utils_Array::value( 'entityId', $_GET );
+        $form->_type     = CRM_Utils_Request::retrieve( 'type', 'String', $form );
+        $form->_subType  = CRM_Utils_Request::retrieve( 'subType', 'String', $form );
+        $form->_entityId = CRM_Utils_Request::retrieve( 'entityId', 'Positive', $form );
 
         $form->_groupTree =& CRM_Core_BAO_CustomGroup::getTree( $form->_type, $form->_entityId, 0, $form->_subType );
-        //$this->setDefaultValues();
     }
 
     static function setDefaultValues( &$form ) 
