d48f926b1480069a2cc65aea4b7d71a67e1dcb81
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-04-30 00:16:57 +0000

    documented civicrm dojo files
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14142 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/dojo/civicrm/TitlePane.js
+++ packages/dojo/civicrm/TitlePane.js
@@ -1,11 +1,14 @@
-/*
-	Copyright (c) 2004-2008, The Dojo Foundation
-	All Rights Reserved.
+/**
+   Copyright (c) 2004-2008, The Dojo Foundation
+   All Rights Reserved.
+   
+   Licensed under the Academic Free License version 2.1 or above OR the
+   modified BSD license. For more information on Dojo licensing, see:
+   
+   http://dojotoolkit.org/book/dojo-book-0-9/introduction/licensing
 
-	Licensed under the Academic Free License version 2.1 or above OR the
-	modified BSD license. For more information on Dojo licensing, see:
+   Custom modified TitlePane for CiviCRM
 
-		http://dojotoolkit.org/book/dojo-book-0-9/introduction/licensing
 */
 
 
