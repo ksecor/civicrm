2b20eadf321848f5a05d5dd68928159721fa9ebe
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-02 03:33:40 +0000

    add an automated click url tester
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14187 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- drupal/tests/CiviTestCase.php
+++ drupal/tests/CiviTestCase.php
@@ -15,6 +15,11 @@ class CiviTestCase extends DrupalTestCase
 
     function civiGet( $path, $params, $abort = false ) {
         $url = CRM_Utils_System::url( $path, $params, true, null, false );
+
+        return $this->civiGetURL( $url, $abort );
+    }
+
+    function civiGetURL( $url, $abort = false ) {
         $html = $this->_browser->get($url);
         
         if ($this->drupalCheckAuth(true)) {
@@ -80,5 +85,22 @@ class CiviTestCase extends DrupalTestCase
         $this->assertTrue($ret, ' [browser] clicked link '. t($label) . " ($url_target) from $url_before");
         
         return $ret;
+    }
+
+    function allPermissions( ) {
+        return array(
+                      1 => 'add contacts'               ,
+                      2 => 'view all contacts'          ,
+                      3 => 'edit all contacts'          ,
+                      4 => 'import contacts'            ,
+                      5 => 'edit groups'                ,
+                      6 => 'administer CiviCRM'         ,
+                      7 => 'access uploaded files'      ,
+                      8 => 'profile listings and forms' ,
+                      9 => 'access all custom data'     ,
+                     10 => 'view all activities'        ,
+                     11 => 'access CiviCRM'             ,
+                     12 => 'access Contact Dashboard'   ,
+                     );
     }
 }
