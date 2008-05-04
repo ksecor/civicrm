6adcb44fb607a4391520950f518daf9ae543781f
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-04 02:25:46 +0000

    helper functions for all contact types
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14230 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/CiviTest/Contact.php
+++ tests/CiviTest/Contact.php
@@ -7,22 +7,56 @@ class Contact extends DrupalTestCase
      *
      * @return $contactID id of created contact
      */
-    function create( ) {
+    function create( $params ) {
+        require_once "CRM/Contact/BAO/Contact.php";
+        $contactID = CRM_Contact_BAO_Contact::createProfileContact( $params, CRM_Core_DAO::$_nullArray );
+        return $contactID;
+    }
+
+    /*
+     * Helper function to create
+     * a contact of type Individual
+     *
+     * @return $contactID id of created Individual
+     */
+    function createIndividual( ) {
         $first_name = 'John';
         $last_name  = 'Doe';
         $contact_source = 'Testing purpose';
-        $email = 'abc@def.com';
+        
         $params = array(
                         'first_name'     => $first_name,
                         'last_name'      => $last_name,
-                        'contact_source' => $contact_source,
-                        'email-Primary'  => $email,
-                        'email'          => $email   
+                        'contact_source' => $contact_source
                         );
 
-        require_once 'CRM/Contact/BAO/Contact.php';
-        $contactID = CRM_Contact_BAO_Contact::createProfileContact( $params, CRM_Core_DAO::$_nullArray );
-        return $contactID;
+        return $this->create($params);
+    }
+
+    /*
+     * Helper function to create
+     * a contact of type Household
+     *
+     * @return $contactID id of created Household
+     */
+    function createHousehold( ) {
+        $household_name = "John Doe's home";
+        $params = array( 'household_name'     => $household_name );
+        
+        return $this->create($params);
+    }
+
+    /*
+     * Helper function to create
+     * a contact of type Organisation
+     *
+     * @return $contactID id of created Organisation
+     */
+    function createOrganisation( ) {
+        $organization_name = "My Organization";
+        $params = array( 'organization_name' => $organization_name );
+        
+        return $this->create($params);
     }
     
     /*
