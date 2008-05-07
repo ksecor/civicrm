d0f34c1cefa0c4941b5c6b72631304e73fddc63a
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-07 23:11:16 +0000

    fix install issues
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14353 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- drupal/install/index.php
+++ drupal/install/index.php
@@ -92,7 +92,7 @@ if ( file_exists( $drupalVersionFile ) ) {
     require_once $drupalVersionFile;
 }
 
-if ( !defined('VERSION') or version_compare(VERSION, '5') < 0 ) {
+if ( !defined('VERSION') or version_compare(VERSION, '6.0') < 0 ) {
     $errorTitle = "Oops! Incorrect Drupal Version";
     $errorMsg = "This installer can only be used with Drupal 5.x. Please ensure that '$drupalVersionFile' exists if you are running Drupal 5.0 and over. Refer to the online <a href='http://wiki.civicrm.org/confluence//x/mQ8' target='_blank' title='Opens Installation Documentation in a new window.'>Installation Guide</a> for information about installing CiviCRM on other Drupal versions OR installing CiviCRM for Joomla!";
     errorDisplayPage( $errorTitle, $errorMsg );
