c3ae59c3f726680226b9698fa4b7471700f69fd2
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-09 15:16:26 +0000

    hook dedupe to event registration
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14406 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration/Confirm.php
+++ CRM/Event/Form/Registration/Confirm.php
@@ -491,16 +491,12 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration
             $ctype = CRM_Core_DAO::getFieldValue("CRM_Contact_DAO_Contact", $contactID, "contact_type");
             $contactID =& CRM_Contact_BAO_Contact::createProfileContact( $params, $fields, $contactID, $addToGroups, null,$ctype);
         } else {
-            require_once "CRM/Core/BAO/UFGroup.php";
-            //formatted submiited fields before sending to dupe contact matching
-            $data = CRM_Core_BAO_UFGroup::formatFields( $params );
+            require_once 'CRM/Dedupe/Finder.php';
+            $dedupeParams = CRM_Dedupe_Finder::formatParams($params, 'Individual');
+            $ids = CRM_Dedupe_Finder::dupesByParams('Individual', $dedupeParams);
 
-            // finding contact record based on duplicate match 
-            $ids = CRM_Core_BAO_UFGroup::findContact( $data );
-            $contactsIDs = explode( ',', $ids );
-            
             // if we find more than one contact, use the first one
-            $contact_id  = $contactsIDs[0];
+            $contact_id  = $ids[0];
             $contactID =& CRM_Contact_BAO_Contact::createProfileContact( $params, $fields, $contact_id, $addToGroups );
             $this->set( 'contactID', $contactID );
         }
