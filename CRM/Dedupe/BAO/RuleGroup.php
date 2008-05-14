35d06c9fadf649bfd90c2d86ea4e36fff57b652f
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-14 15:26:19 +0000

    make dedupe return only contact ids matching given rule group's contact_type
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14479 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Dedupe/BAO/RuleGroup.php
+++ CRM/Dedupe/BAO/RuleGroup.php
@@ -120,7 +120,10 @@ class CRM_Dedupe_BAO_RuleGroup extends CRM_Dedupe_DAO_RuleGroup
      * Return the SQL query for getting only the interesting results out of the dedupe table.
      */
     function thresholdQuery() {
-        return "SELECT id1, id2, SUM(weight) threshold FROM dedupe GROUP BY id1, id2 HAVING threshold >= {$this->threshold}";
+        return "SELECT id1, id2
+            FROM dedupe JOIN civicrm_contact c1 ON id1 = c1.id JOIN civicrm_contact c2 ON id2 = c2.id
+            WHERE c1.contact_type = '{$this->contact_type}' AND c2.contact_type = '{$this->contact_type}'
+            GROUP BY id1, id2 HAVING SUM(weight) >= {$this->threshold}";
     }
 
     /**
