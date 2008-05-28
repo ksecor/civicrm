5285f3588e09ab420f1b6d2641b4802510e643bb
sunil <sunil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-28 11:35:05 +0000

    //change class naming convention for CRM-3086
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14762 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration/AdditionalParticipant.php
+++ CRM/Event/Form/Registration/AdditionalParticipant.php
@@ -40,7 +40,7 @@ require_once 'CRM/Event/Form/Registration.php';
  * This class generates form components for processing Event  
  * 
  */
-class CRM_Event_Form_AdditionalParticipant extends CRM_Event_Form_Registration
+class CRM_Event_Form__Registration_AdditionalParticipant extends CRM_Event_Form_Registration
 {
     /** 
      * Function to set variables up before form is built 
@@ -84,7 +84,7 @@ class CRM_Event_Form_AdditionalParticipant extends CRM_Event_Form_Registration
                                 )
                           );
 
-        $this->addFormRule( array( 'CRM_Event_Form_Registration_RegisterAdditionalParticipant', 'formRule' ),
+        $this->addFormRule( array( 'CRM_Event_Form_Registration_AdditionalParticipant', 'formRule' ),
                             $this );
     }
     /** 
