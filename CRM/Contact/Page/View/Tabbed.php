ef06057b8a7263ce8676df86a2ab805de7c9edd3
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-28 06:26:23 +0000

    more cleanup of menu
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14755 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Page/View/Tabbed.php
+++ CRM/Contact/Page/View/Tabbed.php
@@ -57,6 +57,12 @@ class CRM_Contact_Page_View_Tabbed extends CRM_Contact_Page_View {
         $entityType = CRM_Contact_BAO_Contact::getContactType($this->_contactId);
         $groupTree =& CRM_Core_BAO_CustomGroup::getTree($entityType, $this->_contactId);
         CRM_Core_BAO_CustomGroup::buildViewHTML( $this, $groupTree );
+
+        // also create the form element for the activity links box
+        $controller =& new CRM_Core_Controller_Simple( 'CRM_Activity_Form_ActivityLinks',
+                                                       ts('Activity Links'), null );
+        $controller->setEmbedded( true );
+        $controller->run( );
     }
 
     /**
