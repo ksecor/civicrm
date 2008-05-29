09aae0b2539140a57dfcff347e5f3f7c78aa1f2e
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-05-29 07:37:08 +0000

    worked on Online Contribution - Pro(test cases)
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@14786 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/CiviTest/ContributionPage.php
+++ tests/CiviTest/ContributionPage.php
@@ -7,34 +7,16 @@ class ContributionPage extends DrupalTestCase
      *
      * @return $contributionPage id of created Contribution Page
      */
-    function create( ) 
+    function create( $id ) 
     {
-        require_once "CRM/Core/DAO/PaymentProcessor.php";
-        $paymentProcessor =& new CRM_Core_DAO_PaymentProcessor( );
-        $paymentParams = array(
-                               'domain_id' => 1,
-                               'name' => 'demo',
-                               'payment_processor_type' => 'PayPal',
-                               'is_active' => 1,
-                               'is_default' => 0,
-                               'is_test' => 1,
-                               'user_name' => 'bharat_axnzoom_api1.yahoo.com',
-                               'password' => 'JTU844G729L2ZTJZ',
-                               'signature' => 'An5ns1Kso7MWUdW4ErQKJJJ4qi4-Amskj10OWUIlV7zig.SePDYaN46Z',
-                               'url_site' => 'https://www.sandbox.paypal.com/',
-                               'url_api' => 'https://api-3t.sandbox.paypal.com/',
-                               'url_button' => 'https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif',
-                               'class_name' => 'Payment_PayPalImpl',
-                               'billing_mode' => 3
-                               );
-        $paymentProcessor->copyValues( $paymentParams );
-        $paymentProcessor->save( );
-        
         require_once "CRM/Contribute/BAO/ContributionPage.php";        
-        $params = array('title'                    => 'Help Test CiviCRM!',
+        $domain =  CRM_Core_Config::domainID( );
+        $params = array(
+                        'domain_id'                => $domain,
+                        'title'                    => 'Help Test CiviCRM!',
                         'intro_text'               => 'Created for Test Coverage Online Contribution Page',
                         'contribution_type_id'     => 1,
-                        'payment_processor_id'     => $paymentProcessor->id,
+                        'payment_processor_id'     => $id,
                         'is_monetary'              => 1,
                         'is_allow_other_amount'    => 1,
                         'min_amount'               => 10,
---
+++ tests/CiviTest/PaypalPro.php
@@ -0,0 +1,39 @@
+<?php
+class PaypalPro extends DrupalTestCase 
+{
+    /*
+     * Helper function to create
+     * a payment processor of type Paypal Pro
+     *
+     * @return $paymentProcessor id of created payment processor
+     */
+    function create( ) 
+    {
+        require_once "CRM/Core/DAO/PaymentProcessor.php";
+        $paymentProcessor =& new CRM_Core_DAO_PaymentProcessor( );
+        
+        $domain =  CRM_Core_Config::domainID( );
+        $paymentParams = array(
+                               'domain_id'              => $domain,
+                               'name'                   => 'demo',
+                               'payment_processor_type' => 'PayPal',
+                               'is_active'              => 1,
+                               'is_default'             => 0,
+                               'is_test'                => 1,
+                               'user_name'              => 'bharat_axnzoom_api1.yahoo.com',
+                               'password'               => 'JTU844G729L2ZTJZ',
+                               'signature'              => 'An5ns1Kso7MWUdW4ErQKJJJ4qi4-Amskj10OWUIlV7zig.SePDYaN46Z',
+                               'url_site'               => 'https://www.sandbox.paypal.com/',
+                               'url_api'                => 'https://api-3t.sandbox.paypal.com/',
+                               'url_button'             => 'https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif',
+                               'class_name'             => 'Payment_PayPalImpl',
+                               'billing_mode'           => 3
+                               );
+        $paymentProcessor->copyValues( $paymentParams );
+        $paymentProcessor->save( );
+        return $paymentProcessor->id;
+    }
+  
+}
+
+?>
