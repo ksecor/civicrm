31b880d84365988ab1260b68b6f459e72c89c498
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-06-15 04:49:36 +0000

    add trim filter for mapping name
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15142 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Import/Form/MapField.php
+++ CRM/Import/Form/MapField.php
@@ -215,6 +215,7 @@ class CRM_Import_Form_MapField extends CRM_Core_Form
         //to save the current mappings
         if ( !$this->get('savedMapping') ) {
             $saveDetailsName = ts('Save this field mapping');
+            $this->applyFilter('saveMappingName', 'trim');
             $this->add('text','saveMappingName',ts('Name'));
             $this->add('text','saveMappingDesc',ts('Description'));
         } else {
