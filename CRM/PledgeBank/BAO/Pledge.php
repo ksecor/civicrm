f12fa1a7ee20f0fbc4e94dc33c85c98968260183
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-06-17 09:46:35 +0000

    added basic framework for Pledges, need to cleanup documentation :)
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15178 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/PledgeBank/BAO/Pledge.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2008                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2007
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Pledge/DAO/Pledge.php';
+
+class CRM_Pledge_BAO_Pledge extends CRM_Pledge_DAO_Pledge 
+{
+
+    /**
+     * class constructor
+     */
+    function __construct( ) 
+    {
+        parent::__construct( );
+    }
+    
+    /**
+     * Takes a bunch of params that are needed to match certain criteria and
+     * retrieves the relevant objects. Typically the valid params are only
+     * contact_id. We'll tweak this function to be more full featured over a period
+     * of time. This is the inverse function of create. It also stores all the retrieved
+     * values in the default array
+     *
+     * @param array $params   (reference ) an assoc array of name/value pairs
+     * @param array $defaults (reference ) an assoc array to hold the flattened values
+     *
+     * @return object CRM_Pledge_BAO_Pledge object
+     * @access public
+     * @static
+     */
+    static function retrieve( &$params, &$defaults ) 
+    {
+        $event  = new CRM_Pledge_DAO_Pledge( );
+        $event->copyValues( $params );
+        if ( $event->find( true ) ) {
+            CRM_Core_DAO::storeValues( $event, $defaults );
+            return $event;
+        }
+        return null;
+    }
+
+    /**
+     * update the is_active flag in the db
+     *
+     * @param int      $id        id of the database record
+     * @param boolean  $is_active value we want to set the is_active field
+     *
+     * @return Object             DAO object on sucess, null otherwise
+     * @static
+     */
+    static function setIsActive( $id, $is_active ) 
+    {
+        return CRM_Core_DAO::setFieldValue( 'CRM_Pledge_DAO_Pledge', $id, 'is_active', $is_active );
+    }
+    
+    /**
+     * function to add pledge
+     *
+     * @param array $params reference array contains the values submitted by the form
+     * 
+     * @access public
+     * @static 
+     * @return object
+     */
+    static function add( &$params, &$ids)
+    {
+        require_once 'CRM/Utils/Hook.php';
+        
+        if ( CRM_Utils_Array::value( 'id', $params ) ) {
+            CRM_Utils_Hook::pre( 'edit', 'Pledge', $params['id'], $params );
+        } else {
+            CRM_Utils_Hook::pre( 'create', 'Pledge', null, $params ); 
+        }
+        
+        $pledge =& new CRM_Pledge_DAO_Pledge( );
+        
+        $pledge->copyValues( $params );
+        $result = $pledge->save( );
+        
+        if ( CRM_Utils_Array::value( 'id', $params ) ) {
+            CRM_Utils_Hook::post( 'edit', 'Pledge', $pledge->id, $pledge );
+        } else {
+            CRM_Utils_Hook::post( 'create', 'Pledge', $pledge->id, $pledge );
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Function to delete the pledge
+     *
+     * @param int $id  pledge id
+     *
+     * @access public
+     * @static
+     *
+     */
+    static function del( $id )
+    { 
+
+    }
+
+}
+
---
+++ CRM/PledgeBank/Form/Search.php
@@ -0,0 +1,248 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2008                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing   
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2007
+ * $Id$
+ *
+ */
+
+/**
+ * Files required
+ */
+require_once 'CRM/Pledge/PseudoConstant.php';
+require_once 'CRM/Pledge/Selector/Search.php';
+require_once 'CRM/Core/Selector/Controller.php';
+
+
+/**
+ * This file is for civievent search
+ */
+class CRM_Pledge_Form_Search extends CRM_Core_Form
+{
+    /** 
+     * Are we forced to run a search 
+     * 
+     * @var int 
+     * @access protected 
+     */ 
+    protected $_force; 
+
+    /** 
+     * name of search button 
+     * 
+     * @var string 
+     * @access protected 
+     */ 
+    protected $_searchButtonName;
+
+    /** 
+     * name of print button 
+     * 
+     * @var string 
+     * @access protected 
+     */ 
+    protected $_printButtonName; 
+ 
+    /** 
+     * name of action button 
+     * 
+     * @var string 
+     * @access protected 
+     */ 
+    protected $_actionButtonName;
+
+    /** 
+     * form values that we will be using 
+     * 
+     * @var array 
+     * @access protected 
+     */ 
+    protected $_formValues; 
+
+    /**
+     * the params that are sent to the query
+     * 
+     * @var array 
+     * @access protected 
+     */ 
+    protected $_queryParams;
+    
+    /** 
+     * have we already done this search 
+     * 
+     * @access protected 
+     * @var boolean 
+     */ 
+    protected $_done; 
+
+    /**
+     * are we restricting ourselves to a single contact
+     *
+     * @access protected  
+     * @var boolean  
+     */  
+    protected $_single = false;
+
+    /** 
+     * are we restricting ourselves to a single contact 
+     * 
+     * @access protected   
+     * @var boolean   
+     */   
+    protected $_limit = null;
+
+    /** 
+     * what context are we being invoked from 
+     *    
+     * @access protected      
+     * @var string 
+     */      
+    protected $_context = null; 
+    
+    /** 
+     * prefix for the controller
+     * 
+     */
+    protected $_prefix = "pledge_";
+
+    protected $_defaults;
+
+
+    /** 
+     * processing needed for buildForm and later 
+     * 
+     * @return void 
+     * @access public 
+     */ 
+    function preProcess( ) 
+    { 
+    }
+    
+    /**
+     * Build the form
+     *
+     * @access public
+     * @return void
+     */
+    function buildQuickForm( ) 
+    {
+    }
+    
+    /**
+     * The post processing of the form gets done here.
+     *
+     * Key things done during post processing are
+     *      - check for reset or next request. if present, skip post procesing.
+     *      - now check if user requested running a saved search, if so, then
+     *        the form values associated with the saved search are used for searching.
+     *      - if user has done a submit with new values the regular post submissing is 
+     *        done.
+     * The processing consists of using a Selector / Controller framework for getting the
+     * search results.
+     *
+     * @param
+     *
+     * @return void 
+     * @access public
+     */
+    function postProcess( ) 
+    {
+    }
+    
+    /**
+     * This function is used to add the rules (mainly global rules) for form.
+     * All local rules are added near the element
+     *
+     * @return None
+     * @access public
+     * @see valid_date
+     */
+    function addRules( )
+    {
+        $this->addFormRule( array( 'CRM_Pledge_Form_Search', 'formRule' ) );
+    }
+
+    /**
+     * global validation rules for the form
+     *
+     * @param array $fields posted values of the form
+     * @param array $errors list of errors to be posted back to the form
+     *
+     * @return void
+     * @static
+     * @access public
+     */
+    static function formRule( &$fields )
+    {
+        $errors = array( );
+
+        if ( !empty($errors) ) {
+            return $errors;
+        } 
+        
+        return true;
+    }
+
+    /**
+     * Set the default form values
+     *
+     * @access protected
+     * @return array the default array reference
+     */
+    function &setDefaultValues() 
+    {
+        $defaults = array();
+        $defaults = $this->_formValues;
+        return $defaults;
+    }
+
+
+    function fixFormValues( )
+    {
+    }
+
+    function getFormValues( ) 
+    {
+        return null;
+    }
+
+    /**
+     * Return a descriptive name for the page, used in wizard header
+     *
+     * @return string
+     * @access public
+     */
+    public function getTitle( ) 
+    {
+        return ts('Find Pledge Signer');
+    }
+
+}
+
