e8a932c0ef6ad301e6311a347e6ca7f184bc4691
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-06-20 08:36:01 +0000

    minor fix
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15244 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/Query.php
+++ CRM/Contact/BAO/Query.php
@@ -923,7 +923,7 @@ class CRM_Contact_BAO_Query
         }
 
         if ( ! $likeNames ) {
-            $likeNames = array( 'sort_name', 'email', 'notes', 'display_name', 'pledge_name' );
+            $likeNames = array( 'sort_name', 'email', 'notes', 'display_name' );
         }
 
         if ( ! $useEquals &&
