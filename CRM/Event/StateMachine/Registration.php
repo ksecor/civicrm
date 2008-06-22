6869fbb147f8536151bc5b6cd7daec55cea5637a
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-06-22 23:51:26 +0000

    worked on CRM-3086
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15276 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/StateMachine/Registration.php
+++ CRM/Event/StateMachine/Registration.php
@@ -64,15 +64,16 @@ class CRM_Event_StateMachine_Registration extends CRM_Core_StateMachine
         // check that the controller has some data, hence we dont send the form name                                         
         // which results in an invalid argument error                                                                        
         $values = $controller->exportValues( );
-        if ( isset( $values['additional_participants'] ) ) {
-            $additionalParticipant = $values['additional_participants'];
-        } else if ( isset( $_POST['additional_participants'] ) ) {
+        //first check POST value then QF
+        if ( isset( $_POST['additional_participants'] ) ) {
             // we need to use $_POST since the QF framework has not yet been called
             // and the additional participants page is the next one, so need to set this up
             // now
             $additionalParticipant = $_POST['additional_participants'];
+        } else if ( isset( $values['additional_participants'] ) ) {
+            $additionalParticipant = $values['additional_participants'];
         }
-
+        
         if ( $additionalParticipant ) {
             $additionalParticipant = CRM_Utils_Type::escape( $additionalParticipant, 'Integer' );
             $controller->set( 'addParticipant', $additionalParticipant );
