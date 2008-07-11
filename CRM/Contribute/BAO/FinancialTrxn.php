c61f830e15fe4e659e2873117e802e9251cc8bc2
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-07-11 19:13:35 +0000

    fix CRM-3317
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15775 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/BAO/FinancialTrxn.php
+++ CRM/Contribute/BAO/FinancialTrxn.php
@@ -61,7 +61,16 @@ class CRM_Contribute_BAO_FinancialTrxn extends CRM_Contribute_DAO_FinancialTrxn
             $config =& CRM_Core_Config::singleton();
             $trxn->currency = $config->defaultCurrency;
         }
-        
+
+        // if a transaction already exists for a contribution id, lets get the id
+        $id = CRM_Core_DAO::getFieldValue( 'CRM_Contribute_DAO_FinancialTrxn',
+                                           $trxn->contribution_id,
+                                           'id'
+                                           'contribution_id' );
+        if ( $id ) {
+            $trxn->id = $id;
+        }
+                                           
         return $trxn->save();
     }
 
