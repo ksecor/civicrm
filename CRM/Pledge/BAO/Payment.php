3e1cce2707056eed57c253ec8d83568328975785
shailesh <shailesh@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-07-17 08:18:45 +0000

    //worked on CRM-3244
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@15857 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Pledge/BAO/Payment.php
+++ CRM/Pledge/BAO/Payment.php
@@ -89,49 +89,45 @@ WHERE pledge_id = %1
     { 
         require_once 'CRM/Core/Transaction.php';
         $transaction = new CRM_Core_Transaction( );
-
-        $scheduled_date =  $params['scheduled_date'];
-        $params['scheduled_date'] =  CRM_Utils_Date::format( $params['scheduled_date'] );
-        $params['scheduled_amount'] = ceil($params['scheduled_amount']);
-        $payment = self::add( $params );
-        if ( is_a( $payment, 'CRM_Core_Error') ) {
-            $transaction->rollback( );
-            return $payment;
-        }
         
+        $scheduled_date =  $params['scheduled_date'];
         //calculation of schedule date according to frequency day of period
         //frequency day is not applicable for daily installments
         if ( $params['frequency_unit'] != 'day' ) {
             if ( $params['frequency_unit'] != 'week' ) {
-
+                
                 //for month use day of next month & for year use day of month Jan of next year as next payment date 
                 $scheduled_date['d'] = $params['frequency_day'];
                 if ( $params['frequency_unit'] == 'year' ) {
                     $scheduled_date['M'] = 1;
                 }   
             } else if ( $params['frequency_unit'] == 'week' ) {
-
+                
                 //for week calculate day of week ie. Sunday,Monday etc. as next payment date
                 $dayOfWeek = date('w',mktime(0, 0, 0, $scheduled_date['M'], $scheduled_date['d'], $scheduled_date['Y'] ));
-                $frequencyDay =  $dayOfWeek - $params['frequency_day'];
-                if ( $frequencyDay < 0 ) {
-                    $frequencyDay += 1;
-                }
-                $scheduleDate =  explode ( "-", date( 'n-j-Y', mktime ( 0, 0, 0, $scheduled_date['M'], $scheduled_date['d'] - $frequencyDay, $scheduled_date['Y'] )) );
+                $frequencyDay =   $params['frequency_day'] - $dayOfWeek;
+                
+                $scheduleDate =  explode ( "-", date( 'n-j-Y', mktime ( 0, 0, 0, $scheduled_date['M'], $scheduled_date['d'] + $frequencyDay, $scheduled_date['Y'] )) );
                 $scheduled_date['M'] = $scheduleDate[0];
                 $scheduled_date['d'] = $scheduleDate[1];
                 $scheduled_date['Y'] = $scheduleDate[2];
             }
-        } 
-
+        }
+        
+        //calculate the scheduled date for every installment
+        $prevScheduledDate = array ( );
+        $prevScheduledDate[1] = CRM_Utils_Date::format( $params['scheduled_date'] );
         for ( $i = 1; $i < $params['installments']; $i++ ) {
-            //calculate the scheduled amount for every installment
-            $params['scheduled_date'] =  CRM_Utils_Date::format(CRM_Utils_Date::intervalAdd( $params['frequency_unit'], $i, $scheduled_date ));
-            
-            if ( $i == $params['installments'] - 1 ) {
-                $params['scheduled_amount'] = $params['amount'] - $i * $params['scheduled_amount'];
+            $prevScheduledDate[$i+1] = CRM_Utils_Date::format(CRM_Utils_Date::intervalAdd( $params['frequency_unit'], $i, $scheduled_date ));
+        }
+        
+        $params['scheduled_amount'] = ceil($params['scheduled_amount']);
+        for ( $i = 1; $i <= $params['installments']; $i++ ) {
+            //calculate the scheduled amount for every installment.
+            if ( $i == $params['installments'] ) {
+                $params['scheduled_amount'] = $params['amount'] - ($i-1) * $params['scheduled_amount'];
             }
-                
+            $params['scheduled_date'] = $prevScheduledDate[$i];    
             $payment = self::add( $params );
             if ( is_a( $payment, 'CRM_Core_Error') ) {
                 $transaction->rollback( );
@@ -145,7 +141,7 @@ WHERE pledge_id = %1
         }
         
         $transaction->commit( );
-               
+        
         return $payment;
     }
 
