9b0338ec7c3550b4d9713d526a75b3508f597b77
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-07-22 10:16:07 +0000

    temporary commenting broken code. CRM-3014
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@16004 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Selector/Activity.php
+++ CRM/Activity/Selector/Activity.php
@@ -284,28 +284,30 @@ class CRM_Activity_Selector_Activity extends CRM_Core_Selector_Base implements C
         }
 
         $activityStatus = CRM_Core_PseudoConstant::activityStatus( );
-        
-        foreach ($rows as $k => $row) {
-            $cids[] = $row['assignee_contact_ids'][0];
-            $cids[] = $row['target_contact_ids'][0];
-        }
-        $cidsIn = implode(',',$cids);
-        $qparams = array();
-        $query = "SELECT civicrm_contact.sort_name as sort_name, civicrm_contact.id as id  
-                  FROM civicrm_contact 
-                  where civicrm_contact.id IN ({$cidsIn}) ";
-        $dao = CRM_Core_DAO::executeQuery($query,$qparams );
-        $contactIdName = array();
-        while ( $dao->fetch() ) {
-            $contactIdName[$dao->id] = $dao->sort_name;
-        }
-   
+
+        //Fix ME: temp. commenting the code         
+//         foreach ($rows as $k => $row) {
+//             $cids[] = $row['assignee_contact_ids'][0];
+//             $cids[] = $row['target_contact_ids'][0];
+//         }
+
+//         $cidsIn = implode(',',$cids);
+
+//         $qparams = array();
+//         $query = "SELECT civicrm_contact.sort_name as sort_name, civicrm_contact.id as id  
+//                   FROM civicrm_contact 
+//                   where civicrm_contact.id IN ({$cidsIn}) ";
+//         $dao = CRM_Core_DAO::executeQuery($query,$qparams );
+//         $contactIdName = array();
+//         while ( $dao->fetch() ) {
+//             $contactIdName[$dao->id] = $dao->sort_name;
+//         }
+
         foreach ($rows as $k => $row) {
             $row =& $rows[$k];
             
-            $row['assignee_contact_name'] = $contactIdName[$row['assignee_contact_ids'][0]];
-            
-            $row['target_contact_name']   = $contactIdName[$row['target_contact_ids'][0]];
+//             $row['assignee_contact_name'] = $contactIdName[$row['assignee_contact_ids'][0]];
+//             $row['target_contact_name']   = $contactIdName[$row['target_contact_ids'][0]];
             
             // DRAFTING: provide a facility for db-stored strings
             // localize the built-in activity names for display
