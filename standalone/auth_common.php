3104727755d8d942ae350e1e86618d07119684d6
wmorgan <wmorgan@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-07-23 21:14:43 +0000

    oops, previous revision had the change on the wrong line; corrected
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@16061 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- standalone/auth_common.php
+++ standalone/auth_common.php
@@ -79,7 +79,7 @@ function getScheme() {
 function getReturnTo() {
     $urlPort = getUrlPort();
     
-    return sprintf("%s://%s:%s%s/finish_auth.php",
+    return sprintf("%s://%s%s%s/finish_auth.php",
                    getScheme(), $_SERVER['SERVER_NAME'],
                    $urlPort,
                    dirname($_SERVER['PHP_SELF']));
@@ -88,7 +88,7 @@ function getReturnTo() {
 function getTrustRoot() {
     $urlPort = getUrlPort();
     
-    return sprintf("%s://%s%s%s/",
+    return sprintf("%s://%s:%s%s/",
                    getScheme(), $_SERVER['SERVER_NAME'],
                    $urlPort,
                    dirname($_SERVER['PHP_SELF']));
