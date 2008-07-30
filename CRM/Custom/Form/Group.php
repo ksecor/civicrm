d7972085d1024a3d4870a2856de5e4f5bd61d6a0
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-07-30 05:58:00 +0000

    Status msg fix.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@16232 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Custom/Form/Group.php
+++ CRM/Custom/Form/Group.php
@@ -289,10 +289,10 @@ class CRM_Custom_Form_Group extends CRM_Core_Form {
         CRM_Core_BAO_Cache::deleteGroup( 'contact fields' );
       
         if ($this->_action & CRM_Core_Action::UPDATE) {
-            CRM_Core_Session::setStatus(ts('Your Group \'%1 \' has been saved.', array(1 => $group->title)));
+            CRM_Core_Session::setStatus(ts('Your custom data group \'%1 \' has been saved.', array(1 => $group->title)));
         } else {
             $url = CRM_Utils_System::url( 'civicrm/admin/custom/group/field', 'reset=1&action=add&gid=' . $group->id);
-            CRM_Core_Session::setStatus(ts('Your Group \'%1\' has been added. You can add custom fields to this group now.',
+            CRM_Core_Session::setStatus(ts('Your custom data group \'%1\' has been added. You can add custom fields to this group now.',
                                            array(1 => $group->title)));
             $session =& CRM_Core_Session::singleton( );
             $session->replaceUserContext($url);
