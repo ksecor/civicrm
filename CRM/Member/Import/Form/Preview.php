8b52180d5c5a3726360ec90421233bceec9496a2
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-08-12 02:33:25 +0000

    fix CRM-2779
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@16576 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Member/Import/Form/Preview.php
+++ CRM/Member/Import/Form/Preview.php
@@ -209,7 +209,7 @@ class CRM_Member_Import_Form_Preview extends CRM_Core_Form {
                 $errorMessage[] = $value['message'];
             }
             
-            $errorFile = $fileName . '.error.log';
+            $errorFile = $fileName['name'] . '.error.log';
             
             if ( $fd = fopen( $errorFile, 'w' ) ) {
                 fwrite($fd, implode('\n', $errorMessage));
