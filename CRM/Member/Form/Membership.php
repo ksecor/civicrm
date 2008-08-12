9b71a4474a77d013deaa92ac9c289f2f85f5647b
pankaj <pankaj@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-08-12 14:07:15 +0000

    fix for CRM-3273.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@16595 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Member/Form/Membership.php
+++ CRM/Member/Form/Membership.php
@@ -776,7 +776,7 @@ class CRM_Member_Form_Membership extends CRM_Member_Form
                 if ( CRM_Utils_Array::value( 'billing_last_name', $this->_params ) ) {
                     $name .= " {$this->_params['billing_last_name']}";
                 }
-                $this->assign( 'name', $name );
+                $this->assign( 'billingName', $name );
                 
                 // assign the address formatted up for display
                 $addressParts  = array( "street_address-{$this->_bltID}",
