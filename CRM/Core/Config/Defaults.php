0cfb7b71c0aa7c96170228f0fc4dd6bb196ae970
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-08-25 14:14:34 +0000

    fixed an issue with calculating image url
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.1@16824 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Config/Defaults.php
+++ CRM/Core/Config/Defaults.php
@@ -173,7 +173,7 @@ class CRM_Core_Config_Defaults
                 $tempURL = str_replace( "/administrator/", "/", $baseURL );
                 $defaults['imageUploadURL'] = $tempURL . "media/civicrm/persist/contribute/";
             } else {
-                $defaults['imageUploadURL'] = $baseURL . "files/civicrm/persist/contribute/";
+                $defaults['imageUploadURL'] = $baseURL . "sites/default/files/civicrm/persist/contribute/";
             }
         }
 
