1fbd8769cc32f8d479d74288d9c0c72042cafc84
shailesh <shailesh@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-09-19 08:16:10 +0000

    //-fixed CRM-3251 //List-Unsubscribe header is added
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17144 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Mailing/BAO/Mailing.php
+++ CRM/Mailing/BAO/Mailing.php
@@ -841,16 +841,16 @@ AND    civicrm_mailing.id = civicrm_mailing_job.mailing_id";
         $urls = array(
                       'forward'        => CRM_Utils_System::url('civicrm/mailing/forward', 
                                                                 "reset=1&jid={$job_id}&qid={$event_queue_id}&h={$hash}",
-                                                                false, null, true, true),
+                                                                true, null, true, true),
                       'unsubscribeUrl' => CRM_Utils_System::url('civicrm/mailing/unsubscribe', 
                                                                 "reset=1&jid={$job_id}&qid={$event_queue_id}&h={$hash}",
-                                                                false, null, true, true),
+                                                                true, null, true, true),
                       'resubscribeUrl' => CRM_Utils_System::url('civicrm/mailing/resubscribe', 
                                                                 "reset=1&jid={$job_id}&qid={$event_queue_id}&h={$hash}",
-                                                                false, null, true, true),
+                                                                true, null, true, true),
                       'optOutUrl'      => CRM_Utils_System::url('civicrm/mailing/optout', 
                                                                 "reset=1&jid={$job_id}&qid={$event_queue_id}&h={$hash}",
-                                                                false, null, true, true),
+                                                                true, null, true, true),
                       );
 
         if ( $skipEncode ) {
@@ -860,9 +860,10 @@ AND    civicrm_mailing.id = civicrm_mailing_job.mailing_id";
         }
 
         $headerPart = array(
-                            'Return-Path' => CRM_Utils_Verp::encode($verp['bounce'], $email),
-                            'From'      => "\"{$this->from_name}\" <{$this->from_email}>",
-                            'Subject'   => $this->subject,
+                            'Return-Path'      => CRM_Utils_Verp::encode($verp['bounce'], $email),
+                            'From'             => "\"{$this->from_name}\" <{$this->from_email}>",
+                            'Subject'          => $this->subject,
+                            'List-Unsubscribe' => '<mailto:'.$verp['unsubscribe'].'>'
                             );
 
         $headers = array_merge( $headers, $headerPart ); 
