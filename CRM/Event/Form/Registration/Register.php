32166d29cf1dc987dbb0cc5cf6ceb39bac94cc5f
sunil <sunil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-09-24 09:50:43 +0000

    added comments for price set validation block CRM-2534
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.1@17231 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration/Register.php
+++ CRM/Event/Form/Registration/Register.php
@@ -331,7 +331,7 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration
     {
         //To check if the user is already registered for the event(CRM-2426)
         self::checkRegistration($fields, $self);
-     
+             
         $email = $fields["email-{$self->_bltID}"];
         require_once 'CRM/Core/BAO/UFMatch.php';
         if ( CRM_Core_BAO_UFMatch::isDuplicateUser( $email ) ) {
@@ -385,8 +385,11 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration
             $zeroAmountError = ts( "The Zero amount facility is only for the valid members" );
             if ( $fields['priceSetId'] ) { 
                 $zeroAmount = array( );
+                //collect all option value id in array
                 foreach( $fields as $key => $val  )  {
                     if ( substr( $key, 0, 6 ) == 'price_' ) {
+                        //for Text html type we don't have the option
+                        //value id, validate it using htmlType
                         $htmlType = CRM_Core_DAO::getFieldValue( 'CRM_Core_BAO_PriceField', substr( $key, 6 ) , 'html_type' );
                         if( $htmlType != 'Text' ) {
                             if ( is_array( $val) ) {
@@ -399,20 +402,27 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration
                         } else if ( $htmlType == 'Text' && $val && $val <= 0 ) {
                             $errors[$key] =  ts( "The Number of Items must be greater than zero (0)" );
                         } else if ( $htmlType == 'Text' && !$userID && $val == 0 && is_numeric( $val ) ) {
+                            //for text type option value id is absent
+                            //validate it here
                             $errors[$key] = $zeroAmountError;
                         } 
                     } 
                 }
+                //zero amount is an array of all option value id, only
+                //text field type is not present in this array because
+                //option value id is absent. so text field type is
+                //validate in above block
                 foreach( $zeroAmount as $keyes => $values  )  {
                     if( $values && !$userID && 
-                        CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue', $values, 'name', 'id' ) == 0 ) {
+                        CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue', $values, 'name' ) == 0 ) {
                         $errors['amount'] = $zeroAmountError;
                     }
                 }
             } else {
                 $zeroAmount = $fields['amount'];
+                //$zeroAmount is the id of the option value
                 if ( $zeroAmount && !$userID && 
-                     CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue',$zeroAmount, 'value', 'id' ) == 0 ) {
+                     CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue',$zeroAmount, 'value' ) == 0 ) {
                     $errors['amount'] = $zeroAmountError;
                 }
             }
@@ -428,21 +438,27 @@ class CRM_Event_Form_Registration_Register extends CRM_Event_Form_Registration
                     return empty( $errors ) ? true : $errors;
                 }
             } else if ( $fields['priceSetId'] ) { 
+                //here take all value(amount) of optoin value id
                 $check = array( );
                 foreach( $fields as $key => $val  )  {
                     if ( substr( $key, 0, 6 ) == 'price_' && $val != 0) {
                         $htmlType = CRM_Core_DAO::getFieldValue( 'CRM_Core_BAO_PriceField', substr( $key, 6 ) , 'html_type' );
                         if ( is_array( $val) ) {
+                            //$keys is the id of the option value
                             foreach( $val as $keys => $vals  )  {
                                 $check[] = CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue', $keys, 'name');
                             }
                         } else if( $htmlType == 'Text') {
                             $check[] = $val;  
                         } else {
+                            //$val is the id of the option value
                             $check[] = CRM_Core_DAO::getFieldValue( 'CRM_Core_DAO_OptionValue', $val, 'name');
                         }
                     }
                 }
+                //validation for submitted each value is zero
+                //if not zero give credit card validation error else
+                //bypass it.
                 $level = count ( $check );
                 $j = null;
                 for ($i = 0; $i < $level; $i++ ) {
