c25052a1679d4f4f0add208f423922444b87f019
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-09-26 14:23:31 +0000

    added advanced options for tinyMCE
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.1@17276 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/HTML/QuickForm/tinymce.php
+++ packages/HTML/QuickForm/tinymce.php
@@ -142,12 +142,27 @@ class HTML_QuickForm_TinyMCE extends HTML_QuickForm_textarea
                                    $config->resourceBase . $this->BasePath . 'tiny_mce.js'
                                    );
                 
-                //FIXME: We might want to pass some parameters to TinyMCE
-                $html .= sprintf( '<script type="text/javascript">tinyMCE.init({ mode : "textareas",' .
-                                  'theme : "simple",' .
-                                  'height: "200",' .
-                                  'width : "700",'.
-                                  'editor_selector : "form-TinyMCE"}); </script>'  );
+                $html .= sprintf( '<script type="text/javascript">
+tinyMCE.init({
+mode : "textareas",
+theme : "advanced",
+editor_selector : "form-TinyMCE",
+plugins : "safari,spellchecker,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,pagebreak,imagemanager,filemanager",
+theme_advanced_buttons1_add : "fontselect,fontsizeselect",
+theme_advanced_buttons2_add : "separator,insertdate,inserttime,preview,separator,forecolor,backcolor",
+theme_advanced_buttons2_add_before: "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
+theme_advanced_buttons3_add_before : "tablecontrols,separator",
+theme_advanced_buttons3_add : "emotions,iespell,media,advhr,separator,print,separator,ltr,rtl,separator,fullscreen",
+theme_advanced_toolbar_location : "top",
+theme_advanced_toolbar_align : "left",
+theme_advanced_statusbar_location : "bottom",
+theme_advanced_resize_horizontal : false,
+theme_advanced_resizing : true,
+apply_source_formatting : true,
+spellchecker_languages : "+English=en,Danish=da,Dutch=nl,Finnish=fi,French=fr,German=de,Italian=it,Polish=pl,Portuguese=pt,Spanish=es,Swedish=sv"
+});
+</script>' );
+
                 define('HTML_QUICKFORM_TINYMCEEDITOR_LOADED', true);
             }
                         
