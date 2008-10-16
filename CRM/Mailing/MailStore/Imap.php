448e6e6f98f2170744a18b019bf7164a870d29a1
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-10-16 00:05:19 +0000

    the initial approach to processing IMAP and POP3 CiviMail accounts
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17506 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Mailing/MailStore/Imap.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2008                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2008
+ * $Id$
+ *
+ */
+
+require_once 'ezc/Base/src/ezc_bootstrap.php';
+require_once 'ezc/autoload/mail_autoload.php';
+require_once 'CRM/Mailing/MailStore.php';
+
+class CRM_Mailing_MailStore_Imap extends CRM_Mailing_MailStore
+{
+    /**
+     * Connect to the supplied IMAP server and make sure the two mailboxes exist
+     *
+     * @param string $host      host to connect to
+     * @param string $username  authentication username
+     * @param string $password  authentication password
+     * @param bool   $ssl       whether to use IMAP or IMAPS
+     * @param string $folder    name of the inbox folder
+     * @return void
+     */
+    function __construct($host, $username, $password, $ssl = true, $folder = 'Inbox')
+    {
+        $options = new ezcMailImapTransportOptions;
+        $options->ssl = $ssl;
+        $options->uidReferencing = true;
+        $this->_transport = new ezcMailImapTransport($host, null, $options);
+        $this->_transport->authenticate($username, $password);
+        $this->_transport->selectMailbox($folder);
+
+        $this->_ignored   = 'CiviMail.ignored';
+        $this->_processed = 'CiviMail.processed';
+        $boxes = $this->_transport->listMailboxes();
+        if (!in_array($this->_ignored,   $boxes)) $this->_transport->createMailbox($this->_ignored);
+        if (!in_array($this->_processed, $boxes)) $this->_transport->createMailbox($this->_processed);
+    }
+
+    /**
+     * Move the specified message to the ignored folder
+     *
+     * @param integer $nr  number of the message to move
+     * @return void
+     */
+    function markIgnored($nr)
+    {
+        $this->_transport->copyMessages($nr, $this->_ignored);
+        $this->_transport->delete($nr);
+    }
+
+    /**
+     * Move the specified message to the processed folder
+     *
+     * @param integer $nr  number of the message to move
+     * @return void
+     */
+    function markProcessed($nr)
+    {
+        $this->_transport->copyMessages($nr, $this->_processed);
+        $this->_transport->delete($nr);
+    }
+}
---
+++ bin/CiviMailProcessor.php
@@ -0,0 +1,125 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.1                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2008                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2008
+ * $Id$
+ *
+ */
+
+class CiviMailProcessor {
+
+    function process() {
+
+        // retrieve the emails
+        require_once 'CRM/Mailing/MailStore.php';
+        $store = CRM_Mailing_MailStore::getStore();
+        $mails = $store->allMails();
+
+        require_once 'api/Mailer.php';
+        foreach ($mails as $nr => $mail) {
+
+            // for every addressee: match address elements if it's to CiviMail
+            $matches = array();
+            foreach ($mail->to as $address) {
+                if (preg_match('/^civimail\+(b|confirm|o|r|re|u)\.(\d+).(\d+).(\d+).([0-9a-f]{16})(-.*)?@/', $address->email, $matches)) {
+                    list($match, $action, $domain, $job, $queue, $hash) = $matches;
+                    break;
+                } elseif (preg_match('/^civimail\+(subscribe)\.(\d+).(\d+)@/', $address->email, $matches)) {
+                    list($match, $action, $domain, $group) = $matches;
+                    break;
+                }
+            }
+
+            // if $matches is empty, this email is not CiviMail-bound
+            if (!$matches) {
+                $store->markIgnored($nr);
+                continue;
+            }
+
+            // for bounces and replies get the plaintext and HTML parts of the message
+            // FIXME: this assumes only one plain and one html part per message (and discards the rest)
+            if (in_array($action, array('b', 'r'))) {
+                $text = $html = null;
+                if ($mail->body instanceof ezcMailText) {
+                    $text = $mail->body->text;
+                } elseif ($mail->body instanceof ezcMailMultipart) {
+                    foreach ($mail->body->getParts() as $part) {
+                        switch ($part->subType) {
+                        case 'plain': $text = $part->text; break;
+                        case 'html':  $html = $part->text; break;
+                        }
+                    }
+                }
+            }
+
+            // get $replyTo from either the Reply-To header or from From
+            $replyTo = $mail->getHeader('Reply-To') ? $mail->getHeader('Reply-To') : $mail->from->email;
+
+            // handle the action by passing it to the proper API call
+            switch($action) {
+            case 'b':
+                crm_mailer_event_bounce($job, $queue, $hash, $text);
+                break;
+            case 'confirm':
+                crm_mailer_event_confirm($job, $queue, $hash);
+                break;
+            case 'o':
+                crm_mailer_event_domain_unsubscribe($job, $queue, $hash);
+                break;
+            case 'r':
+                crm_mailer_event_reply($job, $queue, $hash, $text, $replyTo, $html);
+                break;
+            case 're':
+                crm_mailer_event_resubscribe($job, $queue, $hash);
+                break;
+            case 'subscribe':
+                crm_mailer_event_subscribe($mail->from->email, $group);
+                break;
+            case 'u':
+                crm_mailer_event_unsubscribe($job, $queue, $hash);
+                break;
+            }
+
+            $store->markProcessed($nr);
+        }
+    }
+
+}
+
+// bootstrap the environment and run the processor
+session_start();
+require_once '../civicrm.config.php';
+require_once 'CRM/Core/Config.php';
+$config =& CRM_Core_Config::singleton();
+
+CRM_Utils_System::authenticateScript(true);
+
+$processor = new CiviMailProcessor;
+$processor->process();
