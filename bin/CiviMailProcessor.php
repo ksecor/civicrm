97a34a73089fd295cb082916a114f2fa6fb8d480
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-10-20 17:16:22 +0000

    make the CiviMail processor handle all old and new action monikers
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17555 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- bin/CiviMailProcessor.php
+++ bin/CiviMailProcessor.php
@@ -49,10 +49,10 @@ class CiviMailProcessor {
             // FIXME: the regexen should be limited to the domain
             $matches = array();
             foreach ($mail->to as $address) {
-                if (preg_match('/^(b|confirm|o|r|re|u)\.(\d+).(\d+).(\d+).([0-9a-f]{16})(-.*)?@/', $address->email, $matches)) {
+                if (preg_match('/^(b|bounce|c|confirm|o|optOut|r|reply|re|e|resubscribe|u|unsubscribe)\.(\d+).(\d+).(\d+).([0-9a-f]{16})(-.*)?@/', $address->email, $matches)) {
                     list($match, $action, $domain, $job, $queue, $hash) = $matches;
                     break;
-                } elseif (preg_match('/^civimail\+(subscribe)\.(\d+).(\d+)@/', $address->email, $matches)) {
+                } elseif (preg_match('/^(s|subscribe)\.(\d+).(\d+)@/', $address->email, $matches)) {
                     list($match, $action, $domain, $group) = $matches;
                     break;
                 }
@@ -66,7 +66,7 @@ class CiviMailProcessor {
 
             // for bounces and replies get the plaintext and HTML parts of the message
             // FIXME: this assumes only one plain and one html part per message (and discards the rest)
-            if (in_array($action, array('b', 'r'))) {
+            if (in_array($action, array('b', 'bounce', 'r', 'reply'))) {
                 $text = $html = null;
                 if ($mail->body instanceof ezcMailText) {
                     $text = $mail->body->text;
@@ -86,24 +86,32 @@ class CiviMailProcessor {
             // handle the action by passing it to the proper API call
             switch($action) {
             case 'b':
+            case 'bounce':
                 crm_mailer_event_bounce($job, $queue, $hash, $text);
                 break;
+            case 'c':
             case 'confirm':
                 crm_mailer_event_confirm($job, $queue, $hash);
                 break;
             case 'o':
+            case 'optOut':
                 crm_mailer_event_domain_unsubscribe($job, $queue, $hash);
                 break;
             case 'r':
+            case 'reply':
                 crm_mailer_event_reply($job, $queue, $hash, $text, $replyTo, $html);
                 break;
+            case 'e':
             case 're':
+            case 'resubscribe':
                 crm_mailer_event_resubscribe($job, $queue, $hash);
                 break;
+            case 's':
             case 'subscribe':
                 crm_mailer_event_subscribe($mail->from->email, $group);
                 break;
             case 'u':
+            case 'unsubscribe':
                 crm_mailer_event_unsubscribe($job, $queue, $hash);
                 break;
             }
