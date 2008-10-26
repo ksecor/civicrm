55e2d803efc92f6220140b3a5876bed255d89f1e
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-10-26 20:35:54 +0000

    - CRM-3743
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17691 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Case/Form/Activity.php
+++ CRM/Case/Form/Activity.php
@@ -34,6 +34,8 @@
  */
 
 require_once "CRM/Core/Form.php";
+require_once "CRM/Activity/BAO/Activity.php";
+require_once 'CRM/Core/BAO/File.php';
 require_once "CRM/Core/BAO/CustomGroup.php";
 require_once "CRM/Custom/Form/CustomData.php";
 
@@ -45,7 +47,7 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
 {
 
     /**
-     * The activity id 
+     * The case id 
      *
      * @var int
      */
@@ -59,11 +61,11 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
     protected $_activityTypeId;
 
     /**
-     * The id of case 
+     * The activity id 
      *
      * @var int
      */
-    protected $_caseId;
+    protected $_activityId;
 
 
     /**
@@ -74,17 +76,19 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
     protected $_uid;
 
     /**
-     * The id of source (reporter) contact and target contact
-     *
+     * The id of contact being viewed
+     * 
      * @var int
      */
-    protected $_sourceContactId;
-    protected $_assigneeContactId;
-    
-    // not sure if we need both target and currently viewed - since both should be the same ??
-    protected $_targetContactId;
     protected $_currentlyViewedContactId;
-    
+
+    /**
+     * The default values of an activity
+     *
+     * @var array
+     */
+    protected $_defaults = array();
+
     /**
      * Function to build the form
      *
@@ -93,42 +97,58 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
      */
     function preProcess( ) 
     {        
-        $this->_id             = CRM_Utils_Request::retrieve( 'id',    'Positive', $this );
-        $this->_activityTypeId = CRM_Utils_Request::retrieve( 'atype', 'Positive', $this );
-        $this->_caseId         = CRM_Utils_Request::retrieve( 'caseid', 'Positive', $this );
+        $this->_id             = CRM_Utils_Request::retrieve( 'id',    'Positive', $this,  true );
+        $this->_activityTypeId = CRM_Utils_Request::retrieve( 'atype', 'Positive', $this,  true );
+        $this->_activityId     = CRM_Utils_Request::retrieve( 'aid'  , 'Positive', $this );
+
+        $caseSub = CRM_Core_DAO::getFieldValue( 'CRM_Case_DAO_Case',
+                                                $this->_id,
+                                                'subject' );
 
         $session    =& CRM_Core_Session::singleton();
+        // logged in contact
         $this->_uid = $session->get('userID');
-        
+
+        // contact being viewed
         $this->_currentlyViewedContactId = $this->get('contactId');
-        
         if ( ! $this->_currentlyViewedContactId ) {
             $this->_currentlyViewedContactId = CRM_Utils_Request::retrieve( 'cid', 'Positive', $this );
         }
 
         $clientName = CRM_Core_DAO::getFieldValue( 'CRM_Contact_DAO_Contact',
-                                                  $this->_currentlyViewedContactId,
-                                                  'sort_name' );
+                                                   $this->_currentlyViewedContactId,
+                                                   'sort_name' );
         $this->assign( 'client_name', $clientName );
         
-        // add attachments part
-        require_once 'CRM/Core/BAO/File.php';
-        CRM_Core_BAO_File::buildAttachment( $this,
-                                           'civicrm_activity',
-                                           $this->_activityId );
+        $this->_action = CRM_Utils_Request::retrieve('action', 'String', $this );
+        $this->assign( 'action', $this->_action);
 
-        //assign activity type name and description to template
-        // FIXME - needs to retrieve label and description from civicrm_category
-        if ( $this->_activityTypeId ) {
-            require_once 'CRM/Core/BAO/OptionValue.php';
-            list( $activityTypeName, $activityTypeDescription ) = CRM_Core_BAO_OptionValue::getActivityTypeDetails( $this->_activityTypeId );
-            
-            $this->assign( 'activityTypeName', $activityTypeName );
-            $this->assign( 'activityTypeDescription', $activityTypeDescription );
+        $this->_addAssigneeContact = CRM_Utils_Array::value( 'assignee_contact', $_GET );
+        $this->assign('addAssigneeContact', false);
+        if ( $this->_addAssigneeContact ) {
+            $this->assign('addAssigneeContact', true);
         }
+
+        $this->assign( 'urlPath', 'civicrm/contact/view/activity' );
+
+        // add attachments part
+        CRM_Core_BAO_File::buildAttachment( $this,
+                                            'civicrm_activity',
+                                            $this->_activityId );
         
-        $this->setDefaultValues();
+        // assign activity type name and description to template
+        require_once 'CRM/Core/BAO/OptionValue.php';
+        $categoryParams = array('id' => $this->_activityTypeId);
+        CRM_Core_DAO::commonRetrieve('CRM_Core_DAO_Category', $categoryParams, $details );
         
+        $activityTypeName           = $details['label'];
+        $activityTypeDescription    = $details['description'];
+        $this->_defaults['subject'] = $details['label'];
+
+        $this->assign( 'activityTypeName', $activityTypeName );
+        $this->assign( 'activityTypeDescription', $activityTypeDescription );
+
+        CRM_Utils_System::setTitle( ts('%1 >> %2', array('1' => $caseSub,'2' => $activityTypeName)) );
     }
     
     /**
@@ -140,26 +160,39 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
      */
     function setDefaultValues( ) 
     {
-        if ( $this->_cdType ) {
-            return CRM_Custom_Form_CustomData::setDefaultValues( $this );
+        if ( isset($this->_activityId) ) {
+            $params = array( 'id' => $this->_activityId );
+            CRM_Activity_BAO_Activity::retrieve( $params, $this->_defaults );
+        } else {
+            $this->_defaults['due_date_time'] = array( );
+            CRM_Utils_Date::getAllDefaultValues( $this->_defaults['due_date_time'] );
+            $this->_defaults['due_date_time']['i'] = 
+                (int ) ( $this->_defaults['activity_date_time']['i'] / 15 ) * 15;
+
+            $this->_defaults['activity_date_time'] = array( );
+            CRM_Utils_Date::getAllDefaultValues( $this->_defaults['activity_date_time'] );
+            $this->_defaults['activity_date_time']['i'] = 
+                (int ) ( $this->_defaults['activity_date_time']['i'] / 15 ) * 15;
         }
-        
-        $defaults = array( );
-        $params   = array( );
-        $config =& CRM_Core_Config::singleton( );
+        return $this->_defaults;
     }
 
     public function buildQuickForm( ) 
     {
-        // FIXME: Do we need client (target contact) ID as a hidden field in the form?
-        
+        if ( $this->_addAssigneeContact ) {
+            $contactCount = CRM_Utils_Array::value( 'count', $_GET );
+            $this->assign('contactCount', $contactCount );
+            $this->assign('nextContactCount', $contactCount + 1 );
+            $this->assign('contactFieldName', 'assignee_contact' );
+            return CRM_Contact_Form_AddContact::buildQuickForm( $this, "assignee_contact[{$contactCount}]" );
+        }
+
         // add a dojo facility for searching contacts
         $this->assign( 'dojoIncludes', " dojo.require('dojox.data.QueryReadStore'); dojo.require('dojo.parser');" );
-        
         $attributes = array( 'dojoType'       => 'civicrm.FilteringSelect',
-                            'mode'           => 'remote',
-                            'store'          => 'contactStore',
-                            'pageSize'       => 10  );
+                             'mode'           => 'remote',
+                             'store'          => 'contactStore',
+                             'pageSize'       => 10  );
         
         $dataUrl = CRM_Utils_System::url( "civicrm/ajax/search",
                                          "reset=1",
@@ -170,61 +203,41 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
         $this->assign('admin', $admin);
         
         $sourceContactField =& $this->add( 'text','source_contact_id', ts('Reported By'), $attributes, $admin );
-        if ( $sourceContactField->getValue( ) ) {
-            $this->assign( 'source_contact',  $sourceContactField->getValue( ) );
-        } else if ( $this->_sourceContactId ) {
-            // we're setting currently LOGGED IN user as source for this activity
-            $this->assign( 'source_contact', $this->_sourceContactId );
-        }
+        $this->assign( 'source_contact', $this->_uid );
 
         // FIXME: Need to add "Case Role" field as spec'd in CRM-3743
         // FIXME: Need to add fields for "Send Copy To" functionality as spec'd in CRM-3743
         
-        $this->add('text', 'subject', ts('Subject') , CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'subject' ), true );
+        $this->add('text', 'subject', ts('Subject') , 
+                   CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'subject' ));
         
-        $this->add('date', 'due_date_time', ts('Due Date'), CRM_Core_SelectValues::date('dueDatetime'), true);
-        $this->addRule('activity_date_time', ts('Select a valid date.'), 'qfDate');
+        require_once 'CRM/Core/OptionGroup.php';        
+        $this->add('select', 'medium_id',  ts( 'Medium' ), 
+                   CRM_Core_OptionGroup::values('Encounter Medium'), true);
 
-        $this->add('date', 'activity_date_time', ts('Actual Date'), CRM_Core_SelectValues::date('activityDatetime'), true);
+        $this->add('date', 'due_date_time', ts('Due Date'), CRM_Core_SelectValues::date('activityDatetime'), true);
+        $this->addRule('due_date_time', ts('Select a valid date.'), 'qfDate');
+
+        $this->add('date', 'activity_date_time', ts('Actual Date'), 
+                   CRM_Core_SelectValues::date('activityDatetime'), true);
         $this->addRule('activity_date_time', ts('Select a valid date.'), 'qfDate');
         
-        $this->add('select','duration_hours',ts('Duration'),CRM_Core_SelectValues::getHours());
-        $this->add('select','duration_minutes', null,CRM_Core_SelectValues::getMinutes());
-        
-        $this->add('textarea', 'details', ts('Details'), CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'details' ) );
+        $this->add('select','duration_hours',ts('Duration'), CRM_Core_SelectValues::getHours());
+        $this->add('select','duration_minutes', null, CRM_Core_SelectValues::getMinutes());
         
         $this->add('select','status_id',ts('Activity Status'), CRM_Core_PseudoConstant::activityStatus( ), true );
         
-        $config =& CRM_Core_Config::singleton( );
-                
-        //need to assign custom data type and subtype to the template
-        $this->assign('customDataType', 'Activity');
-        $this->assign('customDataSubType',  $this->_activityTypeId );
-        $this->assign('entityId',  $this->_activityId );
-        // if we're viewing, we're assigning different buttons than for adding/editing
-        if ( $this->_action & CRM_Core_Action::VIEW ) { 
-            if ( isset( $this->_groupTree ) ) {
-                CRM_Core_BAO_CustomGroup::buildViewHTML( $this, $this->_groupTree );
-            }
-            
-            $this->freeze();
-            $this->addButtons( array(
-                                     array ( 'type'      => 'cancel',
-                                            'name'      => ts('Done') ),
+        $this->add('textarea', 'details', ts('Details'), 
+                   CRM_Core_DAO::getAttribute( 'CRM_Activity_DAO_Activity', 'details' ) );
+
+        $this->addButtons( array(
+                                 array ( 'type'      => 'next',
+                                         'name'      => ts('Save'),
+                                         'isDefault' => true   ),
+                                 array ( 'type'      => 'cancel',
+                                         'name'      => ts('Cancel') ),
                                      )
-                              );
-        } else {
-            $this->addUploadElement( CRM_Core_BAO_File::uploadNames( ) );
-            $buttonType = $this->buttonType( );
-            $this->addButtons( array(
-                                     array ( 'type'      => $buttonType,
-                                            'name'      => ts('Save'),
-                                            'isDefault' => true   ),
-                                     array ( 'type'      => 'cancel',
-                                            'name'      => ts('Cancel') ),
-                                     )
-                              );
-        }
+                           );
         
         $this->addFormRule( array( 'CRM_Case_Form_Activity', 'formRule' ), $this );
     }
@@ -275,5 +288,46 @@ class CRM_Case_Form_Activity extends CRM_Core_Form
      */
     public function postProcess() 
     {
+        // store the submitted values in an array
+        $params = $this->controller->exportValues( $this->_name );
+
+        if ( isset($this->_activityId) ) { 
+            $params['id'] = $this->_activityId;
+
+            // activity which hasn't been modified by a user yet
+            if ( $this->_defaults['is_auto'] == 1 ) { 
+                $params['is_auto'] = 0;
+            }
+
+            // activity which has been created or modified by a user
+            if ( $this->_defaults['is_auto'] == 0 ) {
+                $newActParams = $params;
+                $params = array();
+                $params['is_current_revision'] = 0;
+            }
+        }
+
+        $activity = CRM_Activity_BAO_Activity::create( $params );
+        
+        // create a new version of activity if activity was found to
+        // have been modified/created by user
+        if ( isset($newActParams) ) {
+            unset($newActParams['id']);
+            // set proper original_id
+            if ( CRM_Utils_Array::value('original_id', $this->_defaults) ) {
+                $newActParams['original_id'] = $this->_defaults['original_id'];
+            } else {
+                $newActParams['original_id'] = $activity->id;
+            }
+            //is_current_revision will be set to 1 by default.
+ 
+            $newActivity = CRM_Activity_BAO_Activity::create( $newActParams );
+        }
+
+        // Insert civicrm_log record for the activity (e.g. store the
+        // created / edited by contact id and date for the activity)
+        
+        // Note - civicrm_log is already created by CRM_Activity_BAO_Activity::create()
+        
     }
 }
--- templates/CRM/Case/Form/Activity.tpl
+++ templates/CRM/Case/Form/Activity.tpl
@@ -1,26 +1,20 @@
 {* this template is used for adding/editing activities for a case. *}
 
-{* this template is used for adding/editing other (custom) activities. *}
-{if $cdType }
-   {include file="CRM/Custom/Form/CustomData.tpl"}
-{elseif $addAssigneeContact }
+{if $addAssigneeContact }
    {include file="CRM/Contact/Form/AddContact.tpl"}
 {else}
 
-{* added onload javascript for source contact*}
-{if $source_contact and $admin and $action neq 4}
-   <script type="text/javascript">
-       dojo.addOnLoad( function( ) {ldelim}
-       dijit.byId( 'source_contact_id' ).setValue( "{$source_contact}")
-       {rdelim} );
-   </script>
-{/if}
+{* onload javascript for source contact *}
+<script type="text/javascript">
+    dojo.addOnLoad( function( ) {ldelim}
+    dijit.byId( 'source_contact_id' ).setValue( "{$source_contact}")
+    {rdelim} );
+</script>
+
 
 <fieldset>
     <legend>
-       {if $single eq false}
-          {ts}New Activity{/ts}
-       {elseif $action eq 1}
+       {if $action eq 1}
           {ts}New{/ts} 
        {elseif $action eq 2}
           {ts}Edit{/ts} 
@@ -28,140 +22,96 @@
           {ts}Delete{/ts}
        {elseif $action eq 4}
           {ts}View{/ts}
+       {elseif $action eq 32768}
+          {ts}Detach{/ts}
        {/if}
        {$activityTypeName}
     </legend>
-      
-        { if $activityTypeDescription }  
-          <div id="help">{$activityTypeDescription}</div>
-        {/if}
-             
-         <table class="form-layout">
-         {if $action eq 1 or $action eq 2  or $action eq 4 or $context eq 'search' }
-             <tr>
-                <td class="label">{ts}Client{/ts}</td>
-                <td class="view-value font-size12pt bold">{$client_name}</td>
-             </tr>
-             <tr>
-                <td class="label">{$form.source_contact_id.label}</td>
-                <td class="view-value">
-                   <div dojoType="dojox.data.QueryReadStore" jsId="contactStore" url="{$dataUrl}" class="tundra" doClientPaging="false">
-                       {if $admin and $action neq 4}{$form.source_contact_id.html} {else} {$source_contact_value} {/if}
-                   </div>
-                </td>
-             </tr>
-             <tr>
-             {if $action neq 4}
-                <td class="label">{ts}Assigned To {/ts}</td>
-                <td class="tundra">                  
-                   <div id="assignee_contact_1"></div>
-                   {edit}<span class="description">{ts}You can optionally assign this activity to someone. Assigned activities will appear in their Contact Dashboard.{/ts}</span>{/edit}
-                </td>
-             {else}
-                <td class="label">{ts}Assigned To {/ts}</td><td class="view-value">{$assignee_contact_value}</td>
-             {/if}
-             </tr>
 
-             <tr>
-                <td class="label">{$form.subject.label}</td><td class="view-value">{$form.subject.html}</td>
-             </tr> 
-             <tr>
-                <td class="label">{$form.due_date_time.label}</td>
-                <td class="view-value">{$form.due_date_time.html | crmDate }</br>
-                    {if $action neq 4}
-                      <span class="description">
-                      {include file="CRM/common/calendar/desc.tpl" trigger=trigger_activity doTime=1}
-                      {include file="CRM/common/calendar/body.tpl" dateVar=due_date_time startDate=currentYear 
-                                      endDate=endYear offset=10 doTime=1 trigger=trigger_activity ampm=1}
-                      </span>
-                   {/if}  
-                </td>
-             </tr> 
-             <tr>
-                <td class="label">{$form.activity_date_time.label}</td>
-                <td class="view-value">{$form.activity_date_time.html | crmDate }</br>
-                    {if $action neq 4}
-                      <span class="description">
+       {if $activityTypeDescription }  
+          <div id="help">{$activityTypeDescription}</div>
+       {/if}
+
+    <table class="form-layout">
+       <tr>
+          <td class="label font-size12pt">{ts}Client{/ts}</td>
+          <td class="view-value font-size12pt bold">{$client_name|escape}</td>
+       </tr>
+       <tr>
+          <td class="label font-size12pt">{ts}Activity Type{/ts}</td>
+          <td class="view-value font-size12pt bold">{$activityTypeName|escape}</td>
+       </tr>
+       <tr>
+          <td class="label">{$form.source_contact_id.label}</td>
+          <td class="view-value">
+              <div dojoType="dojox.data.QueryReadStore" jsId="contactStore" url="{$dataUrl}" class="tundra" doClientPaging="false">
+                  {if $admin}{$form.source_contact_id.html}{/if}
+              </div>
+          </td>
+       </tr>
+       <td class="label">{ts}Assigned To {/ts}</td>
+       <td class="tundra">                  
+          <div id="assignee_contact_1"></div>
+          {edit}<span class="description">{ts}You can optionally assign this activity to someone. Assigned activities will appear in their Contact Dashboard.{/ts}</span>{/edit}
+       </td>
+       <tr>
+          <td class="label">{$form.subject.label}</td><td class="view-value">{$form.subject.html}</td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.medium_id.label}</td><td class="view-value">{$form.medium_id.html}</td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.due_date_time.label}</td>
+          <td class="view-value">{$form.due_date_time.html | crmDate }</br>
+              <span class="description">
+              {include file="CRM/common/calendar/desc.tpl" trigger=trigger_activity doTime=1}
+              {include file="CRM/common/calendar/body.tpl" dateVar=due_date_time startDate=currentYear 
+                             endDate=endYear offset=10 doTime=1 trigger=trigger_activity ampm=1}
+              </span>
+          </td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.activity_date_time.label}</td>
+          <td class="view-value">{$form.activity_date_time.html | crmDate }</br>
+              {if $action neq 4}
+                  <span class="description">
                       {include file="CRM/common/calendar/desc.tpl" trigger=trigger_activity doTime=1}
                       {include file="CRM/common/calendar/body.tpl" dateVar=activity_date_time startDate=currentYear 
-                                      endDate=endYear offset=10 doTime=1 trigger=trigger_activity ampm=1}
-                      </span>
-                   {/if}  
-                </td>
-             </tr> 
-             <tr>
-                <td class="label">{$form.duration_hours.label}</td>
-                <td class="view-value">
-                    {if $action eq 4}
-                        {if $form.duration_hours.value}{$form.duration_hours.html} {ts}Hrs{/ts}&nbsp;&nbsp;{/if}
-                        {if $form.duration_minutes.value}{$form.duration_minutes.html} {ts}Mins{/ts}{/if}
-                    {else}
-                        {$form.duration_hours.html} {ts}Hrs{/ts}&nbsp;&nbsp;{$form.duration_minutes.html} {ts}Mins{/ts}
-                    {/if}
-                </td>
-             </tr> 
-             <tr>
-                <td class="label">{$form.status_id.label}</td><td class="view-value">{$form.status_id.html}</td>
-             </tr> 
-             <tr>
-                <td class="label">{$form.details.label}</td><td class="view-value">{$form.details.html|crmReplace:class:huge}</td>
-             </tr> 
-             <tr>
-             {if $action eq 4} 
-                {if $currentAttachmentURL}
-                    <td class="label">{ts}Current Attachments{/ts}</td>
-                    <td class="view-value">{$currentAttachmentURL}</td>
-                {else}  
-                    <td colspan=2>&nbsp;</td>
-                {/if}
-             {else}
-                <td colspan="2">
-                    {include file="CRM/Form/attachment.tpl"}
-                </td>
-            {/if} 
-            </tr>
-            <tr>
-                <td colspan="2">
-	            {if $action eq 4} 
-                    {include file="CRM/Contact/Page/View/InlineCustomData.tpl"}
-                {else}
-                    <div id="customData"></div>
-                {/if} 
-                </td>
-            </tr> 
-        {elseif $action eq 8}
-             <tr>
-                <td colspan="2">
-                    <div class="status">{ts 1=$delName}Are you sure you want to delete '%1'?{/ts}</div>
-                </td>
-             </tr>  
-             <tr>
-        {/if}
-                <td>&nbsp;</td><td>{$form.buttons.html}</td>
-             </tr> 
-         </table>   
-      </fieldset> 
-
-{if $action eq 1 or $action eq 2 or $context eq 'search'}
-   {*include custom data js file*}
-   {include file="CRM/common/customData.tpl"}
+                                     endDate=endYear offset=10 doTime=1 trigger=trigger_activity ampm=1}
+                  </span>
+              {/if}  
+          </td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.duration_hours.label}</td>
+          <td class="view-value">
+              {if $action eq 4}
+                 {if $form.duration_hours.value}{$form.duration_hours.html} {ts}Hrs{/ts}&nbsp;&nbsp;{/if}
+                 {if $form.duration_minutes.value}{$form.duration_minutes.html} {ts}Mins{/ts}{/if}
+                 {else}
+                     {$form.duration_hours.html} {ts}Hrs{/ts}&nbsp;&nbsp;{$form.duration_minutes.html} {ts}Mins{/ts}
+                 {/if}
+          </td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.status_id.label}</td><td class="view-value">{$form.status_id.html}</td>
+       </tr> 
+       <tr>
+          <td class="label">{$form.details.label}</td><td class="view-value">{$form.details.html|crmReplace:class:huge}</td>
+       </tr> 
+       <tr>
+          <td>&nbsp;</td><td>{$form.buttons.html}</td>
+       </tr> 
+    </table>
+</fieldset>
 {/if}
 
 {* Build add contact *}
 {literal}
 <script type="text/javascript">
-{/literal}
-{if $action eq 1 or $context eq 'search'}
-{literal}
-   buildContact( 1, 'assignee_contact' );
-{/literal}   
-{/if}
-
-{if $action eq 1 or $action eq 2}
-{literal}
+buildContact( 1, 'assignee_contact' );
 
 var assigneeContactCount = {/literal}"{$assigneeContactCount}"{literal}
-
 if ( assigneeContactCount ) {
     for ( var i = 1; i <= assigneeContactCount; i++ ) {
 	buildContact( i, 'assignee_contact' );
@@ -171,59 +121,48 @@ if ( assigneeContactCount ) {
 function buildContact( count, pref )
 {
     if ( count > 1 ) {
-	prevCount = count - 1;
-    {/literal}
-    {if $action eq 1  OR $action eq 2}
-    {literal}
-	hide( pref + '_' + prevCount + '_show');
-    {/literal} 
-    {/if}
-    {literal}
+	    prevCount = count - 1;
+        {/literal}{if $action eq 1  OR $action eq 2}{literal}
+	        hide( pref + '_' + prevCount + '_show');
+        {/literal}{/if}{literal}
     }
 
     // do not recreate if combo widget is already created
     if ( dijit.byId( pref + '[' + count + ']' ) ) {
-	return;
+	    return;
     }
 
     var context = {/literal}"{$context}"{literal}
     var dataUrl = {/literal}"{crmURL p=$urlPath h=0 q='snippet=4&count='}"{literal} + count + '&' + pref + '=1&context=' + context;
 
-{/literal}
-{if $urlPathVar}
-	dataUrl = dataUrl + '&' + '{$urlPathVar}'
-{/if}
-{literal}
+    {/literal}{if $urlPathVar}
+	    dataUrl = dataUrl + '&' + '{$urlPathVar}'
+    {/if}{literal}
 
     var result = dojo.xhrGet({
         url: dataUrl,
         handleAs: "text",
-	sync: true,
+    	sync: true,
         timeout: 5000, //Time in milliseconds
         handle: function(response, ioArgs) {
-                if (response instanceof Error) {
-		    if (response.dojoType == "cancel") {
-			//The request was canceled by some other JavaScript code.
-			console.debug("Request canceled.");
-		    } else if (response.dojoType == "timeout") {
-			//The request took over 5 seconds to complete.
-			console.debug("Request timed out.");
-		    } else {
-			//Some other error happened.
-			console.error(response);
-		    }
-                } else {
-		    // on success
-		    dojo.byId( pref + '_' + count).innerHTML = response;
-		    dojo.parser.parse( pref + '_' + count );
-		}
-	    }
+                    if (response instanceof Error) {
+    	        	    if (response.dojoType == "cancel") {
+            			//The request was canceled by some other JavaScript code.
+            			console.debug("Request canceled.");
+            		    } else if (response.dojoType == "timeout") {
+            			//The request took over 5 seconds to complete.
+            			console.debug("Request timed out.");
+            		    } else {
+            			//Some other error happened.
+            			console.error(response);
+            		    }
+                    } else {
+    		            // on success
+            		    dojo.byId( pref + '_' + count).innerHTML = response;
+            		    dojo.parser.parse( pref + '_' + count );
+            		}
+	            }
 	});
 }
 </script>
-
 {/literal}
-
-{/if} {* closing add contact widget condition*}
-
-{/if} {* end of snippet if*}	
