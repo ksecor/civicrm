88298c84eb16db5583007faa72fbb5e9b06fb658
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-04 02:42:23 +0000

    revert getTree change which is implemented in branch
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17848 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/CustomGroup.php
+++ CRM/Core/BAO/CustomGroup.php
@@ -199,7 +199,6 @@ class CRM_Core_BAO_CustomGroup extends CRM_Core_DAO_CustomGroup
      *
      */
     public static function &getTree( $entityType,
-                                     &$form,
                                      $entityID = null,
                                      $groupID  = null,
                                      $subType  = null )
