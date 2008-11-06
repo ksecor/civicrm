db838f638129dcd6b80810903e3f3a2fe4376469
amit <amit@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-06 08:46:08 +0000

    #fixes for button text at time of edit
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17889 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/PCP/Campaign.php
+++ CRM/Contribute/Form/PCP/Campaign.php
@@ -79,8 +79,11 @@ class CRM_Contribute_Form_PCP_Campaign extends CRM_Core_Form
     {
         $this->add('text', 'title', ts('Page Title'), null, true );
         $this->add('textarea', 'intro_text', ts('Intro Text'), null, true );
-        $this->add('text', 'goal_amount', ts('Goal Amount'), null, true ); 
-        $attributes = array('value' => 'Contribute Now', 'onClick' => 'value="";');
+        $this->add('text', 'goal_amount', ts('Goal Amount'), null, true );
+        $attributes = array( );
+        if ( $this->get('action') & CRM_Core_Action::ADD ) {
+            $attributes = array('value' => 'Contribute Now', 'onClick' => 'value="";');
+        }
         $this->add('text', 'donate_link_text', ts('Donate Button Text'), $attributes); 
         $attrib = Array ('rows' => 8, 'cols' => 60 );
         $this->addWysiwyg( 'page_text', ts('Page Text'), $attrib ); 
