014b827a0f2ad51ee672e59f79bebdac47876388
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-06 08:27:55 +0000

    minor change for PCP
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17888 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/PCP/Campaign.php
+++ CRM/Contribute/Form/PCP/Campaign.php
@@ -78,7 +78,7 @@ class CRM_Contribute_Form_PCP_Campaign extends CRM_Core_Form
     public function buildQuickForm( )  
     {
         $this->add('text', 'title', ts('Page Title'), null, true );
-        $this->add('text', 'intro_text', ts('Intro Text'), null, true ); 
+        $this->add('textarea', 'intro_text', ts('Intro Text'), null, true );
         $this->add('text', 'goal_amount', ts('Goal Amount'), null, true ); 
         $attributes = array('value' => 'Contribute Now', 'onClick' => 'value="";');
         $this->add('text', 'donate_link_text', ts('Donate Button Text'), $attributes); 
