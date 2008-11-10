0ba2632e7498df85d62799f233feb94ee85ad730
sunny <sunny@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-10 08:01:53 +0000

    //Fix for CRM-3594
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@17974 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Page/ManageEvent.php
+++ CRM/Event/Page/ManageEvent.php
@@ -237,9 +237,9 @@ class CRM_Event_Page_ManageEvent extends CRM_Core_Page
   SELECT *
     FROM civicrm_event
    WHERE $whereClause
-ORDER BY start_date asc
+ORDER BY start_date desc
    LIMIT $offset, $rowCount";
-
+        
         $dao = CRM_Core_DAO::executeQuery( $query, $params, true, 'CRM_Event_DAO_Event' );
      
         while ($dao->fetch()) {
