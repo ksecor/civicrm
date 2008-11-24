2d2a9b03b00a9cc191bf01247e1dd3d3e1a0a455
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-24 11:48:55 +0000

    worked on CRM-3660
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@18221 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- api/v2/Event.php
+++ api/v2/Event.php
@@ -143,29 +143,37 @@ function civicrm_event_search( &$params )
     $inputParams      = array( );
     $returnProperties = array( );
     $otherVars = array( 'sort', 'offset', 'rowCount' );
+
+    $sort = false;
+    $offset = 0;
+    $rowCount = 25;
+    
     foreach ( $params as $n => $v ) {
         if ( substr( $n, 0, 7 ) == 'return.' ) {
             $returnProperties[]=substr( $n, 7 );
         } elseif ( in_array( $n, $otherVars ) ) {
-            $n = $v;
+            $$n = $v;
         } else {
             $inputParams[$n] = $v;
         }
     }
+   
     if( !empty($returnProperties ) ) {
         $returnProperties[]='id';
         $returnProperties[]='event_type_id';
     }
+   
     require_once 'CRM/Core/BAO/CustomGroup.php';
     require_once 'CRM/Event/BAO/Event.php';
     $eventDAO = new CRM_Event_BAO_Event( );
     $eventDAO->copyValues( $inputParams );
     $event = array();
-    $returnEvent = array();
     if ( !empty( $returnProperties ) ) {
             $eventDAO->selectAdd( );
             $eventDAO->selectAdd( implode( ',' , $returnProperties ) );
         }
+    $eventDAO->orderBy( $sort );
+    $eventDAO->limit( (int)$offset, (int)$rowCount );
     $eventDAO->find( );
     while ( $eventDAO->fetch( ) ) {
         $event[$eventDAO->id] = array( );
