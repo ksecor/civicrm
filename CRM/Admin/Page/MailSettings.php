6eed987e19ff467c71fa0900ed8a814bfea0fce3
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-11-29 14:05:17 +0000

    //fixed CRM-3869
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@18360 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ CRM/Admin/Page/MailSettings.php
@@ -0,0 +1,121 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.2                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2008                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2007
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Page/Basic.php';
+require_once 'CRM/Core/DAO/MailSettings.php';
+
+/**
+ * Page for displaying list of Mail account settings
+ */
+class CRM_Admin_Page_MailSettings extends CRM_Core_Page_Basic 
+{
+    /**
+     * The action links that we need to display for the browse screen
+     *
+     * @var array
+     * @static
+     */
+    static $_links = null;
+    
+    /**
+     * Get BAO Name
+     *
+     * @return string Classname of BAO.
+     */
+    function getBAOName( ) 
+    {
+        return 'CRM_Core_BAO_MailSettings';
+    }
+    
+    /**
+     * Get action Links
+     *
+     * @return array (reference) of action links
+     */
+    function &links( )
+    {
+        if ( ! ( self::$_links ) ) {
+            // helper variable for nicer formatting
+            self::$_links = array(
+                                  CRM_Core_Action::UPDATE  => array(
+                                                                    'name'  => ts('Edit'),
+                                                                    'url'   => 'civicrm/admin/mailSettings',
+                                                                    'qs'    => 'action=update&id=%%id%%&reset=1',
+                                                                    'title' => ts('Edit Mail Settings') 
+                                                                    ),
+                                  CRM_Core_Action::DELETE  => array(
+                                                                    'name'  => ts('Delete'),
+                                                                    'url'   => 'civicrm/admin/mailSettings',
+                                                                    'qs'    => 'action=delete&id=%%id%%',
+                                                                    'title' => ts('Delete Mail Settings') 
+                                                                    )
+                                  );
+        }
+        
+        return self::$_links;
+    }
+    
+    /**
+     * Get name of edit form
+     *
+     * @return string Classname of edit form.
+     */
+    function editForm( ) 
+    {
+        return 'CRM_Admin_Form_MailSettings';
+    }
+    
+    /**
+     * Get edit form name
+     *
+     * @return string name of this page.
+     */
+    function editName( ) 
+    {
+        return 'Mail Settings';
+    }
+    
+    /**
+     * Get user context.
+     *
+     * @return string user context.
+     */
+    function userContext($mode = null) 
+    {
+        return 'civicrm/admin/mailSettings';
+    }
+}
+
+
