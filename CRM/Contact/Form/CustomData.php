998fd52c1554fc3fb70e188cd70860e6b407fa42
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-12-04 19:11:03 +0000

    fix CRM-1527
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@18524 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/CustomData.php
+++ CRM/Contact/Form/CustomData.php
@@ -161,16 +161,10 @@ class CRM_Contact_Form_CustomData extends CRM_Core_Form
         $this->assign('entityID',  $this->_tableID );
 		$this->assign('groupID',   $this->_groupID );
 
-        $uploadNames = $this->get( 'uploadNames' );
-        if ( is_array( $uploadNames ) && ! empty ( $uploadNames ) ) {
-            $buttonType = 'upload';
-        } else {
-            $buttonType = 'next';
-        }
-        $buttonType = 'upload';
-        
+        // make this form an upload since we dont know if the custom data injected dynamically
+        // is of type file etc
         $this->addButtons(array(
-                                array ( 'type'      => $buttonType,
+                                array ( 'type'      => 'upload',
                                         'name'      => ts('Save'),
                                         'isDefault' => true   ),
                                 array ( 'type'       => 'cancel',
