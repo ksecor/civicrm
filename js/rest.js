22f972febe94d7861a7bc9bf7612a3c857569fb6
xavier <xavier@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-12-09 15:39:04 +0000

    uses the userFrameworkResourceURL config instead of hardcode it CRM-3840
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@18642 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- js/rest.js
+++ js/rest.js
@@ -1,4 +1,9 @@
 /*
+On the template page that includes this js, you have to define a global variable to set the url of the server to be used for the rest
+<script type="text/javascript">
+civicrm_resourceURL="{$config->userFrameworkResourceURL}";
+</script>
+
 eg. CiviREST (contact/search,array(...))
 CiviREST(entitytag,add)
 
@@ -18,14 +23,8 @@ it also should use closure so we can properly interface the result of the call t
 function civiREST (entity,action,params) {
   params ['q']="civicrm/"+entity+"/"+action;
   params ['json'] = 1;
-  //$.post("/sites/all/modules/civicrm/extern/rest.php?json=1",{q:"civicrm/"+entity+"/"+action},
-/*  $.get("/sites/all/modules/civicrm/extern/rest.php",params,
-  function(data){
-    alert("Data Loaded: " + data);
-); 
-  }*/
   jQuery('#restmsg').removeClass('msgok').removeClass('msgnok').html("");
-  $.getJSON("/sites/all/modules/civicrm/extern/rest.php",params,function(result){
+  $.getJSON(civicrm_resourceURL+"extern/rest.php",params,function(result){
   if (result.is_error == 1) {
     jQuery('#restmsg').addClass('msgnok').html(result.error_message);
     return false;
