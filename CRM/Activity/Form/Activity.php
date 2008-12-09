5ba56c0c9817f5c6a525b99d5cbbf97ecd3097e7
shailesh <shailesh@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-12-09 12:34:30 +0000

    //CRM-3896
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@18637 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Form/Activity.php
+++ CRM/Activity/Form/Activity.php
@@ -424,7 +424,6 @@ class CRM_Activity_Form_Activity extends CRM_Contact_Form_Task
             }
             $defaults['activity_date_time'] = array( );
             CRM_Utils_Date::getAllDefaultValues( $defaults['activity_date_time'] );
-            $defaults['activity_date_time']['i'] = (int ) ( $defaults['activity_date_time']['i'] / 15 ) * 15;            
         } else {
             // if it's a new activity, we need to set default values for associated contact fields
             // since those are dojo fields, unfortunately we cannot use defaults directly
