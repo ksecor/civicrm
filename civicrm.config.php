8890358e49c2e43787dffddd4b920178ef7c3d86
wmorgan <wmorgan@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-12-17 23:49:20 +0000

    fixed an error where the installation redirect was incorrect when using a secure standalone installation
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.1@18848 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- civicrm.config.php
+++ civicrm.config.php
@@ -78,11 +78,16 @@ function conf_init() {
         return $confdir;
     }
 
-    if ( ! file_exists( $confdir ) && ! $skipConfigError ) {
+    if ( ! file_exists( $confdir . DIRECTORY_SEPARATOR . 'civicrm.settings.php' ) && ! $skipConfigError ) {
         echo "Could not find valid configuration dir, best guess: $confdir<br/><br/>\n";
         echo "If this is a standalone installation (i.e. not a Drupal or ";
         echo "Joomla module) and you'd like to re-initialize it, ";
-        echo "<a href=\"../install/index.php?mode=standalone\">click here</a>.\n";
+	if (is_dir("install")) {
+	  $install_path = "install";
+	} else {
+	  $install_path = "../install";
+	}
+        echo "<a href=\"$install_path/index.php?mode=standalone\">click here</a>.\n";
         exit( );
     }
             
