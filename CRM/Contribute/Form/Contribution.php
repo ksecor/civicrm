feeb710348b1d7ea8ba541c7ea04fd2bb9243b2f
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2008-12-18 08:41:24 +0000

    fixes for CRM-3934
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.1@18851 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Contribution.php
+++ CRM/Contribute/Form/Contribution.php
@@ -310,9 +310,9 @@ WHERE  contribution_id = {$this->_id}
         
         // when custom data is included in this page
         if ( CRM_Utils_Array::value( "hidden_custom", $_POST ) ) {
-            $this->set('customDataType',    'Contribution');
-            $this->set('customDataSubType', $this->_contributionType );
-            $this->set('entityId',          $this->_id );
+            $this->set('type',     'Contribution');
+            $this->set('subType',  $this->_contributionType );
+            $this->set('entityId', $this->_id );
 
             CRM_Custom_Form_Customdata::preProcess( $this );
             CRM_Custom_Form_Customdata::buildQuickForm( $this );
@@ -700,6 +700,7 @@ WHERE  contribution_id = {$this->_id}
         
         // get the submitted form values.  
         $submittedValues = $this->controller->exportValues( $this->_name );
+        
         $config  =& CRM_Core_Config::singleton( );
         $session =& CRM_Core_Session::singleton( );
         
@@ -945,7 +946,7 @@ WHERE  contribution_id = {$this->_id}
                     $formValues[$d]['i'] = '00';
                     $formValues[$d]['s'] = '00';
                     $params[$d] = CRM_Utils_Date::format( $formValues[$d] );
-                } else{
+                } else if ( array_key_exists( $d, $formValues ) ) {
                     $params[$d] = 'null';
                 }
             }
