8a07bde82c83392adecc0600640f6c34dd3479d1
daved <daved@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-01-09 03:19:45 +0000

    path reference adjusted to match new folder location
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@19209 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Case/xml/physicianhealthbc/reload.cmd
+++ CRM/Case/xml/physicianhealthbc/reload.cmd
@@ -4,7 +4,7 @@ if "%2"=="" GOTO bad
 if "%3"=="" GOTO bad
 if "%4"=="" GOTO bad
 
-echo drop database %4; create database %4; use %4; source ../../../../../sql/civicrm.mysql; source ../../../../../sql/civicrm_data.mysql; source physicianhealthbc.mysql; | mysql -u %2 --password=%3
+echo drop database %4; create database %4; use %4; source ../../../../sql/civicrm.mysql; source ../../../../sql/civicrm_data.mysql; source physicianhealthbc.mysql; | mysql -u %2 --password=%3
 wget -O - %1
 php physicianhealthbc.php %2 %3 %4
 goto end
