18a6c6a1eb5cc237bc68f49bb9e59a4df76b8d82
shailesh <shailesh@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-01-15 10:46:10 +0000

    //minor fix for CRM-3989.
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@19356 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/ContributionPage/Widget.php
+++ CRM/Contribute/Form/ContributionPage/Widget.php
@@ -214,6 +214,8 @@ class CRM_Contribute_Form_ContributionPage_Widget extends CRM_Contribute_Form_Co
 
     function postProcess( ) 
     {
+        //to reset quickform elements of next (pcp) page.
+        $this->controller->resetPage( 'PCP' );
         // get the submitted form values.
         $params = $this->controller->exportValues( $this->_name );
         
