aec20f47df91e4d55bbe0072b2654b5c20e3d3e5
shailesh <shailesh@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-03-05 14:43:25 +0000

    //CRM-4196.
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@20185 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration/Confirm.php
+++ CRM/Event/Form/Registration/Confirm.php
@@ -492,11 +492,12 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration
                 if( $value['amount'] != 0 && CRM_Utils_Array::value( 'is_primary', $value ) ) {
                     //if primary participant contributing additional amount
                     //append (multiple participants) to its fee level. CRM-4196.
+                    $isAdditionalAmount = false;
                     if ( count($params) > 1 ) {
-                        $value['amount_level'] = $value['amount_level'].ts(' (multiple participants)');
+                        $isAdditionalAmount = true;
                     }
                     
-                    $contribution =& self::processContribution( $this, $value, $result, $contactID, $pending );
+                    $contribution =& self::processContribution( $this, $value, $result, $contactID, $pending, $isAdditionalAmount );
                 }
                 $value['contactID']          = $contactID;
                 $value['eventID']            = $this->_eventId;
@@ -607,7 +608,7 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration
      * @return void
      * @access public
      */
-    static function processContribution( &$form, $params, $result, $contactID, $pending = false ) 
+    static function processContribution( &$form, $params, $result, $contactID, $pending = false, $isAdditionalAmount = false ) 
     {
         require_once 'CRM/Core/Transaction.php';
         $transaction = new CRM_Core_Transaction( );
@@ -619,7 +620,11 @@ class CRM_Event_Form_Registration_Confirm extends CRM_Event_Form_Registration
         if ( $form->_values['event']['is_email_confirm'] ) {
             $receiptDate = $now ;
         }
-        
+        //CRM-4196        
+        if ( $isAdditionalAmount ) {
+            $params['amount_level'] = $params['amount_level'].ts(' (multiple participants)');
+        }
+
         $contribParams = array(
                                'contact_id'            => $contactID,
                                'contribution_type_id'  => $form->_values['event']['contribution_type_id'] ?
