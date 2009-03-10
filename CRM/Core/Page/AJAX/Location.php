878fcf55e997b72e71c697aa3fef69a55d339c06
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-03-10 11:12:57 +0000

    - CRM-1971
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@20240 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Page/AJAX/Location.php
+++ CRM/Core/Page/AJAX/Location.php
@@ -42,6 +42,9 @@ class CRM_Core_Page_AJAX_Location
 {
 
     /**
+     * FIXME: we should make this method like getLocBlock() OR use the same method and 
+     * remove this one. 
+     *
      * Function to obtain the location of given contact-id. 
      * This method is used by on-behalf-of form to dynamically generate poulate the 
      * location field values for selected permissioned contact. 
@@ -133,24 +136,32 @@ class CRM_Core_Page_AJAX_Location
             $location = CRM_Core_BAO_Location::getValues($params, $params);
         }
 
-        // lets output only required fields. 
-        $fields   = array( "location[1][address][street_address]",
-                           "location[1][address][supplemental_address_1]",
-                           "location[1][address][supplemental_address_2]",
-                           "location[1][address][city]",
-                           "location[1][address][postal_code]",
-                           "location[1][address][postal_code_suffix]",
-                           "location[1][address][country_id]",
-                           "location[1][address][state_province_id]",
-                           "location[1][address][geo_code_1]",
-                           "location[1][address][geo_code_2]",
-                           "location[1][email][1][email]",
-                           "location[1][phone][1][phone_type_id]",
-                           "location[1][phone][1][phone]" );
         $result = array( );
-        foreach ( $fields as $fld ) {
-            eval("\$value = \${$fld};");
-            $result[str_replace( array('][', '[', "]"), array('_', '_', ''), $fld)] = $value;
+        $addressOptions  = CRM_Core_BAO_Preferences::valueOptions( 'address_options', true, null, true );
+        
+        // lets output only required fields.
+        foreach ( $addressOptions as $element => $isSet ) {
+            if ( $isSet && (! in_array($element, array('im', 'openid'))) ) {
+                if ( in_array($element, array('country', 'state_province', 'county')) ) {
+                    $element .= '_id' ;
+                } else if ( $element == 'address_name' ) {
+                    $element = 'name' ;
+                }
+                $fld = "location[1][address][{$element}]";
+                eval("\$value = \${$fld};");
+                $value = $value ? $value : "";
+                $result[str_replace( array('][', '[', "]"), array('_', '_', ''), $fld)] = $value;
+            }
+        }
+
+        foreach (array('email', 'phone_type_id', 'phone') as $element) {
+            $block = ($element == 'phone_type_id') ? 'phone' : $element;
+            for ( $i = 1; $i < 3; $i++ ) {
+                $fld   = "location[1][{$block}][{$i}][{$element}]";
+                eval("\$value = \${$fld};");
+                $value = $value ? $value : "";
+                $result[str_replace( array('][', '[', "]"), array('_', '_', ''), $fld)] = $value;
+            }
         }
 
         // set the message if loc block is being used by more than one event.
