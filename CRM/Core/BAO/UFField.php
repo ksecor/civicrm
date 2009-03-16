8e3201853713abad94083865b420fb55577c3c51
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-03-16 06:41:15 +0000

    v2.2: svn merge -r 20079:20255 http://svn.civicrm.org/civicrm/branches/v2.1 CRM-4219 fix
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@20296 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFField.php
+++ CRM/Core/BAO/UFField.php
@@ -134,7 +134,7 @@ class CRM_Core_BAO_UFField extends CRM_Core_DAO_UFField
         $ufField->field_type       = $params['field_name'][0];
         $ufField->field_name       = $params['field_name'][1]; 
         $ufField->location_type_id = (CRM_Utils_Array::value( 2, $params['field_name'] )) ? $params['field_name'][2] : 'NULL';
-        $ufField->phone_type_id    = CRM_Utils_Array::value( 3, $params['field_name'] );
+        $ufField->phone_type       = CRM_Utils_Array::value( 3, $params['field_name'] );
         
         if (CRM_Utils_Array::value( 'uf_field', $ids )) {
             $ufField->whereAdd("id <> ".CRM_Utils_Array::value( 'uf_field', $ids ));
