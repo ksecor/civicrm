179c9d3b5068e08d70700b8cf2d5ba4f0c573ce6
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-03-31 21:23:56 +0000

    CiviReport hits the repository
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@20507 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Config/Variables.php
+++ CRM/Core/Config/Variables.php
@@ -321,8 +321,9 @@ class CRM_Core_Config_Variables extends CRM_Core_Config_Defaults
      *
      * @var array
      */
-    public $enableComponents   = array( 'CiviContribute','CiviPledge','CiviMember','CiviEvent', 'CiviMail', 'CiviAuction' );
-    public $enableComponentIDs = array( 1, 6, 2, 3, 4, 8);
+    public $enableComponents   = array( 'CiviContribute','CiviPledge','CiviMember',
+                                        'CiviEvent', 'CiviMail', 'CiviAuction', 'CiviReport' );
+    public $enableComponentIDs = array( 1, 6, 2, 3, 4, 8, 9 );
 
     /**
      * Should payments be accepted only via SSL?
---
+++ CRM/Report/Form.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 2.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2009
+ * $Id$
+ *
+ */
+
+require_once 'CRM/Core/Form.php';
+
+class CRM_Report_Form extends CRM_Core_Form {
+
+    /**
+     * The id of the report instance
+     *
+     * @var integer
+     */
+    protected $_id;
+
+    /**
+     * The id of the report template
+     *
+     * @var integer;
+     */
+    protected $_templateID;
+
+    /**
+     * The report title
+     *
+     * @var string
+     */
+    protected $_title;
+
+    /**
+     * The set of all columns in the report. An associative array
+     * with column name as the key and attribues as the value
+     *
+     * @var array
+     */
+    protected $_columns;
+
+    /**
+     * The set of filters in the report
+     *
+     * @var array
+     */
+    protected $_filters;
+
+    /**
+
+    /**
+     * 
+    function __construct( ) {
+    }
+
+
+}
+
+
+
