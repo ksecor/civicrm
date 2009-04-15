054d38a0db3e3087139e6e9c436efbc481f2eac8
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-04-15 22:42:25 +0000

    #CRM-4379
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@20680 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Activity/Form/Activity.php
+++ CRM/Activity/Form/Activity.php
@@ -602,7 +602,10 @@ class CRM_Activity_Form_Activity extends CRM_Contact_Form_Task
                                      )
                                );
         } else {
-            $js = array( 'onclick' => "return verify( );" ); 
+            $js = null;
+            if ( $this->_action & CRM_Core_Action::UPDATE ) {
+                $js = array( 'onclick' => "return verify( );" ); 
+            }
             $this->addButtons( array(
                                      array ( 'type'      => 'upload',
                                              'name'      => ts('Save'),
