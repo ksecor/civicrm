764e777f9b9ca0e6a494ef811804c482b695f31e
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-04-15 15:24:13 +0000

    enabling another page for revamp demo purposes
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@20670 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Config/Defaults.php
+++ CRM/Core/Config/Defaults.php
@@ -82,7 +82,7 @@ class CRM_Core_Config_Defaults
         $this->civiHRD   = defined( 'CIVICRM_HRD' ) ? true : false;
         
         // add UI revamp pages
-        $this->revampPages = array( 'CRM/Admin/Form/Setting/Url.tpl' );
+        $this->revampPages = array( 'CRM/Admin/Form/Setting/Url.tpl', 'CRM/Admin/Form/Preferences/Address.tpl' );
         
         //profile double opt-in 
         $this->profileDoubleOptIn = defined( 'CIVICRM_PROFILE_DOUBLE_OPTIN' ) ? false : true;
