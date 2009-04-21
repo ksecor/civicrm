6da9c277e3c908eeed901b0a9158e798fe763b86
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-04-21 08:04:59 +0000

    minor fixes
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@20733 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Action.php
+++ CRM/Core/Action.php
@@ -228,7 +228,7 @@ class CRM_Core_Action {
         CRM_Utils_String::append( $resultLink, '&nbsp;|&nbsp;', $actionLink );
         if ( $showDiv ) {
             CRM_Utils_String::append( $resultDiv, '</li><li>', $actionDiv );
-            $resultDiv = "| <img src='{$config->resourceBase}i/menu-collapsed.png' title='".ts('more actions')."'/> ".ts('more actions')."<ul id='panel_xx' class='panel'><li>{$resultDiv}</li></ul>";
+            $resultDiv = "| <img src='{$config->resourceBase}i/menu-collapsed.png' title='".ts('more')."'/> ".ts('more')."<ul id='panel_xx' class='panel'><li>{$resultDiv}</li></ul>";
         }
         $result = "<span>{$resultLink} &nbsp;</span><span class='btn-slide' id=xx>{$resultDiv}</span>";
         return $result;
