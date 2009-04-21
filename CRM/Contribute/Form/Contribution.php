bd1ab00190cb9ceb08556ff35886bb980b5fec27
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-04-21 12:02:42 +0000

    typo fix
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@20740 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Contribution.php
+++ CRM/Contribute/Form/Contribution.php
@@ -393,7 +393,7 @@ WHERE  contribution_id = {$this->_id}
             if ( $defaults['contribution_status_id'] == 2 &&
                  ( strpos( $defaults['contribution_source'], ts( 'Online Contribution' ) ) !== false ||
                    strpos( $defaults['contribution_source'], ts( 'Online Event Registration' ) ) !== false ) ) {
-                $message = ts( 'If you have received payment for this Pending online contribution, record it using <strong>Update Pending Contribution Status</strong> from <strong><a href=\'%1\'>CiviContribute &raquo; Find Contributions</a></strong>. If you update the status from here the contributor may not got complete information on their receipt. Also, if there is an associated membership or event registration record - it\'s status will not be updated.',
+                $message = ts( 'If you have received payment for this Pending online contribution, record it using <strong>Update Pending Contribution Status</strong> from <strong><a href=\'%1\'>CiviContribute &raquo; Find Contributions</a></strong>. If you update the status from here the contributor may not got complete information on their receipt. Also, if there is an associated membership or event registration record - its status will not be updated.',
                                array( 1 => CRM_Utils_System::url( 'civicrm/contribute/search', "reset=1" )) );
                 CRM_Core_Session::setStatus( $message );
             }
