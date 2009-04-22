597ad8018a4b6ef61cffe70cad8c86a3a6ccfcd9
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-04-22 20:59:36 +0000

    worked towards Contribution Summary Civireport
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@20799 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Report/Form/ContributionSummary.php
+++ CRM/Report/Form/ContributionSummary.php
@@ -37,41 +37,31 @@ require_once 'CRM/Report/Form.php';
 
 class CRM_Report_Form_ContributionSummary extends CRM_Report_Form {
 
+    protected $_summary = null;
+
     function __construct( ) {
-        $this->_columns = array( 'civicrm_contact'      =>
-                                 array( 'dao'    => 'CRM_Contact_DAO_Contact',
-                                        'fields' =>
-                                        array( 'display_name' => array( 'label'    => ts( 'Contact Name' ),
-                                                                        'required' => true ) )
-                                        ),
-                                 
+        $this->_columns = array( 
                                  'civicrm_contribution' =>
-                                 array( 'dao'    => 'CRM_Contribute_DAO_Contribution',
-                                        'fields' =>
-                                        array( 'amount'        => array( 'label'    => ts( 'Amount' ),
+                                 array( 'dao'     => 'CRM_Contribute_DAO_Contribution',
+                                        'fields'  =>
+                                        array( 'total_amount'  => array( 'title'    => ts( 'Amount' ),
                                                                          'required' => true ),
                                                'trxn_id'       => null,
-                                               'received_date' => null,
+                                               'receive_date'  => null,
                                                'receipt_date'  => null,
-                                               )
+                                               ),
+                                        'filters' =>             
+                                        array( 'receive_date' => 
+                                               array( 'default'    => 'this month' ),
+                                               'total_amount' => 
+                                               array( 'title'      => ts( 'Total  Amount Between' ) ),
+                                               ),
+                                        'grouping'=> 'contri-fields',
                                         ),
+
                                  );
-                                                          
 
-
-        $this->_filters =
-            array( 'receive_date' => array( 'label'      => ts( 'Date Range' ),
-                                            'table'      => 'civicrm_contribution',
-                                            'operator'   => 'date_range',
-                                            'type'       => 'date',
-                                            'default'    => 'this month' ),
-                   'total_amount' => array( 'label'      => ts( 'Total Amount Between' ),
-                                            'table'      => 'civicrm_contribution',
-                                            'operator'   => 'money_range',
-                                            'type'       => 'money' )
-                   );
-
-        $this->_options = array( 'include_statistics' => array( 'label' => ts( 'Include Grand Totals' ),
+        $this->_options = array( 'include_statistics' => array( 'title' => ts( 'Include Grand Totals' ),
                                                                 'type'  => 'checkbox' ),
                                  );
         
@@ -79,45 +69,132 @@ class CRM_Report_Form_ContributionSummary extends CRM_Report_Form {
     }
 
     function preProcess( ) {
+        parent::preProcess( );
     }
 
     function setDefaultValues( ) {
-        $this->setDefaults( );
+        return parent::setDefaultValues( );
     }
 
-
     function select( ) {
-        
-        $this->_select = "
-SELECT 
-";
+        $select = array( );
+
+        $this->_columnHeaders = array( );
+        foreach ( $this->_columns as $tableName => $table ) {
+            foreach ( $table['fields'] as $fieldName => $field ) {
+                if ( CRM_Utils_Array::value( 'required', $field ) ||
+                     CRM_Utils_Array::value( $fieldName, $this->_params['select_columns'][$table['grouping']] ) ||
+                     CRM_Utils_Array::value( $fieldName, $this->_params['select_columns'][$tableName] ) ) {
+                    $select[] = "{$table['alias']}.{$fieldName} as {$tableName}_{$fieldName}";
+                    $this->_columnHeaders["{$tableName}_{$fieldName}"] = $field['title'];
+                }
+            }
+        }
+
+        $this->_select = "SELECT " . implode( ', ', $select ) . " ";
     }
 
     function from( ) {
         $this->_from = null;
 
         $this->_from = "
-FROM       civicrm_contact c
-INNER JOIN civicrm_contribution co ON c.id = co.contact_id
+FROM       civicrm_contribution {$this->_aliases['civicrm_contribution']}
 ";
-
-        if ( $this->_addressFields ) {
-            $this->_from .= "LEFT JOIN civicrm_address a ON c.id = a.contact_id AND a.is_primary = 1\n";
-        }
-        
-        if ( $this->_emailField ) {
-            $this->_from .= "LEFT JOIN civicrm_email e ON c.id = e.contact_id AND e.is_primary = 1\n";
-        }
     }
 
     function where( ) {
+        $clauses = array( );
+        foreach ( $this->_columns as $tableName => $table ) {
+            if ( array_key_exists('filters', $table) ) {
+                foreach ( $table['filters'] as $fieldName => $field ) {
+                    $clause = null;
+                    if ( $field['type'] & CRM_Utils_Type::T_DATE ) {
+                        $relative = CRM_Utils_Array::value( "{$fieldName}_relative", $this->_params );
+                        $from     = CRM_Utils_Array::value( "{$fieldName}_from"    , $this->_params );
+                        $to       = CRM_Utils_Array::value( "{$fieldName}_to"      , $this->_params );
+                        
+                        if ( $relative || $from || $to ) {
+                            $clause = $this->dateClause( $field, $relative, $from, $to );
+                        }
+                    } else {
+                        $op = CRM_Utils_Array::value( "{$fieldName}_op", $this->_params );
+                        if ( $op ) {
+                            $clause = 
+                                $this->whereClause( $field,
+                                                    $op,
+                                                    CRM_Utils_Array::value( "{$fieldName}_value", $this->_params ),
+                                                    CRM_Utils_Array::value( "{$fieldName}_min", $this->_params ),
+                                                    CRM_Utils_Array::value( "{$fieldName}_max", $this->_params ) );
+                        }
+                    }
+                    
+                    if ( ! empty( $clause ) ) {
+                        $clauses[] = $clause;
+                    }
+                }
+            }
+        }
+
+        if ( empty( $clauses ) ) {
+            $this->_where = "WHERE ( 1 ) ";
+        } else {
+            $this->_where = "WHERE " . implode( ' AND ', $clauses );
+        }
     }
 
 
+    function statistics( ) {
+        $select = "
+SELECT COUNT( contribution.total_amount ) as count,
+       SUM(   contribution.total_amount ) as amount,
+       AVG(   contribution.total_amount ) as avg
+";
+
+        $sql = "{$select} {$this->_from} {$this->_where}";
+        $dao = CRM_Core_DAO::executeQuery( $sql );
+
+        $statistics = null;
+        if ( $dao->fetch( ) ) {
+            $statistics = array( 'count'  => $dao->count,
+                                 'amount' => $dao->amount,
+                                 'avg'    => $dao->avg );
+        }
+        
+        return $statistics;
+    }
+
     function postProcess( ) {
+        $this->_params = $this->controller->exportValues( $this->_name );
+
+        $this->select( );
+        $this->from  ( );
+        $this->where ( );
+
+        $sql = "{$this->_select} {$this->_from} {$this->_where}";
+
+        $dao  = CRM_Core_DAO::executeQuery( $sql );
+        $rows = array( );
+        while ( $dao->fetch( ) ) {
+            $row = array( );
+            foreach ( $this->_columnHeaders as $key => $value ) {
+                $row[$key] = $dao->$key;
+            }
+            $rows[] = $row;
+        }
+
+        $this->assign_by_ref( 'columnHeaders', $this->_columnHeaders );
+        $this->assign_by_ref( 'rows', $rows );
+
+/*         CRM_Core_Error::debug( '$rows', $rows ); */
+/*         CRM_Core_Error::debug( '$this->_columnHeaders', $this->_columnHeaders  ); */
+/*         CRM_Core_Error::debug( 'statistics', $this->statistics( ) ); */
+        if ( CRM_Utils_Array::value( 'include_statistics', $this->_params ) ) {
+            $this->assign( 'statistics',
+                           $this->statistics( ) );
+        }
+
+/*         CRM_Core_Error::debug( '$sql', $sql ); */
+/*         exit( ); */
     }
 
 }
-
-
-
