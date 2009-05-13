a58704e884bad7ca3aaebc249d94aec398840ffb
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-05-13 22:01:57 +0000

    - remove civireport schema changes
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@21175 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/Config/Variables.php
+++ CRM/Core/Config/Variables.php
@@ -322,8 +322,8 @@ class CRM_Core_Config_Variables extends CRM_Core_Config_Defaults
      * @var array
      */
     public $enableComponents   = array( 'CiviContribute','CiviPledge','CiviMember',
-                                        'CiviEvent', 'CiviMail', 'CiviAuction', 'CiviReport' );
-    public $enableComponentIDs = array( 1, 6, 2, 3, 4, 8, 9 );
+                                        'CiviEvent', 'CiviMail', 'CiviAuction' );
+    public $enableComponentIDs = array( 1, 6, 2, 3, 4, 8 );
 
     /**
      * Should payments be accepted only via SSL?
