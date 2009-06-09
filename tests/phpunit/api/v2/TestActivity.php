2d3a39cda0e2ba04050317c67d5c5578d94ce953
walt <walt@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-06-09 02:02:43 +0000

    more old tests ported to api_v2_TestActivity; api_v2_TestContact fails
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@21667 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/phpunit/api/v2/TestActivity.php
+++ tests/phpunit/api/v2/TestActivity.php
@@ -195,14 +195,225 @@ class api_v2_TestActivity extends PHPUnit_Extensions_Database_TestCase
                         );
         
         $result = & civicrm_activity_create( $params );
-        $this->assertEquals( $result['is_error'], 0 );
+        $this->assertEquals( $result['is_error'], 0,
+                             "Error message: " . $result['error_message'] );
         $this->assertEquals( $result['source_contact_id'], 17 );
         $this->assertEquals( $result['subject'], 'Discussion on Apis for v2' );
         $this->assertEquals( $result['activity_date_time'], date('Ymd') );
         $this->assertEquals( $result['location'], 'Pensulvania' );
         $this->assertEquals( $result['details'], 'a test activity' );
         $this->assertEquals( $result['status_id'], 1 );
+    }
+
+    /**
+     * check activity deletion with empty params
+     */
+    function testDeleteActivityForEmptyParams( )
+    {
+        $params = array( );
+        $result =& civicrm_activity_delete($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check activity deletion without activity id
+     */
+    function testDeleteActivityWithoutId( )
+    {
+        $params = array('activity_name' => 'Meeting');
+        $result =& civicrm_activity_delete($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check activity deletion without activity type
+     */
+    function testDeleteActivityWithoutActivityType( )
+    {
+        $params = array( 'id' => $this->_activityId );
+        $result =& civicrm_activity_delete( $params );
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+    
+    /**
+     * check activity deletion with incorrect data
+     */
+    function testDeleteActivityWithIncorrectActivityType( )
+    {
+        $params = array( 'id'            => $this->_activityId,
+                         'activity_name' => 'Test Activity'
+                         );
+
+        $result =& civicrm_activity_delete( $params );
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check activity deletion with correct data
+     */
+    function testDeleteActivity( )
+    {
+        //  Insert rows in civicrm_activity creating activities 4 and
+        //  13 
+        $op = new PHPUnit_Extensions_Database_Operation_Insert( );
+        $op->execute( $this->dbconn,
+                      new PHPUnit_Extensions_Database_DataSet_XMLDataSet(
+                             dirname(__FILE__)
+                             . '/activity_4_13.xml') );
+        $params = array( 'id' => 13,
+                         'activity_type_id' => 5 );
         
+        $result =& civicrm_activity_delete($params);
+        $this->assertEquals( $result['is_error'], 0,
+                             "Error message: " . $result['error_message'] );
+    }
+    
+    /**
+     * check with empty array
+     */
+    function testActivityUpdateEmpty( )
+    {
+        $params = array( );
+        $result =& civicrm_activity_update($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check if required fields are not passed
+     */
+    function testActivityUpdateWithoutRequired( )
+    {
+        $params = array(
+                        'subject'             => 'this case should fail',
+                        'scheduled_date_time' => date('Ymd')
+                        );
+        
+        $result =& civicrm_activity_update($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check with incorrect required fields
+     */
+    function testActivityUpdateWithIncorrectData( )
+    {
+        $params = array(
+                        'activity_name'       => 'Meeting',
+                        'subject'             => 'this case should fail',
+                        'scheduled_date_time' => date('Ymd')
+                        );
+
+        $result =& civicrm_activity_update($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check with incorrect required fields
+     */
+    function testActivityUpdateWithIncorrectId( )
+    {
+        $params = array( 'id'                  => 'lets break it',
+                         'activity_name'       => 'Meeting',
+                         'subject'             => 'this case should fail',
+                         'scheduled_date_time' => date('Ymd')
+                         );
+
+        $result =& civicrm_activity_update($params);
+        $this->assertEquals( $result['is_error'], 1 );
+    }
+
+    /**
+     * check with incorrect required fields
+     */
+    function testActivityUpdateWithIncorrectContactActivityType( )
+    {
+        $params = array(
+                        'id'                  => 4,
+                        'activity_name'       => 'Test Activity',
+                        'subject'             => 'this case should fail',
+                        'scheduled_date_time' => date('Ymd')
+                        );
+
+        $result =& civicrm_activity_update($params);
+        $this->assertEquals( $result['is_error'], 1 );
+        $this->assertEquals( $result['error_message'], 'Invalid Activity Name' );
+    }
+    
+    /**
+     * this should create activity
+     */
+    function testActivityUpdate( )
+    {
+        //  Insert a row in civicrm_activity creating activity 4
+        $op = new PHPUnit_Extensions_Database_Operation_Insert( );
+        $op->execute( $this->dbconn,
+                      new PHPUnit_Extensions_Database_DataSet_XMLDataSet(
+                             dirname(__FILE__)
+                             . '/activity_type_5.xml') );
+        $params = array( 'id' => 13,
+                         'activity_type_id' => 5 );
+        $today = date('Ymd');
+        $params = array(
+                        'id'                  => 4,
+                        'subject'             => 'Update Discussion on Apis for v2',
+                        'activity_date_time'  => $today,
+                        'duration_hours'      => 15,
+                        'duration_minutes'    => 20,
+                        'location'            => '21, Park Avenue',
+                        'details'             => 'Lets update Meeting',
+                        'status_id'           => 1,
+                        'activity_name'       => 'Test activity type',
+                        );
+
+        $result =& civicrm_activity_update( $params );
+        $this->assertNull( $result['is_error'],
+                             "Error message: " . $result['error_message'] );
+        $this->assertEquals( 4, $result['id'],
+                             "In line " . __LINE__ );
+        $this->assertEquals( $today, $result['activity_date_time'],
+                             "In line " . __LINE__ );
+        $this->assertEquals( 'Update Discussion on Apis for v2',
+                             $result['subject'],
+                             "In line " . __LINE__ );
+        $this->assertEquals( $result['location'], '21, Park Avenue',
+                             "In line " . __LINE__); 
+        $this->assertEquals( $result['details'], 'Lets update Meeting',
+                             "In line " . __LINE__);
+        $this->assertEquals( $result['status_id'], 1,
+                             "In line " . __LINE__ );
+        
+    }
+    
+    /**
+     * check activity update with status
+     */
+    function testActivityUpdateWithStatus( )
+    {
+        //  Insert a row in civicrm_activity creating activity 4
+        $op = new PHPUnit_Extensions_Database_Operation_Insert( );
+        $op->execute( $this->dbconn,
+                      new PHPUnit_Extensions_Database_DataSet_XMLDataSet(
+                             dirname(__FILE__)
+                             . '/activity_type_5.xml') );
+        $params = array(
+                        'id'                  => 4,
+                        'source_contact_id'   => 17,
+                        'subject'             => 'Hurry update works', 
+                        'status_id'           => 2,
+                        'activity_name'       => 'Test activity type',
+                        );
+
+        $result =& civicrm_activity_update( $params );
+        $this->assertNull( $result['is_error'],
+                             "Error message: " . $result['error_message'] );
+        $this->assertEquals( $result['id'] , 4,
+                             "In line " . __LINE__ );
+        $this->assertEquals( $result['source_contact_id'] , 17,
+                             "In line " . __LINE__ );
+        $this->assertEquals( $result['subject'], 'Hurry update works',
+                             "In line " . __LINE__ );
+        $this->assertEquals( $result['status_id'], 2,
+                             "In line " . __LINE__ );
     }
 
 } // class api_v2_TestActivity
