3074dec480a1aa6d918496ccb0d38196a2396fad
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-06-24 08:52:26 +0000

    //CRM-4605 code cleanup
    git-svn-id: http://svn.civicrm.org/civicrm/branches/trunk.usability@22087 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Form/Edit/CommunicationPreferences.php
+++ CRM/Contact/Form/Edit/CommunicationPreferences.php
@@ -51,34 +51,30 @@ class CRM_Contact_Form_Edit_CommunicationPreferences
         // since the pcm - preferred comminication method is logically
         // grouped hence we'll use groups of HTML_QuickForm
 
-        $privacy = array( );
-       
+               
         // checkboxes for DO NOT phone, email, mail
         // we take labels from SelectValues
-        $t = CRM_Core_SelectValues::privacy();
-        $privacy[] = HTML_QuickForm::createElement('advcheckbox', 'do_not_phone', null, $t['do_not_phone']);
-        $privacy[] = HTML_QuickForm::createElement('advcheckbox', 'do_not_email', null, $t['do_not_email']);
-        $privacy[] = HTML_QuickForm::createElement('advcheckbox', 'do_not_mail' , null, $t['do_not_mail']);
-        $privacy[] = HTML_QuickForm::createElement('advcheckbox', 'do_not_sms' ,  null, $t['do_not_sms']);
-        $privacy[] = HTML_QuickForm::createElement('advcheckbox', 'do_not_trade', null, $t['do_not_trade']);
-
+        $privacy = array( );
+        $privacyOptions = CRM_Core_SelectValues::privacy( );
+        foreach ( $privacyOptions as $name => $label) {
+            $privacy[] = HTML_QuickForm::createElement('advcheckbox', $name, null, $label );
+        }
         $form->addGroup($privacy, 'privacy', ts('Privacy'), '&nbsp;');
-
+        
         // preferred communication method 
         require_once 'CRM/Core/PseudoConstant.php';
-        $comm = CRM_Core_PseudoConstant::pcm();
-
         $commPreff = array();
-        foreach ( $comm as $k => $v ) {
-            $commPreff[] = HTML_QuickForm::createElement('advcheckbox', $k , null, $v );
+        $comm = CRM_Core_PseudoConstant::pcm();
+        foreach ( $comm as $value => $title ) {
+            $commPreff[] = HTML_QuickForm::createElement('advcheckbox', $value, null, $title );
         }
         $form->addGroup($commPreff, 'preferred_communication_method', ts('Method'));
-
+        
+        //using for display purpose.
+        $form->assign( 'commPreference', array( 'privacy' => $privacyOptions, 'preferred_communication_method' => $comm ) );
+        
         $form->add('select', 'preferred_mail_format', ts('Email Format'), CRM_Core_SelectValues::pmf());
-
         $form->add('checkbox', 'is_opt_out', ts( 'NO BULK EMAILS (User Opt Out)' ) );
-        $commPreference = array( 'privacy' => $t, 'preferred_communication_method' => $comm );
-        $form->assign( 'commPreference', $commPreference );
     }
 }
 
