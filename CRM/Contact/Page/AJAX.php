1da769e61b3e0476527ed640aee7572b82060359
lobo <lobo@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-06-30 17:29:06 +0000

    fix CRM-4677
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@22194 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Page/AJAX.php
+++ CRM/Contact/Page/AJAX.php
@@ -227,18 +227,14 @@ LEFT JOIN civicrm_address ON ( civicrm_contact.id = civicrm_address.contact_id
 WHERE civicrm_contact.contact_type='Organization' AND organization_name LIKE '%$contactName%'
 {$addStreet} {$addCity} {$whereIdClause}
 ORDER BY organization_name ";
-
             } else if ( $shared ) {
-                
                 $query = "
 SELECT CONCAT_WS(':::' , sort_name, supplemental_address_1, sp.abbreviation, postal_code, cc.name )'sort_name' , civicrm_contact.id 'id' , civicrm_contact.display_name 'disp' FROM civicrm_contact LEFT JOIN civicrm_address ON (civicrm_contact.id =civicrm_address.contact_id AND civicrm_address.is_primary =1 )LEFT JOIN civicrm_state_province sp ON (civicrm_address.state_province_id =sp.id )LEFT JOIN civicrm_country cc ON (civicrm_address.country_id =cc.id )WHERE civicrm_contact.contact_type ='{$contactType}' AND {$cName} LIKE '%$name%' {$whereIdClause} ORDER BY {$cName} ";
 
             } else if ( $hh ) {
-                
                 $query = "
 SELECT CONCAT_WS(' :: ' , sort_name, LEFT(street_address,25),city) 'sort_name' , civicrm_contact.id 'id' FROM civicrm_contact LEFT JOIN civicrm_address ON (civicrm_contact.id =civicrm_address.contact_id AND civicrm_address.is_primary =1 )
 WHERE civicrm_contact.contact_type ='Household' AND household_name LIKE '%$contactName%' {$addStreet} {$addCity} {$whereIdClause} ORDER BY household_name ";
-                
             } else if ( $relType ) {
                 if ( CRM_Utils_Array::value( 'case', $_GET ) ) {
                     $query = "
@@ -250,13 +246,13 @@ AND civicrm_relationship.relationship_type_id = $relType
 GROUP BY sort_name 
 ";
                 } else {
-                    
                     $query = "
 SELECT c.sort_name, c.id
 FROM civicrm_contact c, civicrm_relationship_type r
 WHERE c.sort_name LIKE '%$name'
 AND r.id = $relType
-AND c.contact_type = r.contact_type_{$rel} {$whereIdClause} 
+AND ( c.contact_type = r.contact_type_{$rel} OR r.contact_type_{$rel} IS NULL )
+    {$whereIdClause} 
 ORDER BY sort_name" ;
                 }
             } else {
@@ -281,7 +277,7 @@ ORDER BY sort_name ";
             }
             
             $query .= " LIMIT {$start},{$end}";
-            
+
             $dao = CRM_Core_DAO::executeQuery( $query );
             
             if ( $shared ) {
