7c0af21a35e0a4343e8cf4a42bfa75f9050f77fb
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-07-18 21:34:53 +0000

    worked on CRM-4757
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@22641 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/BAO/Individual.php
+++ CRM/Contact/BAO/Individual.php
@@ -174,17 +174,15 @@ class CRM_Contact_BAO_Individual extends CRM_Contact_DAO_Contact
             $contact->display_name = trim( $display_name );
         }
 
-        if ( CRM_Utils_Array::value( 'location', $params ) ) {
-            foreach ($params['location'] as $locBlock) {
-                if (! isset($locBlock['is_primary']) || ! ($locBlock['is_primary']) ) {
-                    continue;
-                }
-                if ( isset($locBlock['email'][1]['email']) ) {
-                    $email = $locBlock['email'][1]['email'];
+        if ( CRM_Utils_Array::value( 'email', $params ) ) {
+            foreach ($params['email'] as $emailBlock) {
+                if ( isset( $emailBlock['is_primary'] ) ) {
+                    $email = $emailBlock['email'];
                     break;
                 }
             }
         }
+
         $uniqId = CRM_Utils_Array::value( 'user_unique_id', $params );
         if (empty($contact->display_name)) {
             if (isset($email)) {
