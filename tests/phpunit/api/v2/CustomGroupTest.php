c531ae3f7c8eedf41f15d622a76ec583c781cc4e
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-08-19 12:42:05 +0000

    CRM-4786 - unified custom group/field and entity tag tests
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@23348 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/phpunit/api/v2/CustomGroupTest.php
+++ tests/phpunit/api/v2/CustomGroupTest.php
@@ -1,27 +1,190 @@
 <?php
 
+/**
+ *  Include class definitions
+ */
+require_once 'tests/phpunit/CiviTest/CiviUnitTestCase.php';
 require_once 'api/v2/CustomGroup.php';
 
-class api_v2_CustomFieldCreateTest extends CiviUnitTestCase 
+/**
+ *  Test APIv2 civicrm_create_custom_group
+ *
+ *  @package   CiviCRM
+ */
+class api_v2_CustomGroupTest extends CiviUnitTestCase
 {
-
+    
     function get_info( )
     {
         return array(
-                     'name'        => 'Custom Field Create',
-                     'description' => 'Test aof Custom Field Create API methods.',
+                     'name'        => 'Custom Group Create',
+                     'description' => 'Test all Custom Group Create API methods.',
                      'group'       => 'CiviCRM API Tests',
                      );
     }
     
     function setUp() 
     {
+        parent::setUp();
     }
     
     function tearDown() 
     {
     }
+     
+    function testCustomGroupCreateNoParam()
+    {
+        $params = array( );
+        $customGroup =& civicrm_custom_group_create($params);
+        $this->assertEquals($customGroup['is_error'], 1); 
+        $this->assertEquals($customGroup['error_message'],'Params must include either \'class_name\' (string) or \'extends\' (array).');
+    }
     
+    function testCustomGroupCreateNoExtends()
+    {
+        $params = array( 'domain_id'        => 1,
+                         'title'            => 'Test_Group_1',
+                         'name'             => 'test_group_1',
+                         'weight'           => 4,
+                         'collapse_display' => 1,
+                         'style'            => 'Tab',
+                         'help_pre'         => 'This is Pre Help For Test Group 1',
+                         'help_post'        => 'This is Post Help For Test Group 1',
+                         'is_active'        => 1
+                         );
+        
+        $customGroup =& civicrm_custom_group_create($params);
+        $this->assertEquals($customGroup['error_message'],'Params must include either \'class_name\' (string) or \'extends\' (array).');
+        $this->assertEquals($customGroup['is_error'],1);
+    }
+    
+    function testCustomGroupCreate()
+    {
+        $params = array( 'title'            => 'Test_Group_1',
+                         'name'             => 'test_group_1',
+                         'extends'          => array('Individual'),
+                         'weight'           => 4,
+                         'collapse_display' => 1,
+                         'style'            => 'Inline',
+                         'help_pre'         => 'This is Pre Help For Test Group 1',
+                         'help_post'        => 'This is Post Help For Test Group 1',
+                         'is_active'        => 1
+                         );
+        
+        $customGroup =& civicrm_custom_group_create($params);
+        $this->assertEquals($customGroup['is_error'],0);
+        $this->assertNotNull($customGroup['id']);
+        $this->customGroupDelete($customGroup['id']);
+    } 
+    
+    function testCustomGroupCreateNoTitle()
+    {
+        $params = array('extends'          => array('Contact'),
+                        'weight'           => 5, 
+                        'collapse_display' => 1,
+                        'style'            => 'Tab',
+                        'help_pre'         => 'This is Pre Help For Test Group 2',
+                        'help_post'        => 'This is Post Help For Test Group 2'
+                        );
+        
+        $customGroup =& civicrm_custom_group_create($params);
+        $this->assertEquals($customGroup['error_message'],'Title parameter is required.');
+        $this->assertEquals($customGroup['is_error'],1);
+	} 
+    
+    function testCustomGroupCreateHouseholdNoWeight()
+    { 
+        $params = array('title'            => 'Test_Group_3',
+                        'name'             => 'test_group_3',
+                        'extends'          => array('Household'),
+                        'collapse_display' => 1,
+                        'style'            => 'Tab',
+                        'help_pre'         => 'This is Pre Help For Test Group 3',
+                        'help_post'        => 'This is Post Help For Test Group 3',
+                        'is_active'        => 1
+                        );
+        
+        $customGroup =& civicrm_custom_group_create($params);
+        $this->assertEquals($customGroup['is_error'],0);
+        $this->assertNotNull($customGroup['id']);
+        $this->customGroupDelete($customGroup['id']);
+    }
+    
+    function testCustomGroupCreateContributionDonation()
+    {
+        $params = array('title'            => 'Test_Group_6',
+                        'name'             => 'test_group_6',
+                        'extends'          => array( 'Contribution', 1 ),
+                        'weight'           => 6,
+                        'collapse_display' => 1,
+                        'style'            => 'Inline',
+                        'help_pre'         => 'This is Pre Help For Test Group 6',
+                        'help_post'        => 'This is Post Help For Test Group 6',
+                        'is_active'        => 1 
+                        );
+        
+        $customGroup =& civicrm_custom_group_create($params); 
+        $this->assertEquals($customGroup['is_error'], 0);
+        $this->assertNotNull($customGroup['id']);
+        $this->customGroupDelete($customGroup['id']);
+    }
+    
+    function testCustomGroupCreateGroup()
+    {
+        $params = array('domain_id'        => 1,
+                        'title'            => 'Test_Group_8',
+                        'name'             => 'test_group_8',
+                        'extends'          => array('Group'),
+                        'weight'           => 7,
+                        'collapse_display' => 1,
+                        'is_active'        => 1,
+                        'style'            => 'Inline',
+                        'help_pre'         => 'This is Pre Help For Test Group 8',
+                        'help_post'        => 'This is Post Help For Test Group 8'
+                        );
+        
+        $customGroup =& civicrm_custom_group_create($params); 
+        $this->assertEquals($customGroup['is_error'], 0);
+        $this->assertNotNull($customGroup['id']);
+        $this->customGroupDelete($customGroup['id']);
+    }
+    
+    function testCustomGroupCreateActivityMeeting()
+    {
+        $params = array(
+                        'title'            => 'Test_Group_10',
+                        'name'             => 'test_group_10',
+                        'extends'          => array('Activity', 1),
+                        'weight'           => 8,
+                        'collapse_display' => 1,
+                        'style'            => 'Inline',
+                        'help_pre'         => 'This is Pre Help For Test Group 10',
+                        'help_post'        => 'This is Post Help For Test Group 10'
+                        );
+        
+        $customGroup =& civicrm_custom_group_create($params); 
+        $this->assertEquals($customGroup['is_error'], 0);
+        $this->assertNotNull($customGroup['id']);
+        $this->customGroupDelete($customGroup['id']);
+    }
+
+
+    function testCustomGroupDeleteWithoutGroupID( )
+    {
+        $params = array( );
+        $customGroup =& civicrm_custom_group_delete($params);
+        $this->assertEquals($customGroup['is_error'], 1);
+        $this->assertEquals($customGroup['error_message'],'Invalid or no value for Custom group ID');
+    }    
+    
+    function testCustomGroupDelete( )
+    {
+        $customGroup = $this->customGroupCreate('Individual', 'test_group'); 
+        $params = array('id' => $customGroup['id']);                         
+        $customGroup =& civicrm_custom_group_delete($params);  
+        $this->assertEquals($customGroup['is_error'], 0);
+    } 
+
     function testCustomFieldCreateNoParam()
     {
         $params = array();
@@ -261,7 +424,34 @@ class api_v2_CustomFieldCreateTest extends CiviUnitTestCase
         $this->customFieldDelete($customField['result']['customFieldId']);
         $this->customGroupDelete($customGroup['id']); 
     }     
+
+    function testCustomFieldDeleteWithoutFieldID( )
+    {
+        $params = array( ); 
+        $customField =& civicrm_custom_field_delete($params); 
+        $this->assertEquals($customField['is_error'], 1);
+        $this->assertEquals($customField['error_message'], 'Invalid or no value for Custom Field ID');
+    }    
+    
+    function testCustomFieldDelete( )
+    {
+        $customGroup = $this->customGroupCreate('Individual','test_group');
+        $customField = $this->customFieldCreate($customGroup['id'],'test_name'); 
+        $this->assertNotNull($customField['result']['customFieldId']);
+        $customField =& civicrm_custom_field_delete( $customField );
+        $this->assertEquals($customField['is_error'], 0);
+        $this->customGroupDelete($customGroup['id']);
+    } 
+    
+    function testCustomFieldOptionValueDelete( )
+    {
+        $customGroup = $this->customGroupCreate('Contact','ABC' );  
+        $customOptionValueFields = $this->customFieldOptionValueCreate($customGroup,'fieldABC' );
+        $customField =& civicrm_custom_field_delete($customOptionValueFields);
+        $this->assertEquals($customField['is_error'], 0);
+        $this->customGroupDelete($customGroup['id']); 
+    } 
+
     
 }
 
- 
---
+++ tests/phpunit/api/v2/EntityTagTest.php
@@ -0,0 +1,334 @@
+<?php
+
+require_once 'api/v2/EntityTag.php';
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+class api_v2_EntityTagTest extends CiviUnitTestCase 
+{
+    protected $_individualID;
+
+    protected $_householdID;
+
+    protected $_organizationID;
+
+    protected $_tagID;
+    
+    function get_info( )
+    {
+        return array(
+                     'name'        => 'EntityTag Add',
+                     'description' => 'Test all EntityTag Add API methods.',
+                     'group'       => 'CiviCRM API Tests',
+                     );
+    }
+
+    function setUp( ) 
+    {
+        parent::setUp();
+
+        $this->_individualID = $this->individualCreate( );
+        $this->_tagID = $this->tagCreate( ); 
+        $this->_householdID = $this->houseHoldCreate( );
+        $this->_organizationID = $this->organizationCreate( );
+    }
+    
+    function tearDown( ) 
+    {
+        $this->contactDelete( $this->_individualID );
+        $this->contactDelete( $this->_householdID );
+        $this->contactDelete( $this->_organizationID );
+        $this->tagDelete( $this->_tagID );
+    }
+
+    function testIndividualEntityTagAddEmptyParams( ) 
+    {
+        $params = array( );                             
+        $individualEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $individualEntity['is_error'], 1 ); 
+        $this->assertEquals( $individualEntity['error_message'], 'contact_id is a required field' );
+       
+    }
+    
+    function testIndividualEntityTagAddWithoutTagID( ) 
+    {
+        $ContactId =  $this->_individualID;
+        $params = array('contact_id' =>  $ContactId);              
+        $individualEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $individualEntity['is_error'], 1 );
+        $this->assertEquals( $individualEntity['error_message'], 'tag_id is a required field' );
+    }
+    
+    function testIndividualEntityTagAdd( ) 
+    {
+        $ContactId = $this->_individualID; 
+        $tagID = $this->_tagID ;  
+        $params = array(
+                        'contact_id' =>  $ContactId,
+                        'tag_id'     =>  $tagID);
+        
+        $individualEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $individualEntity['is_error'], 0 );
+        $this->assertEquals( $individualEntity['added'], 1 );
+    }
+    
+    function testHouseholdEntityTagAddEmptyParams( ) 
+    {
+        $params = array( );
+        $householdEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $householdEntity['is_error'], 1 );
+        $this->assertEquals( $householdEntity['error_message'], 'contact_id is a required field' );
+    }
+    
+    function testHouseholdEntityTagAddWithoutTagID( ) 
+    {
+        $ContactId = $this->_householdID;
+        $params = array('contact_id' =>  $ContactId);
+        $householdEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $householdEntity['is_error'], 1 );
+        $this->assertEquals( $householdEntity['error_message'], 'tag_id is a required field' );
+        
+    }
+    
+    function testHouseholdEntityTagAdd( ) 
+    {
+        $ContactId = $this->_householdID;
+        $tagID = $this->_tagID;
+        $params = array(
+                        'contact_id' =>  $ContactId,
+                        'tag_id'     =>  $tagID );
+                               
+        $householdEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $householdEntity['is_error'], 0 );
+        $this->assertEquals( $householdEntity['added'], 1 );
+    }
+    
+    function testOrganizationEntityTagAddEmptyParams( ) 
+    {
+        $params = array( );
+        $organizationEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $organizationEntity['is_error'], 1 );
+        $this->assertEquals( $organizationEntity['error_message'], 'contact_id is a required field' );
+    }
+    
+    function testOrganizationEntityTagAddWithoutTagID( ) 
+    {
+        $ContactId = $this->_organizationID;
+        $params = array('contact_id' =>  $ContactId);
+        $organizationEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $organizationEntity['is_error'], 1 );
+        $this->assertEquals( $organizationEntity['error_message'], 'tag_id is a required field' );
+    }
+        
+    function testOrganizationEntityTagAdd( ) 
+    {
+        $ContactId = $this->_organizationID;
+        $tagID = $this->_tagID;
+        $params = array(
+                        'contact_id' =>  $ContactId,
+                        'tag_id'     =>  $tagID );
+        
+        $organizationEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $organizationEntity['is_error'], 0 );
+        $this->assertEquals( $organizationEntity['added'], 1 );
+    }
+    
+    function testEntityTagAddIndividualDouble( ) 
+    {
+        $individualId   = $this->_individualID;
+        $organizationId = $this->_organizationID;
+        $tagID = $this->_tagID;
+        $params = array(
+                        'contact_id' =>  $individualId,
+                        'tag_id'     =>  $tagID
+                        );
+        
+        $result = civicrm_entity_tag_add( $params );
+        
+        $this->assertEquals( $result['is_error'], 0 );
+        $this->assertEquals( $result['added'],    1 );
+                
+        $params = array(
+                        'contact_id_i' => $individualId,
+                        'contact_id_o' => $organizationId,
+                        'tag_id'       => $tagID
+                        );
+        
+        $result = civicrm_entity_tag_add( $params );
+        $this->assertEquals( $result['is_error'],  0 );
+        $this->assertEquals( $result['added'],     1 );
+        $this->assertEquals( $result['not_added'], 1 );
+    }
+
+    function testIndividualEntityTagGetWithoutContactID( )
+    {
+        $paramsEntity = array( );
+        $entity       =& civicrm_entity_tag_get( $paramsEntity ); 
+        $this->assertEquals( $entity['is_error'], 1 );
+        $this->assertNotNull( $entity['error_message'] );
+    }
+    
+    function testIndividualEntityTagGet( )
+    {
+        $ContactId = $this->_individualID; 
+        $tagID     = $this->_tagID ;  
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $individualEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $individualEntity['is_error'], 0 );
+        $this->assertEquals( $individualEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' =>  $ContactId );
+        $entity =& civicrm_entity_tag_get( $paramsEntity );
+    }
+    
+    function testHouseholdEntityGetWithoutContactID( )
+    {
+        $paramsEntity = array( );
+        $entity       =& civicrm_entity_tag_get( $paramsEntity );
+        $this->assertEquals( $entity['is_error'], 1 );
+        $this->assertNotNull( $entity['error_message'] );
+    }
+
+    function testHouseholdEntityGet( )
+    {
+       
+        $ContactId = $this->_householdID;
+        $tagID     = $this->_tagID;
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $householdEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $householdEntity['is_error'], 0 );
+        $this->assertEquals( $householdEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' => $ContactId ); 
+        $entity =& civicrm_entity_tag_get( $paramsEntity );
+    }
+    
+    function testOrganizationEntityGetWithoutContactID()
+    {
+        $paramsEntity = array( );
+        $entity =& civicrm_entity_tag_get( $paramsEntity ); 
+        $this->assertEquals( $entity['is_error'], 1 );
+        $this->assertNotNull( $entity['error_message'] );
+    }
+
+    function testOrganizationEntityGet( )
+    {
+        $ContactId = $this->_organizationID;
+        $tagID     = $this->_tagID;
+        $params    = array(
+                           'contact_id' =>  $ContactId,
+                           'tag_id'     =>  $tagID );
+        
+        $organizationEntity = civicrm_entity_tag_add( $params ); 
+        $this->assertEquals( $organizationEntity['is_error'], 0 );
+        $this->assertEquals( $organizationEntity['added'], 1 );
+        
+        $paramsEntity = array('contact_id' => $ContactId );
+        $entity =& civicrm_entity_tag_get( $paramsEntity ); 
+    }
+
+
+    function testEntityTagRemoveNoContactId( )
+    {
+        $entityTagParams = array(
+                                 'contact_id_i' => $this->individualID,
+                                 'contact_id_h' => $this->householdID,
+                                 'tag_id'       => $this->tagID
+                                 );
+        $this->entityTagAdd( $entityTagParams );
+        
+        $params = array(
+                        'tag_id' => $this->tagID
+                        );
+                
+        $result = civicrm_entity_tag_remove( $params );
+        $this->assertEquals( $result['is_error'], 1 );
+        $this->assertEquals( $result['error_message'], 'contact_id is a required field' );
+    }
+    
+    function testEntityTagRemoveNoTagId( )
+    {
+        $entityTagParams = array(
+                                 'contact_id_i' => $this->individualID,
+                                 'contact_id_h' => $this->householdID,
+                                 'tag_id'       => $this->tagID
+                                 );
+        $this->entityTagAdd( $entityTagParams );
+        
+        $params = array(
+                        'contact_id_i' => $this->individualID,
+                        'contact_id_h' => $this->householdID,
+                        );
+                
+        $result = civicrm_entity_tag_remove( $params );
+        $this->assertEquals( $result['is_error'], 1 );
+        $this->assertEquals( $result['error_message'], 'tag_id is a required field' );
+    }
+    
+    function testEntityTagRemoveINDHH( )
+    {
+        $entityTagParams = array(
+                                 'contact_id_i' => $this->individualID,
+                                 'contact_id_h' => $this->householdID,
+                                 'tag_id'       => $this->tagID
+                                 );
+        $this->entityTagAdd( $entityTagParams );
+        
+        $params = array(
+                        'contact_id_i' => $this->individualID,
+                        'contact_id_h' => $this->householdID,
+                        'tag_id'       => $this->tagID
+                        );
+                
+        $result = civicrm_entity_tag_remove( $params );
+        $this->assertEquals( $result['is_error'], 0 );
+        $this->assertEquals( $result['removed'], 2 );
+    }    
+    
+    function testEntityTagRemoveHH( )
+    {
+        $entityTagParams = array(
+                                 'contact_id_i' => $this->individualID,
+                                 'contact_id_h' => $this->householdID,
+                                 'tag_id'       => $this->tagID
+                                 );
+        $this->entityTagAdd( $entityTagParams );
+        
+        $params = array(
+                        'contact_id_h' => $this->householdID,
+                        'tag_id'       => $this->tagID
+                        );
+                
+        $result = civicrm_entity_tag_remove( $params );
+        $this->assertEquals( $result['removed'], 1 );
+    }
+    
+    function testEntityTagRemoveHHORG( )
+    {
+        $entityTagParams = array(
+                                 'contact_id_i' => $this->individualID,
+                                 'contact_id_h' => $this->householdID,
+                                 'tag_id'       => $this->tagID
+                                 );
+        $this->entityTagAdd( $entityTagParams );
+        
+        $params = array(
+                        'contact_id_h' => $this->householdID,
+                        'contact_id_o' => $this->organizationID,
+                        'tag_id'       => $this->tagID
+                        );
+                
+        $result = civicrm_entity_tag_remove( $params );
+        $this->assertEquals( $result['removed'], 1 );
+        $this->assertEquals( $result['not_removed'], 1 );
+    }
+    
+}
+
+
+
