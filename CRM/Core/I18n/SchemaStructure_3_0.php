2d329973fdb54b11a73ba962c0d48813514cd48a
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-08-20 12:25:33 +0000

    re-fix CRM-4906
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@23364 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/I18n/SchemaStructure_3_0.php
+++ CRM/Core/I18n/SchemaStructure_3_0.php
@@ -133,6 +133,14 @@ class CRM_Core_I18n_SchemaStructure_3_0
                     'help_post' => 'text',
                     'label' => 'varchar(255)',
                 ) ,
+                'civicrm_address' => array(
+                    'street_address' => 'varchar(96)',
+                    'supplemental_address_1' => 'varchar(96)',
+                    'supplemental_address_2' => 'varchar(96)',
+                    'supplemental_address_3' => 'varchar(96)',
+                    'city' => 'varchar(64)',
+                    'name' => 'varchar(255)',
+                ) ,
                 'civicrm_event' => array(
                     'title' => 'varchar(255)',
                     'summary' => 'text',
@@ -224,6 +232,14 @@ class CRM_Core_I18n_SchemaStructure_3_0
                             'custom_group_id',
                         ) ,
                         'unique' => 1,
+                    ) ,
+                ) ,
+                'civicrm_address' => array(
+                    'index_city' => array(
+                        'name' => 'index_city',
+                        'field' => array(
+                            'city',
+                        ) ,
                     ) ,
                 ) ,
             );
