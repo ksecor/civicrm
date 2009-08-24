2c8c469c340bc599ca484b1fad286866f0b31540
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-08-24 13:59:27 +0000

    - minor fix for  'Save and Done' button consistency.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@23412 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/ManageEvent/EventInfo.php
+++ CRM/Event/Form/ManageEvent/EventInfo.php
@@ -361,11 +361,20 @@ class CRM_Event_Form_ManageEvent_EventInfo extends CRM_Event_Form_ManageEvent
         }
         
         $this->set( 'id', $event->id );
-
+        
         if ( $this->_action & CRM_Core_Action::ADD ) {
-            CRM_Utils_System::redirect( CRM_Utils_System::url( CRM_Utils_System::currentPath( ), 
-                                                               "action=update&reset=1&subPage=Location&id={$event->id}" ) );
+            $urlParam = "action=update&reset=1&subPage=Location&id={$event->id}";
+            
+            // special case for 'Save and Done' consistency.
+            if ( $this->controller->getButtonName('submit') == "_qf_EventInfo_upload_done" ) {
+                $urlParam = "action=update&reset=1&id={$event->id}";
+                CRM_Core_Session::setStatus( ts("'%1' information has been saved.", 
+                                                array( 1 => $this->getTitle( ) ) ) );
+            }
+            
+            CRM_Utils_System::redirect( CRM_Utils_System::url( CRM_Utils_System::currentPath( ), $urlParam ) );
         }
+        
         parent::endPostProcess( );
     }//end of function
     
