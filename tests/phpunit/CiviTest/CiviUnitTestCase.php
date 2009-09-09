a5e4259abbeceb7a7b0f2760bfe190263adb1a96
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-09 12:51:25 +0000

    lets try here
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@23562 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/phpunit/CiviTest/CiviUnitTestCase.php
+++ tests/phpunit/CiviTest/CiviUnitTestCase.php
@@ -80,9 +80,7 @@ class CiviUnitTestCase extends PHPUnit_Extensions_Database_TestCase {
     function __construct($name = NULL, array $data = array(), $dataName = '' ) {
         parent::__construct($name, $data, $dataName);
         
-        // "initialize" CiviCRM to avoid problems when running single tests
-//        require_once 'CRM/Core/Config.php';
-//        $config =& CRM_Core_Config::singleton();
+
     }
 
     /**
@@ -107,7 +105,9 @@ class CiviUnitTestCase extends PHPUnit_Extensions_Database_TestCase {
      */
     protected function setUp() {
 
-
+        // "initialize" CiviCRM to avoid problems when running single tests
+        require_once 'CRM/Core/Config.php';
+        $config =& CRM_Core_Config::singleton();
 
         //  Get and save a connection to the database
         $this->_dbconn = $this->getConnection();
