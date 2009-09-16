2b3309df60a6ad523d2338d749c68e66d5cfdf66
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-16 15:09:55 +0000

    CRM-5019 - verification and feedback
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@23641 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- tests/phpunit/api/v2/TagTest.php
+++ tests/phpunit/api/v2/TagTest.php
@@ -1,4 +1,28 @@
 <?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.0                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
 
 require_once 'api/v2/Tag.php';
 require_once 'CiviTest/CiviUnitTestCase.php';
@@ -8,9 +32,21 @@ class api_v2_TagTest extends CiviUnitTestCase
     
     function setUp() 
     {
+        parent::setUp();
     }
+
+    function tearDown() 
+    {  
+    }
+
+///////////////// civicrm_tag_create methods
     
-    function testTagCreateEmptyParams()
+    function testCreateWrongParamsType()
+    {
+        $this->markTestIncomplete();
+    }
+
+    function testCreateEmptyParams()
     {
         $params = array( );
         $result =& civicrm_tag_create($params); 
@@ -18,7 +54,7 @@ class api_v2_TagTest extends CiviUnitTestCase
         $this->assertEquals( $result['error_message'],'No input parameters present' );
     }    
     
-    function testTagCreate()
+    function testCreate()
     {
         $params = array(
                         'name'        => 'New Tag3',
@@ -28,21 +64,26 @@ class api_v2_TagTest extends CiviUnitTestCase
         $tag =& civicrm_tag_create($params); 
         $this->assertEquals($tag['is_error'], 0);
         $this->assertNotNull($tag['tag_id']);
-        $this->tagDelete($tag['tag_id']);
     }
 
+///////////////// civicrm_tag_delete methods
 
-    function testTagDeleteEmtyTag( )
+    function testDeleteWrongParamsType()
+    {
+        $this->markTestIncomplete();
+    }
+
+    function testDeleteEmptyParams()
     {
         $tag = array( );
         $tagDelete =& civicrm_tag_delete( $tag );
         $this->assertEquals( $tagDelete['is_error'], 1 );
         $this->assertEquals( $tagDelete['error_message'],'Could not find tag_id in input parameters' );
     }
-    
-    function testTagDeleteError( )
+
+    function testDeleteWithoutTagId()
     {
-        $tag = "noID";
+        $tag = array( 'some_other_key' => 1 );
         
         $tagDelete =& civicrm_tag_delete($tag); 
         $this->assertEquals( $tagDelete['is_error'], 1 ); 
@@ -57,8 +98,6 @@ class api_v2_TagTest extends CiviUnitTestCase
         $this->assertEquals( $tagDelete['is_error'], 0 );
     }
     
-    function tearDown() 
-    {  
-    }
+
 }
 
