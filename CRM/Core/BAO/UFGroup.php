662ca325153bee5e72e81e853b8ba8a2a2d5b72b
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-17 11:19:21 +0000

    add missing require_once
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@23669 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -981,6 +981,7 @@ class CRM_Core_BAO_UFGroup extends CRM_Core_DAO_UFGroup
      */
     static function add(&$params, &$ids) 
     {
+        require_once 'CRM/Utils/Array.php';
         $fields = array( 'is_active', 'add_captcha', 'is_map', 'is_update_dupe', 'is_edit_link', 'is_uf_link', 'is_cms_user' );
         foreach( $fields as $field ) {
             $params[$field] = CRM_Utils_Array::value( $field, $params, false );    
