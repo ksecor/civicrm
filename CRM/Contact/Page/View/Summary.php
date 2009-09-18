b501efda9b7be6d164eac28aa7468da623bb42d0
deepak <deepak@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-18 12:13:14 +0000

    - make contact view screen detect & display custom data extending subtypes, CRM-4975
    git-svn-id: http://svn.civicrm.org/civicrm/branches/subtype@23699 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Page/View/Summary.php
+++ CRM/Contact/Page/View/Summary.php
@@ -55,6 +55,9 @@ class CRM_Contact_Page_View_Summary extends CRM_Contact_Page_View {
 
         //Custom Groups Inline
         $entityType = CRM_Contact_BAO_Contact::getContactType($this->_contactId);
+        $subType    = CRM_Contact_BAO_Contact::getContactSubType($this->_contactId);
+        $entityType = $subType ? "'{$entityType}', '{$subType}'" : $entityType;
+
         $groupTree =& CRM_Core_BAO_CustomGroup::getTree($entityType, $this, $this->_contactId);
 
         CRM_Core_BAO_CustomGroup::buildCustomDataView( $this, $groupTree );
