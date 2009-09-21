6b1d8a5aeaf58f2907d8c88afc7123292fd5f14b
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-21 06:30:12 +0000

    v3.0: svn merge -r 23647:23688 http://svn.civicrm.org/civicrm/branches/v2.2 CRM-5042 and CRM-5053 fixes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@23713 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/BAO/ContributionPage.php
+++ CRM/Contribute/BAO/ContributionPage.php
@@ -173,7 +173,12 @@ class CRM_Contribute_BAO_ContributionPage extends CRM_Contribute_DAO_Contributio
             }
 
             require_once 'CRM/Contact/BAO/Contact/Location.php';
-            list( $displayName, $email ) = CRM_Contact_BAO_Contact_Location::getEmailDetails( $contactID, false, $billingLocationTypeId );
+            if ( !array_key_exists('related_contact', $values) ) {
+                list( $displayName, $email ) = CRM_Contact_BAO_Contact_Location::getEmailDetails( $contactID, false, $billingLocationTypeId );
+            } else {
+                list( $displayName, $email ) = CRM_Contact_BAO_Contact_Location::getEmailDetails( $contactID );
+            }
+
             
             //for display profile need to get individual contact id,  
             //hence get it from related_contact if on behalf of org true CRM-3767.
