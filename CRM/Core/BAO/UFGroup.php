0bb2dd02a5cc2212d8bf686310c3ed4c57c0b079
kurund <kurund@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-21 21:59:28 +0000

    fixed CRM-5094
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v2.2@23731 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/UFGroup.php
+++ CRM/Core/BAO/UFGroup.php
@@ -1296,7 +1296,7 @@ AND    ( entity_id IS NULL OR entity_id <= 0 )
             return;
         }
         
-        if ($contactId) {
+        if ( $contactId && !$online ) {
             $name = "field[$contactId][$fieldName]";
         } else {
             $name = $fieldName;
