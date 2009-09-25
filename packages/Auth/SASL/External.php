870be8b78382f1878a500a53b4903c1fa5a9d096
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-25 15:39:18 +0000

    upgrade Auth_SASL to 1.0.3, CRM-5110 works
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.1.packages@23824 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/Auth/SASL/External.php
+++ packages/Auth/SASL/External.php
@@ -1,6 +1,6 @@
 <?php
 // +-----------------------------------------------------------------------+ 
-// | Copyright (c) 2002-2003 Richard Heyes                                 | 
+// | Copyright (c) 2008 Christoph Schulz                                   | 
 // | All rights reserved.                                                  | 
 // |                                                                       | 
 // | Redistribution and use in source and binary forms, with or without    | 
@@ -29,40 +29,35 @@
 // | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  | 
 // |                                                                       | 
 // +-----------------------------------------------------------------------+ 
-// | Author: Richard Heyes <richard@php.net>                               | 
+// | Author: Christoph Schulz <develop@kristov.de>                         | 
 // +-----------------------------------------------------------------------+ 
 // 
-// $Id: CramMD5.php 286825 2009-08-05 06:23:42Z cweiske $
+// $Id: External.php 286825 2009-08-05 06:23:42Z cweiske $
 
 /**
-* Implmentation of CRAM-MD5 SASL mechanism
+* Implmentation of EXTERNAL SASL mechanism
 *
-* @author  Richard Heyes <richard@php.net>
+* @author  Christoph Schulz <develop@kristov.de>
 * @access  public
-* @version 1.0
+* @version 1.0.3
 * @package Auth_SASL
 */
 
 require_once('Auth/SASL/Common.php');
 
-class Auth_SASL_CramMD5 extends Auth_SASL_Common
+class Auth_SASL_External extends Auth_SASL_Common
 {
     /**
-    * Implements the CRAM-MD5 SASL mechanism
-    * This DOES NOT base64 encode the return value,
-    * you will need to do that yourself.
+    * Returns EXTERNAL response
     *
-    * @param string $user      Username
-    * @param string $pass      Password
-    * @param string $challenge The challenge supplied by the server.
-    *                          this should be already base64_decoded.
-    *
-    * @return string The string to pass back to the server, of the form
-    *                "<user> <digest>". This is NOT base64_encoded.
+    * @param  string $authcid   Authentication id (username)
+    * @param  string $pass      Password
+    * @param  string $authzid   Autorization id
+    * @return string            EXTERNAL Response
     */
-    function getResponse($user, $pass, $challenge)
+    function getResponse($authcid, $pass, $authzid = '')
     {
-        return $user . ' ' . $this->_HMAC_MD5($pass, $challenge);
+        return $authzid;
     }
 }
 ?>
