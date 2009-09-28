1c46dd1501c0f6e2983cf891d3ffc08afa88e272
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-28 13:41:08 +0000

    upgrade DB_Table to 1.5.6, CRM-5110 works
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.1.packages@23847 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- packages/DB/Table/Base.php
+++ packages/DB/Table/Base.php
@@ -2,8 +2,6 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-require_once 'PEAR.php';
-
 /**
  * DB_Table_Base Base class for DB_Table and DB_Table_Database
  *
@@ -14,16 +12,51 @@ require_once 'PEAR.php';
  *   - SELECT queries [ select*() methods, $sql & $fetchmode* properties]
  *   - buildSQL() and quote() SQL utilities
  *   - _swapModes() method 
+ * 
+ * PHP versions 4 and 5
  *
+ * LICENSE:
+ * 
+ * Copyright (c) 1997-2007, Paul M. Jones <pmjones@php.net>
+ *                          David C. Morse <morse@php.net>
+ *                          Mark Wiesemann <wiesemann@php.net>
+ * All rights reserved.
  *
- * PHP version 4 and 5
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the 
+ *      documentation and/or other materials provided with the distribution.
+ *    * The names of the authors may not be used to endorse or promote products 
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @category Database
  * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
  * @author   David C. Morse <morse@php.net>
- * @license  http://www.gnu.org/copyleft/lesser.html LGPL
- * @version  $Id: Base.php,v 1.3 2007/06/14 05:10:46 morse Exp $
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  CVS: $Id: Base.php,v 1.4 2007/12/13 16:52:14 wiesemann Exp $
+ * @link     http://pear.php.net/package/DB_Table
  */
+
+require_once 'PEAR.php';
 
 // {{{ DB_Table_Base
 
@@ -32,8 +65,11 @@ require_once 'PEAR.php';
  *
  * @category Database
  * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
  * @author   David C. Morse <morse@php.net>
- *
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @version  Release: 1.5.6
+ * @link     http://pear.php.net/package/DB_Table
  */
 class DB_Table_Base
 {
--- packages/DB/Table/QuickForm.php
+++ packages/DB/Table/QuickForm.php
@@ -1,20 +1,52 @@
 <?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
 /**
-* 
-* DB_Table_QuickForm creates HTML_QuickForm objects from DB_Table properties.
-* 
-* @category Database
-* 
-* @package DB_Table
-*
-* @author Paul M. Jones <pmjones@php.net>
-* @author Mark Wiesemann <wiesemann@php.net>
-* 
-* @license http://www.gnu.org/copyleft/lesser.html LGPL
-* 
-* @version $Id: QuickForm.php,v 1.43 2007/04/03 00:06:54 morse Exp $
-*
-*/
+ * DB_Table_QuickForm creates HTML_QuickForm objects from DB_Table properties.
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ * 
+ * Copyright (c) 1997-2007, Paul M. Jones <pmjones@php.net>
+ *                          David C. Morse <morse@php.net>
+ *                          Mark Wiesemann <wiesemann@php.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the 
+ *      documentation and/or other materials provided with the distribution.
+ *    * The names of the authors may not be used to endorse or promote products 
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category Database
+ * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
+ * @author   David C. Morse <morse@php.net>
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  CVS: $Id: QuickForm.php,v 1.45 2008/03/28 20:00:38 wiesemann Exp $
+ * @link     http://pear.php.net/package/DB_Table
+ */
 
 /**
 * Needed to build forms.
@@ -44,20 +76,19 @@ if (!isset($GLOBALS['_DB_TABLE']['qf_class_name'])) {
 }
 
 /**
-* 
-* DB_Table_QuickForm creates HTML_QuickForm objects from DB_Table properties.
-* 
-* DB_Table_QuickForm provides HTML form creation facilities based on
-* DB_Table column definitions transformed into HTML_QuickForm elements.
-* 
-* @category Database
-* 
-* @package DB_Table
-*
-* @author Paul M. Jones <pmjones@ciaweb.net>
-* @author Mark Wiesemann <wiesemann@php.net>
-*
-*/
+ * DB_Table_QuickForm creates HTML_QuickForm objects from DB_Table properties.
+ * 
+ * DB_Table_QuickForm provides HTML form creation facilities based on
+ * DB_Table column definitions transformed into HTML_QuickForm elements.
+ * 
+ * @category Database
+ * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
+ * @author   David C. Morse <morse@php.net>
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @version  Release: 1.5.6
+ * @link     http://pear.php.net/package/DB_Table
+ */
 
 class DB_Table_QuickForm {
     
@@ -581,6 +612,19 @@ class DB_Table_QuickForm {
             if (isset($setval)) {
                 $element->setValue($setval);
             }
+
+            break;
+
+        case 'link':
+
+            $element =& HTML_QuickForm::createElement(
+                $col['qf_type'],
+                $elemname,
+                $col['qf_label'],
+                $col['qf_href'], // link href
+                $setval,  // link text
+                $col['qf_attrs']
+            );
 
             break;
 
