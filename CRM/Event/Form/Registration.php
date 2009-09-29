591546e55d6baada497820f0fb56737876ca65c6
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-29 11:02:17 +0000

    v3.0: svn merge -r 23688:23839 http://svn.civicrm.org/civicrm/branches/v2.2 including CRM-5092, CRM-5094, CRM-5135, CRM-5136 works/fixes
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@23902 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration.php
+++ CRM/Event/Form/Registration.php
@@ -629,7 +629,7 @@ class CRM_Event_Form_Registration extends CRM_Core_Form
                         $stateCountryMap[$index][$prefixName] = $key;
                     }
 
-                    CRM_Core_BAO_UFGroup::buildProfile( $this, $field,CRM_Profile_Form::MODE_CREATE, null, true );
+                    CRM_Core_BAO_UFGroup::buildProfile( $this, $field,CRM_Profile_Form::MODE_CREATE, $contactID, true );
 
                     $this->_fields[$key] = $field;
                 }
