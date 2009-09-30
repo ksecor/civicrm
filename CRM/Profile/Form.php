47dc147dc1a396950d83a1d275c7162a1e199f47
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-09-30 17:27:55 +0000

    CRM-5140 clarify location of Save Matching Contact button in duplicates found formRule message.
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@23960 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Profile/Form.php
+++ CRM/Profile/Form.php
@@ -538,7 +538,7 @@ class CRM_Profile_Form extends CRM_Core_Form
                         $errors['_qf_default'] .= '<br />';
                         $errors['_qf_default'] .= ts('If you need to verify if this is the same contact, click here - %1 - to VIEW the existing contact in a new tab.', array(1 => $viewUrl, 'count' => count($viewUrls), 'plural' => 'If you need to verify whether one of these is the same contact, click here - %1 - to VIEW the existing contact in a new tab.'));
                         $errors['_qf_default'] .= '<br />';
-                        $errors['_qf_default'] .= ts('If this is actually a new contact, click Save Matching Contact to create a new contact record.');
+                        $errors['_qf_default'] .= ts('If you are sure this is NOT a duplicate, click the Save Matching Contact button to create a new contact record.');
                         // let smarty know that there are duplicates
                         $template =& CRM_Core_Smarty::singleton( );
                         $template->assign( 'isDuplicate', 1 );
