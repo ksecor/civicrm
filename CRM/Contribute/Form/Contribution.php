b67836c67b5b4f3a00358109a9a54e28f12b18fd
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-15 12:41:58 +0000

    CRM-5095 -minor fixes for offline and credit card contributions.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@24239 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Contribution.php
+++ CRM/Contribute/Form/Contribution.php
@@ -737,9 +737,9 @@ WHERE  contribution_id = {$this->_id}
         
         $this->add('textarea', 'cancel_reason', ts('Cancellation Reason'), $attributes['cancel_reason'] );
         
-        $element =& $this->add( 'select', 'payment_processor_id',
-                                ts( 'Payment Processor' ),
-                                $this->_processors );
+        $element = $this->add( 'select', 'payment_processor_id',
+                               ts( 'Payment Processor' ),
+                               $this->_processors );
         if ( $this->_online ) {
             $element->freeze( );
         }
@@ -753,6 +753,7 @@ WHERE  contribution_id = {$this->_id}
                 $element = $this->add( 'select', 'price_set_id', ts( 'Choose price set' ),
                                        array( '' => ts( 'Choose price set' )) + $priceSets,
                                        null, array('onchange' => "buildAmount( this.value );" ) );
+                if ( $this->_online ) $element->freeze( );
             }
             $this->assign( 'hasPriceSets', $hasPriceSets );
             $element =& $this->add( 'text', 'total_amount', ts('Total Amount'),
