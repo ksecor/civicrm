410023e5c0031ffa50d21e8706d40d3aefd83f9d
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-18 21:47:18 +0000

    move offline contribution receipts to the new framework, CRM-3507
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.1.email@24324 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Member/Form/Membership.php
+++ CRM/Member/Form/Membership.php
@@ -951,9 +951,14 @@ class CRM_Member_Form_Membership extends CRM_Member_Form
             $this->assign( 'membership_name', CRM_Core_DAO::getFieldValue( 'CRM_Member_DAO_MembershipType',
                                                                            $formValues['membership_type_id'][1] ) );
             $this->assign( 'customValues', $customValues );
-            $template =& CRM_Core_Smarty::singleton( );
-            $subject = trim( $template->fetch( 'CRM/Contribute/Form/ReceiptSubjectOffline.tpl' ) );
-            $message = $template->fetch( 'CRM/Contribute/Form/ReceiptMessageOffline.tpl' );
+
+            require_once 'CRM/Core/BAO/MessageTemplates.php';
+            list ($subject, $message, $html) = CRM_Core_BAO_MessageTemplates::getSubjectTextHTML(
+                    'msg_tpl_workflow_contribution',
+                    'contribution_offline_receipt',
+                    $this->_contactID,
+                    array()
+            );
 
             require_once 'CRM/Utils/Mail.php';
             $mailSend = CRM_Utils_Mail::send( $receiptFrom,
