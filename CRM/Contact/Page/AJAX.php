dc3834ea795fe397e6df1b9fb7f9d762b4cdcc62
amit <amit@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-21 15:30:32 +0000

    #CRM-5106 #minor query fixes for duplicate value
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@24385 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contact/Page/AJAX.php
+++ CRM/Contact/Page/AJAX.php
@@ -46,8 +46,8 @@ class CRM_Contact_Page_AJAX
         $limit  = '10';
         $list   = array_keys( CRM_Core_BAO_Preferences::valueOptions( 'autocomplete_contact_search_options' ), '1' );
         $select = array( 'sort_name' );
-        $where  = $from = '';
-        $address = false;
+        $where  = '';
+        $from   = array( );
         foreach( $list as $value ) {
             $suffix = substr( $value, 0, 2 ) . substr( $value, -1 );
             switch( $value ) {
@@ -60,22 +60,22 @@ class CRM_Contact_Page_AJAX
             case 'phone':                
             case 'email':
                 $select[] = ( $value == 'address' ) ? $selectText : $value;
-                if( $value == 'address' && $address ) break;
-                $from    .= " LEFT JOIN civicrm_{$value} {$suffix} ON cc.id = {$suffix}.contact_id";
+                $from[$value] = "LEFT JOIN civicrm_{$value} {$suffix} ON cc.id = {$suffix}.contact_id";
                 $where   .= " AND {$suffix}.is_primary = 1";
-                if( $value == 'address' )  $address = true;
                 break;
                 
             case 'country':
             case 'state_province':
                 $select[] = "{$suffix}.name";
-                if( ! in_array( 'street_address', $select ) && ! in_array( 'city', $select ) ) 
-                    $from .= ' LEFT JOIN civicrm_address sts ON cc.id = sts.contact_id ';
-                $from     .= " LEFT JOIN civicrm_{$value} {$suffix} ON sts.{$value}_id = {$suffix}.id";
+                if( ! in_array( 'address', $from ) ) 
+                    $from ['address'] = 'LEFT JOIN civicrm_address sts ON cc.id = sts.contact_id ';
+                $from[$value] = " LEFT JOIN civicrm_{$value} {$suffix} ON sts.{$value}_id = {$suffix}.id";
+                $where   .= " AND sts.is_primary = 1";
                 break;
             }
         }
         $select = implode( ', ', $select );
+        $from   = implode( ' ' , $from   );
         if ( CRM_Utils_Array::value( 'limit', $_GET) ) {
             $limit = CRM_Utils_Type::escape( $_GET['limit'], 'Positive' );
         }
@@ -88,7 +88,7 @@ class CRM_Contact_Page_AJAX
             $where .= " AND $aclWhere ";
         }
 
-        $query = "
+        echo $query = "
 SELECT DISTINCT(cc.id) as id, CONCAT_WS( ' :: ', {$select} ) as data
 FROM civicrm_contact cc {$from}
 {$aclFrom}
