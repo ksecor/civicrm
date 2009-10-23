980b56c95d5e2a2809a1227aa6d26d95cc08ce40
dgg <dgg@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-23 21:33:48 +0000

    Add link to fatal error message on SMTP settings.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@24495 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Admin/Form/Setting/Smtp.php
+++ CRM/Admin/Form/Setting/Smtp.php
@@ -97,7 +97,9 @@ class CRM_Admin_Form_Setting_Smtp extends CRM_Admin_Form_Setting
                 list( $domainEmailName, $domainEmailAddress ) = CRM_Core_BAO_Domain::getNameAndEmail( );
 
                 if ( !$domainEmailAddress || $domainEmailAddress == 'info@FIXME.ORG' ) {
-                    CRM_Core_Error::fatal( ts( 'The site administrator needs to enter a valid \'FROM Email Address\' in Administer CiviCRM &raquo; Configure &raquo; Domain Information. The email address used may need to be a valid mail account with your email service provider.' ) );
+                    require_once 'CRM/Utils/System.php';
+                    $fixUrl = CRM_Utils_System::url("civicrm/admin/domain", 'action=update&reset=1');
+                    CRM_Core_Error::fatal( ts( 'The site administrator needs to enter a valid \'FROM Email Address\' in <a href="%1">Administer CiviCRM &raquo; Configure &raquo; Domain Information</a>. The email address used may need to be a valid mail account with your email service provider.', array( 1 => $fixUrl ) ) );
                 }
                 
                 if ( ! $toEmail ) {
