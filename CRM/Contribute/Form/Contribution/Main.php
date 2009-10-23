a26ae3d16f60647598e3fbaa79ca939b53ac2dc1
sunil <sunil@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-23 07:10:34 +0000

    small fix for notices http://forum.civicrm.org/index.php/topic,10196.0.html
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@24457 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -932,6 +932,9 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
                     $params['cancelURL' ] = CRM_Utils_System::url( 'civicrm/contribute/transact', '_qf_Main_display=1', true, null, false ); 
                     $params['returnURL' ] = CRM_Utils_System::url( 'civicrm/contribute/transact', '_qf_Confirm_display=1&rfp=1', true, null, false ); 
                     $params['invoiceID' ] = $invoiceID;
+
+                    //default action is Sale
+                    $params['payment_action'] = 'Sale';
                     
                     $payment =& CRM_Core_Payment::singleton( $this->_mode, 'Contribute', $this->_paymentProcessor ); 
                     $token = $payment->setExpressCheckout( $params ); 
