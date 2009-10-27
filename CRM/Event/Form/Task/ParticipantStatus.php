6e87d3f20332a3a702a3dc9463a485045a5ac9eb
yashodha <yashodha@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-27 12:36:13 +0000

    #CRM-5184 (minor fixes)
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.0@24562 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Task/ParticipantStatus.php
+++ CRM/Event/Form/Task/ParticipantStatus.php
@@ -57,7 +57,7 @@ class CRM_Event_Form_Task_ParticipantStatus extends CRM_Event_Form_Task_Batch
 
         # CRM-4321: display info on users being notified if any of the below statuses is enabled
         require_once 'CRM/Event/PseudoConstant.php';
-        $notifyingStatuses = array(ts('Pending from waitlist'), ts('Pending from approval'), ts('Rejected'));
+        $notifyingStatuses = array(ts('Pending from waitlist'), ts('Pending from approval'), ts('Expired'), ts('Cancelled'));
         $notifyingStatuses = array_intersect($notifyingStatuses, CRM_Event_PseudoConstant::participantStatus());
         $this->assign('notifyingStatuses', implode(', ', $notifyingStatuses));
 
