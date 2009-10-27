cb0776f678da4ea2716fcd48e02c1117896cd5cc
mover <mover@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-27 13:28:22 +0000

    skeleton for CRM-5291
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@24563 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

---
+++ tests/phpunit/api/v2/MembershipContributionLinkTest.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 3.0                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2009                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007.                                       |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License along with this program; if not, contact CiviCRM LLC       |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+
+require_once 'api/v2/Contribute.php';
+require_once 'CiviTest/CiviUnitTestCase.php';
+
+class api_v2_MembershipContributionLinkTest extends CiviUnitTestCase 
+{
+    
+    function setUp() 
+    {
+        parent::setUp();
+    }
+    
+    function tearDown() 
+    {
+    }
+
+///////////////// civicrm_membershipcontributionlink_create methods
+
+    /**
+     * Test civicrm_membershipcontributionlink_create with wrong params type.
+     */
+    public function testCreateWrongParamsType()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * Test civicrm_membershipcontributionlink_create with empty params.
+     */
+    public function testCreateEmptyParams()
+    {
+        $this->markTestIncomplete();
+    }
+    
+    /**
+     * Test civicrm_membershipcontributionlink_create - success expected.
+     */
+    public function testCreate()
+    {
+        $this->markTestIncomplete();
+    }    
+
+
+///////////////// civicrm_membershipcontributionlink_get methods
+
+    /**
+     * Test civicrm_membershipcontributionlink_get with wrong params type.
+     */
+    public function testGetWrongParamsType()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * Test civicrm_membershipcontributionlink_get with empty params.
+     */
+    public function testGetEmptyParams()
+    {
+        $this->markTestIncomplete();
+    }
+    
+    /**
+     * Test civicrm_membershipcontributionlink_get - success expected.
+     */
+    public function testGet()
+    {
+        $this->markTestIncomplete();
+    }    
+   
+}
+
