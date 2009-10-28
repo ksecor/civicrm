a3edb32117cb87cf31ec413e24d48fabe3165fed
kiran <kiran@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-28 11:04:53 +0000

    -fix require once.
    git-svn-id: http://svn.civicrm.org/civicrm/trunk@24606 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Event/Form/Registration/AdditionalParticipant.php
+++ CRM/Event/Form/Registration/AdditionalParticipant.php
@@ -405,7 +405,7 @@ class CRM_Event_Form_Registration_AdditionalParticipant extends CRM_Event_Form_R
 
                 } else {
                     $lineItem = array( ); 
-                    require_once 'CRM/Event/Form/Registration/Register.php';
+                    require_once 'CRM/Price/BAO/Set.php';
                     CRM_Price_BAO_Set::processAmount( $this->_values['fee']['fields'], 
                                                       $params, $lineItem );
                     //build the line item..
