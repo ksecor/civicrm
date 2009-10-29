67d4b8bf6cfab160d0267d9daa6816f99117c311
shot <shot@cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e>
2009-10-29 14:14:50 +0000

    honour the target user’s email format preferences, CRM-3507
    git-svn-id: http://svn.civicrm.org/civicrm/branches/v3.1.email@24658 cff41f8d-9fe8-0310-9d4e-fb156a3ffe5e

--- CRM/Core/BAO/MessageTemplates.php
+++ CRM/Core/BAO/MessageTemplates.php
@@ -408,9 +408,16 @@ class CRM_Core_BAO_MessageTemplates extends CRM_Core_DAO_MessageTemplates
             $$elem = $smarty->fetch("string:{$$elem}");
         }
 
-        // send the template
+        // send the template, honouring the target user’s preferences (if any)
         $sent = false;
         if ($params['toEmail']) {
+            $contactParams = array('email' => $params['toEmail']);
+            $contact =& civicrm_contact_get($contactParams);
+            $prefs = array_pop($contact);
+
+            if (isset($prefs['preferred_mail_format']) and $prefs['preferred_mail_format'] == 'HTML') $text = null;
+            if (isset($prefs['preferred_mail_format']) and $prefs['preferred_mail_format'] == 'Text') $html = null;
+
             require_once 'CRM/Utils/Mail.php';
             $sent = CRM_Utils_Mail::send($params['from'], $params['toName'], $params['toEmail'], $subject, $text, $params['cc'], $params['bcc'], $params['replyTo'], $html, $params['attachments']);
         }
